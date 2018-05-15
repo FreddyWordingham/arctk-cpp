7f7a29893dc681ae117e8bf13d25d4539fe6fd98
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-15 02:16:08 +0100

    Added search function.

--- res/bash/utl.sh
+++ res/bash/utl.sh
@@ -10,6 +10,22 @@ arctk_list_source()
     cd - > /dev/null;
 }
 
+arctk_search()
+{
+    if [ "$#" != "1" ]; then
+        printf "Error! Unable to search arctk files.\n";
+        printf "Search requires a single regex string.\n";
+
+        return;
+    fi
+
+    cd $ARCTK_DIR > /dev/null;
+
+    grep -r $1 .;
+
+    cd - > /dev/null;
+}
+
 
 
 printf "arctk utility bash functions loaded.\n";
