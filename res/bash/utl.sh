6272a5bbd07de7d1c2514e1956293e653b965bd0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-15 02:15:37 +0100

    Merged list source and list header functions into a single list source function.

--- res/bash/utl.sh
+++ res/bash/utl.sh
@@ -5,19 +5,10 @@ arctk_list_source()
     cd $ARCTK_DIR > /dev/null;
 
     find . -name "*.cpp"
-
-    cd - > /dev/null;
-}
-
-cptk_list_headers()
-{
-    cd $ARCTK_DIR > /dev/null;
-
     find . -name "*.hpp"
 
     cd - > /dev/null;
 }
-
 
 
 
