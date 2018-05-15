0c2c79ccef19896e153c78695f7da4e44ea23aa3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-15 15:55:59 +0100

    Added framework.

---
+++ include/arctk/math.hpp
@@ -0,0 +1,31 @@
+//  == GUARD ==
+#ifndef ARCTK_MATH_HPP
+#define ARCTK_MATH_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_CORE_MOD
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace math
+    {
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_CORE_MOD
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_HPP
