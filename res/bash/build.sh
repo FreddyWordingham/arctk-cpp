77713636961bc21684f27415a2630475c0153b49
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-16 23:20:25 +0100

    Updated build command to prepend variable name with arctk.

--- res/bash/build.sh
+++ res/bash/build.sh
@@ -38,7 +38,7 @@ arctk_build()
         -DCLANG_TIDY=$4                             \
         -DIWYU=$5                                   \
         -DDOCUMENT=$6                               \
-        -DMOD_CORE=$7                               \
+        -DARCTK_MOD_CORE=$7                         \
         ..
 
     local cmake_build_status=$?;
