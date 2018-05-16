4fda050503481261d8a5e7f0ca5de9356819353e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-17 00:26:12 +0100

    Added file documentation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/**
+ *  @author Freddy Wordingham
+ *  @email  fjmw201@exeter.ac.uk
+ *
+ *  @date   15/05/2018.
+ *
+ *  Main source file of arctk.
+ *  Lists all header files fo static-analysis.
+ */
+
+
+
 //  == INCLUDES ==
 //  -- Arctk --
 #include <arctk/config.hpp> // IWYU pragma: keep
