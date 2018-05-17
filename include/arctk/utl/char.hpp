ea6394e8695151862a91c173b784fbd81c083c41
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-17 19:36:02 +0100

    Added character utility header file.

---
+++ include/arctk/utl/char.hpp
@@ -0,0 +1,49 @@
+/**
+ *  @file   char.hpp
+ *  @date   17/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Character utility functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_CHAR_HPP
+#define ARCTK_UTL_CHAR_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace utl //! utility namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_CHAR_HPP
