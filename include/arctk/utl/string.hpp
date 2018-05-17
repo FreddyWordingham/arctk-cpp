43fa3f2a174e43bfa62dd0b76494694e4fe7f2a9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-17 17:38:03 +0100

    Added string utility header file.

---
+++ include/arctk/utl/string.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   config.hpp
+ *  @date   17/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  String utility functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STRING_HPP
+#define ARCTK_STRING_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace utl //! utility namespace
+    {
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STRING_HPP
