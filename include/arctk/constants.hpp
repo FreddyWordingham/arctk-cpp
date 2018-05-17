95e8ec9646fa1d6d8c3f97a232f35873f0efc82b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-17 03:05:31 +0100

    Added common math constants.

--- include/arctk/constants.hpp
+++ include/arctk/constants.hpp
@@ -29,6 +29,19 @@ namespace arc //! arc namespace
 {
     namespace math //! mathematical namespace
     {
+
+
+
+        //  == COMMON ==
+        //  -- Archimedes Constant --
+        constexpr const double PI = 3.141592653589793238462643383279502884197169399375105820974;
+
+
+        //  -- Euler's Number --
+        constexpr const double EXP = 2.718281828459045235360287471352662497757247093699959574966;
+
+
+
     } // namespace math
 
 
