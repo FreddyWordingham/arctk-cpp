e4f928983eddb179d311a8632dafa530a57a23fb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-18 01:32:05 +0100

    Added constants collection source file.

---
+++ include/arctk/constants.cpp
@@ -0,0 +1,12 @@
+/**
+ *  @file   constants.cpp
+ *  @date   18/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of mathematical and physical constants.
+ */
+
+
+
+//  == HEADER ==
+#include <arctk/constants.hpp>
