a08e00c648b9436cfcfc6c64bb8c59db4a3cea4f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-18 19:44:48 +0100

    Added framework.

---
+++ include/arctk/constants/phys.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   phys.hpp
+ *  @date   18/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Physical constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONSTANTS_PHYS_HPP
+#define ARCTK_CONSTANTS_PHYS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace con //! constants namespace
+    {
+
+
+
+    } // namespace con
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONSTANTS_PHYS_HPP
