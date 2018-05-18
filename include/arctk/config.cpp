8fbd8be3939c2ac0828c4ff33faeb21cd6fbbb0a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-18 01:33:43 +0100

    Added config collection source file.

---
+++ include/arctk/config.cpp
@@ -0,0 +1,12 @@
+/**
+ *  @file   config.cpp
+ *  @date   18/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Library build-time configuration constants.
+ */
+
+
+
+//  == HEADER ==
+#include <arctk/config.hpp>
