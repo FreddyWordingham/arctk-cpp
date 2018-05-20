7ca8ab63b4962fff52224ae00c559bab8fa88847
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-20 02:18:58 +0100

    Added framework.

---
+++ include/arctk/utl/array.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/utl/char.hpp
+ *  @date   20/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Std::array utility functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_ARRAY_HPP
+#define ARCTK_UTL_ARRAY_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace utl //! utility namespace
+    {
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_ARRAY_HPP
