1237e7f0c965114ac4bc7dd740c8654503984db2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-20 03:22:10 +0100

    Removed braced initialisation for readability.

--- include/arctk/constant/math.hpp
+++ include/arctk/constant/math.hpp
@@ -29,11 +29,11 @@ namespace arc //! arc namespace
 
         //  == COMMON ==
         //  -- Archimedes' Constant --
-        constexpr const double PI{3.141592653589793238462643383279502884197169399375105820974}; //!<  Archimedes' Constant.
+        constexpr const double PI = 3.141592653589793238462643383279502884197169399375105820974; //!<  Archimedes' Constant.
 
 
         //  -- Euler's Number --
-        constexpr const double EXP{2.718281828459045235360287471352662497757247093699959574966}; //!< Euler's Number.
+        constexpr const double EXP = 2.718281828459045235360287471352662497757247093699959574966; //!< Euler's Number.
 
 
 
