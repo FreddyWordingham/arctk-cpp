1bc318d1a33263db0aedbf5fd2204856b08af5ab
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-21 02:53:13 +0100

    Added framework.

---
+++ include/arctk/log/ansi.hpp
@@ -0,0 +1,80 @@
+/**
+ *  @file   arctk/log/message.hpp
+ *  @date   21/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Ansi escape code definition class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_LOG_ANSI_HPP
+#define ARCTK_LOG_ANSI_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace log
+    {
+
+
+
+        //  == CLASS ==
+        class Ansi
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          private:
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == OPERATORS ==
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace log
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_LOG_ANSI_HPP
