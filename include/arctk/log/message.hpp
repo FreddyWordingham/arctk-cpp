03c147f1a4679f73fa866053c9e95450d2a75234
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-21 02:42:02 +0100

    Added framework.

---
+++ include/arctk/log/message.hpp
@@ -0,0 +1,80 @@
+/**
+ *  @file   arctk/log/message.hpp
+ *  @date   21/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Message log class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_LOG_MESSAGE_HPP
+#define ARCTK_LOG_MESSAGE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+#include <sstream>
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace log
+    {
+
+
+
+        //  == CLASS ==
+        class Message : public std::stringstream
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          private:
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == OPERATORS ==
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace log
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_LOG_MESSAGE_HPP
