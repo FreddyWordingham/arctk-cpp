ccba881709d189f0280226df95b8056482851a29
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-21 15:52:50 +0100

    Added colouring text fields.

--- include/arctk/log/ansi.hpp
+++ include/arctk/log/ansi.hpp
@@ -45,6 +45,24 @@ namespace arc //! arc namespace
             //  -- General --
             const std::string reset; //!< Reset ansi settings to default.
 
+            //  -- Text Colouring --
+            const std::string black;          //!< Colour the text black.
+            const std::string red;            //!< Colour the text red.
+            const std::string green;          //!< Colour the text green.
+            const std::string yellow;         //!< Colour the text yellow.
+            const std::string blue;           //!< Colour the text blue.
+            const std::string magenta;        //!< Colour the text magenta.
+            const std::string cyan;           //!< Colour the text cyan.
+            const std::string white;          //!< Colour the text white.
+            const std::string bright_black;   //!< Colour the text bright black.
+            const std::string bright_red;     //!< Colour the text bright red.
+            const std::string bright_green;   //!< Colour the text bright green.
+            const std::string bright_yellow;  //!< Colour the text bright yellow.
+            const std::string bright_blue;    //!< Colour the text bright blue.
+            const std::string bright_magenta; //!< Colour the text bright magenta.
+            const std::string bright_cyan;    //!< Colour the text bright cyan.
+            const std::string bright_white;   //!< Colour the text bright white.
+
 
             //  == INSTANTIATION ==
           public:
