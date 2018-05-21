a8a0441e72c75cdbe1868ca530b42dbea519e14a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-21 22:12:47 +0100

    Added special methods.

--- include/arctk/log/message.hpp
+++ include/arctk/log/message.hpp
@@ -45,9 +45,18 @@ namespace arc //! arc namespace
           public:
             //  -- Constructors --
             inline Message(const std::string& file_, const std::string& func_, int line_) noexcept;
+            inline Message(const Message&) = delete; //!< Deleted copy constructor.
+            inline Message(Message&&)      = delete; //!< Deleted move constructor.
 
             //  -- Destructors --
             inline ~Message() noexcept override;
+
+
+            //  == OPERATORS ==
+          public:
+            //  -- Assignment --
+            inline Message& operator=(const Message&) = delete; //!< Deleted copy operator. @return Reference to copied object.
+            inline Message& operator=(Message&&) = delete;      //!< Deleted move operator. @return Reference to moved object.
         };
 
 
