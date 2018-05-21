d8c5933140967d6062bad56c62699a3ca1656c95
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-21 02:47:19 +0100

    Added constructor method.

--- include/arctk/log/message.hpp
+++ include/arctk/log/message.hpp
@@ -39,33 +39,48 @@ namespace arc
          */
         class Message : public std::stringstream
         {
-            //  == FIELDS ==
-          private:
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
+            inline Message(const std::string& file_, const std::string& func_, int line_) noexcept;
 
 
             //  == OPERATORS ==
           private:
-            //  == METHODS ==
-          public:
-            //  -- Getters --
         };
 
 
 
         //  == INSTANTIATION --
         //  -- Constructors --
+        /**
+         *  Construct a message object which, when destructed, will write its contents to the terminal.
+         *  Boolean values are printed as words.
+         *  If LOC_OUPUT is defined, then its value is used to colour the output location of the message.
+         *
+         *  @param  file_   File location of the message.
+         *  @param  func_   Function location of the message.
+         *  @param  line_   Line location of the message.
+         *
+         *  @pre    file_ must not be empty.
+         *  @pre    func_ must not be empty.
+         *  @pre    line_ must be greater than zero.
+         */
+        inline Message::Message(const std::string& file_, const std::string& func_, const int line_) noexcept
+        {
+            assert(!file_.empty());
+            assert(!func_.empty());
+            assert(line_ > 0);
 
-
-
-        //  == OPERATORS ==
-
-
-
-        //  == METHODS ==
-        //  -- Getters --
+            *this << std::boolalpha;
+#ifdef LOC_OUTPUT
+            *this << LOC_OUTPUT << file_ << " :: " << func_ << " :: " << line_ << ANSI.reset << "\n";
+#else
+            (void)(file_);
+            (void)(func_);
+            (void)(line_);
+#endif
+        }
 
 
 
