313bca67d6df1fa6404a0665b48d9cdb77e3c702
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-21 22:04:06 +0100

    Added constructor method.

--- include/arctk/log/message.hpp
+++ include/arctk/log/message.hpp
@@ -21,6 +21,10 @@
 
 //  == INCLUDES ==
 //  -- Std --
+#include <cassert>
+
+//  -- Arctk --
+#include <arctk/log/ansi.hpp>
 
 
 
@@ -35,7 +39,44 @@ namespace arc //! arc namespace
         //  == CLASS ==
         class Message : public std::stringstream
         {
+            //  == INSTANTIATION --
+          public:
+            //  -- Constructors --
+            inline Message(const std::string& file_, const std::string& func_, int line_) noexcept;
         };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+        /**
+         *  Construct a message object which, when destructed, will write its contents to the terminal.
+         *  Boolean values are printed as words.
+         *  If LOCATION is defined, then its value is used to colour the output location of the message.
+         *
+         *  @param  file_   File location of the message.
+         *  @param  func_   Function location of the message.
+         *  @param  line_   Line location of the message.
+         *
+         *  @pre    file_ must not be empty.
+         *  @pre    func_ must not be empty.
+         *  @pre    line_ must be greater than zero.
+         */
+        inline Message::Message(const std::string& file_, const std::string& func_, const int line_) noexcept
+        {
+            assert(!file_.empty());
+            assert(!func_.empty());
+            assert(line_ > 0);
+
+            *this << std::boolalpha;
+#ifdef LOCATION
+            *this << LOCATION << file_ << " :: " << func_ << " :: " << line_ << ANSI.reset << "\n";
+#else
+            (void)(file_);
+            (void)(func_);
+            (void)(line_);
+#endif
+        }
 
 
 
