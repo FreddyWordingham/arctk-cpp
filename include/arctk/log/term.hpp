60a8347a81c3c48da194f49de5ff2fb7e2d6f3a6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-21 02:11:12 +0100

    Added framework.

---
+++ include/arctk/log/term.hpp
@@ -0,0 +1,46 @@
+/**
+ *  @file   arctk/log/term.hpp
+ *  @date   21/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Terminal logger class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_LOG_TERM_HPP
+#define ARCTK_LOG_TERM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace log //! logging namespace
+    {
+
+
+
+    } // namespace log
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_LOG_TERM_HPP
