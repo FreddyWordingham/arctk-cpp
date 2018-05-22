524e06c924afc1577b4ac98356e1e65904078880
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-22 03:07:01 +0100

    Added WARN macro.

--- include/arctk/log.hpp
+++ include/arctk/log.hpp
@@ -38,6 +38,7 @@
 #else
 #define VERB arc::log::output::Verbose() << "" //!< Discard following following stream.
 #endif
+#define WARN arc::log::output::Warning(__FILE__, __func__, __LINE__) << ""                        //!< Log a warning message to console output.
 #define ERROR(exit_val_) arc::log::output::Error(__FILE__, __func__, __LINE__, (exit_val_)) << "" //!< Log an error message to console output.
 
 
