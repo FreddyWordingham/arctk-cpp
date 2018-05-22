a3c1e501a41fb54ab0b403b450fe9659fc1d1f8b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-22 02:51:23 +0100

    Added framework.

---
+++ include/arctk/log/output/verbose.hpp
@@ -0,0 +1,54 @@
+/**
+ *  @file   arctk/log/output/verbose.hpp
+ *  @date   21/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Standard console output verbose log class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_LOG_OUTPUT_VERBOSE_HPP
+#define ARCTK_LOG_OUTPUT_VERBOSE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace log //! logging namespace
+    {
+        namespace output //! console output namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Standard output verbose logging.
+             */
+            class Verbose
+            {
+            };
+
+
+
+        } // namespace output
+    }     // namespace log
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_LOG_OUTPUT_VERBOSE_HPP
