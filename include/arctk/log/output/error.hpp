93d3781b5fe2cdd49e05bef149d971d67800a2d8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-22 02:33:10 +0100

    Added constructor method.

--- include/arctk/log/output/error.hpp
+++ include/arctk/log/output/error.hpp
@@ -49,7 +49,7 @@ namespace arc //! arc namespace
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-                inline Error(const std::string& file_, const std::string& func_, int line_) noexcept;
+                inline Error(const std::string& file_, const std::string& func_, int line_, int exit_val_) noexcept;
                 inline Error(const Error&) = delete; //!< Deleted copy constructor.
                 inline Error(Error&&)      = delete; //!< Deleted move constructor.
 
@@ -69,22 +69,29 @@ namespace arc //! arc namespace
             //  == INSTANTIATION --
             //  -- Constructors --
             /**
-             *  Construct a message object which, when destructed, will write its contents to the terminal.
+             *  Construct a message object which, when destructed, will write its contents to the terminal and exit the program.
+             *  Error call location is reported.
              *
-             *  @param  file_   File location of the message.
-             *  @param  func_   Function location of the message.
-             *  @param  line_   Line location of the message.
+             *  @param  file_       File location of the message.
+             *  @param  func_       Function location of the message.
+             *  @param  line_       Line location of the message.
+             *  @param  exit_val_   Exit value of program used after printing the error message.
              *
              *  @pre    file_ must not be empty.
              *  @pre    func_ must not be empty.
              *  @pre    line_ must be greater than zero.
+             *  @pre    exit_val_ must be positive.
              */
-            inline Error::Error(const std::string& file_, const std::string& func_, const int line_) noexcept
-              : Output(file_, func_, line_)
+            inline Error::Error(const std::string& file_, const std::string& func_, const int line_, const int exit_val_) noexcept
+              : Output(file_, func_, line_, exit_val_)
             {
                 assert(!file_.empty());
                 assert(!func_.empty());
                 assert(line_ > 0);
+                assert(exit_val_ > 0);
+
+                *this << ANSI.red << file_ << " :: " << func_ << " :: " << line_ << ANSI.reset << "\n";
+                *this << "[" << ANSI.red << "!ERROR!" << ANSI.reset << "] : " << ANSI.red;
             }
 
 
