0d3e8dadd07071bfc1e0a537764ff608491e7257
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-22 04:06:33 +0100

    Linked header to main.

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 #include <arctk/log/output.hpp>
 #include <arctk/log/output/error.hpp>
 #include <arctk/log/output/message.hpp>
+#include <arctk/log/output/progress.hpp>
 #include <arctk/log/output/stopwatch.hpp>
 #include <arctk/log/output/verbose.hpp>
 #include <arctk/log/output/warning.hpp>
