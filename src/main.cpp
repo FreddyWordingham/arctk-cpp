e644b022c7fb55bde32e55bad7b46073e8ac75ba
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-22 01:58:38 +0100

    Removed cerr include.

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@
 #include <arctk/constant/phys.hpp>
 #include <arctk/log.hpp>
 #include <arctk/log/ansi.hpp>
-#include <arctk/log/cerr.hpp>
 #include <arctk/log/cout.hpp>
 #include <arctk/log/cout/message.hpp>
 #include <arctk/log/term.hpp>
