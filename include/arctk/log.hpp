e0d1f89732993e30aa5ae3a1da3ebb8d1efae4f7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-22 02:27:58 +0100

    Linked header to log collection.

--- include/arctk/log.hpp
+++ include/arctk/log.hpp
@@ -23,6 +23,7 @@
 //  -- Arctk --
 #include <arctk/log/ansi.hpp>
 #include <arctk/log/output.hpp>
+#include <arctk/log/output/error.hpp>
 #include <arctk/log/output/message.hpp>
 #include <arctk/log/term.hpp>
 
