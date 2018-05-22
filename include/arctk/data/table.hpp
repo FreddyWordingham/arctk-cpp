5cee42a186bc05a0cf48fde293f60a75315cd640
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-22 17:02:12 +0100

    Added framework.

---
+++ include/arctk/data/table.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @file   arctk/data/table.hpp
+ *  @date   22/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Data table class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_TABLE_HPP
+#define ARCTK_DATA_TABLE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Table
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_TABLE_HPP
