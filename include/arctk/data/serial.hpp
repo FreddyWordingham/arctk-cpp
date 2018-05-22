4840c465369dbb19f1a5ea71428a7b793427256a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-22 20:13:12 +0100

    Added framework.

---
+++ include/arctk/data/serial.hpp
@@ -0,0 +1,62 @@
+/**
+ *  @file   arctk/data/serial.hpp
+ *  @date   22/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Serial data class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_SERIAL_HPP
+#define ARCTK_DATA_SERIAL_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace log //! logging namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Serial
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+            //  == OPERATORS ==
+          public:
+        };
+
+
+
+    } // namespace log
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_SERIAL_HPP
