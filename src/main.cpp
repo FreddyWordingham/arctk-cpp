e65a61b4ae480a0fca554ce098e8d0bf16eef8ba
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-23 15:41:34 +0100

    Removed includes.

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,6 @@
 #include <arctk/constant/math.hpp>
 #include <arctk/constant/phys.hpp>
 #include <arctk/data.hpp>
-#include <arctk/data/column.hpp>
-#include <arctk/data/serial.hpp>
-#include <arctk/data/table.hpp>
 #include <arctk/log.hpp>
 #include <arctk/log/ansi.hpp>
 #include <arctk/log/output.hpp>
