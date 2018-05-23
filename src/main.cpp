c75953ffbc3d327a42e5559ef21d2957a90e2407
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-23 15:43:44 +0100

    Linked header to main.

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
 #include <arctk/constant/math.hpp>
 #include <arctk/constant/phys.hpp>
 #include <arctk/data.hpp>
+#include <arctk/data/table.hpp>
 #include <arctk/log.hpp>
 #include <arctk/log/ansi.hpp>
 #include <arctk/log/output.hpp>
