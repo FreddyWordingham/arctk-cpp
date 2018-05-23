1de42005bf2c405d1a0153c2e5e89f32208eff25
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-23 01:37:16 +0100

    Added framework.

---
+++ include/arctk/data/column.hpp
@@ -0,0 +1,72 @@
+/**
+ *  @file   arctk/data/column.hpp
+ *  @date   23/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Column data class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_COLUMN_HPP
+#define ARCTK_DATA_COLUMN_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+#include <vector>
+
+//  -- Arctk --
+#include <arctk/data/serial.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Data column class.
+         *  Stores a column of serialised objects.
+         */
+        class Column
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          public:
+            //  == METHODS ==
+          public:
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_COLUMN_HPP
