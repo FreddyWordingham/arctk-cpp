f6835449c37773645eb22c3946ed38638212710b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 17:26:56 +0100

    Added framework.

---
+++ include/arctk/vector.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @file   arctk/vector.hpp
+ *  @date   25/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Std::vector encapsulation class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_VECTOR_HPP
+#define ARCTK_VECTOR_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+
+
+
+    //  == CLASS ==
+    /**
+     */
+    class Name
+    {
+        //  == FIELDS ==
+      private:
+        //  == INSTANTIATION ==
+      public:
+        //  -- Constructors --
+
+
+        //  == METHODS ==
+      public:
+    };
+
+
+
+    //  == INSTANTIATION --
+
+
+
+    //  == METHODS ==
+
+
+
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_VECTOR_HPP
