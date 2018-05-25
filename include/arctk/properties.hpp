080c0c1e2bd03a36cdb5d07290091e6da5d0fbdd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 22:47:24 +0100

    Added framework.

---
+++ include/arctk/properties.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/math.hpp
+ *  @date   25/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of property functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PROPERTIES_HPP
+#define ARCTK_PROPERTIES_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PROPERTIES_HPP
