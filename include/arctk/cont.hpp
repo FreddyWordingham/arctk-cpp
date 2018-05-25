be358449a3fa1136b1407b8ae44ba7089138de53
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 18:57:41 +0100

    Added containter collection header.

---
+++ include/arctk/cont.hpp
@@ -0,0 +1,34 @@
+/**
+ *  @file   arctk/cont.hpp
+ *  @date   25/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of container classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONT_HPP
+#define ARCTK_CONT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Arctk --
+#include <arctk/cont/vector.hpp>
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONT_HPP
