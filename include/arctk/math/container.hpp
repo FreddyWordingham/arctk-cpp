c03b130ff988b9ecf982e19effc6ce0bb118b14c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 22:09:01 +0100

    Added framework.

---
+++ include/arctk/math/container.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/math/container.hpp
+ *  @date   25/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Mathematical container functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_CONTAINER_HPP
+#define ARCTK_MATH_CONTAINER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+#include <cmath>
+#include <iterator>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_CONTAINER_HPP
