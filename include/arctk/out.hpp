22bb650172742195a3a88b464aa69267a8d173ba
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 19:10:11 +0100

    Added framework.

---
+++ include/arctk/out.hpp
@@ -0,0 +1,34 @@
+/**
+ *  @file   arctk/out.hpp
+ *  @date   25/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of output classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_OUT_HPP
+#define ARCTK_OUT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Arctk --
+#include <arctk/out/printable.hpp>
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_OUT_HPP
