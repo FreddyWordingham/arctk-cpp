90dd2ca705947cb2eadb00a6d3ef8ec9e3d582b1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 21:58:49 +0100

    Added iterator typename.

--- include/arctk/container.hpp
+++ include/arctk/container.hpp
@@ -40,19 +40,19 @@ namespace arc //! arc namespace
         inline size_t size(const C& cont_) noexcept;
 
         //  -- Mathematical --
-        template <typename C, typename T = typename C::value_type>
+        template <typename C, typename I = typename C::const_iterator, typename T = typename C::value_type>
         inline T sum(const C& cont_) noexcept;
         template <typename C>
         inline double ave(const C& cont_) noexcept;
-        template <typename C, typename T = typename C::value_type>
+        template <typename C, typename I = typename C::const_iterator, typename T = typename C::value_type>
         inline double mag(const C& cont_) noexcept;
-        template <typename C>
+        template <typename C, typename I = typename C::const_iterator>
         inline size_t min_index(const C& cont_) noexcept;
-        template <typename C>
+        template <typename C, typename I = typename C::const_iterator>
         inline size_t max_index(const C& cont_) noexcept;
-        template <typename C, typename T = typename C::value_type>
+        template <typename C, typename I = typename C::const_iterator, typename T = typename C::value_type>
         inline T min(const C& cont_) noexcept;
-        template <typename C, typename T = typename C::value_type>
+        template <typename C, typename I = typename C::const_iterator, typename T = typename C::value_type>
         inline T max(const C& cont_) noexcept;
 
 
@@ -66,7 +66,7 @@ namespace arc //! arc namespace
          *
          *  @return Size of the container.
          */
-        template <typename C>
+        template <typename C, typename I>
         inline size_t size(const C& cont_) noexcept
         {
             return (std::distance(std::begin(cont_), std::end(cont_)));
@@ -81,7 +81,7 @@ namespace arc //! arc namespace
          *
          *  @return Sum of the containers elements.
          */
-        template <typename C, typename T>
+        template <typename C, typename I, typename T>
         inline T sum(const C& cont_) noexcept
         {
             T sum{};
@@ -101,7 +101,7 @@ namespace arc //! arc namespace
          *
          *  @return Average of the containers elements.
          */
-        template <typename C>
+        template <typename C, typename I>
         inline double ave(const C& cont_) noexcept
         {
             return (static_cast<double>(sum(cont_)) / cont::size(cont_));
@@ -114,7 +114,7 @@ namespace arc //! arc namespace
          *
          *  @return Magnitude the containers elements.
          */
-        template <typename C, typename T>
+        template <typename C, typename I, typename T>
         inline double mag(const C& cont_) noexcept
         {
             T sq_sum{};
@@ -134,20 +134,20 @@ namespace arc //! arc namespace
          *
          *  @return Index of the minimum element within the container.
          */
-        template <typename C>
+        template <typename C, typename I>
         inline size_t min_index(const C& cont_) noexcept
         {
-            size_t min_index = 0;
+            I min_index = std::begin(cont_);
 
-            for (auto it = std::begin(cont_); it != std::end(cont_); ++it)
+            for (I it = min_index + 1; it != std::end(cont_); ++it)
             {
-                if (*it < min_index)
+                if (*it < *min_index)
                 {
-                    min_index = *it;
+                    min_index = it;
                 }
             }
 
-            return (min_index);
+            return (std::distance(std::begin(cont_), min_index));
         }
 
         /**
@@ -157,20 +157,20 @@ namespace arc //! arc namespace
          *
          *  @return Index of the maximum element within the container.
          */
-        template <typename C>
+        template <typename C, typename I>
         inline size_t max_index(const C& cont_) noexcept
         {
-            size_t max_index = 0;
+            I max_index = std::begin(cont_);
 
-            for (auto it = std::begin(cont_); it != std::end(cont_); ++it)
+            for (I it = max_index + 1; it != std::end(cont_); ++it)
             {
-                if (*it > max_index)
+                if (*it > *max_index)
                 {
-                    max_index = *it;
+                    max_index = it;
                 }
             }
 
-            return (max_index);
+            return (std::distance(std::begin(cont_), max_index));
         }
 
         /**
@@ -180,7 +180,7 @@ namespace arc //! arc namespace
          *
          *  @return Value of the minimum element within the container.
          */
-        template <typename C, typename T>
+        template <typename C, typename I, typename T>
         inline T min(const C& cont_) noexcept
         {
             return (cont_[min_index(cont_)]);
@@ -193,7 +193,7 @@ namespace arc //! arc namespace
          *
          *  @return Value of the maximum element within the container.
          */
-        template <typename C, typename T>
+        template <typename C, typename I, typename T>
         inline T max(const C& cont_) noexcept
         {
             return (cont_[max_index(cont_)]);
