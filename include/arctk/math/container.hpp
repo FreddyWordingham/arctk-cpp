760935bf27508cc03ad66388ccae3e4038572e1a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 22:12:09 +0100

    Copied container math functions.

--- include/arctk/math/container.hpp
+++ include/arctk/math/container.hpp
@@ -19,8 +19,7 @@
 
 
 
-//  == INCLUDES ==
-//  -- Std --
+//  == IMPORTS ==
 #include <cmath>
 #include <iterator>
 
@@ -31,6 +30,72 @@ namespace arc //! arctk namespace
 {
     namespace math //! mathematical namespace
     {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        template <typename C, typename I = typename C::const_iterator, typename T = typename C::value_type>
+        inline T sum(const C& cont_) noexcept;
+        template <typename C>
+        inline double ave(const C& cont_) noexcept;
+        template <typename C, typename I = typename C::const_iterator, typename T = typename C::value_type>
+        inline double mag(const C& cont_) noexcept;
+
+
+
+        //  == FUNCTIONS ==
+        /**
+         *  Calculate the sum of a container's elements.
+         *
+         *  @param  cont_   Container to determine the sum of.
+         *
+         *  @return Sum of the containers elements.
+         */
+        template <typename C, typename I, typename T>
+        inline T sum(const C& cont_) noexcept
+        {
+            T sum{};
+
+            for (I it = std::begin(cont_); it != std::end(cont_); ++it)
+            {
+                sum += *it;
+            }
+
+            return (sum);
+        }
+
+        /**
+         *  Calculate the average of a container's elements.
+         *
+         *  @param  cont_   Container to determine the average of.
+         *
+         *  @return Average of the containers elements.
+         */
+        template <typename C>
+        inline double ave(const C& cont_) noexcept
+        {
+            return (static_cast<double>(sum(cont_)) / cont::size(cont_));
+        }
+
+        /**
+         *  Calculate the magnitude of a container's elements.
+         *
+         *  @param  cont_   Container to determine the magnitude of.
+         *
+         *  @return Magnitude the containers elements.
+         */
+        template <typename C, typename I, typename T>
+        inline double mag(const C& cont_) noexcept
+        {
+            T sq_sum{};
+
+            for (I it = std::begin(cont_); it != std::end(cont_); ++it)
+            {
+                sq_sum += *it * *it;
+            }
+
+            return (std::sqrt(static_cast<double>(sq_sum)));
+        }
 
 
 
