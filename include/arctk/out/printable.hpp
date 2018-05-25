61ff82a04f13a731a49bcee3e497d8e8b3fa0eae
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 19:14:43 +0100

    Added framework.

---
+++ include/arctk/out/printable.hpp
@@ -0,0 +1,77 @@
+/**
+ *  @file   arctk/cont/vector.hpp
+ *  @date   25/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Printable base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_OUT_PRINTABLE_HPP
+#define ARCTK_OUT_PRINTABLE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == INCLUDES ==
+//  -- Std --
+#include <vector>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arc namespace
+{
+    namespace out //! output namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Printable
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          public:
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == OPERATORS ==
+
+
+
+        //  == METHODS ==
+
+
+    } // namespace out
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_OUT_PRINTABLE_HPP
