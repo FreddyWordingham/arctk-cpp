bd2d617164ee019418d459aedf5c1270a665df0d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 22:32:42 +0100

    Added framework.

---
+++ include/arctk/search/container.hpp
@@ -0,0 +1,55 @@
+/**
+ *  @file   arctk/search/container.hpp
+ *  @date   25/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Search container functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SEARCH_CONTAINER_HPP
+#define ARCTK_SEARCH_CONTAINER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+#include <cmath>
+#include <iterator>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SEARCH_CONTAINER_HPP
