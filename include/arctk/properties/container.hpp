137f3064974b6913b0b134af89c37e6e26e2b724
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 22:50:18 +0100

    Added framework.

---
+++ include/arctk/properties/container.hpp
@@ -0,0 +1,54 @@
+/**
+ *  @file   arctk/search/container.hpp
+ *  @date   25/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Container property functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PROPERTIES_CONTAINER_HPP
+#define ARCTK_PROPERTIES_CONTAINER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+#include <iterator>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace prop //! properties namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace prop
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PROPERTIES_CONTAINER_HPP
