39c167ed4d2e2a64b6937572826c89f6f2cbf759
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-25 23:24:29 +0100

    Added always_less_than function.

--- include/arctk/properties/container.hpp
+++ include/arctk/properties/container.hpp
@@ -35,6 +35,8 @@ namespace arc //! arctk namespace
         //  == FUNCTION PROTOTYPES ==
         template <typename C>
         inline size_t size(const C& cont_) noexcept;
+        template <typename C, typename I = typename C::const_iterator, typename T = typename C::value_type>
+        inline bool always_less_than(const C& cont_) noexcept;
 
 
 
@@ -50,6 +52,28 @@ namespace arc //! arctk namespace
         inline size_t size(const C& cont_) noexcept
         {
             return (std::distance(std::begin(cont_), std::end(cont_)));
+        }
+
+        /**
+         *  Determine if all elements of a container are less than a given limit.
+         *
+         *  @param  cont_   Container to test.
+         *  @param  limit_  Limit to compare elements against.
+         *
+         *  @return True if all elements are less than the limit.
+         */
+        template <typename C, typename I, typename T>
+        inline bool always_less_than(const C& cont_, const T limit_) noexcept
+        {
+            for (I it = std::begin(cont_); it != std::end(cont_); ++it)
+            {
+                if (!(*it < limit_))
+                {
+                    return (false);
+                }
+            }
+
+            return (true);
         }
 
 
