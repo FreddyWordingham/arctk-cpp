0e3ac8f53093f86e0270e0ac68cbb07c909ac067
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-26 07:15:57 +0100

    Added framework.

---
+++ include/arctk/print/container.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/print/container.hpp
+ *  @date   26/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Container print functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PRINT_CONTAINER_HPP
+#define ARCTK_PRINT_CONTAINER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace print //! print namespace
+    {
+
+
+
+    } // namespace print
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PRINT_CONTAINER_HPP
