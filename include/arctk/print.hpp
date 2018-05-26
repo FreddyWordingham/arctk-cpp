71425f6b247222e5977659ae9a3b05353c106dee
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-26 07:13:30 +0100

    Added framework.

---
+++ include/arctk/print.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/print.hpp
+ *  @date   26/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of print classes and functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PRINT_HPP
+#define ARCTK_PRINT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PRINT_HPP
