e2ee7ab7d35a40e4b3c44552e7d138a9104c67f2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-26 07:05:47 +0100

    Added pseed of light constant.

---
+++ include/arctk/constant/phys.hpp
@@ -0,0 +1,46 @@
+/**
+ *  @file   arctk/constants/phys.hpp
+ *  @date   26/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Physical constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONSTANT_PHYS_HPP
+#define ARCTK_CONSTANT_PHYS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace constant //! constants namespace
+    {
+
+
+
+        //  == CONSTANTS ==
+        constexpr const double SPEED_OF_LIGHT = 2.99792458e+8; //!< Speed of light in vacuum.
+
+
+
+    } // namespace constant
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONSTANT_PHYS_HPP
