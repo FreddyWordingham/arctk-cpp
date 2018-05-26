212d70919e524642048fc0d8f60b0ad849851a6d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-26 06:49:36 +0100

    Added framework.

---
+++ include/arctk/constant.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/constant.hpp
+ *  @date   26/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of mathematical and physical constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONSTANT_HPP
+#define ARCTK_CONSTANT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONSTANT_HPP
