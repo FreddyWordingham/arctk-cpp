c344c1b8eb1a9a83007469e37737856f13de279e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 23:13:33 +0100

    Added index printing to table function.

--- include/arctk/str/format.hpp
+++ include/arctk/str/format.hpp
@@ -53,10 +53,13 @@ namespace arc //! arctk namespace
         inline std::string table(const C& cont_) noexcept
         {
             std::stringstream stream;
+            size_t            index = 0;
 
             for (I it = std::begin(cont_); it != std::end(cont_); std::advance(it, 1))
             {
-                stream << *it << '\n';
+                stream << index << "\t: " << *it << '\n';
+
+                ++index;
             }
 
             return (stream.str());
