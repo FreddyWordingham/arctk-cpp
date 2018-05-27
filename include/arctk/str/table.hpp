fe750b5559809f5a55140b46731e47f0cd386ad6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 22:14:16 +0100

    Added framework.

---
+++ include/arctk/str/table.hpp
@@ -0,0 +1,52 @@
+/**
+ *  @file   arctk/str/table.hpp
+ *  @date   27/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Table formatting string functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STR_TABLE_HPP
+#define ARCTK_STR_TABLE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace str //! string namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace str
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STR_TABLE_HPP
