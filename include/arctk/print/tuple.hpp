642d00be6108d08f35dc6db6a4bf14328ac6d31d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 17:07:12 +0100

    Added tuple print operator method.

--- include/arctk/print/tuple.hpp
+++ include/arctk/print/tuple.hpp
@@ -19,11 +19,36 @@
 
 
 
+//  == IMPORTS ==
+#include <arctk/utl.hpp>
+
+
+
 //  == OPERATOR PROTOTYPES ==
+template <typename S, typename T>
+inline S& operator<<(S& stream_, const T& tup_) noexcept;
 
 
 
 //  == OPERATORS ==
+/**
+ *  Print tuple's contents to a stream.
+ *
+ *  @tparam S   Stream type.
+ *  @tparam T   Tuple type.
+ *
+ *  @param  stream_ Stream to print to.
+ *  @param  tup_    Tuple to print.
+ *
+ *  @return Reference to stream post-print.
+ */
+template <typename S, typename T>
+inline S& operator<<(S& stream_, const T& tup_) noexcept
+{
+    stream_ << '(';
+    arc::utl::apply_with_index(tup_, [&stream_]<typename L>(const L& val_, const size_t i, const size_t total) { stream_ << ((i == 0) ? "" : ", ") << val_; });
+    stream_ << ')';
+}
 
 
 
