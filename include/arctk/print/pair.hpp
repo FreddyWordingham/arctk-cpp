6e02c690b2580294fbe0212fc41396e5b973ca4d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 20:21:31 +0100

    Added framework.

---
+++ include/arctk/print/pair.hpp
@@ -0,0 +1,36 @@
+/**
+ *  @file   arctk/print/pair.hpp
+ *  @date   26/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Pair print functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PRINT_PAIR_HPP
+#define ARCTK_PRINT_PAIR_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == OPERATOR PROTOTYPES ==
+
+
+
+//  == OPERATORS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PRINT_PAIR_HPP
