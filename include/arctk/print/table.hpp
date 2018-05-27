1812c2c7f27d93d9bb7c08f07179f8025406640f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 22:06:51 +0100

    Added framework.

---
+++ include/arctk/print/table.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/print/table.hpp
+ *  @date   27/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Table formatting print functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PRINT_TABLE_HPP
+#define ARCTK_PRINT_TABLE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PRINT_TABLE_HPP
