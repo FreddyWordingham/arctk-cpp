e2869bba9f30a4f8406a4656bc68847f7d9599f6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 03:44:11 +0100

    Added namespace.

--- include/arctk/utl/tuple.hpp
+++ include/arctk/utl/tuple.hpp
@@ -19,11 +19,35 @@
 
 
 
-//  == OPERATOR PROTOTYPES ==
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace utl //! utility namespace
+    {
 
 
 
-//  == OPERATORS ==
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+        template <typename T, typename F, size_t... Is>
+        void for_each(T&& t, F f, std::index_sequence<Is...>)
+        {
+            auto l = {(f(std::get<Is>(t)), 0)...};
+        }
+
+        template <typename... Ts, typename F>
+        void apply(std::tuple<Ts...> const& t, F f)
+        {
+            for_each(t, f, std::index_sequence_for<Ts...>{});
+        }
+
+
+
+    } // namespace utl
+} // namespace arc
 
 
 
