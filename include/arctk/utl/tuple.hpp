549d8e470c92be44ff6450413730c8faadf088c2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 03:53:37 +0100

    Added apply and apply_to_each functions.

--- include/arctk/utl/tuple.hpp
+++ include/arctk/utl/tuple.hpp
@@ -28,20 +28,45 @@ namespace arc //! arctk namespace
 
 
         //  == FUNCTION PROTOTYPES ==
+        template <typename T, typename F, size_t... I>
+        void apply_to_each(T&& tuple_, F func_, std::index_sequence<I...>);
+        template <template <typename...> typename T, typename... Args, typename F>
+        void apply(const T<Args...>& tuple_, F func_);
 
 
 
         //  == FUNCTIONS ==
-        template <typename T, typename F, size_t... Is>
-        void for_each(T&& t, F f, std::index_sequence<Is...>)
+        /**
+         *  Apply helper function.
+         *  Call a functor on each element of a functor.
+         *
+         *  @tparam T       Type of tuple.
+         *  @tparam Args    Types stored by tuple.
+         *  @tparam F       Functor to be applied.
+         *
+         *  @param  tuple_  Tuple to be applied to.
+         *  @param  func_   Functor to be applied.
+         */
+        template <typename T, typename F, size_t... I>
+        void apply_to_each(T&& tuple_, F func_, std::index_sequence<I...>)
         {
-            auto l = {(f(std::get<Is>(t)), 0)...};
+            ((func_(std::get<I>(tuple_)), 0), ...);
         }
 
-        template <typename... Ts, typename F>
-        void apply(std::tuple<Ts...> const& t, F f)
+        /**
+         *  Apply a given functor to each element of a tuple.
+         *
+         *  @tparam T       Type of tuple.
+         *  @tparam Args    Types stored by tuple.
+         *  @tparam F       Functor to be applied.
+         *
+         *  @param  tuple_  Tuple to be applied to.
+         *  @param  func_   Functor to be applied.
+         */
+        template <template <typename...> typename T, typename... Args, typename F>
+        void apply(const T<Args...>& tuple_, F func_)
         {
-            for_each(t, f, std::index_sequence_for<Ts...>{});
+            apply_to_each(tuple_, func_, std::index_sequence_for<Args...>{});
         }
 
 
