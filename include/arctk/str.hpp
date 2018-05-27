1e8ae1aa0c00eb155a637647d80bc5d778e622dd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 22:12:37 +0100

    Added framework.

---
+++ include/arctk/str.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/str.hpp
+ *  @date   27/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of string functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STR_HPP
+#define ARCTK_STR_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STR_HPP
