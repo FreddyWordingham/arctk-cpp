d41d04e507dfe335836e9780bcc482f799fe2742
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-27 03:34:38 +0100

    Added framework.

---
+++ include/arctk/utl/tuple.hpp
@@ -0,0 +1,36 @@
+/**
+ *  @file   arctk/utl/tuple.hpp
+ *  @date   27/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Tuple utilitu functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_TUPLE_HPP
+#define ARCTK_UTL_TUPLE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == OPERATOR PROTOTYPES ==
+
+
+
+//  == OPERATORS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_TUPLE_HPP
