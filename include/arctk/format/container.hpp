416ada5335738108991df739eae2f2629cce6a6c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-28 18:13:37 +0100

    Simplified table function and added delim parameter.

--- include/arctk/format/container.hpp
+++ include/arctk/format/container.hpp
@@ -48,7 +48,7 @@ namespace arc //! arctk namespace
         template <typename... A>
         inline std::string row(const std::tuple<A...>& tup_) noexcept;
         template <typename C, typename I = typename C::const_iterator>
-        inline std::string table(const C& cont_) noexcept;
+        inline std::string table(const C& cont_, const std::string& delim_ = ", ") noexcept;
 
 
         //  == FUNCTIONS ==
@@ -88,17 +88,18 @@ namespace arc //! arctk namespace
          *  @tparam I   Iterator type of C.
          *
          *  @param  cont_   Container to form into a table string.
+         *  @param  delim_  Delimiter string.
          *
          *  @return Tabulated string of cont_.
          */
         template <typename C, typename I = typename C::const_iterator>
-        inline std::string table(const C& cont_) noexcept
+        inline std::string table(const C& cont_, const std::string& delim_) noexcept
         {
             std::stringstream stream;
 
             for (I it = std::begin(cont_); it != std::end(cont_); std::advance(it, 1))
             {
-                stream << row(*it) << '\n';
+                stream << str::to_string(tup_, "", delim_, "") << '\n';
             }
 
             return (stream.str());
