711e6efcea51f9152b63f414d77efc6f3a05f830
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-28 19:10:22 +0100

    Added framework.

---
+++ include/arctk/str/convert.hpp
@@ -0,0 +1,53 @@
+/**
+ *  @file   arctk/str/convert.hpp
+ *  @date   28/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Conversion to string functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STR_CONVERT_HPP
+#define ARCTK_STR_CONVERT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace str //! string namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace str
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STR_CONVERT_HPP
