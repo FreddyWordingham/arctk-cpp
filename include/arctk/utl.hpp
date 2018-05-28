f30b8651f189a566f6f2fc4be7d0ff495a6406d9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-28 20:44:47 +0100

    Added framework.

---
+++ include/arctk/utl.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/utl.hpp
+ *  @date   28/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of utility functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTK_HPP
+#define ARCTK_UTK_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTK_HPP
