95f56ee7035128beeca140095cad150e9f1af259
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-28 15:33:26 +0100

    Added framework.

---
+++ include/arctk/str/container.hpp
@@ -0,0 +1,54 @@
+/**
+ *  @file   arctk/str/container.hpp
+ *  @date   27/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Container string functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STR_CONTAINER_HPP
+#define ARCTK_STR_CONTAINER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+#include <sstream>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace str //! string namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace str
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STR_CONTAINER_HPP
