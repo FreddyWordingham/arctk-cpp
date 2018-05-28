514b6737b9f606c259e6741576e0dcbce1d1325d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-28 17:44:14 +0100

    Added framework.

---
+++ include/arctk/format/container.hpp
@@ -0,0 +1,53 @@
+/**
+ *  @file   arctk/format.hpp
+ *  @date   28/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Container formatting functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_FORMAT_CONTAINER_HPP
+#define ARCTK_FORMAT_CONTAINER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace format //! formatting namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace format
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_FORMAT_CONTAINER_HPP
