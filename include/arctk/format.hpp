0e76f645cc2c07e82d1fa3b3ee88c55425f2552e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-28 17:40:00 +0100

    Added framework.

---
+++ include/arctk/format.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/format.hpp
+ *  @date   28/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of formatting functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_FORMAT_HPP
+#define ARCTK_FORMAT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_FORMAT_HPP
