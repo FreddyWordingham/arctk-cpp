4bd0251cd7a3dd1fd748e26f8ea234aceb3be2eb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-28 17:45:00 +0100

    Moved table function to container format header file.

--- include/arctk/format/container.hpp
+++ include/arctk/format/container.hpp
@@ -32,10 +32,34 @@ namespace arc //! arctk namespace
 
 
         //  == FUNCTION PROTOTYPES ==
+        template <typename C, typename I = typename C::const_iterator>
+        inline std::string table(const C& cont_) noexcept;
 
 
 
         //  == FUNCTIONS ==
+        /**
+         *  Form a container into a tabulated string.
+         *
+         *  @tparam C   Container type.
+         *  @tparam I   Iterator type of C.
+         *
+         *  @param  cont_   Container to form into a table string.
+         *
+         *  @return Tabulated string of cont_.
+         */
+        template <typename C, typename I = typename C::const_iterator>
+        inline std::string table(const C& cont_) noexcept
+        {
+            std::stringstream stream;
+
+            for (I it = std::begin(cont_); it != std::end(cont_); std::advance(it, 1))
+            {
+                stream << *it << '\n';
+            }
+
+            return (stream.str());
+        }
 
 
 
