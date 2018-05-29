07b72ed774830a77f9464f94e2f9079122edd258
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-29 23:42:10 +0100

    Added framework.

---
+++ include/arctk/search.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/search.hpp
+ *  @date   29/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of search functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SEARCH_HPP
+#define ARCTK_SEARCH_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SEARCH_HPP
