797fafbceee40b242aa53f51e4c40d59f383a6dd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-29 23:10:45 +0100

    Added framework.

---
+++ include/arctk/utl/pair.hpp
@@ -0,0 +1,54 @@
+/**
+ *  @file   arctk/utl/pair.hpp
+ *  @date   29/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Pair utility functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_PAIR_HPP
+#define ARCTK_UTL_PAIR_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+#include <pair>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace utl //! utility namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_PAIR_HPP
