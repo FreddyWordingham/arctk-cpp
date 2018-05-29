c432204a9046f17281c2186a0b92ec74b50616b0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-29 23:50:50 +0100

    Added framework.

---
+++ include/arctk/prop.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/search.hpp
+ *  @date   29/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of property functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PROP_HPP
+#define ARCTK_PROP_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PROP_HPP
