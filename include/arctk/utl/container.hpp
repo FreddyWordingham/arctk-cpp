b71cbcefb9da3e51c95b7f204165d87eddeb063e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-29 23:31:10 +0100

    Added framework.

---
+++ include/arctk/utl/container.hpp
@@ -0,0 +1,54 @@
+/**
+ *  @file   arctk/utl/container.hpp
+ *  @date   29/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Container utility functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_CONTAINER_HPP
+#define ARCTK_UTL_CONTAINER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+#include <iterator>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace utl //! utility namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_CONTAINER_HPP
