27d4bebfdec45c6065b8d64f8c7988246c6db4b7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-30 21:49:17 +0100

    Added col enumeration.

--- include/arctk/log/ansi.hpp
+++ include/arctk/log/ansi.hpp
@@ -42,29 +42,56 @@ namespace arc //! arctk namespace
          */
         class Ansi
         {
+            //  == ENUMERATIONS ==
+          public:
+            //  -- Colours --
+            enum class Col
+            {
+                BLACK,
+                RED,
+                GREEN,
+                YELLOW,
+                BLUE,
+                MAGENTA,
+                CYAN,
+                WHITE,
+                BRIGHT_BLACK,
+                BRIGHT_RED,
+                BRIGHT_GREEN,
+                BRIGHT_YELLOW,
+                BRIGHT_BLUE,
+                BRIGHT_MAGENTA,
+                BRIGHT_CYAN,
+                BRIGHT_WHITE,
+                TOTAL
+            };
+
+
             //  == FIELDS ==
           public:
             //  -- General --
             const std::string reset; //!< Reset ansi settings to default.
 
             //  -- Text Colouring --
-            const std::string black;          //!< Colour the text black.
-            const std::string red;            //!< Colour the text red.
-            const std::string green;          //!< Colour the text green.
-            const std::string yellow;         //!< Colour the text yellow.
-            const std::string blue;           //!< Colour the text blue.
-            const std::string magenta;        //!< Colour the text magenta.
-            const std::string cyan;           //!< Colour the text cyan.
-            const std::string white;          //!< Colour the text white.
-            const std::string bright_black;   //!< Colour the text bright black.
-            const std::string bright_red;     //!< Colour the text bright red.
-            const std::string bright_green;   //!< Colour the text bright green.
-            const std::string bright_yellow;  //!< Colour the text bright yellow.
-            const std::string bright_blue;    //!< Colour the text bright blue.
-            const std::string bright_magenta; //!< Colour the text bright magenta.
-            const std::string bright_cyan;    //!< Colour the text bright cyan.
-            const std::string bright_white;   //!< Colour the text bright white.
-
+            const std::array<std::string, static_cast<Col::TOTAL>> col;
+            /*
+                        const std::string black;          //!< Colour the text black.
+                        const std::string red;            //!< Colour the text red.
+                        const std::string green;          //!< Colour the text green.
+                        const std::string yellow;         //!< Colour the text yellow.
+                        const std::string blue;           //!< Colour the text blue.
+                        const std::string magenta;        //!< Colour the text magenta.
+                        const std::string cyan;           //!< Colour the text cyan.
+                        const std::string white;          //!< Colour the text white.
+                        const std::string bright_black;   //!< Colour the text bright black.
+                        const std::string bright_red;     //!< Colour the text bright red.
+                        const std::string bright_green;   //!< Colour the text bright green.
+                        const std::string bright_yellow;  //!< Colour the text bright yellow.
+                        const std::string bright_blue;    //!< Colour the text bright blue.
+                        const std::string bright_magenta; //!< Colour the text bright magenta.
+                        const std::string bright_cyan;    //!< Colour the text bright cyan.
+                        const std::string bright_white;   //!< Colour the text bright white.
+            */
             //  -- Utility --
             const std::string overwrite; //!< Overwrite the previous printed line.
 
