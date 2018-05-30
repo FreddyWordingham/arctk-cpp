33277fa3ce7f00a210a4e04e3a168653289aa492
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-30 16:42:34 +0100

    Added framework.

---
+++ include/arctk/log.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/log.hpp
+ *  @date   30/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of logging classes and macros.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_LOG_HPP
+#define ARCTK_LOG_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_LOG_HPP
