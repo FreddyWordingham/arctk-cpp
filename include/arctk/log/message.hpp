ec5c640128bba5b4f335a1bf625affa59e1bc2ba
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 00:34:04 +0100

    Added framework.

---
+++ include/arctk/log/message.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @file   arctk/log/message.hpp
+ *  @date   31/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Standard console output message log class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_LOG_ANSI_HPP
+#define ARCTK_LOG_ANSI_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace log //! logging namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Message
+        {
+            //  == FIELDS ==
+          public:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+    } // namespace log
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_LOG_ANSI_HPP
