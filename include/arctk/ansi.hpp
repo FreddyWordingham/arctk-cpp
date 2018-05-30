19c0d865db8328bc7c10627661c526ac3515366d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 00:01:03 +0100

    Added framework.

---
+++ include/arctk/ansi.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/constant.hpp
+ *  @date   31/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of ANSI constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_ANSI_HPP
+#define ARCTK_ANSI_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_ANSI_HPP
