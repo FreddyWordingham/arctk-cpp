e2988f39ab576a352347e26a88d71571742d33a0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 00:11:01 +0100

    Added framework.

---
+++ include/arctk/ansi/utl.hpp
@@ -0,0 +1,46 @@
+/**
+ *  @file   arctk/ansi/utl.hpp
+ *  @date   31/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Ansi utility constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_ANSI_UTL_HPP
+#define ARCTK_ANSI_UTL_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace ansi //! ansi namespace
+    {
+
+
+
+        //  == CONSTANTS ==
+        constexpr const char* const ANSI_FG_BLACK = "\033[0;30m";
+
+
+
+    } // namespace ansi
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_ANSI_UTL_HPP
