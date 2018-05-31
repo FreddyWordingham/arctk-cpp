116d2c7993d37a59eec6601b48e6ef52699c61e2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 21:53:33 +0100

    Added framework.

---
+++ include/arctk/math/power.hpp
@@ -0,0 +1,57 @@
+/**
+ *  @file   arctk/math/power.hpp
+ *  @date   31/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Math power functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_POWER_HPP
+#define ARCTK_MATH_POWER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/constant.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Power --
+
+
+
+        //  == FUNCTIONS ==
+        //  -- Power --
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_POWER_HPP
