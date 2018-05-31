98d014e51e161b4a987622ccbd1ff4c7ee30a5da
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 21:03:27 +0100

    Corrected namespace.

--- include/arctk/log/output/stopwatch.hpp
+++ include/arctk/log/output/stopwatch.hpp
@@ -122,7 +122,7 @@ namespace arc //! arc namespace
              */
             inline Stopwatch::~Stopwatch() noexcept
             {
-                *this << _col << " took: " << utl::time(static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - _construct_time).count()));
+                *this << _col << " took: " << str::time(static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - _construct_time).count()));
                 *this << Term::instance().reset() << '\n';
             }
 
