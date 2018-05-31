5c7c6385ae51ffb569ab4ca1c6c5de9dbf933aec
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 22:05:07 +0100

    Added framework.

---
+++ include/arctk/math/vec3.hpp
@@ -0,0 +1,75 @@
+/**
+ *  @file   arctk/math/vec3.hpp
+ *  @date   31/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Mathematical size three vector class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_VEC3_HPP
+#define ARCTK_MATH_VEC3_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Size three mathematical vector class.
+         */
+        class Vec3
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+            inline Vec3() noexcept;
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_VEC3_HPP
