9a5498814db2e02f28f38399a087bba9e37d8305
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 21:52:13 +0100

    Added angle conversion functions.

--- include/arctk/math/conversion.hpp
+++ include/arctk/math/conversion.hpp
@@ -35,11 +35,36 @@ namespace arc //! arctk namespace
 
         //  == FUNCTION PROTOTYPES ==
         //  -- Angle --
+        inline double rad_to_deg(double val_) noexcept;
+        inline double deg_to_rad(double val_) noexcept;
 
 
 
         //  == FUNCTIONS ==
         //  -- Angle --
+        /**
+         *  Convert a value in radians to a value in degrees.
+         *
+         *  @param  val_  Value to convert to radians.
+         *
+         *  @return Value in degrees.
+         */
+        inline double rad_to_deg(const double val_) noexcept
+        {
+            return (180.0 / constant::math::PI * val_);
+        }
+
+        /**
+         *  Convert a value in degrees to a value in radians.
+         *
+         *  @param  val_  Value to convert to degrees.
+         *
+         *  @return Value in radians.
+         */
+        inline double deg_to_rad(const double val_) noexcept
+        {
+            return (constant::math::PI / 180.0 * val_);
+        }
 
 
 
