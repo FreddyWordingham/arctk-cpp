348f0ae525d65dc02cc9348c6a6846c64fc2cd14
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 22:00:46 +0100

    Added framework.

---
+++ include/arctk/math/vec.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/math/vec.hpp
+ *  @date   31/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of vector classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_VEC_HPP
+#define ARCTK_MATH_VEC_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_VEC_HPP
