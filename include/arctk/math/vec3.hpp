06ec512598b57a3c59568e75b381f0ea255c5e64
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 22:06:14 +0100

    Added data fields.

--- include/arctk/math/vec3.hpp
+++ include/arctk/math/vec3.hpp
@@ -36,10 +36,17 @@ namespace arc //! arctk namespace
         /**
          *  Size three mathematical vector class.
          */
+        template <typename T>
         class Vec3
         {
             //  == FIELDS ==
-          private:
+          public:
+            //  -- Data --
+            T x{0};
+            T y{0};
+            T z{0};
+
+
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
