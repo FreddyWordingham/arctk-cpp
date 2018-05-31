fed9140bcd6f5ef0fd51d30922bde05c32020943
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-05-31 12:01:48 +0100

    Linked to log collection.

--- include/arctk/log.hpp
+++ include/arctk/log.hpp
@@ -21,6 +21,7 @@
 
 //  == IMPORTS ==
 #include <arctk/log/output.hpp>
+#include <arctk/log/output/error.hpp>
 #include <arctk/log/output/message.hpp>
 #include <arctk/log/output/warning.hpp>
 #include <arctk/log/term.hpp>
