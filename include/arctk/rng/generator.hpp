4d794a3db35b163c16e4eb3d074d47951245d475
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-01 09:10:10 +0100

    Added framework.

---
+++ include/arctk/rng/generator.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @file   arctk/rng/generator.hpp
+ *  @date   01/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Pseudo random number generator base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_GENERATOR_HPP
+#define ARCTK_RNG_GENERATOR_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace rng //! random number generation namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Random generator base class.
+         */
+        class Generator
+        {
+            //  == FIELDS ==
+          public:
+            //  -- Data --
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == OPERATORS ==
+
+
+
+    } // namespace rng
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_GENERATOR_HPP
