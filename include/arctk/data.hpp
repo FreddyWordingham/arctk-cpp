bdbbe80a60adcebe58c71b18c9931f2696adfd0e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-02 00:41:47 +0100

    Added data collection header.

---
+++ include/arctk/data.hpp
@@ -0,0 +1,34 @@
+/**
+ *  @file   arctk/data.hpp
+ *  @date   01/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of data functions and classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_HPP
+#define ARCTK_DATA_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+//#include <arctk/format/table.hpp>
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_HPP
