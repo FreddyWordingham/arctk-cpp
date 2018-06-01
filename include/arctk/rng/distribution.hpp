fab03cc0c4edfc84176524a624227419e5ba996e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-01 11:32:51 +0100

    Added framework.

---
+++ include/arctk/rng/distribution.hpp
@@ -0,0 +1,68 @@
+/**
+ *  @file   arctk/rng/generator.hpp
+ *  @date   01/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Distribution conversion
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_DISTRIBUTION_HPP
+#define ARCTK_RNG_DISTRIBUTION_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace rng //! random number generation namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Distribution conversion class.
+         *  Converts a uniform floating point number between zero and unity to a random value of the probability distribution.
+         */
+        class Distribution
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace rng
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_DISTRIBUTION_HPP
