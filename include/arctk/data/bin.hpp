bbb27f33358599048f2fc125b7f3e3438b5e84ae
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-02 00:46:37 +0100

    Added framework.

---
+++ include/arctk/data/bin.hpp
@@ -0,0 +1,58 @@
+/**
+ *  @file   arctk/data/bin.hpp
+ *  @date   02/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Data binning base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_BIN_HPP
+#define ARCTK_DATA_BIN_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Bin
+        {
+            //  == FIELDS ==
+          public:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_BIN_HPP
