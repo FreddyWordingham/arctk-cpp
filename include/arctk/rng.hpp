03be778aff124834b9bdbb329116b3c4124b3420
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-01 09:01:45 +0100

    Renamed random header to rng.

---
+++ include/arctk/rng.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/prop.hpp
+ *  @date   29/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of property functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RANDOM_HPP
+#define ARCTK_RANDOM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RANDOM_HPP
