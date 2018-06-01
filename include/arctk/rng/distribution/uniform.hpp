21162328b6a87f95d1fa68d14785e55265c92fdb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-01 11:43:22 +0100

    Added framework.

---
+++ include/arctk/rng/distribution/uniform.hpp
@@ -0,0 +1,75 @@
+/**
+ *  @file   arctk/rng/distribution/uniform.hpp
+ *  @date   01/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Uniform range probability distribution.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_DISTRIBUTION_UNIFORM_HPP
+#define ARCTK_RNG_DISTRIBUTION_UNIFORM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace rng //! random number generation namespace
+    {
+        namespace generator //! random number generator engine
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Form a random number drawn from a uniform probability distribution.
+             */
+            class Uniform : public Distribution
+            {
+                //  == FIELDS ==
+              public:
+                //  -- Generation --
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Generation --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace generator
+    }     // namespace rng
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_DISTRIBUTION_UNIFORM_HPP
