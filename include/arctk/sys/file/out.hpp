b11b21df70c8db3197a826a2c696b5f19c6d2fe6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-03 01:41:07 +0100

    Added framework.

---
+++ include/arctk/sys/file/out.hpp
@@ -0,0 +1,82 @@
+/**
+ *  @file   arctk/sys/file/out.hpp
+ *  @date   03/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Output file system class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SYS_FILE_OUT_HPP
+#define ARCTK_SYS_FILE_OUT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace sys //! system namespace
+    {
+        namespace file //! file namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Output file class.
+             */
+            class Out
+            {
+                //  == FIELDS ==
+              private:
+                //  -- Path --
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+            //  == INSTANTIATION --
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+            //  -- Getters --
+
+
+
+        } // namespace file
+    }     // namespace sys
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SYS_FILE_OUT_HPP
