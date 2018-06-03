8f2a5ad9b4e8b60a8d48e68320a21d43bd4253bf
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-04 00:18:12 +0100

    Added framework.

---
+++ include/arctk/gui/uniform.hpp
@@ -0,0 +1,73 @@
+/**
+ *  @file   arctk/gui/uniform.hpp
+ *  @date   04/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Shader uniform class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_UNIFORM_HPP
+#define ARCTK_GUI_UNIFORM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_GUI
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical user interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Uniform
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_GUI
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_UNIFORM_HPP
