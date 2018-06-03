74cfb9b6d2468a66a0368a4cb15b7bdf143ac178
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-03 16:57:28 +0100

    Added framework.

---
+++ include/arctk/gui/keymap.hpp
@@ -0,0 +1,73 @@
+/**
+ *  @file   arctk/gui/keymap.hpp
+ *  @date   03/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Graphical key mapping class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_KEYMAP_HPP
+#define ARCTK_GUI_KEYMAP_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_GUI
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical user interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Keymap
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_GUI
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_KEYMAP_HPP
