510464275f66d06f6ee7d25a6a9b5a3ab06ba7e3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-03 16:53:58 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@
 #include <arctk/format.hpp>
 #include <arctk/format/table.hpp>
 #include <arctk/gui.hpp>
+#include <arctk/gui/keymap.hpp>
 #include <arctk/gui/window.hpp>
 #include <arctk/log.hpp>
 #include <arctk/log/output.hpp>
