80ed8e5c9a3140cc0b6710d5a83910ffc7200d96
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-03 23:50:08 +0100

    Added unbind method.

--- include/arctk/gui/keymap.hpp
+++ include/arctk/gui/keymap.hpp
@@ -68,6 +68,7 @@ namespace arc //! arctk namespace
           public:
             //  -- Setters --
             inline void bind(int key_, const std::function<void()>& func_, bool sticky_ = false, int state_ = GLFW_RELEASE) noexcept;
+            inline void unbind(int key_) noexcept;
 
             //  -- Operation --
             inline bool poll(const Window& win_) noexcept;
@@ -87,6 +88,14 @@ namespace arc //! arctk namespace
             assert(key_ != QUIT_KEY);
 
             _map.emplace(std::make_pair(key_, Keybind(func_, sticky_, state_)));
+        }
+
+        inline void Keymap::unbind(const int key_) noexcept
+        {
+            assert(key_ != QUIT_KEY);
+            assert(_map.find(key_) != _map.end());
+
+            _map.erase(key_);
         }
 
 
