47bba31da58989376ebb98276430d07b62947a05
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-04 00:35:53 +0100

    Added shader gui class file.

---
+++ include/arctk/gui/shader.hpp
@@ -0,0 +1,70 @@
+/**
+ *  @file   arctk/gui/shader.hpp
+ *  @date   04/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Graphical shader class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_SHADER_HPP
+#define ARCTK_GUI_SHADER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_GUI
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical user interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Shader
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_GUI
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_SHADER_HPP
