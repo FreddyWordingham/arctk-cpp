4a7e77bfac815d77105fc12b4da5604802306309
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-03 19:19:12 +0100

    Documented sum method.

--- include/arctk/math/vec3.hpp
+++ include/arctk/math/vec3.hpp
@@ -616,6 +616,13 @@ namespace arc //! arctk namespace
 
         //  == METHODS ==
         //  -- Mathematical --
+        /**
+         *  Calculate the sum of the vec elements.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @return Sum of the vec elements.
+         */
         template <typename T>
         constexpr inline T Vec3<T>::sum() const noexcept
         {
