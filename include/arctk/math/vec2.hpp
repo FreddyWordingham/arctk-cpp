11ddddf3ef07c1d8815338175bfc5ca1f903c47a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-03 13:34:29 +0100

    Added framework.

---
+++ include/arctk/math/vec2.hpp
@@ -0,0 +1,646 @@
+/**
+ *  @file   arctk/math/vec2.hpp
+ *  @date   03/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Mathematical size two vector class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_VEC2_HPP
+#define ARCTK_MATH_VEC2_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <cmath>
+
+//  -- Graphical --
+#ifdef ARCTK_MOD_CORE
+#include <glm/glm.hpp>
+#endif
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Size two mathematical vector class.
+         *
+         *  @tparam T   Type stored by the vec.
+         */
+        template <typename T>
+        class Vec2
+        {
+            //  == FIELDS ==
+          public:
+            //  -- Data --
+            /**
+             *  First vec element.
+             */
+            union
+            {
+                T x,   //!< Cartesian x-dimension.
+                  rho, //!< Polar radial-dimension.
+                  r;   //!< Colour red-dimension.
+            };
+
+            /**
+             *  Second vec element.
+             */
+            union
+            {
+                T y,     //!< Cartesian y-dimension.
+                  theta, //!< Polar inclination-dimension.
+                  g;     //!< Colour green-dimension.
+            };
+
+            /**
+             *  Third vec element.
+             */
+            union
+            {
+                T z,   //!< Cartesian z-dimension.
+                  phi, //!< Polar azimuthal-dimension.
+                  b;   //!< Colour blue-dimension.
+            };
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+            constexpr inline Vec2() noexcept;
+            constexpr inline Vec2(T x_, T y_, T z_) noexcept;
+
+
+            //  == OPERATORS ==
+          public:
+            //  -- Conversion --
+#ifdef ARCTK_MOD_CORE
+            constexpr inline operator glm::vec2() const noexcept;
+#endif
+
+            //  -- Stream --
+            template <typename S, typename L>
+            friend inline S& operator<<(S& stream_, const Vec2<T>& vec_) noexcept;
+
+            //  -- Assignment --
+            constexpr inline Vec2<T>& operator+=(T val_) noexcept;
+            constexpr inline Vec2<T>& operator+=(const Vec2<T>& vec_) noexcept;
+            constexpr inline Vec2<T>& operator-=(T val_) noexcept;
+            constexpr inline Vec2<T>& operator-=(const Vec2<T>& vec_) noexcept;
+            constexpr inline Vec2<T>& operator*=(T val_) noexcept;
+            constexpr inline Vec2<T>& operator/=(T val_) noexcept;
+            constexpr inline Vec2<T>& operator^=(const Vec2<T>& vec_) noexcept;
+
+            //  -- Increment / Decrement --
+            constexpr inline Vec2<T>&      operator++() noexcept;
+            constexpr inline const Vec2<T> operator++(int) noexcept;
+            constexpr inline Vec2<T>&      operator--() noexcept;
+            constexpr inline const Vec2<T> operator--(int) noexcept;
+
+            //  -- Arithmetic --
+            constexpr inline Vec2<T> operator+() const noexcept;
+            constexpr inline Vec2<T> operator-() const noexcept;
+            constexpr inline Vec2<T> operator+(T val_) const noexcept;
+            constexpr inline Vec2<T> operator+(const Vec2<T>& vec_) const noexcept;
+            constexpr inline Vec2<T> operator-(T val_) const noexcept;
+            constexpr inline Vec2<T> operator-(const Vec2<T>& vec_) const noexcept;
+            constexpr inline Vec2<T> operator*(T val_) const noexcept;
+            constexpr inline T       operator*(const Vec2<T>& vec_) const noexcept;
+            constexpr inline Vec2<T> operator/(T val_) const noexcept;
+            constexpr inline Vec2<T> operator^(const Vec2<T>& vec_) const noexcept;
+
+            //  -- Access --
+            constexpr inline T&       operator[](size_t index_) noexcept;
+            constexpr inline const T& operator[](size_t index_) const noexcept;
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+            constexpr inline T      sum() const noexcept;
+            constexpr inline double mag() const noexcept;
+            constexpr inline void   normalise() const noexcept;
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+        /**
+         *  Construct a vec2 with initial element values of zero.
+         *
+         *  @tparam T   Type stored by the vec.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>::Vec2() noexcept
+          : x({})
+          , y({})
+          , z({})
+        {
+        }
+
+        /**
+         *  Construct a vec2 with initial element values.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  x_  Initial value of the x component.
+         *  @param  y_  Initial value of the y component.
+         *  @param  z_  Initial value of the z component.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>::Vec2(const T x_, const T y_, const T z_) noexcept
+          : x(x_)
+          , y(y_)
+          , z(z_)
+        {
+        }
+
+
+
+        //  == OPERATORS ==
+        //  -- Conversion --
+#ifdef ARCTK_MOD_CORE
+        template <typename T>
+        constexpr inline Vec2<T>::operator glm::vec2() const noexcept
+        {
+            return (glm::vec2(static_cast<float>(x), static_cast<float>(y), static_cast<float>(z)));
+        }
+#endif
+
+        //  -- Stream --
+        /**
+         *  Print a vec3 as a human-readable string.
+         *
+         *  @tparam S   Type of stream to print to.
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  stream_ Stream to print to.
+         *  @param  vec_    Vector to print.
+         *
+         *  @return A reference to the stream post-print.
+         */
+        template <typename S, typename T>
+        inline S& operator<<(S& stream_, const Vec2<T>& vec_) noexcept
+        {
+            stream_ << '{' << vec_.x << ", " << vec_.y << ", " << vec_.z << '}';
+
+            return (stream_);
+        }
+
+
+        //  -- Assignment --
+        /**
+         *  Add a value to all elements of the vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  val_    Value to add to each vec element.
+         *
+         *  @return Reference to this vec post-operation.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator+=(const T val_) noexcept
+        {
+            x += val_;
+            y += val_;
+            z += val_;
+
+            return (*this);
+        }
+
+        /**
+         *  Add another vec's elements to each corresponding element of the vec.
+         *
+         *  @tparam T   Type stored by the vecs.
+         *
+         *  @param  vec_    Vec to add to this vec.
+         *
+         *  @return Reference to this vec post-operation.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator+=(const Vec2<T>& vec_) noexcept
+        {
+            x += vec_.x;
+            y += vec_.y;
+            z += vec_.z;
+
+            return (*this);
+        }
+
+        /**
+         *  Subtract a value from all elements of the vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  val_    Value to subtract from each vec element.
+         *
+         *  @return Reference to this vec post-operation.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator-=(const T val_) noexcept
+        {
+            x -= val_;
+            y -= val_;
+            z -= val_;
+
+            return (*this);
+        }
+
+        /**
+         *  Subtract another vec's elements from each corresponding element of the vec.
+         *
+         *  @tparam T   Type stored by the vecs.
+         *
+         *  @param  vec_    Vec to subtract from this vec.
+         *
+         *  @return Reference to this vec post-operation.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator-=(const Vec2<T>& vec_) noexcept
+        {
+            x -= vec_.x;
+            y -= vec_.y;
+            z -= vec_.z;
+
+            return (*this);
+        }
+
+        /**
+         *  Multiply all elements of the vec by a value.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  val_    Value used to multiply each vec element by.
+         *
+         *  @return Reference to this vec post-operation.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator*=(const T val_) noexcept
+        {
+            x *= val_;
+            y *= val_;
+            z *= val_;
+
+            return (*this);
+        }
+
+        /**
+         *  Divide all elements of the vec by a value.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  val_    Value used to divide each vec element by.
+         *
+         *  @return Reference to this vec post-operation.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator/=(const T val_) noexcept
+        {
+            x /= val_;
+            y /= val_;
+            z /= val_;
+
+            return (*this);
+        }
+
+        /**
+         *  Perform the cross product using another vec.
+         *  This vec forms the left-hand-side of the cross operator.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  vec_    Vec used to perform the cross product with.
+         *
+         *  @return Reference to this vec post-operation.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator^=(const Vec2<T>& vec_) noexcept
+        {
+            const T pre_x{x};
+            const T pre_y{y};
+            const T pre_z{z};
+
+            x = (pre_y * vec_.z) - (pre_z * vec_.y);
+            y = (pre_z * vec_.x) - (pre_x * vec_.z);
+            z = (pre_x * vec_.y) - (pre_y * vec_.x);
+
+            return (*this);
+        }
+
+
+        //  -- Increment / Decrement --
+        /**
+         *  Increment each element of the vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @return A reference to this vec post-increment.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator++() noexcept
+        {
+            ++x;
+            ++y;
+            ++z;
+
+            return (*this);
+        }
+
+        /**
+         *  Increment each element of the vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @return A copy of this vec pre-increment.
+         */
+        template <typename T>
+        constexpr inline const Vec2<T> Vec2<T>::operator++(int) noexcept
+        {
+            const Vec2 vec = *this;
+
+            ++x;
+            ++y;
+            ++z;
+
+            return (vec);
+        }
+
+        /**
+         *  Decrement each element of the vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @return A reference to this vec post-decrement.
+         */
+        template <typename T>
+        constexpr inline Vec2<T>& Vec2<T>::operator--() noexcept
+        {
+            --x;
+            --y;
+            --z;
+
+            return (*this);
+        }
+
+        /**
+         *  Decrement each element of the vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @return A copy of this vec pre-decrement.
+         */
+        template <typename T>
+        constexpr inline const Vec2<T> Vec2<T>::operator--(int) noexcept
+        {
+            const Vec2 vec = *this;
+
+            --x;
+            --y;
+            --z;
+
+            return (vec);
+        }
+
+
+        //  -- Arithmetic --
+        /**
+         *  Positive copy the vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @return Positive copy of the vec.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator+() const noexcept
+        {
+            return (Vec2(+x, +y, +z));
+        }
+
+        /**
+         *  Negative copy the vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @return Negative copy of the vec.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator-() const noexcept
+        {
+            return (Vec2(-x, -y, -z));
+        }
+
+        /**
+         *  Add a value to a vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  val_    Value to add to the vec.
+         *
+         *  @return Vec formed by adding the value to the vec.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator+(T val_) const noexcept
+        {
+            return (Vec2(x + val_, y + val_, z + val_));
+        }
+
+        /**
+         *  Add a vec to a vec.
+         *
+         *  @tparam T   Type stored by the vecs.
+         *
+         *  @param  vec_    Vec to add to the vec.
+         *
+         *  @return Vec formed by adding the vec to the vec.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator+(const Vec2<T>& vec_) const noexcept
+        {
+            return (Vec2(x + vec_.x, y + vec_.y, z + vec_.z));
+        }
+
+        /**
+         *  Subtract a value from a vec.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  val_    Value to subtract from the vec.
+         *
+         *  @return Vec formed by subtracting the value from the vec.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator-(T val_) const noexcept
+        {
+            return (Vec2(x - val_, y - val_, z - val_));
+        }
+
+        /**
+         *  Subtract a vec from a vec.
+         *
+         *  @tparam T   Type stored by the vecs.
+         *
+         *  @param  vec_    Vec to subtract from the vec.
+         *
+         *  @return Vec formed by subtracting the vec from the vec.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator-(const Vec2<T>& vec_) const noexcept
+        {
+            return (Vec2(x - vec_.x, y - vec_.y, z - vec_.z));
+        }
+
+        /**
+         *  Multiply a vecs elements by a value.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  val_    Value used to multiply the vec elements.
+         *
+         *  @return Vec formed by multiplying the vec elements by the value.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator*(T val_) const noexcept
+        {
+            return (Vec2(x * val_, y * val_, z * val_));
+        }
+
+        /**
+         *  Calculate the dot-product of two vecs.
+         *
+         *  @tparam T   Type stored by the vecs.
+         *
+         *  @param  vec_    Vec to perform the dot-product with.
+         *
+         *  @return Dot-product of the vecs.
+         */
+        template <typename T>
+        constexpr inline T Vec2<T>::operator*(const Vec2<T>& vec_) const noexcept
+        {
+            return ((x * vec_.x) + (y * vec_.y) + (z * vec_.z));
+        }
+
+        /**
+         *  Divide a vecs elements by a value.
+         *
+         *  @tparam T   Type stored by the vec.
+         *
+         *  @param  val_    Value used to divide the vec elements.
+         *
+         *  @return Vec formed by dividing the vec elements by the value.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator/(T val_) const noexcept
+        {
+            return (Vec2(x / val_, y / val_, z / val_));
+        }
+
+        /**
+         *  Calculate the cross-product of two vecs.
+         *
+         *  @tparam T   Type stored by the vecs.
+         *
+         *  @param  vec_    Vec to perform the cross-product with.
+         *
+         *  @return Cross-product of the vecs.
+         */
+        template <typename T>
+        constexpr inline Vec2<T> Vec2<T>::operator^(const Vec2<T>& vec_) const noexcept
+        {
+            return (Vec2((y * vec_.z) - (z * vec_.y), (z * vec_.x) - (x * vec_.z), (x * vec_.y) - (y * vec_.x)));
+        }
+
+
+
+        //  -- Access --
+        /**
+         *  Access an element of the vec.
+         *
+         *  @param  index_  Index of the element to access.
+         *
+         *  @pre    index_ must be less than two.
+         *
+         *  @return A reference to the element requested.
+         */
+        template <typename T>
+        constexpr inline T& Vec2<T>::operator[](const size_t index_) noexcept
+        {
+            assert(index_ < 3);
+
+            return ((&x)[index_]);
+        }
+
+        /**
+         *  Access an element of the vec.
+         *
+         *  @param  index_  Index of the element to access.
+         *
+         *  @pre    index_ must be less than two.
+         *
+         *  @return A const reference to the element requested.
+         */
+        template <typename T>
+        constexpr inline const T& Vec2<T>::operator[](const size_t index_) const noexcept
+        {
+            assert(index_ < 3);
+
+            return ((&x)[index_]);
+        }
+
+
+
+        //  == METHODS ==
+        //  -- Mathematical --
+        template <typename T>
+        constexpr inline T Vec2<T>::sum() const noexcept
+        {
+            return (x + y + z);
+        }
+
+        template <typename T>
+        constexpr inline double Vec2<T>::mag() const noexcept
+        {
+            return (std::sqrt(static_cast<double>((x * x) + (y * y) + (z * z))));
+        }
+
+        template <typename T>
+        constexpr inline void Vec2<T>::normalise() const noexcept
+        {
+            const double m = 1.0 / mag();
+
+            x *= m;
+            y *= m;
+            z *= m;
+        }
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_VEC2_HPP
