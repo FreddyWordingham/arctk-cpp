76f4680336212b04f30e0d91186f030fcde768a8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-04 23:51:43 +0100

    Added framework.

---
+++ include/arctk/parse/string.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file   arctk/parse/string.hpp
+ *  @date   04/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  String parsing functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PARSE_STRING_HPP
+#define ARCTK_PARSE_STRING_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace parse //! parsing namespace
+    {
+
+
+
+        //  == FUNCTORS ==
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Conversion --
+
+
+
+    } // namespace parse
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PARSE_STRING_HPP
