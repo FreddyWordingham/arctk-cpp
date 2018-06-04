9cfe7a683c91381ee4d59a5f48c3c9f9efbe9468
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-04 13:31:31 +0100

    Made special methods defaulted.

--- include/arctk/log/output/progress.hpp
+++ include/arctk/log/output/progress.hpp
@@ -61,8 +61,8 @@ namespace arc //! arctk namespace
               public:
                 //  -- Constructors --
                 inline Progress(const std::string& str_, double frac_) noexcept;
-                inline Progress(const Progress&) = delete; //!< Deleted copy constructor.
-                inline Progress(Progress&&)      = delete; //!< Deleted move constructor.
+                inline Progress(const Progress&) = default; //!< Defaulted copy constructor.
+                inline Progress(Progress&&)      = default; //!< Defaulted move constructor.
 
                 //  -- Destructors --
                 inline ~Progress() noexcept override;
@@ -71,8 +71,8 @@ namespace arc //! arctk namespace
                 //  == OPERATORS ==
               public:
                 //  -- Assignment --
-                inline Progress& operator=(const Progress&) = delete; //!< Deleted copy operator. @return Reference to copied object.
-                inline Progress& operator=(Progress&&) = delete;      //!< Deleted move operator. @return Reference to moved object.
+                inline Progress& operator=(const Progress&) = default; //!< Defaulted copy operator. @return Reference to copied object.
+                inline Progress& operator=(Progress&&) = default;      //!< Defaulted move operator. @return Reference to moved object.
 
 
                 //  == METHODS ==
