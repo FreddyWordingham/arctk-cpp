f6e9febacf464a25ad8ebe91b41c8e901e4a22d3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-04 23:43:01 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,7 @@
 #include <arctk/math/vec2.hpp>
 #include <arctk/math/vec3.hpp>
 #include <arctk/math/vec4.hpp>
+#include <arctk/parse.hpp>
 #include <arctk/print.hpp>
 #include <arctk/prop.hpp>
 #include <arctk/prop/container.hpp>
