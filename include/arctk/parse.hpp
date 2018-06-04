52f2ac0b8afe7947d1b2b45cbc367891b5b19df7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-04 23:42:43 +0100

    Added framework.

---
+++ include/arctk/parse.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/parse.hpp
+ *  @date   04/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of parsing functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PARSE_HPP
+#define ARCTK_PARSE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PARSE_HPP
