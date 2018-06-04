d73dd0384641fa4fd76c66a672327b5718945fb6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-04 21:47:28 +0100

    Removed vector conversion functions.

--- include/arctk/math/conversion.hpp
+++ include/arctk/math/conversion.hpp
@@ -43,10 +43,6 @@ namespace arc //! arctk namespace
         template <typename T>
         inline T deg_to_rad(T val_) noexcept;
 
-        //  -- Co-ordinate --
-        template <typename T>
-        inline T polar_to_cart(const Vec2<T>& polar_) noexcept;
-
 
 
         //  == FUNCTIONS ==
@@ -75,14 +71,6 @@ namespace arc //! arctk namespace
         inline T deg_to_rad(const T val_) noexcept
         {
             return (static_cast<T>(constant::PI / 180.0) * val_);
-        }
-
-
-        //  -- Co-ordinate --
-        template <typename T>
-        inline T polar_to_cart(const Vec2<T>& polar_) noexcept
-        {
-            return (Vec2<T>(polar_.rho * std::cos(polar_.theta), polar_.rho * std::sin(polar_.theta)));
         }
 
 
