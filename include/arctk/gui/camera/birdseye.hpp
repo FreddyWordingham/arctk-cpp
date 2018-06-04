85611a029c77c073db9dd7a7a9079d24bd5ca1ff
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-04 01:56:10 +0100

    Added framework.

---
+++ include/arctk/gui/camera/birdseye.hpp
@@ -0,0 +1,78 @@
+/**
+ *  @file   arctk/gui/camera/birdseye.hpp
+ *  @date   04/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Birdseye camera class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_CAMERA_BIRDSEYE_HPP
+#define ARCTK_GUI_CAMERA_BIRDSEYE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_GUI
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/gui/camera.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical user interface namespace
+    {
+        namespace camera //! camera namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Birdseye camera class.
+             */
+            class Birdseye
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+            //  == INSTANTIATION --
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace camera
+    }     // namespace gui
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_GUI
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_CAMERA_BIRDSEYE_HPP
