4e68087431541200da3d002cc2aa343d39b15210
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-05 01:57:22 +0100

    Added framework.

---
+++ include/arctk/phys/particle.hpp
@@ -0,0 +1,67 @@
+/**
+ *  @file   arctk/phys/particle.hpp
+ *  @date   05/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Particle physics class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_PARTICLE_HPP
+#define ARCTK_PHYS_PARTICLE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Particle
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace phys
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_PARTICLE_HPP
