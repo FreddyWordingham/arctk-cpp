693094535d4673b1d91e400325c896c0a1f83af8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-06 22:43:23 +0100

    Added default parameter.

--- include/arctk/geom/shape/sphere.hpp
+++ include/arctk/geom/shape/sphere.hpp
@@ -55,7 +55,7 @@ namespace arc //! arctk namespace
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-                inline Sphere(double radius_, const math::Vec3<double>& centre_) noexcept;
+                inline Sphere(double radius_, const math::Vec3<double>& centre_ = {0.0, 0.0, 0.0}) noexcept;
 
 
                 //  == METHODS ==
