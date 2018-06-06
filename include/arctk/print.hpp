2b26f63e1c72ae0b3fa1f5d267d67c1f8311d093
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-06 12:28:59 +0100

    Moved print operators to global scope.

--- include/arctk/print.hpp
+++ include/arctk/print.hpp
@@ -29,89 +29,79 @@
 
 
 
-//  == NAMESPACE ==
-namespace arc //! arctk namespace
+//  == OPERATOR PROTOTYPES ==
+//  -- Stream --
+template <typename S, typename C, typename T = typename C::value_type, typename I = typename C::const_iterator, typename = typename std::enable_if<!std::is_same<C, std::string>::value>::type>
+inline S& operator<<(S& stream_, const C& cont_);
+template <typename S, typename A0, typename A1>
+inline S& operator<<(S& stream_, const std::pair<A0, A1>& pair_);
+template <typename S, typename... A>
+inline S& operator<<(S& stream_, const std::tuple<A...>& tup_);
+
+
+
+//  == OPERATORS ==
+//  -- Stream --
+/**
+ *  Print a container as a human-readable string.
+ *  This function is disabled for the std::string container type.
+ *
+ *  @tparam S   Type of stream to print to.
+ *  @tparam C   Type of container to print.
+ *  @tparam T   Type stored by C.
+ *  @tparam I   Type of const iterator of C.
+ *
+ *  @param  stream_ Stream to print to.
+ *  @param  cont_   Container to print.
+ *
+ *  @return A reference to the stream post-print.
+ */
+template <typename S, typename C, typename T, typename I, typename>
+inline S& operator<<(S& stream_, const C& cont_)
 {
+    stream_ << arc::str::to_string(cont_);
 
+    return (stream_);
+}
 
+/**
+ *  Print a pair as a human-readable string.
+ *
+ *  @tparam S   Type of stream to print to.
+ *  @tparam A0  First type stored by pair.
+ *  @tparam A1  Second type stored by pair.
+ *
+ *  @param  stream_ Stream to print to.
+ *  @param  pair_   pair to print.
+ *
+ *  @return A reference to the stream post-print.
+ */
+template <typename S, typename A0, typename A1>
+inline S& operator<<(S& stream_, const std::pair<A0, A1>& pair_)
+{
+    stream_ << arc::str::to_string(pair_);
 
-    //  == OPERATOR PROTOTYPES ==
-    //  -- Stream --
-    template <typename S, typename C, typename T = typename C::value_type, typename I = typename C::const_iterator, typename = typename std::enable_if<!std::is_same<C, std::string>::value>::type>
-    inline S& operator<<(S& stream_, const C& cont_);
-    template <typename S, typename A0, typename A1>
-    inline S& operator<<(S& stream_, const std::pair<A0, A1>& pair_);
-    template <typename S, typename... A>
-    inline S& operator<<(S& stream_, const std::tuple<A...>& tup_);
+    return (stream_);
+}
 
+/**
+ *  Print a tuple as a human-readable string.
+ *
+ *  @tparam S   Type of stream to print to.
+ *  @tparam A   Types stored by the tuple.
+ *
+ *  @param  stream_ Stream to print to.
+ *  @param  tup_    Tuple to print.
+ *
+ *  @return A reference to the stream post-print.
+ */
+template <typename S, typename... A>
+inline S& operator<<(S& stream_, const std::tuple<A...>& tup_)
+{
+    stream_ << arc::str::to_string(tup_);
 
-
-    //  == OPERATORS ==
-    //  -- Stream --
-    /**
-     *  Print a container as a human-readable string.
-     *  This function is disabled for the std::string container type.
-     *
-     *  @tparam S   Type of stream to print to.
-     *  @tparam C   Type of container to print.
-     *  @tparam T   Type stored by C.
-     *  @tparam I   Type of const iterator of C.
-     *
-     *  @param  stream_ Stream to print to.
-     *  @param  cont_   Container to print.
-     *
-     *  @return A reference to the stream post-print.
-     */
-    template <typename S, typename C, typename T, typename I, typename>
-    inline S& operator<<(S& stream_, const C& cont_)
-    {
-        stream_ << arc::str::to_string(cont_);
-
-        return (stream_);
-    }
-
-    /**
-     *  Print a pair as a human-readable string.
-     *
-     *  @tparam S   Type of stream to print to.
-     *  @tparam A0  First type stored by pair.
-     *  @tparam A1  Second type stored by pair.
-     *
-     *  @param  stream_ Stream to print to.
-     *  @param  pair_   pair to print.
-     *
-     *  @return A reference to the stream post-print.
-     */
-    template <typename S, typename A0, typename A1>
-    inline S& operator<<(S& stream_, const std::pair<A0, A1>& pair_)
-    {
-        stream_ << arc::str::to_string(pair_);
-
-        return (stream_);
-    }
-
-    /**
-     *  Print a tuple as a human-readable string.
-     *
-     *  @tparam S   Type of stream to print to.
-     *  @tparam A   Types stored by the tuple.
-     *
-     *  @param  stream_ Stream to print to.
-     *  @param  tup_    Tuple to print.
-     *
-     *  @return A reference to the stream post-print.
-     */
-    template <typename S, typename... A>
-    inline S& operator<<(S& stream_, const std::tuple<A...>& tup_)
-    {
-        stream_ << arc::str::to_string(tup_);
-
-        return (stream_);
-    }
-
-
-
-} // namespace arc
+    return (stream_);
+}
 
 
 
