f85fab259d9400778e7cfeb3e0b0a034d5fb7208
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-06 12:09:15 +0100

    Moved stream operator to global namespace.

--- include/arctk/math/vec3.hpp
+++ include/arctk/math/vec3.hpp
@@ -201,26 +201,6 @@ namespace arc //! arctk namespace
         }
 #endif
 
-        //  -- Stream --
-        /**
-         *  Print a vec3 as a human-readable string.
-         *
-         *  @tparam S   Type of stream to print to.
-         *  @tparam T   Type stored by the vec.
-         *
-         *  @param  stream_ Stream to print to.
-         *  @param  vec_    Vector to print.
-         *
-         *  @return A reference to the stream post-print.
-         */
-        template <typename S, typename T>
-        inline S& operator<<(S& stream_, const Vec3<T>& vec_) noexcept
-        {
-            stream_ << '{' << vec_.x << ", " << vec_.y << ", " << vec_.z << '}';
-
-            return (stream_);
-        }
-
 
         //  -- Assignment --
         /**
@@ -731,6 +711,29 @@ namespace arc //! arctk namespace
 
     } // namespace math
 } // namespace arc
+
+
+
+//  == OPERATORS ==
+//  -- Stream --
+/**
+ *  Print a vec3 as a human-readable string.
+ *
+ *  @tparam S   Type of stream to print to.
+ *  @tparam T   Type stored by the vec.
+ *
+ *  @param  stream_ Stream to print to.
+ *  @param  vec_    Vector to print.
+ *
+ *  @return A reference to the stream post-print.
+ */
+template <typename S, typename T>
+inline S& operator<<(S& stream_, const Vec3<T>& vec_) noexcept
+{
+    stream_ << '{' << vec_.x << ", " << vec_.y << ", " << vec_.z << '}';
+
+    return (stream_);
+}
 
 
 
