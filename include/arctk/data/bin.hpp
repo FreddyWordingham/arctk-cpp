c8f4a1b163db682d41ea0419817e93c6c7b81491
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-06 19:24:42 +0100

    Corrected template names.

--- include/arctk/data/bin.hpp
+++ include/arctk/data/bin.hpp
@@ -120,15 +120,15 @@ namespace arc //! arctk namespace
          *  Print a bin as a human-readable string.
          *
          *  @tparam S   Type of stream to print to.
-         *  @tparam T   Type of value to be counted.
+         *  @tparam L   Type of value to be counted.
          *
          *  @param  stream_ Stream to print to.
          *  @param  bin_    Bin to print.
          *
          *  @return A reference to the stream post-print.
          */
-        template <typename S, typename T>
-        inline S& operator<<(S& stream_, const Bin<T>& bin_) noexcept
+        template <typename S, typename L>
+        inline S& operator<<(S& stream_, const Bin<L>& bin_) noexcept
         {
             stream_ << format::cols(10, ", ", bin_.centres(), bin_._counts);
 
