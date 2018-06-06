58a31a83065fe2571d0cb393da84efe50b7e9996
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-06 22:38:05 +0100

    Added framework.

---
+++ include/arctk/geom/shape/plane.hpp
@@ -0,0 +1,75 @@
+/**
+ *  @file   arctk/geom/shape/plane.hpp
+ *  @date   06/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Geometric plane shape class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_SHAPE_PLANE_HPP
+#define ARCTK_GEOM_SHAPE_PLANE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/geom/shape.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+        namespace shape //! shape namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             */
+            class Plane
+            {
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace shape
+    }     // namespace geom
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_SHAPE_PLANE_HPP
