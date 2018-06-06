af5523e6154e9cdecd25a8453f748b5dd7736ed8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-06 19:12:13 +0100

    Added framework.

---
+++ include/arctk/geom/shape/sphere.hpp
@@ -0,0 +1,78 @@
+/**
+ *  @file   arctk/geom/shape/sphere.hpp
+ *  @date   06/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Geometric sphere shape class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_SHAPE_SPHERE_HPP
+#define ARCTK_GEOM_SHAPE_SPHERE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/geom/shape.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+        namespace shape //! shape namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Geometric sphere shape.
+             */
+            class Sphere
+            {
+                //  == FIELDS ==
+              protected:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace shape
+    }     // namespace geom
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_SHAPE_SPHERE_HPP
