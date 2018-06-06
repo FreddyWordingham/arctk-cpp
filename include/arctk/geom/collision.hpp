0d075c4e2251ab2e152bdc729c225e89bd3e550a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-06 23:27:01 +0100

    Added framework.

---
+++ include/arctk/geom/collision.hpp
@@ -0,0 +1,60 @@
+/**
+ *  @file   arctk/geom/collision.hpp
+ *  @date   06/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collision information class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_COLLISION_HPP
+#define ARCTK_GEOM_COLLISION_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Collision
+        {
+            //  == FIELDS ==
+          public:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_COLLISION_HPP
