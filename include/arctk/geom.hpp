646fcb88d0156b937ac075fbc679b27e2d10a572
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-06 16:08:31 +0100

    Added geometry collection header.

---
+++ include/arctk/geom.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/gui.hpp
+ *  @date   03/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of graphical classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_HPP
+#define ARCTK_GEOM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_GUI
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_GUI
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_HPP
