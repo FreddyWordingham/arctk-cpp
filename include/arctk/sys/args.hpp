a4e29a01b2f42eb483080503002b3ce31d936a97
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-07 21:40:34 +0100

    Added framework.

---
+++ include/arctk/sys/args.hpp
@@ -0,0 +1,77 @@
+/**
+ *  @file   arctk/sys/args.hpp
+ *  @date   07/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Program arguments class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SYS_ARGS_HPP
+#define ARCTK_SYS_ARGS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <string>
+#include <vector>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace sys //! system namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        class Args
+        {
+            //  == FIELDS ==
+          protected:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace sys
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SYS_ARGS_HPP
