23bc101714532aa4d5a112edc9476c9662b0212a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-08 11:29:06 +0100

    Cleaned build files and updated documentation.

--- docs/search/variables_1.js
+++ docs/search/variables_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['a',['a',['../classarc_1_1math_1_1_vec4.html#aa4cfe0579b24fe973d6f73f2fbc42695',1,'arc::math::Vec4']]]
+  ['a',['a',['../classarc_1_1math_1_1_vec4.html#aa4cfe0579b24fe973d6f73f2fbc42695',1,'arc::math::Vec4']]],
+  ['ansi_5fsupport',['ANSI_SUPPORT',['../namespacearc_1_1log.html#a73bed03d5c4b647d423a22514f544206',1,'arc::log']]]
 ];
