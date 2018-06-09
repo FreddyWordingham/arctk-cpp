00e0b6c317e0846fd52cb41c637bd12126c96793
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-09 21:30:20 +0100

    Added framework.

---
+++ include/arctk/data/bucket/fixed.hpp
@@ -0,0 +1,75 @@
+/**
+ *  @file   arctk/data/bucket/fixed.hpp
+ *  @date   09/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Fixed range data binning class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_BUCKET_FIXED_HPP
+#define ARCTK_DATA_BUCKET_FIXED_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <cassert>
+
+//  -- Arctk --
+#include <arctk/data/bucket.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+        namespace bucket //! bucketing namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             */
+            template <typename T>
+            class Fixed : public Bucket<T>
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+            };
+
+
+
+            //  == INSTANTIATION ==
+
+
+
+        } // namespace bucket
+    }     // namespace data
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_BUCKET_FIXED_HPP
