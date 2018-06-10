9e2a653ccc4001c3c4452d0ae6d0ce581b2b0a81
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-10 02:51:54 +0100

    Added framework.

---
+++ include/arctk/std/multi_vec.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/multi_vec.hpp
+ *  @date   01/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Mulit-dimensional vector class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STD_MULTIVEC_HPP
+#define ARCTK_STD_MULTIVEC_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace std //! standard library expansion namespace
+    {
+
+
+
+    } // namespace std
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STD_MULTIVEC_HPP
