79d5b3bf35a08d19148f9f7189d6323708cd6fd9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-10 02:45:24 +0100

    Added framework.

---
+++ include/arctk/std.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/std.hpp
+ *  @date   10/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of expanded standard classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STD_HPP
+#define ARCTK_STD_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STD_HPP
