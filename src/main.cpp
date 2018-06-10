828877654ee16ea708fd4556f49461836cc8fe01
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-10 03:49:34 +0100

    Removed redundant imports.

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,6 @@
 #include <arctk/constant.hpp>
 #include <arctk/constant/math.hpp>
 #include <arctk/constant/phys.hpp>
-#include <arctk/data.hpp>
-#include <arctk/data/bucket.hpp>
-#include <arctk/data/bucket/fixed.hpp>
 #include <arctk/format.hpp>
 #include <arctk/format/table.hpp>
 #include <arctk/geom.hpp>
