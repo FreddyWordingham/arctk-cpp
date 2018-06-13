328d6ccc575e03ba26da3fb24cbd82a9795699fe
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-14 00:42:19 +0200

    Fixed type.

--- include/arctk/math/vec2.hpp
+++ include/arctk/math/vec2.hpp
@@ -538,7 +538,7 @@ namespace arc //! arctk namespace
         template <typename T>
         constexpr inline bool Vec<T, 2>::normalised(const T tol_) const noexcept
         {
-            return (std::fabs(1.0 - mag()) <= tol_);
+            return (std::fabs(T{1.0} - mag()) <= tol_);
         }
 
 
