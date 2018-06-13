e3d2393a9b417d35949ef49bd0ace19ae36c47fd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-13 22:15:21 +0200

    Fixed typo.

--- include/arctk/math/vec.hpp
+++ include/arctk/math/vec.hpp
@@ -199,7 +199,7 @@ namespace arc //! arctk namespace
         {
             for (size_t i = 0; i < N; ++i)
             {
-                _data[i] += val_;
+                _data[i] += val_._data[i];
             }
 
             return (*this);
