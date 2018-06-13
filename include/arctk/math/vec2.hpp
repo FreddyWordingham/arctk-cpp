3175b6f1360a17bf74392373104653698801c650
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-13 23:35:45 +0200

    Added type assertion.

--- include/arctk/math/vec2.hpp
+++ include/arctk/math/vec2.hpp
@@ -45,6 +45,11 @@ namespace arc //! arctk namespace
         template <typename T>
         class Vec<T, 2>
         {
+            //  == ASSERTIONS ==
+            //  -- Type --
+            static_assert(std::is_floating_point<T>::value);
+
+
             //  == FIELDS ==
           public:
             //  -- Data --
