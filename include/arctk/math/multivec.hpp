e495ac9a86bc0500de0afa06a820c3d3716c8e55
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-14 17:46:57 +0200

    Added framework.

---
+++ include/arctk/math/multivec.hpp
@@ -0,0 +1,57 @@
+/**
+ *  @file   arctk/math/multivec.hpp
+ *  @date   14/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Mulit-vector mathematical functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_MULTIVEC_HPP
+#define ARCTK_MATH_MULTIVEC_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/utl.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Arithmetic --
+
+
+
+        //  == FUNCTIONS ==
+        //  -- Arithmetic --
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_MULTIVEC_HPP
