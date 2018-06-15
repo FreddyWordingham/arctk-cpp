16b5dc8809790ed4cc9cb511f99a25495b4e23bf
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-15 16:47:30 +0200

    Added framework.

---
+++ include/arctk/col/map.hpp
@@ -0,0 +1,55 @@
+/**
+ *  @file   arctk/col/map.hpp
+ *  @date   15/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Colour mapping functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_COL_MAP_HPP
+#define ARCTK_COL_MAP_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace col //! colour namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace col
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_COL_MAP_HPP
