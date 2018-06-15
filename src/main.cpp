d2b7a439580030f8137d9976e65ed537ddd57684
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-15 23:41:30 +0200

    Removed redundant import.

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,6 @@
 #include <arctk/math.hpp>
 #include <arctk/math/container.hpp>
 #include <arctk/math/conversion.hpp>
-#include <arctk/math/multivec.hpp>
 #include <arctk/math/power.hpp>
 #include <arctk/math/vec.hpp>
 #include <arctk/math/vec2.hpp>
