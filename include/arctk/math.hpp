a6647d6a5042f6c36dfae8bd28016d62b9577161
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-15 23:42:08 +0200

    Removed redundant import.

--- include/arctk/math.hpp
+++ include/arctk/math.hpp
@@ -23,7 +23,6 @@
 //  -- Arctk --
 #include <arctk/math/container.hpp>
 #include <arctk/math/conversion.hpp>
-#include <arctk/math/multivec.hpp>
 #include <arctk/math/power.hpp>
 #include <arctk/math/vec.hpp>
 #include <arctk/math/vec2.hpp>
