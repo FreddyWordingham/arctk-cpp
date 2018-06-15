26ceea8aa92d9822e6ba8d774d9c568258474da0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-15 11:16:24 +0200

    Added framework.

---
+++ include/arctk/data/image.hpp
@@ -0,0 +1,76 @@
+/**
+ *  @file   arctk/data/image.hpp
+ *  @date   15/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Image forming class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_IMAGE_HPP
+#define ARCTK_DATA_IMAGE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/math.hpp>
+#include <arctk/utl.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Image data writer.
+         */
+        class Image
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_IMAGE_HPP
