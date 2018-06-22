32a162f0b5132717dd02b6f28a0036b07d6a17ca
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-22 18:58:52 +0100

    Fixed parameter naming.

--- include/arctk/parse/string.hpp
+++ include/arctk/parse/string.hpp
@@ -97,7 +97,7 @@ namespace arc //! arctk namespace
         inline bool parsable_from_str<bool>(const std::string& str_) noexcept
         {
             std::string str = str_;
-            std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) { return std::tolower(c); });
+            std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c_) { return std::tolower(c_); });
 
             if (str == "false")
             {
