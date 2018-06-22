dfb678e268d9c6a8cf7d89de0caa1a4a7c39cf03
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-22 20:00:32 +0100

    Added framework.

---
+++ include/arctk/math/comparison.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file   arctk/math/comparison.hpp
+ *  @date   22/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Math comparison functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_COMPARISON_HPP
+#define ARCTK_MATH_COMPARISON_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <cmath>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Mathematical --
+
+
+        //  == FUNCTIONS ==
+        //  -- Mathematical --
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_COMPARISON_HPP
