957447a5606fbab8c7036acece53b6141d42dd66
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-22 22:23:12 +0100

    Added framework.

---
+++ include/arctk/parse/ppm.hpp
@@ -0,0 +1,57 @@
+/**
+ *  @file   arctk/parse/ppm.hpp
+ *  @date   22/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  PPM parsing functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PARSE_PPM_HPP
+#define ARCTK_PARSE_PPM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/sys.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace parse //! parsing namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Parsing --
+
+
+
+        //  == FUNCTIONS ==
+        //  -- Parsing --
+
+
+
+    } // namespace parse
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PARSE_PPM_HPP
