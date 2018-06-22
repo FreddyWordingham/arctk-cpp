4dff62f099d3feb235a1f6385e46261b138ae3d0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-22 22:40:39 +0100

    Added framework.

---
+++ include/arctk/prop/multivec.hpp
@@ -0,0 +1,57 @@
+/**
+ *  @file   arctk/prop/multivec.hpp
+ *  @date   22/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Multi-Vec proprty functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PROP_MULTIVEC_HPP
+#define ARCTK_PROP_MULTIVEC_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <vector>
+
+//  -- Arctk --
+#include <arctk/utl.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace prop //! properties namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace prop
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PROP_MULTIVEC_HPP
