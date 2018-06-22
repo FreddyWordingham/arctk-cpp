70f16451b5a0353c0717ce22aea093cf02b46720
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-22 22:26:55 +0100

    Added to_ppm method.

--- include/arctk/parse/ppm.hpp
+++ include/arctk/parse/ppm.hpp
@@ -20,8 +20,12 @@
 
 
 //  == IMPORTS ==
+//  -- Std --
+#include <algorithm>
+
 //  -- Arctk --
 #include <arctk/sys.hpp>
+#include <arctk/utl.hpp>
 
 
 
@@ -35,11 +39,37 @@ namespace arc //! arctk namespace
 
         //  == FUNCTION PROTOTYPES ==
         //  -- Parsing --
+        inline void to_ppm(const std::string& path_, const utl::MultiVec<vec3, 2>& pixels) noexcept;
 
 
 
         //  == FUNCTIONS ==
         //  -- Parsing --
+        inline void to_ppm(const std::string& path_, const utl::MultiVec<vec3, 2>& pixels) noexcept
+        {
+            assert(!path_.empty());
+            assert(pixels.size() > 0);
+            assert(pixels.front().size() > 0);
+
+            sys::file::Out file(path_);
+
+            file << "P3\n"
+                 << _width << " " << _height << "\n"
+                 << "255\n";
+
+            for (size_t i = 0; i < _height; ++i)
+            {
+                for (size_t j = 0; j < _width; ++j)
+                {
+                    for (size_t k = 0; k < 3; ++k)
+                    {
+                        file << std::clamp(static_cast<int>(255 * pixels[j][_height - i - 1][k]), 0, 255) << "\t";
+                    }
+                    file << "\t";
+                }
+                file << "\n";
+            }
+        }
 
 
 
