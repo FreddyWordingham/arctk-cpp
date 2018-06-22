90c9796056d52c70f4b96fe15015ac6bc4b46896
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-22 21:00:45 +0100

    Added framework.

---
+++ include/arctk/data/image/greyscale.hpp
@@ -0,0 +1,81 @@
+/**
+ *  @file   arctk/data/image/greyscale.hpp
+ *  @date   22/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Scalar image forming class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_IMAGE_GREYSCALE_HPP
+#define ARCTK_DATA_IMAGE_GREYSCALE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == BASE ==
+//  -- Arctk --
+#include <arctk/data/image.hpp>
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+        namespace image //! image namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Greyscale data writer.
+             */
+            class Greyscale
+            {
+                //  == FIELDS ==
+              private:
+                //  -- Data --
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+            //  == INSTANTIATION ==
+
+
+
+        } // namespace image
+    }     // namespace data
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_IMAGE_GREYSCALE_HPP
