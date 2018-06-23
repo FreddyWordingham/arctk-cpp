00fef66cec0bbb009ddc4853c2c8ac1a97a8f7bd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-23 22:27:53 +0100

    Added framework.

---
+++ include/arctk/data/bucket.hpp
@@ -0,0 +1,79 @@
+/**
+ *  @file   arctk/data/bucket.hpp
+ *  @date   23/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Data binning class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_BUCKET_HPP
+#define ARCTK_DATA_BUCKET_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <vector>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Data binning class.
+         *
+         *  @tparam T   Type binned.
+         */
+        template <typename T>
+        class Bucket
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Bounds --
+
+            //  == INSTANTIATION ==
+          public:
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_BUCKET_HPP
