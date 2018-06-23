9b4d2f4a0d2b080d5dd8c6d3f4c77162ec0faa25
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-23 02:52:46 +0100

    Added framework.

---
+++ include/arctk/col/conversion.hpp
@@ -0,0 +1,57 @@
+/**
+ *  @file   arctk/col/conversion.hpp
+ *  @date   23/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Colour conversion functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_COL_CONVERSION_HPP
+#define ARCTK_COL_CONVERSION_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace col //! colour namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- HSV --
+
+
+
+        //  == FUNCTIONS ==
+        //  -- HSV --
+
+
+
+    } // namespace col
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_COL_CONVERSION_HPP
