6cb7ce0a10996e2011ae8945117965cdb2317a73
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-23 03:07:27 +0100

    Added hsv_to_rgb function.

--- include/arctk/col/conversion.hpp
+++ include/arctk/col/conversion.hpp
@@ -20,6 +20,10 @@
 
 
 //  == IMPORTS ==
+//  -- Std --
+#include <cassert>
+#include <cmath>
+
 //  -- Arctk --
 #include <arctk/math.hpp>
 
@@ -35,11 +39,58 @@ namespace arc //! arctk namespace
 
         //  == FUNCTION PROTOTYPES ==
         //  -- HSV --
+        inline vec3 hsv_to_rgb(double hue_, double sat_, double val_) noexcept;
 
 
 
         //  == FUNCTIONS ==
         //  -- HSV --
+        inline vec3 hsv_to_rgb(double hue_, const double sat_, const double val_) noexcept
+        {
+            assert((sat_ >= 0.0) && (sat_ <= 1.0));
+            assert((val_ >= 0.0) && (val_ <= 1.0));
+
+            hue_ = std::mod(hue_, 360.0);
+
+            const double c = val_ * sat_;
+            const double h = hue_ / 60.0;
+            const double x = c * (1.0 - std::abs(std::mod(h, 2.0) - 1.0));
+
+            vec3 col;
+
+            if ((h >= 0.0) && (h < 1.0))
+            {
+                col.r = c;
+                col.g = x;
+            }
+            else if ((h >= 1.0) && (h < 2.0))
+            {
+                col.r = x;
+                col.g = c;
+            }
+            else if ((h >= 2.0) && (h < 3.0))
+            {
+                col.g = c;
+                col.b = x;
+            }
+            else if ((h >= 3.0) && (h < 4.0))
+            {
+                col.g = x;
+                col.b = c;
+            }
+            else if ((h >= 4.0) && (h < 5.0))
+            {
+                col.r = x;
+                col.b = c;
+            }
+            else
+            {
+                col.r = c;
+                col.b = x;
+            }
+
+            return (col + (val_ - c));
+        }
 
 
 
