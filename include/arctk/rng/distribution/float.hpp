cf118386a1b1810338f383d4bb4bfb7b85653ea3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 12:14:42 +0100

    Added framework.

---
+++ include/arctk/rng/distribution/float.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file   arctk/rng/distribution/float.hpp
+ *  @date   25/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Floating point random number distribution functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_DISTRIBUTION_FLOAT_HPP
+#define ARCTK_RNG_DISTRIBUTION_FLOAT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace rng //! utility namespace
+    {
+        namespace distribution //! distribution namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace distribution
+    }     // namespace rng
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_DISTRIBUTION_FLOAT_HPP
