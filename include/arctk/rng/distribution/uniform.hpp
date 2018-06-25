6d922678b349db5f1b0dacea157368441f622333
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 15:00:17 +0100

    Added documentation to int uniform function.

--- include/arctk/rng/distribution/uniform.hpp
+++ include/arctk/rng/distribution/uniform.hpp
@@ -63,6 +63,17 @@ namespace arc //! arctk namespace
                 return (static_cast<size_t>(rng_.gen() * static_cast<double>(max_ - min_ + 1)) + min_);
             }
 
+            /**
+             *  Generate a random value between given limits.
+             *
+             *  @param  rng_    Generator used to draw base values from.
+             *  @param  min_    Lower bound of the genration range.
+             *  @param  max_    Upper bound of the genration range.
+             *
+             *  @pre    min_ must be less than max_.
+             *
+             *  @return Random uniform value.
+             */
             template <>
             inline int uniform(Generator& rng_, const int min_, const int max_) noexcept
             {
