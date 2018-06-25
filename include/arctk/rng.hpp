d74aaa4345f8d93088d7cdf9168971c0cf374d5e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 14:27:36 +0100

    Updated import path.

--- include/arctk/rng.hpp
+++ include/arctk/rng.hpp
@@ -22,7 +22,7 @@
 //  == IMPORTS ==
 //  -- Arctk --
 #include <arctk/rng/distribution.hpp>
-#include <arctk/rng/distribution/henyey_greenstein.hpp>
+#include <arctk/rng/distribution/scientific.hpp>
 #include <arctk/rng/distribution/uniform.hpp>
 #include <arctk/rng/generator.hpp>
 #include <arctk/rng/generator/quality.hpp>
