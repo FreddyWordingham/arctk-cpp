c41050390cda16d637b24064de4be5b7daffaf2a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 22:35:40 +0100

    Added default parameter.

--- include/arctk/data/table.hpp
+++ include/arctk/data/table.hpp
@@ -57,7 +57,7 @@ namespace arc //! arctk namespace
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
-            inline Table(const std::string& serial_, char delim_) noexcept;
+            inline Table(const std::string& serial_, char delim_ = ',') noexcept;
 
             //  -- Initialisation --
             inline std::vector<std::tuple<A...>> init_rows(const std::string& serial_, char delim_ = ',') noexcept;
