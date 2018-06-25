80253467d83908b697d315e293c235059dd26b36
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 13:05:36 +0100

    Added framework.

---
+++ include/arctk/rng/distribution/integer.hpp
@@ -0,0 +1,67 @@
+/**
+ *  @file   arctk/rng/distribution/integer.hpp
+ *  @date   25/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Integer random number distribution functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_DISTRIBUTION_INTEGER_HPP
+#define ARCTK_RNG_DISTRIBUTION_INTEGER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/rng/generator.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace rng //! utility namespace
+    {
+        namespace distribution //! distribution namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Floating Point --
+            template <typename T, typename = std::enable_if_t<std::is_floating_point<T>::value>>
+            inline T uniform(Generator& rng_, const T min_, const T max_) noexcept;
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Floating Point --
+            template <typename T, typename>
+            inline T uniform(Generator& rng_, const T min_, const T max_) noexcept
+            {
+                return ((static_cast<T>(rng_.gen()) * (max_ - min_)) + min_);
+            }
+
+
+
+        } // namespace distribution
+    }     // namespace rng
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_DISTRIBUTION_INTEGER_HPP
