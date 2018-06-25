565497c8304e31dcbf4b241f9cda379982b2cf0e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 13:02:35 +0100

    Updated import path.

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@
 #include <arctk/prop/multivec.hpp>
 #include <arctk/rng.hpp>
 #include <arctk/rng/distribution.hpp>
-#include <arctk/rng/distribution/float.hpp>
+#include <arctk/rng/distribution/floating.hpp>
 #include <arctk/rng/generator.hpp>
 #include <arctk/rng/generator/quality.hpp>
 #include <arctk/search.hpp>
