454f4206323bd1dad6c4654d463db5fa80e35136
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 14:33:43 +0100

    Added framework.

---
+++ include/arctk/rng/distribution/mathematical.hpp
@@ -0,0 +1,54 @@
+/**
+ *  @file   arctk/rng/distribution/mathematical.hpp
+ *  @date   25/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Math transformation distribution functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_DISTRIBUTION_MATHEMATICAL_HPP
+#define ARCTK_RNG_DISTRIBUTION_MATHEMATICAL_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/rng/generator.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace rng //! utility namespace
+    {
+        namespace distribution //! distribution namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Guassian --
+
+
+        } // namespace distribution
+    }     // namespace rng
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_DISTRIBUTION_MATHEMATICAL_HPP
