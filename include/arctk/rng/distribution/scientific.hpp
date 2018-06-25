6993602e2309368e9fe1d54e0852aec7dd9eded7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 14:29:41 +0100

    Added henyey_greenstein function.

--- include/arctk/rng/distribution/scientific.hpp
+++ include/arctk/rng/distribution/scientific.hpp
@@ -21,6 +21,7 @@
 
 //  == IMPORTS ==
 //  -- Arctk --
+#include <arctk/rng/distribution/uniform.hpp>
 #include <arctk/rng/generator.hpp>
 
 
@@ -37,11 +38,24 @@ namespace arc //! arctk namespace
 
             //  == FUNCTION PROTOTYPES ==
             //  -- Henyey-Greenstein --
+            inline double henyey_greenstein(Generator& rng_, double g_) noexcept;
 
 
 
             //  == FUNCTIONS ==
             //  -- Henyey-Greenstein --
+            inline double henyey_greenstein(Generator& rng_, const double g_) noexcept
+            {
+                assert((g_ >= -1.0) && (g_ <= 1.0));
+
+                if (math::is_zero(g_))
+                {
+                    return (uniform<double>(rng_, 0.0, constant::PI));
+                }
+
+                const double s = uniform<double>(rng_, -1.0, 1.0);
+                return ((1.0 + math::sq(g_) - math::sq((1.0 - math::sq(g_)) / (1.0 + (g_ * s)))) / (2.0 * g_));
+            }
 
 
 
