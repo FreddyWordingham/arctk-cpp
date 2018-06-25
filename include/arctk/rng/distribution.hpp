9950a075c9a3b017b3070f42321e3c2a15a50336
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 12:09:47 +0100

    Added framework.

---
+++ include/arctk/rng/distribution.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/rng/distribution.hpp
+ *  @date   25/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of random number generator distribution transformation functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_DISTRIBUTION_HPP
+#define ARCTK_RNG_DISTRIBUTION_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_DISTRIBUTION_HPP
