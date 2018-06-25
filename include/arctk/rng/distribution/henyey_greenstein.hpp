08f95eb522c897f25344166dcdd8ca21e06011d3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 14:25:44 +0100

    Added framework.

---
+++ include/arctk/rng/distribution/henyey_greenstein.hpp
@@ -0,0 +1,60 @@
+/**
+ *  @file   arctk/rng/distribution/henyey-greenstein.hpp
+ *  @date   25/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Henyey-greenstein transformation distribution functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_DISTRIBUTION_HENYEYGREENSTEIN_HPP
+#define ARCTK_RNG_DISTRIBUTION_HENYEYGREENSTEIN_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/rng/generator.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace rng //! utility namespace
+    {
+        namespace distribution //! distribution namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Henyey-Greenstein --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Henyey-Greenstein --
+
+
+
+        } // namespace distribution
+    }     // namespace rng
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_DISTRIBUTION_HENYEYGREENSTEIN_HPP
