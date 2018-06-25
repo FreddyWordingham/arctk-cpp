fda45d8d674437216e0bc39fc8948f4ed7467bd1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-25 13:34:27 +0100

    Added framework.

---
+++ include/arctk/rng/distribution/vec.hpp
@@ -0,0 +1,58 @@
+/**
+ *  @file   arctk/rng/distribution/vec.hpp
+ *  @date   25/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Vec distribution functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RNG_DISTRIBUTION_VEC_HPP
+#define ARCTK_RNG_DISTRIBUTION_VEC_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/rng/generator.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace rng //! utility namespace
+    {
+        namespace distribution //! distribution namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace distribution
+    }     // namespace rng
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RNG_DISTRIBUTION_VEC_HPP
