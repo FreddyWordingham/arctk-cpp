8a1f0100fc44a5ede0d707ccb941838662821837
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 00:13:34 +0100

    Added equal method.

--- include/arctk/math/comparison.hpp
+++ include/arctk/math/comparison.hpp
@@ -37,6 +37,8 @@ namespace arc //! arctk namespace
         //  -- Mathematical --
         template <typename T>
         inline bool is_zero(T x_) noexcept;
+        template <typename T>
+        inline bool equal(const T x_, const T y_) noexcept;
 
 
 
@@ -56,6 +58,23 @@ namespace arc //! arctk namespace
         inline bool is_zero(T x_) noexcept
         {
             return ((x_ > -std::numeric_limits<double>::epsilon()) && (x_ < std::numeric_limits<double>::epsilon()));
+        }
+
+        /**
+         *  Test if a pair of values can be considered equal to one another.
+         *  Values are considered equal to zero if they are equal to within epsilon.
+         *
+         *  @tparam T   Type of values to test.
+         *
+         *  @param  x_  Fist value to test.
+         *  @param  y_  Second value to test.
+         *
+         *  @return true if the values can be considered equal.
+         */
+        template <typename T>
+        inline bool equal(const T x_, const T y_) noexcept
+        {
+            return (zero(x_ - y_));
         }
 
 
