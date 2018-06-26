40f551487a8584e029a617e17fd22b864bb91938
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 00:32:50 +0100

    Added framework and moved format settings into settings formatting.

---
+++ include/arctk/settings/format.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/settings/format.hpp
+ *  @date   27/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Formatting settings.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SETTINGS_FORMAT_HPP
+#define ARCTK_SETTINGS_FORMAT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace settings //! settings namespace
+    {
+
+
+
+        //  == CONSTANTS ==
+        //  -- Formatting --
+        constexpr const char DEFAULT_DELIM       = ',';
+        constexpr const char DEFAULT_PRINT_WIDTH = 16;
+
+
+
+    } // namespace settings
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SETTINGS_FORMAT_HPP
