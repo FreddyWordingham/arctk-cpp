9fa782f21e2406f8328916bcd07eab1ea499be6c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 12:39:28 +0100

    Added framework.

---
+++ include/arctk/math/mat3.hpp
@@ -0,0 +1,85 @@
+/**
+ *  @file   arctk/math/mat3.hpp
+ *  @date   27/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Mathematical three by three matrix class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_MAT3_HPP
+#define ARCTK_MATH_MAT3_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == BASE ==
+//  -- Arctk --
+#include <arctk/math/mat.hpp>
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         */
+        template <typename T>
+        class Mat<T, 3>
+        {
+            //  == FIELDS ==
+          public:
+            //  -- Data --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          public:
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == OPERATORS ==
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_MAT3_HPP
