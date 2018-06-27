674677b6fe81069086281cd3ddffd0cb78753673
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 23:54:11 +0100

    Removed redundant const qualifications in method declerations.

--- include/arctk/rng/distribution/math.hpp
+++ include/arctk/rng/distribution/math.hpp
@@ -41,9 +41,8 @@ namespace arc //! arctk namespace
             inline T normal(Generator* rng_) noexcept; //!< Generate a random number drawn from the normal distribution. @tparam T Type to draw from normal distribution. @param  rng_    Generator used to draw base values from. @return Value randomly drawn
                                                        //!< from normal distribution.
             template <typename T>
-            inline T guassian(Generator* rng_, const T mu_,
-                              const T sigma_) noexcept; //!< Generate a random number drawn from a gaussian distribution.  @tparam T Type to draw from guassian distribution. @param  rng_    Generator used to draw base values from. @param  mu_     Average
-                                                        //!< value of the gaussian distribution. @param  sigma_  Variance of the gaussian distribution. @return Value randomly drawn from the gaussian distribution.
+            inline T guassian(Generator* rng_, T mu_, T sigma_) noexcept; //!< Generate a random number drawn from a gaussian distribution.  @tparam T Type to draw from guassian distribution. @param  rng_    Generator used to draw base values from. @param
+                                                                          //!< mu_     Average value of the gaussian distribution. @param  sigma_  Variance of the gaussian distribution. @return Value randomly drawn from the gaussian distribution.
 
 
 
