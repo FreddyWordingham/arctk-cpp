2b5e36addf5ccd97b0f69320a5e1f249938bfee3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 21:16:13 +0100

    Removed redundant default in switch.

--- include/arctk/gui/window.hpp
+++ include/arctk/gui/window.hpp
@@ -278,8 +278,6 @@ namespace arc //! arctk namespace
                         case Uniform::stored::MAT4:
                             glUniformMatrix4fv(uni.handle(), 1, GL_FALSE, &cam_.mat4_uniform(key)[0][0]);
                             break;
-                        default:
-                            ERROR(42) << "Unreachable code reached.";
                     }
                 }
             }
