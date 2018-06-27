ba0323bce3286cfa500e4461b27e90b5f8df983d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 12:46:06 +0100

    Added matN math class file.

---
+++ include/arctk/math/matN.hpp
@@ -0,0 +1,80 @@
+/**
+ *  @file   arctk/math/mat.hpp
+ *  @date   27/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Mathematical square matrix class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_MAT_HPP
+#define ARCTK_MATH_MAT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+asda            //  == NAMESPACE ==
+  namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Mathematical  class.
+         */
+        template <typename T, size_t N>
+        class Mat
+        {
+            //  == ASSERTIONS ==
+            //  -- Size --
+            static_assert(N > 4);
+
+
+            //  == FIELDS ==
+          private:
+            //  -- Data --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          public:
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == OPERATORS ==
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_MAT_HPP
