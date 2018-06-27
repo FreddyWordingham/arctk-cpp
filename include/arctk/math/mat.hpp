2c6541ab23298882ec986d6c657549935547bb61
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 14:55:20 +0100

    Optimised pre-increment method.

--- include/arctk/math/mat.hpp
+++ include/arctk/math/mat.hpp
@@ -288,7 +288,10 @@ namespace arc //! arctk namespace
         {
             for (size_t i = 0; i < N; ++i)
             {
-                ++_data[i];
+                for (size_t j = 0; j < N; ++j)
+                {
+                    ++_data[i][j];
+                }
             }
 
             return (*this);
