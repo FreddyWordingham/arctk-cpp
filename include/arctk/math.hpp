7a9955ed9cb548c6369f7d94bf2100cb9cb83783
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 12:48:27 +0100

    Corrected import path.

--- include/arctk/math.hpp
+++ include/arctk/math.hpp
@@ -24,8 +24,8 @@
 #include <arctk/math/comparison.hpp>
 #include <arctk/math/container.hpp>
 #include <arctk/math/conversion.hpp>
+#include <arctk/math/mat.hpp>
 #include <arctk/math/mat3.hpp>
-#include <arctk/math/matN.hpp>
 #include <arctk/math/power.hpp>
 #include <arctk/math/vec.hpp>
 #include <arctk/math/vec2.hpp>
