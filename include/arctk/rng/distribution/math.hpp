f35e2f3ea7733bb7106f9b6f00778f460cfe0a6c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 23:55:10 +0100

    Corrected name of static variable.

--- include/arctk/rng/distribution/math.hpp
+++ include/arctk/rng/distribution/math.hpp
@@ -58,13 +58,13 @@ namespace arc //! arctk namespace
             template <>
             inline double normal(Generator* rng_) noexcept
             {
-                static bool   generate = false;
-                static double z1;
+                static bool   static_generate = false;
+                static double static_z1;
 
-                generate = !generate;
-                if (!generate)
+                static_generate = !static_generate;
+                if (!static_generate)
                 {
-                    return (z1);
+                    return (static_z1);
                 }
 
                 const double u0 = rng_->gen();
@@ -72,7 +72,7 @@ namespace arc //! arctk namespace
 
                 const double m  = std::sqrt(-2.0 * std::log(u0));
                 const double z0 = m * std::cos(2.0 * constant::PI * u1);
-                z1              = m * std::sin(2.0 * constant::PI * u1);
+                static_z1       = m * std::sin(2.0 * constant::PI * u1);
 
                 return (z0);
             }
