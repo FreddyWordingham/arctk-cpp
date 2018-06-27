cf14003eda3a47c29443dfe01b336ec5f009a36e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-27 23:53:11 +0100

    Changed generation functions to take a pointer to a generator rather than a non-const reference.

--- include/arctk/rng/distribution/math.hpp
+++ include/arctk/rng/distribution/math.hpp
@@ -38,14 +38,12 @@ namespace arc //! arctk namespace
             //  == FUNCTION PROTOTYPES ==
             //  -- Guassian --
             template <typename T>
-            inline T normal(
-              Generator& rng_) noexcept; // NOLINT //!< Generate a random number drawn from the normal distribution. @tparam T Type to draw from normal distribution. @param  rng_    Generator used to draw base values from. @return Value randomly drawn
-                                         //!< from normal distribution.
+            inline T normal(Generator* rng_) noexcept; //!< Generate a random number drawn from the normal distribution. @tparam T Type to draw from normal distribution. @param  rng_    Generator used to draw base values from. @return Value randomly drawn
+                                                       //!< from normal distribution.
             template <typename T>
-            inline T guassian(
-              Generator& rng_, const T mu_,
-              const T sigma_) noexcept; // NOLINT //!< Generate a random number drawn from a gaussian distribution.  @tparam T Type to draw from guassian distribution. @param  rng_    Generator used to draw base values from. @param  mu_     Average
-                                        //!< value of the gaussian distribution. @param  sigma_  Variance of the gaussian distribution. @return Value randomly drawn from the gaussian distribution.
+            inline T guassian(Generator* rng_, const T mu_,
+                              const T sigma_) noexcept; //!< Generate a random number drawn from a gaussian distribution.  @tparam T Type to draw from guassian distribution. @param  rng_    Generator used to draw base values from. @param  mu_     Average
+                                                        //!< value of the gaussian distribution. @param  sigma_  Variance of the gaussian distribution. @return Value randomly drawn from the gaussian distribution.
 
 
 
@@ -59,7 +57,7 @@ namespace arc //! arctk namespace
              *  @return Value randomly drawn from normal distribution.
              */
             template <>
-            inline double normal(Generator& rng_) noexcept
+            inline double normal(Generator* rng_) noexcept
             {
                 static bool   generate = false;
                 static double z1;
@@ -70,8 +68,8 @@ namespace arc //! arctk namespace
                     return (z1);
                 }
 
-                const double u0 = rng_.gen();
-                const double u1 = rng_.gen();
+                const double u0 = rng_->gen();
+                const double u1 = rng_->gen();
 
                 const double m  = std::sqrt(-2.0 * std::log(u0));
                 const double z0 = m * std::cos(2.0 * constant::PI * u1);
@@ -92,7 +90,7 @@ namespace arc //! arctk namespace
              *  @return Value randomly drawn from the gaussian distribution.
              */
             template <>
-            inline double guassian(Generator& rng_, const double mu_, const double sigma_) noexcept
+            inline double guassian(Generator* rng_, const double mu_, const double sigma_) noexcept
             {
                 assert(sigma_ > 0.0);
 
@@ -105,8 +103,8 @@ namespace arc //! arctk namespace
                     return ((z1 * sigma_) + mu_);
                 }
 
-                const double u0 = rng_.gen();
-                const double u1 = rng_.gen();
+                const double u0 = rng_->gen();
+                const double u1 = rng_->gen();
 
                 const double m  = std::sqrt(-2.0 * std::log(u0));
                 const double z0 = m * std::cos(2.0 * constant::PI * u1);
