47596357bf706101973501de8875137a84162ebe
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-28 01:59:52 +0100

    Added access operator methods.

--- include/arctk/math/mat4.hpp
+++ include/arctk/math/mat4.hpp
@@ -625,7 +625,7 @@ namespace arc //! arctk namespace
         template <typename T>
         constexpr inline Vec<T, 4>& Mat<T, 4>::operator[](const size_t index_) noexcept
         {
-            assert(index_ < 3);
+            assert(index_ < 4);
 
             return ((&x)[index_]);
         }
@@ -644,7 +644,7 @@ namespace arc //! arctk namespace
         template <typename T>
         constexpr inline const Vec<T, 4>& Mat<T, 4>::operator[](const size_t index_) const noexcept
         {
-            assert(index_ < 3);
+            assert(index_ < 4);
 
             return ((&x)[index_]);
         }
