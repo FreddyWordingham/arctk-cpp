e6bbda9f31cd5305e48b00acfbba2144ea0eb9af
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-28 01:39:46 +0100

    Fixed typo.

--- include/arctk/math/mat2.hpp
+++ include/arctk/math/mat2.hpp
@@ -437,7 +437,7 @@ namespace arc //! arctk namespace
         template <typename T>
         constexpr inline Mat<T, 2> Mat<T, 2>::operator*(const Mat<T, 2>& mat_) const noexcept
         {
-            return (Mat(Vec<T, 2>(x.x * val_, x.y * val_), Vec<T, 2>(y.x * val_, y.y * val_)));
+            return (Mat(Vec<T, 2>(x * Vec<T, 2>(mat_.x.x, mat_.y.x), x * Vec<T, 2>(mat_.x.y, mat_.y.y)), Vec<T, 2>(y * Vec<T, 2>(mat_.x.x, mat_.y.x), y * Vec<T, 2>(mat_.x.y, mat_.y.y))));
         }
 
         /**
