cf7dfbd796deea4a248905ef8f8cb581a388a2c7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-28 12:17:17 +0100

    Fixed typo.

--- include/arctk/data/histogram1.hpp
+++ include/arctk/data/histogram1.hpp
@@ -249,7 +249,7 @@ namespace arc //! arctk namespace
          *  @return Human readable string of the histogram data.
          */
         template <typename T>
-        inline std::string Bucket<T>::str(const char delim_, const size_t width_) const noexcept
+        inline std::string Histogram<T>::str(const char delim_, const size_t width_) const noexcept
         {
             const std::vector<double> centres = centres();
 
