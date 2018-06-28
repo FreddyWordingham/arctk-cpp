f0efef9dc1950b52f26ebff031ce5453b45c4dd1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-06-28 12:13:21 +0100

    Added framework.

---
+++ include/arctk/data/histogram/fixed1.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @file   arctk/data/histogram/fixed1.hpp
+ *  @date   28/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  One-dimensional, fixed-range, data binning class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_HISTOGRAM_FIXED1_HPP
+#define ARCTK_DATA_HISTOGRAM_FIXED1_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == BASE ==
+//  -- Arctk --
+#include <arctk/data/histogram/fixed.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+        namespace histogram //! histogram namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  One-dimensional, fixed-range, data binning class.
+             *
+             *  @tparam T   Type binned.
+             */
+            template <typename T>
+            class Fixed1<T> : public Histogram<T, 1>
+            {
+            };
+
+
+
+        } // namespace histogram
+    }     // namespace data
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_HISTOGRAM_FIXED1_HPP
