d82e8398a5dad8f829b11ce573d91904a087aaa3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-01 17:30:09 +0100

    Added framework.

---
+++ include/arctk/contracts.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/contracts.hpp
+ *  @date   01/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of contract macros.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONTRACTS_HPP
+#define ARCTK_CONTRACTS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONTRACTS_HPP
