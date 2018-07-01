ee179d48e0e4b0550c20185b893e973a9563500d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-01 22:39:22 +0100

    Added framework.

---
+++ include/arctk/contracts/stacktrace.hpp
@@ -0,0 +1,55 @@
+/**
+ *  @file   arctk/contracts/stacktrace.hpp
+ *  @date   01/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Stacktracing functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONTRACTS_STACKTRACE_HPP
+#define ARCTK_CONTRACTS_STACKTRACE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/constant.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace contracts //! contracts namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace contracts
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONTRACTS_STACKTRACE_HPP
