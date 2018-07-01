7cb5ab95a7efe30cc4b5c319da6355f767eec56f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-01 17:27:53 +0100

    Added framework.

---
+++ include/arctk/settings.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/settings.hpp
+ *  @date   01/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of all settings.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SETTINGS_HPP
+#define ARCTK_SETTINGS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SETTINGS_HPP
