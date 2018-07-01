fd83778c5cabb33b8ba4f7c8285e51788f539c7b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-01 22:34:42 +0100

    Added PRE pre-condition macro.

--- include/arctk/contracts.hpp
+++ include/arctk/contracts.hpp
@@ -20,7 +20,26 @@
 
 
 //  == IMPORTS ==
-//  -- Arctk --
+//  -- Std --
+#include <iostream>
+
+
+
+//  == MACROS ==
+//  -- Contracts --
+#ifdef NDEBUG
+#define PRE(condition_) ((void)0)
+#else
+#define PRE(condition_)                                                     \
+    if (!(condition_))                                                      \
+    {                                                                       \
+        std::cout << "Pre-condition : `" << (#condition_) << "` failed.\n"; \
+        std::cout << "File          : " << __FILE__ << '\n';                \
+        std::cout << "Line          : " << __LINE__ << '\n';                \
+        std::cout << "Function      : " << __func__ << '\n';                \
+        std::cout << "Stacktrace ---- \n" << stacktrace();                  \
+    }
+#endif
 
 
 
