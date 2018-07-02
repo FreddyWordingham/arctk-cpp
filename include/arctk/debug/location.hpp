0eb7ceb5d7ec644a692c957a92146128d251fa60
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-02 18:51:54 +0100

    Added stacktrace function.

--- include/arctk/debug/location.hpp
+++ include/arctk/debug/location.hpp
@@ -21,6 +21,14 @@
 
 //  == IMPORTS ==
 //  -- Std --
+#include <cstdio>
+#include <cstdlib>
+#include <cxxabi.h>
+#include <dlfcn.h>
+#include <execinfo.h>
+#include <sstream>
+#include <string>
+#include <vector>
 
 
 
@@ -33,10 +41,67 @@ namespace arc //! arctk namespace
 
 
         //  == FUNCTION PROTOTYPES ==
+        //  -- Stacktracing --
+        std::vector<std::string> stacktrace(const int skip = 1);
 
 
 
         //  == FUNCTIONS ==
+        //  -- Stacktracing --
+        std::vector<std::string> stacktrace(const int skip)
+        {
+            void*     callstack[128];
+            const int max_num_frames = sizeof(callstack) / sizeof(callstack[0]);
+            int       num_frames     = backtrace(callstack, max_num_frames);
+            char**    symbols        = backtrace_symbols(callstack, num_frames);
+
+            std::vector<std::string> trace;
+            for (int i = skip; i < (num_frames - 1); i++)
+            {
+                Dl_info info;
+                if (dladdr(callstack[i], &info) && info.dli_sname)
+                {
+                    char* demangled = nullptr;
+                    int   status    = -1;
+
+                    if (info.dli_sname[0] == '_')
+                    {
+                        demangled = abi::__cxa_demangle(info.dli_sname, nullptr, nullptr, &status);
+                    }
+
+                    if (status == 0)
+                    {
+                        trace.emplace_back(demangled);
+                    }
+                    else
+                    {
+                        if (info.dli_sname == nullptr)
+                        {
+                            trace.emplace_back(symbols[i]);
+                        }
+                        else
+                        {
+                            trace.emplace_back(info.dli_sname);
+                        }
+                    }
+
+                    free(demangled);
+                }
+                else
+                {
+                    trace.emplace_back(symbols[i]);
+                }
+            }
+
+            free(symbols);
+
+            if (num_frames == max_num_frames)
+            {
+                trace.emplace_back("\n[truncated]");
+            }
+
+            return (trace);
+        }
 
 
 
