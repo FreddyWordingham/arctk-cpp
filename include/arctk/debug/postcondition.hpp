46f6e348804f3e1a86296b847ee284b201abc786
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-03 00:28:56 +0100

    Added framework.

---
+++ include/arctk/debug/postcondition.hpp
@@ -0,0 +1,50 @@
+/**
+ *  @file   arctk/debug/postcondition.hpp
+ *  @date   03/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Location postcondition debug class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DEBUG_POSTCONDITION_HPP
+#define ARCTK_DEBUG_POSTCONDITION_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace debug //! debugging namespace
+    {
+
+
+
+        //  == CLASS ==
+
+
+
+    } // namespace debug
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DEBUG_POSTCONDITION_HPP
