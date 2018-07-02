742f143585c0ee7dfac638c15a86bf91020d0c0f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-02 20:27:57 +0100

    Added missing namespace qualifier.

--- include/arctk/debug.hpp
+++ include/arctk/debug.hpp
@@ -27,6 +27,16 @@
 
 //  == MACROS ==
 //  -- Location --
+#ifdef NDEBUG
+#define PRE(condition_) ((void)0)
+#else
+#define PRE(condition_)                                                      \
+    if (!(condition_))                                                       \
+    {                                                                        \
+        std::cout << "Pre-condition  : `" << (#condition_) << "` failed.\n"; \
+        std::cout << "Located at     : " << arc::debug::location() << '\n';  \
+    }
+#endif
 
 
 
