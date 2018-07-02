9ba43722b50b10a83b121b419d5ebf489884d757
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-03 00:18:42 +0100

    Added framework.

---
+++ include/arctk/macro/utility.hpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   arctk/macro/utility.hpp
+ *  @date   03/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of utility macros.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MACRO_UTILITY_HPP
+#define ARCTK_MACRO_UTILITY_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MACRO_UTILITY_HPP
