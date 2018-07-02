891bf4a1048f893a45c48cd7dd4a92bda72f2ef8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-03 00:53:07 +0100

    Set stacktrace skips in post-condition macro.

--- include/arctk/debug/postcondition.hpp
+++ include/arctk/debug/postcondition.hpp
@@ -81,7 +81,7 @@ namespace arc //! arctk namespace
             if (!_expr())
             {
                 std::cout << "Post-condition : `" << _expr_str << "` failed.\n";
-                std::cout << "Located at     :\n" << location(_file, _line, _func) << '\n';
+                std::cout << "Located at     :\n" << location(_file, _line, _func, 4) << '\n';
 
                 exit(43);
             }
