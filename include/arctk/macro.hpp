4ba49a50aa2e6c569caf731172bfde05019bc54a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-03 00:15:56 +0100

    Added framework.

---
+++ include/arctk/macro.hpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   arctk/macro.hpp
+ *  @date   03/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of useful macros.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MACRO_HPP
+#define ARCTK_MACRO_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MACRO_HPP
