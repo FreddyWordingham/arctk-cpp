25589e5a52630acc4849fd23cd5fa1ad9867d76b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-02 18:33:46 +0100

    Added framework.

---
+++ include/arctk/debug.hpp
@@ -0,0 +1,36 @@
+/**
+ *  @file   arctk/debug.hpp
+ *  @date   02/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of debugging functions and macros.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DEBUG_HPP
+#define ARCTK_DEBUG_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == MACROS ==
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DEBUG_HPP
