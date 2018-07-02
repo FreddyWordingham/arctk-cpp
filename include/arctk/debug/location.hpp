994b7537bf7afc51da0018baaf8c61e89a9777d5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-02 18:41:54 +0100

    Added framework.

---
+++ include/arctk/debug/location.hpp
@@ -0,0 +1,54 @@
+/**
+ *  @file   arctk/debug/location.hpp
+ *  @date   02/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Location debug functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DEBUG_LOCATION_HPP
+#define ARCTK_DEBUG_LOCATION_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace debug //! debugging namespace
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+    } // namespace debug
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DEBUG_LOCATION_HPP
