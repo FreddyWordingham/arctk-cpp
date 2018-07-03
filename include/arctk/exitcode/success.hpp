c992c6e1d27db8be00357bffd85d0ecf913aecd9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-03 23:30:40 +0100

    Added framework.

---
+++ include/arctk/exitcode/success.hpp
@@ -0,0 +1,50 @@
+/**
+ *  @file   arctk/exitcode/success.hpp
+ *  @date   03/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Enumeration of all exit code success values.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EXITCODE_SUCCESS_HPP
+#define ARCTK_EXITCODE_SUCCESS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace exitcode //! exit codes namespace
+    {
+        namespace success //! success namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Contracts --
+            constexpr const int COMPLETE = 0; //!< Program successfully has completed.
+
+
+
+        } // namespace success
+    }     // namespace exitcode
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EXITCODE_SUCCESS_HPP
