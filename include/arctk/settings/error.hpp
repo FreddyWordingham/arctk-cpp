44ef394eed9a92aa83b0d5bbaa060b0c309e046f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-03 15:32:10 +0100

    Added framework.

---
+++ include/arctk/settings/error.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/settings/error.hpp
+ *  @date   03/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Error settings.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SETTINGS_ERROR_HPP
+#define ARCTK_SETTINGS_ERROR_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace settings //! settings namespace
+    {
+        namespace error //! error namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+        } // namespace error
+    }     // namespace settings
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SETTINGS_ERROR_HPP
