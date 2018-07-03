4863809d4099bb3dc0e98d0662c4f2fa36c41e7c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-03 21:58:44 +0100

    Added error enumeration exit value.

--- include/arctk/debug/postcondition.hpp
+++ include/arctk/debug/postcondition.hpp
@@ -99,7 +99,7 @@ namespace arc //! arctk namespace
                 std::cerr << "Post-condition : `" << _expr_str << "` failed.\n";
                 std::cerr << "Located at     :\n" << location::info(_file, _line, _func, 4) << '\n';
 
-                exit(43);
+                exit(settings::error::type::POST_CONDITION_CONTRACT_FAILURE);
             }
         }
 
