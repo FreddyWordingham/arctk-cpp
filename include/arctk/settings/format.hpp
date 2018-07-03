4b2a1315f2ca76892e5b0d2adc7f503f8fed56ad
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-04 00:24:49 +0100

    Added framework.

---
+++ include/arctk/settings/format.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/settings/format.hpp
+ *  @date   04/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of formatting settings.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SETTINGS_FORMAT_HPP
+#define ARCTK_SETTINGS_FORMAT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace settings //! settings namespace
+    {
+        namespace format //! format namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+
+
+
+        } // namespace format
+    }     // namespace settings
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SETTINGS_FORMAT_HPP
