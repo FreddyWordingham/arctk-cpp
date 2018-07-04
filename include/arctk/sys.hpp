e4094bc7c6e499bc4e95aa33ba389b13ec5f1e5d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-04 02:05:36 +0100

    Added framework.

---
+++ include/arctk/sys.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/sys.hpp
+ *  @date   04/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of system classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SYS_HPP
+#define ARCTK_SYS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SYS_HPP
