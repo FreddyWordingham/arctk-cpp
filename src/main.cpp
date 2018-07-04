0aeaef75e070c0a07468ef0e15b2c9b0909eceba
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-04 13:54:28 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 #include <arctk/exit/success.hpp>
 #include <arctk/macro.hpp>
 #include <arctk/macro/utility.hpp>
+#include <arctk/parse.hpp>
 #include <arctk/settings.hpp>
 #include <arctk/settings/format.hpp>
 #include <arctk/sys.hpp>
