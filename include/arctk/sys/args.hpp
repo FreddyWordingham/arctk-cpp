bae5facb1355d768861e398ec33c99d149734aa8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-04 15:02:58 +0100

    Corrected function call.

--- include/arctk/sys/args.hpp
+++ include/arctk/sys/args.hpp
@@ -133,7 +133,7 @@ namespace arc //! arctk namespace
             size_t i = 0;
             ((check_parsable<A>(argv[i]), ++i), ...);
 
-            return (str::parse::from<A...>(argv));
+            return (str::parse::to<A...>(argv));
         }
 
         /**
