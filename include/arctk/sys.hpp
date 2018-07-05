67b0f90903833a2f6bf4e5abcb46c2453a0afad1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 16:27:01 +0100

    Linked to sys collection header.

--- include/arctk/sys.hpp
+++ include/arctk/sys.hpp
@@ -23,6 +23,7 @@
 //  -- Arctk --
 #include <arctk/sys/args.hpp>
 #include <arctk/sys/file.hpp>
+#include <arctk/sys/file/app.hpp>
 #include <arctk/sys/file/in.hpp>
 #include <arctk/sys/file/out.hpp>
 
