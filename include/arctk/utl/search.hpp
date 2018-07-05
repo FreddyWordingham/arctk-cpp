8c1318fea8217f8df0daa715f4edf7ede3c6c80a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 19:20:13 +0100

    Added max function.

--- include/arctk/utl/search.hpp
+++ include/arctk/utl/search.hpp
@@ -43,6 +43,8 @@ namespace arc //! arctk namespace
             inline size_t max_index(const C& cont_) noexcept;
             template <typename C, typename T = typename C::value_type, typename IT = typename C::const_iterator>
             inline T min(const C& cont_) noexcept;
+            template <typename C, typename T = typename C::value_type, typename IT = typename C::const_iterator>
+            inline T max(const C& cont_) noexcept;
 
 
 
@@ -129,6 +131,27 @@ namespace arc //! arctk namespace
                 PRE(!cont_.empty());
 
                 return (cont_[min_index(cont_)]);
+            }
+
+            /**
+             *  Find the maximum element within a container.
+             *
+             *  @tparam C   Type of container.
+             *  @tparam T   Type stored by C.
+             *  @tparam IT   Type of const iterator of C.
+             *
+             *  @param  cont_   Container to determine the maximum element of.
+             *
+             *  @pre    cont_ must not be empty.
+             *
+             *  @return Value of the maximum element within the container.
+             */
+            template <typename C, typename T, typename IT>
+            inline T max(const C& cont_) noexcept
+            {
+                PRE(!cont_.empty());
+
+                return (cont_[max_index(cont_)]);
             }
 
 
