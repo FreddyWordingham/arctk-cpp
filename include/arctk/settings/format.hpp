43fcdce9d8145d890748525740a9c3a15611d427
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 18:54:58 +0100

    Reduced number of limiter characters to two catagories.

--- include/arctk/settings/format.hpp
+++ include/arctk/settings/format.hpp
@@ -34,17 +34,11 @@ namespace arc //! arctk namespace
             constexpr const int PRINT_WIDTH = 10; //!< Width allocated to data prints.
 
             //  -- Limiters --
-            constexpr const char DELIMITER    = ','; //!< Character used to delimit consecutive data.
-            constexpr const char ARRAY_START  = '['; //!< Character used to indicate array end point.
-            constexpr const char ARRAY_END    = ']'; //!< Character used to indicate array start point.
-            constexpr const char MAP_START    = '<'; //!< Character used to indicate map end point.
-            constexpr const char MAP_END      = '>'; //!< Character used to indicate map start point.
-            constexpr const char PAIR_START   = '('; //!< Character used to indicate pair end point.
-            constexpr const char PAIR_END     = ')'; //!< Character used to indicate pair start point.
-            constexpr const char TUPLE_START  = '('; //!< Character used to indicate tuple end point.
-            constexpr const char TUPLE_END    = ')'; //!< Character used to indicate tuple start point.
-            constexpr const char VECTOR_START = '{'; //!< Character used to indicate vector end point.
-            constexpr const char VECTOR_END   = '}'; //!< Character used to indicate vector start point.
+            constexpr const char DELIMITER       = ','; //!< Character used to delimit consecutive data.
+            constexpr const char CONTAINER_START = '{'; //!< Character used to indicate container end point.
+            constexpr const char CONTAINER_END   = '}'; //!< Character used to indicate container start point.
+            constexpr const char TUPLE_START     = '('; //!< Character used to indicate tuple end point.
+            constexpr const char TUPLE_END       = ')'; //!< Character used to indicate tuple start point.
 
 
 
