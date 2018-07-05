ee3bfc98907902b4f08c846520178deaa8577d2c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 20:05:08 +0100

    Added framework.

---
+++ include/arctk/term.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/term.hpp
+ *  @date   05/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of terminal constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_TERM_HPP
+#define ARCTK_TERM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_TERM_HPP
