9b55b51810c13fec98bd3de9f995ad7afa03d0fe
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 20:11:21 +0100

    Added framework.

---
+++ include/arctk/term/ansi.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/term/ansi.hpp
+ *  @date   05/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection terminal ansi escape strings.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_TERM_ANSI_HPP
+#define ARCTK_TERM_ANSI_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace term //! terminal namespace
+    {
+        namespace ansi //! ansi namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+
+
+
+        } // namespace ansi
+    }     // namespace term
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_TERM_ANSI_HPP
