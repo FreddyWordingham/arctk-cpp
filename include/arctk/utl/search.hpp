a53c591b1c82ba09a6ff2085c0a6c149081d4d4e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 18:25:14 +0100

    Added framework.

---
+++ include/arctk/utl/search.hpp
@@ -0,0 +1,57 @@
+/**
+ *  @file   arctk/utl/search.hpp
+ *  @date   05/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of searching functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_SEARCH_HPP
+#define ARCTK_UTL_SEARCH_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace utl //! utility namespace
+    {
+        namespace search //! searching namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace search
+    }     // namespace utl
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_SEARCH_HPP
