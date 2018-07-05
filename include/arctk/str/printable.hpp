c2ad685fafa5708991f376e07cb7cbedcfc701e4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 17:36:37 +0100

    Added framework.

---
+++ include/arctk/str/printable.hpp
@@ -0,0 +1,70 @@
+/**
+ *  @file   arctk/str/printable.hpp
+ *  @date   05/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Printable class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STR_PRINTABLE_HPP
+#define ARCTK_STR_PRINTABLE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace sys //! system namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Printable interface class.
+         */
+        class Printable
+        {
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace sys
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STR_PRINTABLE_HPP
