6d512a8156fe52568a29307632f08b194507025f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 16:27:14 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@
 #include <arctk/sys.hpp>
 #include <arctk/sys/args.hpp>
 #include <arctk/sys/file.hpp>
+#include <arctk/sys/file/app.hpp>
 #include <arctk/sys/file/in.hpp>
 #include <arctk/sys/file/out.hpp>
 
