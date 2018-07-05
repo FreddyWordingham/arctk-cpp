2a2ed3dad00d298fe53eae44be7c2a5afab7fdb9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 19:32:35 +0100

    Added framework.

---
+++ include/arctk/utl/properties.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file   arctk/utl/properties.hpp
+ *  @date   05/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of property related functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_PROPERTIES_HPP
+#define ARCTK_UTL_PROPERTIES_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace utl //! utility namespace
+    {
+        namespace properties //! properties namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace properties
+    }     // namespace utl
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_PROPERTIES_HPP
