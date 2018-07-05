d4a9ca385e60c133b230c3381b1fc0920e4e9550
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-05 20:14:58 +0100

    Added colour escape strings.

--- include/arctk/term/ansi.hpp
+++ include/arctk/term/ansi.hpp
@@ -34,6 +34,26 @@ namespace arc //! arctk namespace
             constexpr const char* const RESET     = "\033[0;0m";        //!< Reset ansi options.
             constexpr const char* const OVERWRITE = "\n\033[A\33[2K\r"; //!< Overwrite the previous string.
 
+            //  -- Standard Colours --
+            constexpr const char* const FG_BLACK   = "\033[0;30m"; //!< Colour text black.
+            constexpr const char* const FG_RED     = "\033[0;31m"; //!< Colour text red.
+            constexpr const char* const FG_GREEN   = "\033[0;32m"; //!< Colour text green.
+            constexpr const char* const FG_YELLOW  = "\033[0;33m"; //!< Colour text yellow.
+            constexpr const char* const FG_BLUE    = "\033[0;34m"; //!< Colour text blue.
+            constexpr const char* const FG_MAGENTA = "\033[0;35m"; //!< Colour text magenta.
+            constexpr const char* const FG_CYAN    = "\033[0;36m"; //!< Colour text cyan.
+            constexpr const char* const FG_WHITE   = "\033[0;37m"; //!< Colour text white.
+
+            //  -- Bright Colours --
+            constexpr const char* const FG_BRIGHT_BLACK   = "\033[0;90m"; //!< Colour text bright black.
+            constexpr const char* const FG_BRIGHT_RED     = "\033[0;91m"; //!< Colour text bright red.
+            constexpr const char* const FG_BRIGHT_GREEN   = "\033[0;92m"; //!< Colour text bright green.
+            constexpr const char* const FG_BRIGHT_YELLOW  = "\033[0;93m"; //!< Colour text bright yellow.
+            constexpr const char* const FG_BRIGHT_BLUE    = "\033[0;94m"; //!< Colour text bright blue.
+            constexpr const char* const FG_BRIGHT_MAGENTA = "\033[0;95m"; //!< Colour text bright magenta.
+            constexpr const char* const FG_BRIGHT_CYAN    = "\033[0;96m"; //!< Colour text bright cyan.
+            constexpr const char* const FG_BRIGHT_WHITE   = "\033[0;97m"; //!< Colour text bright white.
+
 
 
         } // namespace ansi
