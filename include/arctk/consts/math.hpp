d45dbb5129980c8b796dfd84fcf108429844b5d8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 12:05:18 +0100

    Added missing namespace.

--- include/arctk/consts/math.hpp
+++ include/arctk/consts/math.hpp
@@ -24,17 +24,20 @@ namespace arc //! arctk namespace
 {
     namespace consts //! constants namespace
     {
+        namespace math //! mathematical namespace
+        {
 
 
 
-        //  == CONSTANTS ==
-        //  -- Fundamental --
-        constexpr const double PI  = 3.141592653589793238462643383279502884197169399375105820974; //!<  Archimedes' Constant.
-        constexpr const double EXP = 2.718281828459045235360287471352662497757247093699959574966; //!< Euler's Number.
+            //  == CONSTANTS ==
+            //  -- Fundamental --
+            constexpr const double PI  = 3.141592653589793238462643383279502884197169399375105820974; //!<  Archimedes' Constant.
+            constexpr const double EXP = 2.718281828459045235360287471352662497757247093699959574966; //!< Euler's Number.
 
 
 
-    } // namespace consts
+        } // namespace math
+    }     // namespace consts
 } // namespace arc
 
 
