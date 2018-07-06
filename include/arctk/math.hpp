53ff8b15a0b89c49db3798993b7252c6f2eada39
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 13:32:59 +0100

    Added framework.

---
+++ include/arctk/math.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/math.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of mathematical classes and functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_HPP
+#define ARCTK_MATH_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_HPP
