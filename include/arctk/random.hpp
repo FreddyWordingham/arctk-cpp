fc1713e170afd430f5f31067481a11f02948af1f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 11:07:14 +0100

    Added framework.

---
+++ include/arctk/random.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/random.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of random generator classes and distribution functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RANDOM_HPP
+#define ARCTK_RANDOM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RANDOM_HPP
