c4d8a5b1712c895eaf0e8f50971f6e74827c3c6b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 13:25:32 +0100

    Added framework.

---
+++ include/arctk/phys.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/phys.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of physics classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_HPP
+#define ARCTK_PHYS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_HPP
