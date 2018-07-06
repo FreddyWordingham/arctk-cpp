ae67065bc5b58bef5c6d4c9528f47d858a7fcd6f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 11:59:17 +0100

    Added framework.

---
+++ include/arctk/consts.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/consts.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONSTS_HPP
+#define ARCTK_CONSTS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONSTS_HPP
