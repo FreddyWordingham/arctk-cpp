7c30ea388afd94865f4e3c4f2d64713d1ca3f8cb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 13:30:46 +0100

    Added compare implimentation.

---
+++ include/arctk/math/compare.hpp
@@ -0,0 +1,96 @@
+/**
+ *  @file   arctk/math/compare.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Math comparison functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_COMPARE_HPP
+#define ARCTK_MATH_COMPARE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <limits>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+        namespace compare //! comparison namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Mathematical --
+            template <typename T>
+            inline bool zero(T x_) noexcept;
+            template <typename T>
+            inline bool equal(T x_, T y_) noexcept;
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Mathematical --
+            /**
+             *  Test if a value can be considered equal to zero.
+             *  Value is considered equal to zero if it is within epsilon of zero.
+             *
+             *  @tparam T   Type of value to test.
+             *
+             *  @param  x_  Value to test.
+             *
+             *  @return true if the value can be considered equal to zero.
+             */
+            template <typename T>
+            inline bool zero(T x_) noexcept
+            {
+                return ((x_ > -std::numeric_limits<double>::epsilon()) && (x_ < std::numeric_limits<double>::epsilon()));
+            }
+
+            /**
+             *  Test if a pair of values can be considered equal to one another.
+             *  Values are considered equal to zero if they are equal to within epsilon.
+             *
+             *  @tparam T   Type of values to test.
+             *
+             *  @param  x_  Fist value to test.
+             *  @param  y_  Second value to test.
+             *
+             *  @return true if the values can be considered equal.
+             */
+            template <typename T>
+            inline bool equal(const T x_, const T y_) noexcept
+            {
+                return (zero(x_ - y_));
+            }
+
+
+
+        } // namespace compare
+    }     // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_COMPARE_HPP
