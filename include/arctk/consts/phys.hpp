fa044581346f104c350f59475a35d44095559d92
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 12:04:12 +0100

    Added phys constants.

---
+++ include/arctk/consts/phys.hpp
@@ -0,0 +1,47 @@
+/**
+ *  @file   arctk/consts/phys.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Physical constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONSTS_PHYS_HPP
+#define ARCTK_CONSTS_PHYS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace consts //! constants namespace
+    {
+
+
+
+        //  == CONSTANTS ==
+        //  -- Derived --
+        constexpr const double SPEED_OF_LIGHT = 2.99792458e+8; //!< Speed of light in vacuum.
+
+
+
+    } // namespace consts
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONSTS_PHYS_HPP
