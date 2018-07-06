3bc0cc7c0a7347e8691d71ecf074421f106d7f6d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 12:02:10 +0100

    Added math constants.

---
+++ include/arctk/consts/math.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/consts/math.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Mathematical constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONSTS_MATH_HPP
+#define ARCTK_CONSTS_MATH_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace consts //! constants namespace
+    {
+
+
+
+        //  == CONSTANTS ==
+        //  -- Fundamental --
+        constexpr const double PI  = 3.141592653589793238462643383279502884197169399375105820974; //!<  Archimedes' Constant.
+        constexpr const double EXP = 2.718281828459045235360287471352662497757247093699959574966; //!< Euler's Number.
+
+
+
+    } // namespace consts
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONSTS_MATH_HPP
