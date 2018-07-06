bfdb6a9c2decdef8dae9c9eb1763b83268ec062f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 13:07:35 +0100

    Added framework.

---
+++ include/arctk/utl/access.hpp
@@ -0,0 +1,59 @@
+/**
+ *  @file   arctk/utl/access.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of access related functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_ACCESS_HPP
+#define ARCTK_UTL_ACCESS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace utl //! utility namespace
+    {
+        namespace access //! access namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Tuple --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Tuple --
+
+
+
+        } // namespace access
+    }     // namespace utl
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_ACCESS_HPP
