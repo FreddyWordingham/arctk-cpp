7193887fde7ff986961069798c4a9ccdf891ae73
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-06 12:45:50 +0100

    Added framework.

---
+++ include/arctk/random/distribution/phys.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @file   arctk/random/distribution/phys.hpp
+ *  @date   06/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Physics distribution functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RANDOM_DISTRIBUTION_PHYS_HPP
+#define ARCTK_RANDOM_DISTRIBUTION_PHYS_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <cmath>
+
+//  -- Arctk --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace random //! utility namespace
+    {
+        namespace distribution //! distribution namespace
+        {
+            namespace phys //! physics namespace
+            {
+
+
+
+                //  == FUNCTION PROTOTYPES ==
+
+
+
+                //  == FUNCTIONS ==
+
+
+
+            } // namespace phys
+        }     // namespace distribution
+    }         // namespace random
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RANDOM_DISTRIBUTION_PHYS_HPP
