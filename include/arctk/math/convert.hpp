0e56eaea03eccb78b54c79501f99ccb0e41ba086
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-07 16:41:13 +0100

    Added framework.

---
+++ include/arctk/math/convert.hpp
@@ -0,0 +1,59 @@
+/**
+ *  @file   arctk/math/convert.hpp
+ *  @date   07/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Math conversion functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_CONVERT_HPP
+#define ARCTK_MATH_CONVERT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+        namespace convert //! convertion namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Convertsion --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Convertsion --
+
+
+
+        } // namespace convert
+    }     // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_CONVERT_HPP
