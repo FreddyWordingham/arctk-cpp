adcd01a8fe4a05887262f276ba8f61bfdd65e0de
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-10 01:31:52 +0100

    Set pre-conditions.

--- include/arctk/math/vec.hpp
+++ include/arctk/math/vec.hpp
@@ -673,7 +673,7 @@ namespace arc //! arctk namespace
         template <typename T, size_t N>
         constexpr inline T& Vec<T, N>::operator[](const size_t index_) noexcept
         {
-            assert(index_ < N);
+            PRE(index_ < N);
 
             return (_data[index_]);
         }
@@ -693,7 +693,7 @@ namespace arc //! arctk namespace
         template <typename T, size_t N>
         constexpr inline const T& Vec<T, N>::operator[](const size_t index_) const noexcept
         {
-            assert(index_ < N);
+            PRE(index_ < N);
 
             return (_data[index_]);
         }
