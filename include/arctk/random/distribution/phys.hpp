bbd6151414983c6a9b23128948b9049c59aae503
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-10 13:45:00 +0100

    Added phys distribution functions.

--- include/arctk/random/distribution/phys.hpp
+++ include/arctk/random/distribution/phys.hpp
@@ -24,6 +24,7 @@
 #include <cmath>
 
 //  -- Arctk --
+#include <arctk/debug.hpp>
 
 
 
@@ -40,10 +41,53 @@ namespace arc //! arctk namespace
 
 
                 //  == FUNCTION PROTOTYPES ==
+                //  -- Scattering --
+                inline vec3   isotropic(Generator* const rng_) noexcept;
+                inline double henyey_greenstein(Generator* const rng_, double g_) noexcept;
 
 
 
                 //  == FUNCTIONS ==
+                //  -- Scattering --
+                /**
+                 *  Generate a random isotropic unit vector.
+                 *
+                 *  @param  rng_    Generator used to draw base values from.
+                 *
+                 *  @return Random isotropic unit vector.
+                 */
+                inline vec3 isotropic(Generator* const rng_) noexcept
+                {
+                    const double theta = arc::rng::distribution::uniform<double>(rng_, 0.0, 2.0 * arc::constant::PI);
+                    const double z     = arc::rng::distribution::uniform<double>(rng_, -1.0, 1.0);
+
+                    return (vec3(std::sqrt(1.0 - arc::math::sq(z)) * std::cos(theta), std::sqrt(1.0 - arc::math::sq(z)) * std::sin(theta), z));
+                }
+
+                /**
+                 *  Draw a value from the Henyey-Greenstein phase function.
+                 *  Function description can be found at: https://www.astro.umd.edu/~jph/HG_note.pdf
+                 *
+                 *  @param  rng_    Generator used to draw base values from.
+                 *  @param  g_      Asymmetry-parameter.
+                 *
+                 *  @pre    g_ must be between -1.0 and +1.0 inclusively.
+                 *
+                 *  @return Angle drawn from Henyey-Greenstein phase function.
+                 */
+                inline double henyey_greenstein(Generator* const rng_, const double g_) noexcept
+                {
+                    PRE((g_ >= -1.0) && (g_ <= 1.0));
+
+                    const double s = uniform<double>(rng_, -1.0, 1.0);
+
+                    if (math::zero(g_))
+                    {
+                        return (s);
+                    }
+
+                    return ((1.0 + math::sq(g_) - math::sq((1.0 - math::sq(g_)) / (1.0 + (g_ * s)))) / (2.0 * g_));
+                }
 
 
 
