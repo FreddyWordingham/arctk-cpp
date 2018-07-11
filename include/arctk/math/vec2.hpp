07d10538c60d765c7a1e8997b6f75517cb2a4b61
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-11 02:59:27 +0100

    Fixed template specialisation.

--- include/arctk/math/vec2.hpp
+++ include/arctk/math/vec2.hpp
@@ -566,8 +566,8 @@ namespace arc //! arctk namespace
          *
          *  @return Human readable string of the vec.
          */
-        template <typename T, size_t N>
-        inline std::string Vec<T, N>::str() noexcept
+        template <typename T>
+        inline std::string Vec<T, 2>::str() noexcept
         {
             std::stringstream stream;
 
