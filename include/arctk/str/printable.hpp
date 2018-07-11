61ef2d6b9f51374b0858b425caadc501d8ee31b4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-11 02:53:06 +0100

    Added definition for destructor method.

--- include/arctk/str/printable.hpp
+++ include/arctk/str/printable.hpp
@@ -69,6 +69,12 @@ namespace arc //! arctk namespace
 
 
 
+        //  == INSTANTIATION ==
+        //  -- Destructors --
+        inline Printable::~Printable() noexcept = default;
+
+
+
         //  == METHODS ==
         //  -- Saving --
         /**
