7288db7a7f56e8085f19010bdc7e276ac43efc99
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-11 21:17:15 +0100

    Added framework.

---
+++ include/arctk/debug/contract.hpp
@@ -0,0 +1,60 @@
+/**
+ *  @file   arctk/debug/contract.hpp
+ *  @date   11/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Contract debug functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DEBUG_CONTRACT_HPP
+#define ARCTK_DEBUG_CONTRACT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace debug //! debugging namespace
+    {
+        namespace contract //! contracts namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Contracts --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Contracts --
+
+
+
+        } // namespace contract
+    }     // namespace debug
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DEBUG_CONTRACT_HPP
