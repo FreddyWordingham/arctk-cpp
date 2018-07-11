3e606aa44cd5f018cd71212b68a18da603c17563
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-11 14:26:00 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@
 #include <arctk/math/compare.hpp>
 #include <arctk/math/container.hpp>
 #include <arctk/math/convert.hpp>
+#include <arctk/math/mat.hpp>
 #include <arctk/math/pow.hpp>
 #include <arctk/math/vec.hpp>
 #include <arctk/math/vec2.hpp>
