7afe36c04000ea574742f10e4a615c7a54811a9d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-11 19:15:29 +0100

    Added framework.

---
+++ include/arctk/data.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/data.hpp
+ *  @date   11/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of data classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_HPP
+#define ARCTK_DATA_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_HPP
