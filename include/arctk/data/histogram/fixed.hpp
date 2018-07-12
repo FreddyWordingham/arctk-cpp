58cfa7024126e7bd856ccc50f203cb1685062063
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-12 18:04:16 +0100

    Removed inheritance.

--- include/arctk/data/histogram/fixed.hpp
+++ include/arctk/data/histogram/fixed.hpp
@@ -19,12 +19,6 @@
 
 
 
-//  == BASE ==
-//  -- Arctk --
-#include <arctk/data/histogram.hpp>
-
-
-
 //  == NAMESPACE ==
 namespace arc //! arctk namespace
 {
@@ -43,7 +37,7 @@ namespace arc //! arctk namespace
              *  @tparam N   Dimensionality of the histogram.
              */
             template <typename T, size_t N>
-            class Fixed : public Histogram<T, N>
+            class Fixed
             {
             };
 
