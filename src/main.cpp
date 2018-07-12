41cbbcf83658caa496250ee408f2e3479824df3f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-12 02:14:53 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 //  -- Arctk --
 #include <arctk/col.hpp>
 #include <arctk/col/convert.hpp>
+#include <arctk/col/map.hpp>
 #include <arctk/config.hpp>
 #include <arctk/consts.hpp>
 #include <arctk/consts/math.hpp>
