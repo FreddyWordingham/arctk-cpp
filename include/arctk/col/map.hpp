9189949ce799de59d14051ded9d0e7d13e6fa670
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-12 12:38:23 +0100

    Renamed contour to jet_block.

--- include/arctk/col/map.hpp
+++ include/arctk/col/map.hpp
@@ -44,7 +44,7 @@ namespace arc //! arctk namespace
             //  -- Matlab --
             inline vec3 jet(double x_) noexcept;
             inline vec3 jet_line(double x_) noexcept;
-            inline vec3 contour(double x_, int num_ = 10) noexcept;
+            inline vec3 jet_block(double x_, int num_ = 10) noexcept;
 
 
 
@@ -117,12 +117,12 @@ namespace arc //! arctk namespace
                 return (jet(x_));
             }
 
-            inline vec3 contour(const double x_, const int num_) noexcept
+            inline vec3 jet_block(const double x_, const int num_) noexcept
             {
                 PRE((x_ >= 0.0) && (x_ <= 1.0));
                 PRE(num_ > 0);
 
-                return (convert::hsv_to_rgb(360.0 * (x_ - std::fmod(x_, 1.0 / static_cast<double>(num_))), 1.0, 1.0));
+                return (jet(x_ - std::fmod(x_, 1.0 / static_cast<double>(num_))));
             }
 
 
