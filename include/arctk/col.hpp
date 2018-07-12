cdce88cac4885ebeb0d863e3a25c1017913bdc59
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-12 02:09:33 +0100

    Added framework.

---
+++ include/arctk/col.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/col.hpp
+ *  @date   12/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection colour functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_COL_HPP
+#define ARCTK_COL_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_COL_HPP
