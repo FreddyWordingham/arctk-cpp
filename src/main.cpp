58099f7950977cdbffccfb2ba8b85821944ab210
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-13 13:48:24 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,7 @@
 #include <arctk/phys/particle.hpp>
 #include <arctk/random.hpp>
 #include <arctk/random/distribution.hpp>
+#include <arctk/random/distribution/discrete.hpp>
 #include <arctk/random/distribution/math.hpp>
 #include <arctk/random/distribution/phys.hpp>
 #include <arctk/random/generator.hpp>
