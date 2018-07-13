d664d642c48fb0cdf8acce071869eae3d25642df
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-13 14:36:51 +0100

    Removed old header files.

--- include/arctk/random.hpp
+++ include/arctk/random.hpp
@@ -22,9 +22,6 @@
 //  == IMPORTS ==
 //  -- Arctk --
 #include <arctk/random/distribution.hpp>
-#include <arctk/random/distribution/discrete.hpp>
-#include <arctk/random/distribution/math.hpp>
-#include <arctk/random/distribution/phys.hpp>
 #include <arctk/random/generator.hpp>
 #include <arctk/random/generator/quality.hpp>
 
