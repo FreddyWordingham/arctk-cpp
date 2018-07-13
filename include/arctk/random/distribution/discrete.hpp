9dbd03e7a2f27aa226c8e599a3eae29b7b48bd56
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-13 13:50:41 +0100

    Added framework.

---
+++ include/arctk/random/distribution/discrete.hpp
@@ -0,0 +1,76 @@
+/**
+ *  @file   arctk/random/distribution/discrete.hpp
+ *  @date   13/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Discrete distribution class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RANDOM_DISTRIBUTION_DISCRETE_HPP
+#define ARCTK_RANDOM_DISTRIBUTION_DISCRETE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace random //! random number namespace
+    {
+        namespace distribution //! distribution namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             */
+            class Discrete
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+            //  == INSTANTIATION ==
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace distribution
+    }     // namespace random
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RANDOM_DISTRIBUTION_DISCRETE_HPP
