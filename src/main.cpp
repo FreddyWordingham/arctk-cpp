a7304ede9985311e42815ed32488be3837fab801
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-15 23:08:28 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,7 @@
 #include <arctk/random.hpp>
 #include <arctk/random/distribution.hpp>
 #include <arctk/random/distribution/discrete.hpp>
+#include <arctk/random/distribution/gaussian.hpp>
 #include <arctk/random/distribution/uniform.hpp>
 #include <arctk/random/generator.hpp>
 #include <arctk/random/generator/quality.hpp>
