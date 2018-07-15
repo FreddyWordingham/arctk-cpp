72c453070ae9dd1835d80fad50d3c43fbbd83b5f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-15 22:12:38 +0100

    Added framework.

---
+++ include/arctk/random/distribution/uniform.hpp
@@ -0,0 +1,77 @@
+/**
+ *  @file   arctk/random/distribution/uniform.hpp
+ *  @date   15/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Random uniform probability distribution function sampling.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RANDOM_DISTRIBUTION_UNIFORM_HPP
+#define ARCTK_RANDOM_DISTRIBUTION_UNIFORM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace random //! random number namespace
+    {
+        namespace distribution //! probability distribution namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             */
+            template <typename T>
+            class Uniform : public Distribution<T>
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  == METHODS ==
+              public:
+                //  -- Sampling --
+                inline T sample(Generator* const rng_) const noexcept override;
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace distribution
+    }     // namespace random
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RANDOM_DISTRIBUTION_UNIFORM_HPP
