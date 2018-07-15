3ddceb3cae9396b1405c0bb5f33b9e82caf3d834
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-15 23:38:31 +0100

    Marked str method as const.

--- include/arctk/math/vec4.hpp
+++ include/arctk/math/vec4.hpp
@@ -147,7 +147,7 @@ namespace arc //! arctk namespace
             constexpr inline void normalise() noexcept;
 
             //  -- Printing --
-            inline std::string str() noexcept override;
+            inline std::string str() const noexcept override;
         };
 
 
@@ -638,7 +638,7 @@ namespace arc //! arctk namespace
          *  @return Human readable string of the vec.
          */
         template <typename T>
-        inline std::string Vec<T, 4>::str() noexcept
+        inline std::string Vec<T, 4>::str() const noexcept
         {
             std::stringstream stream;
 
