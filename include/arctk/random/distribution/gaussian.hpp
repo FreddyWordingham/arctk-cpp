0fd9cf9008e035dd4f4ad9f2b092b532c8459ab2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-15 23:12:48 +0100

    Added framework.

---
+++ include/arctk/random/distribution/gaussian.hpp
@@ -0,0 +1,93 @@
+/**
+ *  @file   arctk/random/distribution/gaussian.hpp
+ *  @date   15/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Random gaussian probability distribution function sampling.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RANDOM_DISTRIBUTION_GAUSSIAN_HPP
+#define ARCTK_RANDOM_DISTRIBUTION_GAUSSIAN_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace random //! random number namespace
+    {
+        namespace distribution //! probability distribution namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Gaussian probability distribution base class used to sample random numbers.
+             *
+             *  @tparam T   Type returned from sampling the distribution.
+             */
+            template <typename T>
+            class Gaussian : public Distribution<T>
+            {
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+                inline Gaussian(const T min_, const T max_) noexcept;
+
+
+                //  == METHODS ==
+              public:
+                //  -- Sampling --
+                inline T sample(Generator* const rng_) const noexcept override;
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+            template <typename T>
+            inline Gaussian<T>::Gaussian(const T min_, const T max_) noexcept
+              : Distribution<T>(min_, max_)
+            {
+            }
+
+
+
+            //  == METHODS ==
+            //  -- Sampling --
+            template <typename T>
+            inline T Gaussian<T>::Gaussian(Generator* const rng_) const noexcept
+            {
+                return (Distribution<T>::_min + (rng_->gen() * (Distribution<T>::_max - Distribution<T>::_min)));
+            }
+
+
+
+        } // namespace distribution
+    }     // namespace random
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RANDOM_DISTRIBUTION_GAUSSIAN_HPP
