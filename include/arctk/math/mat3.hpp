c9653be91954114d292c3d9c9b392a7a2ef5b499
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-15 23:42:18 +0100

    Marked str method as const.

--- include/arctk/math/mat3.hpp
+++ include/arctk/math/mat3.hpp
@@ -104,7 +104,7 @@ namespace arc //! arctk namespace
             constexpr inline T sum() const noexcept;
 
             //  -- Printing --
-            inline std::string str() noexcept override;
+            inline std::string str() const noexcept override;
         };
 
 
@@ -554,7 +554,7 @@ namespace arc //! arctk namespace
          *  @return Human readable string of the mat.
          */
         template <typename T>
-        inline std::string Mat<T, 3>::str() noexcept
+        inline std::string Mat<T, 3>::str() const noexcept
         {
             std::stringstream stream;
 
