0042d033d46d3ed68b2171cdda9badd81c833fe8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-16 12:10:11 +0100

    Added framework.

---
+++ include/arctk/geom.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/geom.hpp
+ *  @date   16/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of geometry classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_HPP
+#define ARCTK_GEOM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_HPP
