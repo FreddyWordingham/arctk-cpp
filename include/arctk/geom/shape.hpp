fec4b26bb3d0125c27eea40ea65dfc8adc30c59d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-16 12:35:20 +0100

    Added framework.

---
+++ include/arctk/geom/shape.hpp
@@ -0,0 +1,74 @@
+/**
+ *  @file   arctk/geom/shape.hpp
+ *  @date   16/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Geometric shape class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_SHAPE_HPP
+#define ARCTK_GEOM_SHAPE_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Geometric shape.
+         */
+        class Shape
+        {
+            //  == FIELDS ==
+          protected:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_SHAPE_HPP
