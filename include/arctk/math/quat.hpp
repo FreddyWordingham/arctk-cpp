27d5eb8e63bc7cefedd1a1b108d557e20dc40266
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-16 12:53:29 +0100

    Added framework.

---
+++ include/arctk/math/quat.hpp
@@ -0,0 +1,80 @@
+/**
+ *  @file   arctk/math/quat.hpp
+ *  @date   16/06/2018
+ *  @author Freddy Wordingham
+ *
+ *  Quaternion mathematical class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_QUAT_HPP
+#define ARCTK_MATH_QUAT_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <cmath>
+
+//  -- Arctk --
+#include <arctk/str.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Quaternion mathematical class.
+         */
+        class Quat : public str::Printable
+        {
+            //  == FIELDS ==
+          public:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          public:
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+        //  == OPERATORS ==
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_QUAT_HPP
