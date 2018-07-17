b8da79a62d4c2b7519ac33d71e6093a6dcd4562a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-17 20:17:51 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,7 @@
 #include <arctk/gui.hpp>
 #include <arctk/gui/actor.hpp>
 #include <arctk/gui/camera.hpp>
+#include <arctk/gui/camera/fly.hpp>
 #include <arctk/gui/keybind.hpp>
 #include <arctk/gui/keymap.hpp>
 #include <arctk/gui/shader.hpp>
