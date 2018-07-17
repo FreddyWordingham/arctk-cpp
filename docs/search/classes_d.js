d73fd52c1a5b79aec9006fab620cf0ae406cb1b7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-17 16:30:59 +0100

    Cleaned build files and updated documentation.

--- docs/search/classes_d.js
+++ docs/search/classes_d.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['shape',['Shape',['../classarc_1_1geom_1_1_shape.html',1,'arc::geom']]]
+  ['shape',['Shape',['../classarc_1_1geom_1_1_shape.html',1,'arc::geom']]],
+  ['sphere',['Sphere',['../classarc_1_1geom_1_1shape_1_1_sphere.html',1,'arc::geom::shape']]]
 ];
--- docs/search/files_e.js
+++ docs/search/files_e.js
@@ -3,6 +3,7 @@ var searchData=
   ['search_2ehpp',['search.hpp',['../search_8hpp.html',1,'']]],
   ['settings_2ehpp',['settings.hpp',['../settings_8hpp.html',1,'']]],
   ['shape_2ehpp',['shape.hpp',['../shape_8hpp.html',1,'']]],
+  ['sphere_2ehpp',['sphere.hpp',['../sphere_8hpp.html',1,'']]],
   ['str_2ehpp',['str.hpp',['../str_8hpp.html',1,'']]],
   ['success_2ehpp',['success.hpp',['../success_8hpp.html',1,'']]],
   ['sys_2ehpp',['sys.hpp',['../sys_8hpp.html',1,'']]]
