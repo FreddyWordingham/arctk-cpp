7b69d3d60a984e37798740c2c9dc2d856a861468
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-17 19:30:24 +0100

    Added shader_compilation_failed error.

--- include/arctk/exit/error.hpp
+++ include/arctk/exit/error.hpp
@@ -46,7 +46,8 @@ namespace arc //! arctk namespace
             constexpr const int WINDOW_POINTER_INIT_FAILED = 140; //!< Unable initialise a glfw window pointer.
             constexpr const int GLFW_INIT_FAILED           = 141; //!< Unable initialise glfw.
             constexpr const int GLEW_INIT_FAILED           = 142; //!< Unable initialise glew.
-            constexpr const int SHADER_LINKING_FAILED      = 143; //!< Shader linking stage failed.
+            constexpr const int SHADER_LINKING_FAILED      = 143; //!< Shader linking failed.
+            constexpr const int SHADER_COMPILATION_FAILED  = 143; //!< Shader compilation failed.
 
 
 
