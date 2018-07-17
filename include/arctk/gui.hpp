2637979141602e9c1aa63865ecd4b0a79f87a808
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-17 16:35:40 +0100

    Added framework.

---
+++ include/arctk/gui.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file   arctk/gui.hpp
+ *  @date   17/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of graphical-user-interface classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_HPP
+#define ARCTK_GUI_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_HPP
