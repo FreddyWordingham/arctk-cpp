68781b91cc785e30cb554d18b61b655905742d8f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-17 16:44:26 +0100

    Added window_pointer_init_failed error code.

--- include/arctk/exit/error.hpp
+++ include/arctk/exit/error.hpp
@@ -43,7 +43,8 @@ namespace arc //! arctk namespace
             constexpr const int FILE_OPEN_FAILED = 130; //!< Unable to open a file.
 
             //  -- GUI --
-            constexpr const int GLFW_INIT_FAILED = 140; //!< Unable initialise glfw.
+            constexpr const int GLFW_INIT_FAILED           = 140; //!< Unable initialise glfw.
+            constexpr const int WINDOW_POINTER_INIT_FAILED = 141; //!< Unable initialise a glfw window pointer.
 
 
 
