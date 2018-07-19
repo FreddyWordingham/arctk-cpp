0ef086c3501684c9a579890188d129424368d49e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-20 00:23:46 +0100

    Added framework.

---
+++ include/arctk/index/colours.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/index/col.hpp
+ *  @date   20/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection colour focused enumerations.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_INDEX_COL_HPP
+#define ARCTK_INDEX_COL_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace col //! colour namespace
+    {
+
+
+
+    } // namespace col
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_INDEX_COL_HPP
