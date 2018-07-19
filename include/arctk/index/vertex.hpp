89fe598bef4d00ecb26ac0e5543118673bace1f6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-20 00:32:19 +0100

    Added framework.

---
+++ include/arctk/index/vertex.hpp
@@ -0,0 +1,45 @@
+/**
+ *  @file   arctk/index/vertex.hpp
+ *  @date   20/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of vertex focused enumerations.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_INDEX_VERTEX_HPP
+#define ARCTK_INDEX_VERTEX_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace vertex //! vertex namespace
+    {
+
+
+
+        //  == Enumerations ==
+
+
+
+    } // namespace vertex
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_INDEX_VERTEX_HPP
