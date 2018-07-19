0aa1990a5e7b8f0abf88e20d05422834e5769d2b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-20 00:11:45 +0100

    Added framework.

---
+++ include/arctk/index.hpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   arctk/index.hpp
+ *  @date   20/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of index enumerations.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_INDEX_HPP
+#define ARCTK_INDEX_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_GUI
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_GUI
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_INDEX_HPP
