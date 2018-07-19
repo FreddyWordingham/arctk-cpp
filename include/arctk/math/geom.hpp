60b66bbcbc9968e908d9fe9cda8072256de05fc8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-19 23:46:39 +0100

    Added framework.

---
+++ include/arctk/math/geom.hpp
@@ -0,0 +1,59 @@
+/**
+ *  @file   arctk/math/geom.hpp
+ *  @date   19/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Math geometry functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_GEOM_HPP
+#define ARCTK_MATH_GEOM_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_CORE
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+        namespace geom //! geometry namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Area --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Area --
+
+
+
+        } // namespace geom
+    }     // namespace math
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_CORE
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_GEOM_HPP
