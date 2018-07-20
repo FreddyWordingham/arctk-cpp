89be166bda10a62e75a1db7c7084f288f66cf3ff
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-20 15:33:40 +0100

    Fixed pre-condition bugs.

--- include/arctk/gui/actor.hpp
+++ include/arctk/gui/actor.hpp
@@ -500,7 +500,7 @@ namespace arc //! arctk namespace
          */
         inline void Actor::set_int_uniform(const std::string& name_, const int int_) noexcept
         {
-            PRE(_int_uniform.find(name_) != _int_uniform.end());
+            PRE(_int_uniform.find(name_) == _int_uniform.end());
 
             _int_uniform.emplace(std::make_pair(name_, int_));
         }
@@ -515,7 +515,7 @@ namespace arc //! arctk namespace
          */
         inline void Actor::set_float_uniform(const std::string& name_, const float float_) noexcept
         {
-            PRE(_float_uniform.find(name_) != _float_uniform.end());
+            PRE(_float_uniform.find(name_) == _float_uniform.end());
 
             _float_uniform.emplace(std::make_pair(name_, float_));
         }
@@ -530,7 +530,7 @@ namespace arc //! arctk namespace
          */
         inline void Actor::set_vec2_uniform(const std::string& name_, const glm::vec2& vec2_) noexcept
         {
-            PRE(_vec2_uniform.find(name_) != _vec2_uniform.end());
+            PRE(_vec2_uniform.find(name_) == _vec2_uniform.end());
 
             _vec2_uniform.emplace(std::make_pair(name_, vec2_));
         }
@@ -560,7 +560,7 @@ namespace arc //! arctk namespace
          */
         inline void Actor::set_vec4_uniform(const std::string& name_, const glm::vec4& vec4_) noexcept
         {
-            PRE(_vec4_uniform.find(name_) != _vec4_uniform.end());
+            PRE(_vec4_uniform.find(name_) == _vec4_uniform.end());
 
             _vec4_uniform.emplace(std::make_pair(name_, vec4_));
         }
@@ -575,7 +575,7 @@ namespace arc //! arctk namespace
          */
         inline void Actor::set_mat2_uniform(const std::string& name_, const glm::mat2& mat2_) noexcept
         {
-            PRE(_mat2_uniform.find(name_) != _mat2_uniform.end());
+            PRE(_mat2_uniform.find(name_) == _mat2_uniform.end());
 
             _mat2_uniform.emplace(std::make_pair(name_, mat2_));
         }
@@ -590,7 +590,7 @@ namespace arc //! arctk namespace
          */
         inline void Actor::set_mat3_uniform(const std::string& name_, const glm::mat3& mat3_) noexcept
         {
-            PRE(_mat3_uniform.find(name_) != _mat3_uniform.end());
+            PRE(_mat3_uniform.find(name_) == _mat3_uniform.end());
 
             _mat3_uniform.emplace(std::make_pair(name_, mat3_));
         }
@@ -605,7 +605,7 @@ namespace arc //! arctk namespace
          */
         inline void Actor::set_mat4_uniform(const std::string& name_, const glm::mat4& mat4_) noexcept
         {
-            PRE(_mat4_uniform.find(name_) != _mat4_uniform.end());
+            PRE(_mat4_uniform.find(name_) == _mat4_uniform.end());
 
             _mat4_uniform.emplace(std::make_pair(name_, mat4_));
         }
