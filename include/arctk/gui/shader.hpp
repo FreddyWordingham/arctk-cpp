66abb384fb89ffb5e04f293b23cb0369f847ee9a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-21 16:44:33 +0100

    Added framework.

---
+++ include/arctk/gui/shader.hpp
@@ -0,0 +1,83 @@
+/**
+ *  @file   arctk/gui/shader.hpp
+ *  @date   21/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Graphical shader class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_SHADER_HPP
+#define ARCTK_GUI_SHADER_HPP
+
+
+
+//  == MODULE ==
+#ifdef ARCTK_MOD_GUI
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <iostream>
+#include <map>
+
+//  -- Graphical --
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/exit.hpp>
+#include <arctk/gui/uniform.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical user interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Graphical shader class used for rendering actors.
+         */
+        class Shader
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == MODULE END ==
+#endif // ARCTK_MOD_GUI
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_SHADER_HPP
