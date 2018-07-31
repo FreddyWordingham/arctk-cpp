d75e01b51a9aa92e5fdc92e0b1df1ba687b6478f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-31 23:28:47 +0100

    Removed module guards.

--- include/arctk/geom/shape/plane.hpp
+++ include/arctk/geom/shape/plane.hpp
@@ -14,11 +14,6 @@
 
 
 
-//  == MODULE ==
-#ifdef ARCTK_MOD_CORE
-
-
-
 //  == IMPORTS ==
 //  -- Std --
 #include <optional>
@@ -116,11 +111,6 @@ namespace arc //! arctk namespace
         } // namespace shape
     }     // namespace geom
 } // namespace arc
-
-
-
-//  == MODULE END ==
-#endif // ARCTK_MOD_CORE
 
 
 
