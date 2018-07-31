5a563f9b78bbe30735fb8ca02efb79096adbe358
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-31 23:27:03 +0100

    Removed module guards.

--- include/arctk/math/geom.hpp
+++ include/arctk/math/geom.hpp
@@ -14,11 +14,6 @@
 
 
 
-//  == MODULE ==
-#ifdef ARCTK_MOD_CORE
-
-
-
 //  == IMPORTS ==
 //  -- Std --
 #include <cmath>
@@ -97,11 +92,6 @@ namespace arc //! arctk namespace
         } // namespace geom
     }     // namespace math
 } // namespace arc
-
-
-
-//  == MODULE END ==
-#endif // ARCTK_MOD_CORE
 
 
 
