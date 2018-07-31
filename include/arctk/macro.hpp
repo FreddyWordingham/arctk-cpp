eb3ffb860f7fb360f492b1abeafaecc30cf08621
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-31 23:18:36 +0100

    Removed module guards.

--- include/arctk/macro.hpp
+++ include/arctk/macro.hpp
@@ -14,19 +14,9 @@
 
 
 
-//  == MODULE ==
-#ifdef ARCTK_MOD_CORE
-
-
-
 //  == IMPORTS ==
 //  -- Arctk --
 #include <arctk/macro/utility.hpp>
-
-
-
-//  == MODULE END ==
-#endif // ARCTK_MOD_CORE
 
 
 
