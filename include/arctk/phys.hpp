88628e4469b39798f89da8ea85505799052e9953
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-31 23:18:17 +0100

    Removed module guards.

--- include/arctk/phys.hpp
+++ include/arctk/phys.hpp
@@ -14,19 +14,9 @@
 
 
 
-//  == MODULE ==
-#ifdef ARCTK_MOD_CORE
-
-
-
 //  == IMPORTS ==
 //  -- Arctk --
 #include <arctk/phys/particle.hpp>
-
-
-
-//  == MODULE END ==
-#endif // ARCTK_MOD_CORE
 
 
 
