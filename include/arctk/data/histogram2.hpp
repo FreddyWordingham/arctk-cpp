5a89e49c394063d72e34ea67560f6553b967bea5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-31 23:30:38 +0100

    Removed module guards.

--- include/arctk/data/histogram2.hpp
+++ include/arctk/data/histogram2.hpp
@@ -14,11 +14,6 @@
 
 
 
-//  == MODULE ==
-#ifdef ARCTK_MOD_CORE
-
-
-
 //  == IMPORTS ==
 //  -- Std --
 #include <iomanip>
@@ -302,11 +297,6 @@ namespace arc //! arctk namespace
 
     } // namespace data
 } // namespace arc
-
-
-
-//  == MODULE END ==
-#endif // ARCTK_MOD_CORE
 
 
 
