107a307531c76ec3ec20728f249e640e9a3cf1eb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-31 23:26:24 +0100

    Removed module guards.

--- include/arctk/math/mat3.hpp
+++ include/arctk/math/mat3.hpp
@@ -14,11 +14,6 @@
 
 
 
-//  == MODULE ==
-#ifdef ARCTK_MOD_CORE
-
-
-
 //  == IMPORTS ==
 //  -- Std --
 #include <iomanip>
@@ -672,11 +667,6 @@ namespace arc //! arctk namespace
 
     } // namespace math
 } // namespace arc
-
-
-
-//  == MODULE END ==
-#endif // ARCTK_MOD_CORE
 
 
 
