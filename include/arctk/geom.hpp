e6b1a1abf4183e55590744ba48cfdee669352cc8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-07-31 23:18:51 +0100

    Removed module guards.

--- include/arctk/geom.hpp
+++ include/arctk/geom.hpp
@@ -14,22 +14,12 @@
 
 
 
-//  == MODULE ==
-#ifdef ARCTK_MOD_CORE
-
-
-
 //  == IMPORTS ==
 //  -- Arctk --
 #include <arctk/geom/shape.hpp>
 #include <arctk/geom/shape/plane.hpp>
 #include <arctk/geom/shape/sphere.hpp>
 #include <arctk/geom/shape/triangle.hpp>
-
-
-
-//  == MODULE END ==
-#endif // ARCTK_MOD_CORE
 
 
 
