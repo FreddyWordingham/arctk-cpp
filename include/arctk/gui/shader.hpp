c6d99bf60cc1c8f8a1c43ac9179dee95725e43ce
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-01 23:09:23 +0100

    Added framework.

---
+++ include/arctk/gui/shader.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file   arctk/gui/shader.hpp
+ *  @date   01/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Shader class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_SHADER_HPP
+#define ARCTK_GUI_SHADER_HPP
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Shader class.
+         */
+        class Shader
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_SHADER_HPP
