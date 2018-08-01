3950d0221d5223a0afff362b34d324c0df40107a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-01 23:39:05 +0100

    Added constructor method.

--- include/arctk/gui/shader.hpp
+++ include/arctk/gui/shader.hpp
@@ -15,6 +15,13 @@
 
 
 //  == IMPORTS ==
+//  -- Std --
+#include <map>
+#include <string>
+#include <vector>
+
+//  -- Arctk --
+#include <arctk/debug.hpp>
 
 
 
@@ -47,6 +54,8 @@ namespace arc //! arctk namespace
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
+            inline Shader(const std::string& vert_code_, const std::string& frag_code_, const std::vector<std::string>& uniform_names_) noexcept;
+            inline Shader(const std::string& vert_code_, const std::string& geom_code_, const std::string& frag_code_, const std::vector<std::string>& uniform_names_) noexcept;
 
 
             //  == METHODS ==
@@ -56,6 +65,26 @@ namespace arc //! arctk namespace
 
 
         //  == INSTANTIATION ==
+        //  -- Constructors --
+        /**
+         *  Construct a shader program from vertex and fragment sub-shader programs.
+         *
+         *  @param  vert_code_  Vertex sub-shader code.
+         *  @param  frag_code_  Fragment sub-shader code.
+         *
+         *  @pre    vert_code_ must not be empty.
+         *  @pre    frag_code_ must not be empty.
+         */
+        inline Shader::Shader(const std::string& vert_code_, const std::string& frag_code_, const std::vector<std::string>& uniform_names_) noexcept
+          : _handle(init_handle(vert_code_, frag_code_))
+          , _mvp(init_mvp())
+          , _model(init_model())
+          , _proj(init_proj())
+          , _uniform(init_uniform())
+        {
+            PRE(!vert_code_.empty());
+            PRE(!frag_code_.empty());
+        }
 
 
 
