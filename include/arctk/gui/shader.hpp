13057d0725ed5b0fcaf7dfbcd178a8f71c82268e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 01:54:22 +0100

    Added documentation to vec4 set_uniform method.

--- include/arctk/gui/shader.hpp
+++ include/arctk/gui/shader.hpp
@@ -454,6 +454,11 @@ namespace arc //! arctk namespace
             glUniform3fv(_uniforms.at(name_), 1, &val_[0]);
         }
 
+        /**
+         *  Set the value for a vec4 uniform.
+         *
+         *  @param  val_    Value to set the uniform handle to.
+         */
         inline void Shader::set_uniform(const std::string& name_, const glm::vec4& val_) noexcept
         {
             PRE(_uniforms.find(name_) != _uniforms.end());
