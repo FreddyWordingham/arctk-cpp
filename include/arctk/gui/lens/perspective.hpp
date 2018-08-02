ace48e15e924e74f9323d9e2a3725f827c62f2bb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 16:19:25 +0100

    Added framework.

---
+++ include/arctk/gui/lens/perspective.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @file   arctk/gui/lens/perspective.hpp
+ *  @date   02/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Perspective camera lens.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_LENS_PERSPECTIVE_HPP
+#define ARCTK_GUI_LENS_PERSPECTIVE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Graphical --
+#include <glm/glm.hpp>
+
+//  -- Arctk --
+#include <arctk/gui/lens.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+        namespace lens //! lens namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Perspecive lens.
+             */
+            class Perspective : public Lens
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace lens
+    }     // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_LENS_PERSPECTIVE_HPP
