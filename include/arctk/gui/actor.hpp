6c8aa5e09fefb39cc143d19e2d67681b889db660
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 02:23:55 +0100

    Added framework.

---
+++ include/arctk/gui/actor.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file   arctk/gui/actor.hpp
+ *  @date   02/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Graphical actor class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_ACTOR_HPP
+#define ARCTK_GUI_ACTOR_HPP
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Renderable actor class.
+         */
+        class Actor
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_ACTOR_HPP
