2f3997bc90ba3549032ffd94f3eea12ddc19b60e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 16:50:27 +0100

    Fixed typos.

--- include/arctk/gui/camera/fly.hpp
+++ include/arctk/gui/camera/fly.hpp
@@ -86,9 +86,9 @@ namespace arc //! arctk namespace
              */
             inline void Fly::move(const glm::vec3& vec_) noexcept
             {
-                _pos += dir_ * vec_.x;
-                _pos += glm::normalise(glm::cross(dir_, vec_)) * vec_.y;
-                _pos += up_ * vec_.z;
+                _pos += _dir * vec_.x;
+                _pos += glm::normalise(glm::cross(_dir, vec_)) * vec_.y;
+                _pos += _up * vec_.z;
 
                 update_view();
             }
@@ -101,7 +101,7 @@ namespace arc //! arctk namespace
             inline void Fly::rotate(const glm::vec3& vec_) noexcept
             {
                 _up  = glm::rotate(_up, vec_.x, _dir);
-                _up  = glm::rotate(_up, vec_.y, glm::normalise(glm::cross(dir_, up_)));
+                _up  = glm::rotate(_up, vec_.y, glm::normalise(glm::cross(_dir, _up)));
                 _dir = glm::rotate(_dir, vec_.z, _up);
 
                 update_view();
