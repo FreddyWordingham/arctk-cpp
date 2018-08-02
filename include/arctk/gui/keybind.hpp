289d8b96f469dd1817f78a8a6bc8611723e87f35
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 17:04:47 +0100

    Added framework.

---
+++ include/arctk/gui/keybind.hpp
@@ -0,0 +1,67 @@
+/**
+ *  @file   arctk/gui/keybind.hpp
+ *  @date   02/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Graphical keybinding class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_KEYBIND_HPP
+#define ARCTK_GUI_KEYBIND_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Graphical --
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Keybinding class mapping a key to a function call.
+         */
+        class Keybind
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_KEYBIND_HPP
