435a8dadca52ebf182904cde140ddb1d9d22edd2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 01:54:08 +0100

    Added documentation to vec3 set_uniform method.

--- include/arctk/gui/shader.hpp
+++ include/arctk/gui/shader.hpp
@@ -441,6 +441,11 @@ namespace arc //! arctk namespace
             glUniform2fv(_uniforms.at(name_), 1, &val_[0]);
         }
 
+        /**
+         *  Set the value for a vec3 uniform.
+         *
+         *  @param  val_    Value to set the uniform handle to.
+         */
         inline void Shader::set_uniform(const std::string& name_, const glm::vec3& val_) noexcept
         {
             PRE(_uniforms.find(name_) != _uniforms.end());
