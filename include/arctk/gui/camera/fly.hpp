a85335f167714b89a036e918b4ef2df6d8987e6b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 15:16:54 +0100

    Added framework.

---
+++ include/arctk/gui/camera/fly.hpp
@@ -0,0 +1,67 @@
+/**
+ *  @file   arctk/gui/camera/fly.hpp
+ *  @date   02/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Scene camera class with fly controls.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_CAMERA_FLY_HPP
+#define ARCTK_GUI_CAMERA_FLY_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Graphical --
+#include <glm/glm.hpp>
+
+//  -- Arctk --
+#include <arctk/camera/fly.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+        namespace camera //! camera namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Scene camera class with fly controls.
+             */
+            class Fly
+            {
+                //  == FIELDS ==
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+        } // namespace camera
+    }     // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_CAMERA_FLY_HPP
