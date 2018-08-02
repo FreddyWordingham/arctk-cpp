743da48c30e9f67989b08d1fa812f713d85c75d4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 13:00:21 +0100

    Added framework.

---
+++ include/arctk/gui/camera.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file   arctk/gui/camera.hpp
+ *  @date   02/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Scene camera class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_CAMERA_HPP
+#define ARCTK_GUI_CAMERA_HPP
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Scene camera class.
+         */
+        class Camera
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_CAMERA_HPP
