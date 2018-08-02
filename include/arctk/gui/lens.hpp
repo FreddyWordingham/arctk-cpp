45cad612c92daa1deddaf98fec3923c231768243
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-02 14:16:25 +0100

    Added lens class.

---
+++ include/arctk/gui/lens.hpp
@@ -0,0 +1,59 @@
+/**
+ *  @file   arctk/gui/lens.hpp
+ *  @date   02/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Scene lens class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_CAMERA_HPP
+#define ARCTK_GUI_CAMERA_HPP
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Scene camera lens class.
+         */
+        class Lens
+        {
+            //  == FIELDS ==
+          protected:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_CAMERA_HPP
