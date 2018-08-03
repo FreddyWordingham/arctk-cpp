37b46d2e3509fe92a810e0a48cb4224cddb75d3b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-03 21:47:00 +0100

    Added framework.

---
+++ include/arctk/gui/window.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @file   arctk/gui/window.hpp
+ *  @date   03/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Graphical window class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_WINDOW_HPP
+#define ARCTK_GUI_WINDOW_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Graphical --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Graphical window rendering class.
+         */
+        class Window
+        {
+            //  == FIELDS ==
+          protected:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_WINDOW_HPP
