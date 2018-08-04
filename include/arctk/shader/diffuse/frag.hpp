3ec14b51204a20d6521dbe7dec8c330ff4f68d72
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-04 16:12:10 +0100

    Added framework.

---
+++ include/arctk/shader/diffuse/frag.hpp
@@ -0,0 +1,34 @@
+/**
+ *  @file   arctk/shader/diffuse/frag.hpp
+ *  @date   04/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Diffuse lighting fragment subshader source code.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SHADER_DIFFUSE_FRAG_HPP
+#define ARCTK_SHADER_DIFFUSE_FRAG_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace shader //! shader namespace
+    {
+        namespace diffuse //! diffuse shader namespace
+        {
+
+
+
+        } // namespace diffuse
+    }     // namespace shader
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SHADER_DIFFUSE_FRAG_HPP
