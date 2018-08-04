66e89fc7c8f33942fe4fa3e58b2dd1315f66f77f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-04 15:37:24 +0100

    Added framework.

---
+++ include/arctk/shader.hpp
@@ -0,0 +1,23 @@
+/**
+ *  @file   arctk/shader.hpp
+ *  @date   04/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of shaders.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SHADER_HPP
+#define ARCTK_SHADER_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SHADER_HPP
