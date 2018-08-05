aa5811078eb85d001088c7b9672f5ca970325e24
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-05 18:37:30 +0100

    Added framework.

---
+++ include/arctk/gui/actor/grid.hpp
@@ -0,0 +1,58 @@
+/**
+ *  @file   arctk/gui/actor/grid.hpp
+ *  @date   05/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Grid actor class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_ACTOR_GRID_HPP
+#define ARCTK_GUI_ACTOR_GRID_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/gui/actor.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+        namespace actor //! actor namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Grid actor object.
+             */
+            class Grid : public Actor
+            {
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+        } // namespace actor
+    }     // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_ACTOR_GRID_HPP
