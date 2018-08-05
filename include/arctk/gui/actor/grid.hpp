6309c44ffc836aabdb4acc8a650b03cf0fb8deae
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-05 20:05:24 +0100

    Added init_verts method.

--- include/arctk/gui/actor/grid.hpp
+++ include/arctk/gui/actor/grid.hpp
@@ -15,6 +15,9 @@
 
 
 //  == IMPORTS ==
+//  -- Std --
+#include <cmath>
+
 //  -- Arctk --
 #include <arctk/gui/actor.hpp>
 
@@ -59,6 +62,46 @@ namespace arc //! arctk namespace
             //  -- Initialisation --
             std::vector<GLfloat> Grid::init_verts(const glm::vec2& min_, const glm::vec2& max_, const glm::vec2& cell_size_) const noexcept
             {
+                const int x_start = static_cast<int>(std::trunc(min_.x / cell_size_.x));
+                const int x_end   = static_cast<int>(std::trunc(max_.x / cell_size_.x));
+                const int y_start = static_cast<int>(std::trunc(min_.y / cell_size_.y));
+                const int y_end   = static_cast<int>(std::trunc(max_.y / cell_size_.y));
+
+                std::cout << "x_start: " << x_start << '\n';
+                std::cout << "x_end  : " << x_end << '\n';
+                std::cout << "y_start: " << y_start << '\n';
+                std::cout << "y_end  : " << y_end << '\n';
+
+                std::vector<GLfloat> verts(3 * 2 * ((x_end - x_start + 1) + (y_end - y_start + 1)));
+
+                size_t index = 0;
+                for (int i = x_start; i <= x_end; ++i)
+                {
+                    verts[index]     = i * cell_size_.x;
+                    verts[index + 1] = min_.y;
+                    verts[index + 2] = 0.0f;
+
+                    verts[index + 3] = i * cell_size_.x;
+                    verts[index + 4] = max_.y;
+                    verts[index + 5] = 0.0f;
+
+                    index += 6;
+                }
+
+                for (int i = y_start; i <= y_end; ++i)
+                {
+                    verts[index]     = min_.x;
+                    verts[index + 1] = i * cell_size_.y;
+                    verts[index + 2] = 0.0f;
+
+                    verts[index + 3] = max_.x;
+                    verts[index + 4] = i * cell_size_.y;
+                    verts[index + 5] = 0.0f;
+
+                    index += 6;
+                }
+
+                return (verts);
             }
 
 
