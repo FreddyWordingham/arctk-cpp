33a52312031668e14ff4eddd8acfb817ce4a0987
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-05 20:32:56 +0100

    Added framework.

---
+++ include/arctk/shader/ambient.hpp/frag.hpp
@@ -0,0 +1,75 @@
+/**
+ *  @file   arctk/shader/ambient/frag.hpp
+ *  @date   05/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Ambient lighting fragment subshader source code.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SHADER_AMBIENT_FRAG_HPP
+#define ARCTK_SHADER_AMBIENT_FRAG_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace shader //! shader namespace
+    {
+        namespace ambient //! ambient shader namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Source --
+            static const char* frag = R"(
+                //  == VERSION ==
+                #version 330 core
+
+
+
+                //  == SETTINGS ==
+                //  -- Light Properties --
+                uniform float light_pow   = 10.0;
+                uniform float ambient_pow = 0.1;
+                uniform vec3  sun_pos     = vec3(6.0, 8.0, 10.0);
+
+
+
+                //  == IN/OUTPUT ==
+                //  -- Input --
+                in vec4 vert_col;
+                in vec3 cam_space_norm;
+                in vec3  cam_space_light_dir;
+                in float dist;
+
+                //  -- Output --
+                out vec4 frag_col;
+
+
+
+                //  == MAIN ==
+                void main()
+                {
+                    float cos_theta = clamp(dot(cam_space_norm, cam_space_light_dir), 0.0, 1.0);
+
+                    frag_col = vert_col;
+
+                    frag_col.xyz *= (ambient_pow + ((light_pow * cos_theta) / (dist * dist)));
+                }
+            )"; //!< Diffuse fragment subshader source code.
+
+
+
+        } // namespace ambient
+    }     // namespace shader
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SHADER_AMBIENT_FRAG_HPP
