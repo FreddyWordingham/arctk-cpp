db4583d96d47f641d74a6bd037f4204b957a4058
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-05 20:35:08 +0100

    Added fragment vertex subshader source code.

--- include/arctk/shader/ambient.hpp/frag.hpp
+++ include/arctk/shader/ambient.hpp/frag.hpp
@@ -32,20 +32,9 @@ namespace arc //! arctk namespace
 
 
 
-                //  == SETTINGS ==
-                //  -- Light Properties --
-                uniform float light_pow   = 10.0;
-                uniform float ambient_pow = 0.1;
-                uniform vec3  sun_pos     = vec3(6.0, 8.0, 10.0);
-
-
-
                 //  == IN/OUTPUT ==
                 //  -- Input --
                 in vec4 vert_col;
-                in vec3 cam_space_norm;
-                in vec3  cam_space_light_dir;
-                in float dist;
 
                 //  -- Output --
                 out vec4 frag_col;
@@ -55,11 +44,7 @@ namespace arc //! arctk namespace
                 //  == MAIN ==
                 void main()
                 {
-                    float cos_theta = clamp(dot(cam_space_norm, cam_space_light_dir), 0.0, 1.0);
-
                     frag_col = vert_col;
-
-                    frag_col.xyz *= (ambient_pow + ((light_pow * cos_theta) / (dist * dist)));
                 }
             )"; //!< Diffuse fragment subshader source code.
 
