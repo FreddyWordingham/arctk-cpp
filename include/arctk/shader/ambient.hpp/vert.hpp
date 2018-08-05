825f558f6a4e55dd39a97d3e0b27631f138b2fbb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-05 20:34:25 +0100

    Added ambient vertex subshader source code.

--- include/arctk/shader/ambient.hpp/vert.hpp
+++ include/arctk/shader/ambient.hpp/vert.hpp
@@ -35,7 +35,6 @@ namespace arc //! arctk namespace
                 //  == LINKING ==
                 //  -- Layout --
                 layout(location = 0) in vec3 pos;
-                layout(location = 1) in vec3 norm;
 
 
                 //  -- Uniforms --
@@ -43,16 +42,12 @@ namespace arc //! arctk namespace
                 uniform mat4 view;
                 uniform mat4 proj;
                 uniform vec4 col;
-                uniform vec3 sun_pos;
 
 
 
                 //  == IN/OUTPUT ==
                 //  -- Output --
                 out vec4 vert_col;
-                out vec3 cam_space_norm;
-                out vec3  cam_space_light_dir;
-                out float dist;
 
 
 
@@ -62,13 +57,8 @@ namespace arc //! arctk namespace
                     gl_Position = proj * view * model * vec4(pos, 1.0);
 
                     vert_col = col;
-
-                    cam_space_light_dir = normalize((view * vec4(sun_pos, 1.0)).xyz - (view * vec4(pos, 1.0)).xyz);
-                    cam_space_norm      = normalize((view * vec4(norm, 0.0)).xyz);
-
-                    dist = length(sun_pos - pos);
                 }
-            )"; //!< Diffuse vertex subshader source code.
+            )"; //!< Ambient vertex subshader source code.
 
 
 
