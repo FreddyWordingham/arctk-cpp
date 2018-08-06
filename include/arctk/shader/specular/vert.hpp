bfa813c83280662fac875d6a05c9f93da36c6ea3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 04:44:50 +0100

    Added framework.

---
+++ include/arctk/shader/specular/vert.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/shader/specular/vert.hpp
+ *  @date   06/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Specular vertex subshader source code.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SHADER_SPECULAR_VERT_HPP
+#define ARCTK_SHADER_SPECULAR_VERT_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace shader //! shader namespace
+    {
+        namespace specular //! specular shader namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Source --
+            static const char* vert = R"(
+            )"; //!< Specular vertex subshader source code.
+
+
+
+        } // namespace specular
+    }     // namespace shader
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SHADER_SPECULAR_VERT_HPP
