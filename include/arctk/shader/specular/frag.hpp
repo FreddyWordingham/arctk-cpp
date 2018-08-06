418ff64a1f533417c274820273fb44b51ec59d46
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 04:43:38 +0100

    Added framework.

---
+++ include/arctk/shader/specular/frag.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/shader/specular/frag.hpp
+ *  @date   06/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Specular fragment subshader source code.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SHADER_SPECULAR_FRAG_HPP
+#define ARCTK_SHADER_SPECULAR_FRAG_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace shader //! shader namespace
+    {
+        namespace specular //! specular shader namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Source --
+            static const char* frag = R"(
+            )"; //!< Specular fragment subshader source code.
+
+
+
+        } // namespace specular
+    }     // namespace shader
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SHADER_SPECULAR_FRAG_HPP
