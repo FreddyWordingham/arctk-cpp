0c7e759c7c8414029414a23cf604555269239662
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 03:35:37 +0100

    Added framework.

---
+++ include/arctk/shader/normal/geom.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/shader/normal/geom.hpp
+ *  @date   06/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Normal geometry subshader source code.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SHADER_NORMAL_GEOM_HPP
+#define ARCTK_SHADER_NORMAL_GEOM_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace shader //! shader namespace
+    {
+        namespace normal //! normal shader namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Source --
+            static const char* geom = R"(
+            )"; //!< Normal geometry subshader source code.
+
+
+
+        } // namespace normal
+    }     // namespace shader
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SHADER_NORMAL_GEOM_HPP
