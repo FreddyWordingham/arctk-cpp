4299c393467fbe25e56a4c5fbc05559702142e49
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 03:37:34 +0100

    Added vertex subshader source code.

--- include/arctk/shader/normal/vert.hpp
+++ include/arctk/shader/normal/vert.hpp
@@ -27,6 +27,31 @@ namespace arc //! arctk namespace
             //  == CONSTANTS ==
             //  -- Source --
             static const char* vert = R"(
+                //  == VERSION ==
+                #version 330 core
+
+
+
+                //  == LINKING ==
+                //  -- Layout --
+                layout(location = 0) in vec3 pos;
+                layout(location = 1) in vec3 norm;
+
+
+
+                //  == IN/OUTPUT ==
+                //  -- Output --
+                out vec3 vert_pos;
+                out vec3 vert_norm;
+
+
+
+                //  == MAIN ==
+                void main()
+                {
+                    vert_pos  = pos;
+                    vert_norm = norm;
+                }
             )"; //!< Normal vertex subshader source code.
 
 
