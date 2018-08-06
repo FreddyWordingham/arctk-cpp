35ae4c9a1f4cf39d8a6a074b73c76330c3fcff96
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 01:24:12 +0100

    Added bounding box function.

--- include/arctk/gui/actor.hpp
+++ include/arctk/gui/actor.hpp
@@ -417,6 +417,7 @@ namespace arc //! arctk namespace
             inline Actor axis_helper_x(float length_ = 1.0f, float width_ = 0.01f) noexcept;
             inline Actor axis_helper_y(float length_ = 1.0f, float width_ = 0.01f) noexcept;
             inline Actor axis_helper_z(float length_ = 1.0f, float width_ = 0.01f) noexcept;
+            inline Actor bounding_box(const glm::vec3& min_, const glm::vec3& max_) noexcept;
 
 
 
@@ -652,6 +653,45 @@ namespace arc //! arctk namespace
                 helper.set_col(0.0f, 0.0f, 1.0f);
 
                 return (helper);
+            }
+
+            inline Actor bounding_box(const glm::vec3& min_, const glm::vec3& max_) noexcept
+            {
+                PRE(min_.x < max_.x);
+                PRE(min_.y < max_.y);
+                PRE(min_.z < max_.z);
+
+                std::vector<glm::vec3> verts;
+                verts.reserve(3 * 2 * 12);
+
+                verts.emplace_back(glm::vec3(min_.x, min_.y, min_.z));
+                verts.emplace_back(glm::vec3(max_.x, min_.y, min_.z));
+                verts.emplace_back(glm::vec3(min_.x, max_.y, min_.z));
+                verts.emplace_back(glm::vec3(max_.x, max_.y, min_.z));
+                verts.emplace_back(glm::vec3(min_.x, min_.y, max_.z));
+                verts.emplace_back(glm::vec3(max_.x, min_.y, max_.z));
+                verts.emplace_back(glm::vec3(min_.x, max_.y, max_.z));
+                verts.emplace_back(glm::vec3(max_.x, max_.y, max_.z));
+
+                verts.emplace_back(glm::vec3(min_.x, min_.y, min_.z));
+                verts.emplace_back(glm::vec3(min_.x, max_.y, min_.z));
+                verts.emplace_back(glm::vec3(min_.x, min_.y, max_.z));
+                verts.emplace_back(glm::vec3(min_.x, max_.y, max_.z));
+                verts.emplace_back(glm::vec3(max_.x, min_.y, min_.z));
+                verts.emplace_back(glm::vec3(max_.x, max_.y, min_.z));
+                verts.emplace_back(glm::vec3(max_.x, min_.y, max_.z));
+                verts.emplace_back(glm::vec3(max_.x, max_.y, max_.z));
+
+                verts.emplace_back(glm::vec3(min_.x, min_.y, min_.z));
+                verts.emplace_back(glm::vec3(min_.x, min_.y, max_.z));
+                verts.emplace_back(glm::vec3(max_.x, min_.y, min_.z));
+                verts.emplace_back(glm::vec3(max_.x, min_.y, max_.z));
+                verts.emplace_back(glm::vec3(min_.x, max_.y, min_.z));
+                verts.emplace_back(glm::vec3(min_.x, max_.y, max_.z));
+                verts.emplace_back(glm::vec3(max_.x, max_.y, min_.z));
+                verts.emplace_back(glm::vec3(max_.x, max_.y, max_.z));
+
+                return (Actor(verts, {1}, GL_LINES));
             }
 
 
