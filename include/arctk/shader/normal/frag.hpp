cd71aa7d7d2649ac398d29accb670072a1ff42d4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 03:35:12 +0100

    Added framework.

---
+++ include/arctk/shader/normal/frag.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/shader/normal/frag.hpp
+ *  @date   06/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Normal fragment subshader source code.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SHADER_NORMAL_FRAG_HPP
+#define ARCTK_SHADER_NORMAL_FRAG_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace shader //! shader namespace
+    {
+        namespace normal //! normal shader namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Source --
+            static const char* frag = R"(
+            )"; //!< Normal fragment subshader source code.
+
+
+
+        } // namespace normal
+    }     // namespace shader
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SHADER_NORMAL_FRAG_HPP
