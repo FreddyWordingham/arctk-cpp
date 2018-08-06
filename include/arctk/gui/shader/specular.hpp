4a705717d8be0cfad47271b8263977d387ca4eae
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 22:30:05 +0100

    Added framework.

---
+++ include/arctk/gui/shader/specular.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @file   arctk/gui/camera/orbit.hpp
+ *  @date   06/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Specular shader class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_SHADER_SPECULAR_HPP
+#define ARCTK_GUI_SHADER_SPECULAR_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+        namespace shader //! shader namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             */
+            class Specular : public Shader
+            {
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace shader
+    }     // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_SHADER_SPECULAR_HPP
