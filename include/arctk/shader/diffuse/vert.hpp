70150f1e8c4ad453331645663110c2d70a653342
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 03:22:25 +0100

    Fixed padding.

--- include/arctk/shader/diffuse/vert.hpp
+++ include/arctk/shader/diffuse/vert.hpp
@@ -37,7 +37,6 @@ namespace arc //! arctk namespace
                 layout(location = 0) in vec3 pos;
                 layout(location = 1) in vec3 norm;
 
-
                 //  -- Uniforms --
                 uniform mat4 model;
                 uniform mat4 view;
