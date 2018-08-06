aedd70f18afc7f05747962199f2de2426efbfe4b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 23:57:35 +0100

    Added special operator methods.

--- include/arctk/gui/shader.hpp
+++ include/arctk/gui/shader.hpp
@@ -62,6 +62,13 @@ namespace arc //! arctk namespace
             virtual inline ~Shader() noexcept = 0;
 
 
+            //  == OPERATORS ==
+          public:
+            //  -- Assignment --
+            inline Shader& operator=(const Shader&) = default; //!< Defaulted copy operator. @return Reference to copied object.
+            inline Shader& operator=(Shader&&) = default;      //!< Defaulted move operator. @return Reference to moved object.
+
+
             //  == INSTANTIATION ==
           public:
             //  -- Initialisation --
