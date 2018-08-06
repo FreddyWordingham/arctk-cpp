671239bb1574c9e6ec3f65d3cc80d3f6bf596adc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-06 03:36:08 +0100

    Added framework.

---
+++ include/arctk/shader/normal/vert.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/shader/normal/vert.hpp
+ *  @date   06/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Normal vertex subshader source code.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SHADER_NORMAL_VERT_HPP
+#define ARCTK_SHADER_NORMAL_VERT_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace shader //! shader namespace
+    {
+        namespace normal //! normal shader namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Source --
+            static const char* vert = R"(
+            )"; //!< Normal vertex subshader source code.
+
+
+
+        } // namespace normal
+    }     // namespace shader
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SHADER_NORMAL_VERT_HPP
