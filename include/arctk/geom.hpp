10c2270ef51bcf4818edd3552de804f7e0914056
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 14:20:21 +0100

    Linked to geom collection header file.

--- include/arctk/geom.hpp
+++ include/arctk/geom.hpp
@@ -17,6 +17,7 @@
 //  == IMPORTS ==
 //  -- Arctk --
 #include <arctk/geom/shape.hpp>
+#include <arctk/geom/shape/aabb.hpp>
 #include <arctk/geom/shape/plane.hpp>
 #include <arctk/geom/shape/sphere.hpp>
 #include <arctk/geom/shape/triangle.hpp>
