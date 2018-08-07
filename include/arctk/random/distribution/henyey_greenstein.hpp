5e678fb25fb07e57d6182b54f3605a3e71b2ba9c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 16:31:02 +0100

    Added framework.

---
+++ include/arctk/random/distribution/henyey_greenstein.hpp
@@ -0,0 +1,55 @@
+/**
+ *  @file   arctk/random/distribution/henyey_greenstein.hpp
+ *  @date   07/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Henyey-Greenstein phase function sampling.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_RANDOM_DISTRIBUTION_HENYEYGREENSTEIN_HPP
+#define ARCTK_RANDOM_DISTRIBUTION_HENYEYGREENSTEIN_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/math.hpp>
+#include <arctk/random/generator.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace random //! random number namespace
+    {
+        namespace distribution //! probability distribution namespace
+        {
+            namespace henyey_greenstein //! Henyey-Greenstein namespace
+            {
+
+
+
+                //  == FUNCTION PROTOTYPES ==
+                //  -- Distribution --
+
+
+
+                //  == FUNCTIONS ==
+                //  -- Distribution --
+
+
+
+            } // namespace henyey_greenstein
+        }     // namespace distribution
+    }         // namespace random
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_RANDOM_DISTRIBUTION_HENYEYGREENSTEIN_HPP
