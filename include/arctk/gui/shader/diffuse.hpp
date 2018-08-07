5b248f0e6257e1ca197f865933e28177f7b7e06e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 13:30:17 +0100

    Added documentation to render method.

--- include/arctk/gui/shader/diffuse.hpp
+++ include/arctk/gui/shader/diffuse.hpp
@@ -177,6 +177,11 @@ namespace arc //! arctk namespace
                 glUniformMatrix4fv(_view, 1, GL_FALSE, &cam_.view()[0][0]);
             }
 
+            /**
+             *  Render an actor using the ambient shader.
+             *
+             *  @param  act_    Actor to render.
+             */
             inline void Diffuse::render(const Actor& act_) noexcept
             {
                 glPolygonMode(GL_FRONT_AND_BACK, act_.fill_mode());
