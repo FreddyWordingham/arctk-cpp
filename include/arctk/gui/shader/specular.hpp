5df554fdcc025ebb016f0d31d96d5cd4f316b762
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 12:37:27 +0100

    Moved specular vertex subshader source code into string literal.

--- include/arctk/gui/shader/specular.hpp
+++ include/arctk/gui/shader/specular.hpp
@@ -37,9 +37,40 @@ namespace arc //! arctk namespace
 
             //  == CONSTANTS ==
             //  -- Shader Source Code --
-            constexpr const char* SPECULAR_SOURCE_VERT =
-#include <arctk/gui/shader/specular.vert>
-              ; //!< Specular vertex subshader source code.
+            constexpr const char* SPECULAR_SOURCE_VERT = R""(
+                //  == VERSION ==
+                #version 330 core
+
+
+
+                //  == LINKING ==
+                //  -- Layout --
+                layout(location = 0) in vec3 pos;
+                layout(location = 1) in vec3 norm;
+
+                //  -- Uniforms --
+                uniform mat4 model;
+                uniform mat4 view;
+                uniform mat4 proj;
+
+
+
+                //  == IN/OUTPUT ==
+                //  -- Output --
+                out vec3 vert_pos;
+                out vec3 vert_norm;
+
+
+
+                //  == MAIN ==
+                void main()
+                {
+                    vert_pos  = vec3(model * vec4(pos, 1.0));
+                    vert_norm = mat3(transpose(inverse(model))) * norm;
+
+                    gl_Position = proj * view * vec4(vert_pos, 1.0);
+                }
+            )""; //!< Specular vertex subshader source code.
             constexpr const char* SPECULAR_SOURCE_FRAG =
 #include <arctk/gui/shader/specular.frag>
               ; //!< Specular fragment subshader source code.
