cc9e322faac736917c8f4fdc03d84679a065aac8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 12:28:45 +0100

    Moved ambient vertex subshader source code into string literal.

--- include/arctk/gui/shader/ambient.hpp
+++ include/arctk/gui/shader/ambient.hpp
@@ -36,9 +36,38 @@ namespace arc //! arctk namespace
 
             //  == CONSTANTS ==
             //  -- Shader Source Code --
-            constexpr const char* AMBIENT_SOURCE_VERT =
-#include <arctk/gui/shader/ambient.vert>
-              ;  //!< Ambient vertex subshader source code.
+            constexpr const char* AMBIENT_SOURCE_VERT = R""(
+                //  == VERSION ==
+                #version 330 core
+
+
+
+                //  == LINKING ==
+                //  -- Layout --
+                layout(location = 0) in vec3 pos;
+
+                //  -- Uniforms --
+                uniform mat4 model;
+                uniform mat4 view;
+                uniform mat4 proj;
+                uniform vec4 col;
+
+
+
+                //  == IN/OUTPUT ==
+                //  -- Output --
+                out vec4 vert_col;
+
+
+
+                //  == MAIN ==
+                void main()
+                {
+                    gl_Position = proj * view * model * vec4(pos, 1.0);
+
+                    vert_col = col;
+                }
+            )""; //!< Ambient vertex subshader source code.
             constexpr const char* AMBIENT_SOURCE_FRAG = R""(
                 //  == VERSION ==
                 #version 330 core
