b0936ecd64cbbbbf3b46e6fa17a7fd1a5fa7f273
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 03:06:17 +0100

    Added init_cubemap method.

--- include/arctk/gui/shader.hpp
+++ include/arctk/gui/shader.hpp
@@ -326,6 +326,7 @@ namespace arc //! arctk namespace
                 else
                 {
                     glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
                     stbi_image_free(data);
                 }
             }
