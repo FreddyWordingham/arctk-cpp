7683bcc384391579ef103d7e7e9fecb5ef8cb2ed
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 01:02:23 +0100

    Linked to gui collection header file.

--- include/arctk/gui.hpp
+++ include/arctk/gui.hpp
@@ -24,6 +24,7 @@
 #include <arctk/gui/lens.hpp>
 #include <arctk/gui/lens/perspective.hpp>
 #include <arctk/gui/shader.hpp>
+#include <arctk/gui/shader/ambient.hpp>
 #include <arctk/gui/shader/specular.hpp>
 #include <arctk/gui/window.hpp>
 
