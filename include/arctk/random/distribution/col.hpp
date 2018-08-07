a5ab673608fda31dde3577ca01f7c50753b8ebec
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 15:43:40 +0100

    Removed class definition.

--- include/arctk/random/distribution/col.hpp
+++ include/arctk/random/distribution/col.hpp
@@ -18,7 +18,6 @@
 //  -- Arctk --
 #include <arctk/debug.hpp>
 #include <arctk/math.hpp>
-#include <arctk/random/distribution.hpp>
 #include <arctk/random/generator.hpp>
 
 
@@ -30,45 +29,6 @@ namespace arc //! arctk namespace
     {
         namespace distribution //! probability distribution namespace
         {
-
-
-
-            //  == CLASS ==
-            /**
-             *  Random colour distribution.
-             *
-             *  @tparam T   Type returned from sampling the distribution.
-             */
-            template <typename T>
-            class Col : public Distribution<T>
-            {
-                //  == INSTANTIATION ==
-              public:
-                //  -- Constructors --
-                inline Col() noexcept = default;
-
-
-                //  == METHODS ==
-              public:
-                //  -- Sampling --
-                inline T sample(Generator* rng_) const noexcept override;
-            };
-
-
-
-            //  == METHODS ==
-            //  -- Sampling --
-            template <typename T>
-            inline T Col<T>::sample(Generator* const rng_) const noexcept
-            {
-                T col;
-
-                col.r = rng_->gen();
-                col.g = rng_->gen();
-                col.b = rng_->gen();
-
-                return (col);
-            }
 
 
 
