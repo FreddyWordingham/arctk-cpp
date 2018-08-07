65e9358aed8a0a0ee88a98b416162b38fbd98dcc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 14:52:14 +0100

    Fixed typo.

--- include/arctk/gui/shader/diffuse.hpp
+++ include/arctk/gui/shader/diffuse.hpp
@@ -178,7 +178,7 @@ namespace arc //! arctk namespace
             }
 
             /**
-             *  Render an actor using the ambient shader.
+             *  Render an actor using the diffuse shader.
              *
              *  @param  act_    Actor to render.
              */
