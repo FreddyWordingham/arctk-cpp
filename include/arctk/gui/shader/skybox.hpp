75dbfb44962c510079d57a8c7502e74219bd4894
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 12:36:01 +0100

    Moved skybox vertex subshader source code into string literal.

--- include/arctk/gui/shader/skybox.hpp
+++ include/arctk/gui/shader/skybox.hpp
@@ -36,9 +36,37 @@ namespace arc //! arctk namespace
 
             //  == CONSTANTS ==
             //  -- Shader Source Code --
-            constexpr const char* SKYBOX_SOURCE_VERT =
-#include <arctk/gui/shader/skybox.vert>
-              ; //!< Skybox vertex subshader source code.
+            constexpr const char* SKYBOX_SOURCE_VERT = R""(
+                //  == VERSION ==
+                #version 330 core
+
+
+
+                //  == LINKING ==
+                //  -- Layout --
+                layout(location = 0) in vec3 pos;
+                layout(location = 1) in vec3 norm;
+
+                //  -- Uniforms --
+                uniform mat4 proj;
+                uniform mat4 view;
+
+
+
+                //  == IN/OUTPUT ==
+                //  -- Output --
+                out vec3 texture_coor;
+
+
+
+                //  == MAIN ==
+                void main()
+                {
+                    gl_Position = (proj * view * vec4(pos, 1.0)).xyww;
+
+                    texture_coor = pos;
+                }
+            )""; //!< Skybox vertex subshader source code.
             constexpr const char* SKYBOX_SOURCE_FRAG =
 #include <arctk/gui/shader/skybox.frag>
               ; //!< Skybox fragment subshader source code.
