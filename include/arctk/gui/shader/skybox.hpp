d699626210a6093d8b1a6943b7f426b845810930
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-07 02:48:43 +0100

    Added framework.

---
+++ include/arctk/gui/shader/skybox.hpp
@@ -0,0 +1,93 @@
+/**
+ *  @file   arctk/gui/shader/skybox.hpp
+ *  @date   07/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Skybox shader class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_SHADER_SKYBOX_HPP
+#define ARCTK_GUI_SHADER_SKYBOX_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Graphical --
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+//  -- Arctk --
+#include <arctk/gui/shader.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+        namespace shader //! shader namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Shader Source Code --
+            static const char* SKYBOX_SOURCE_VERT =
+#include <arctk/gui/shader/skybox.vert>
+              ; //!< Skybox vertex subshader source code.
+            static const char* SKYBOX_SOURCE_FRAG =
+#include <arctk/gui/shader/skybox.frag>
+              ; //!< Skybox fragment subshader source code.
+
+
+
+            //  == CLASS ==
+            /**
+             *  Shader drawing a skybox.
+             */
+            class Skybox : public Shader
+            {
+                //  == FIELDS ==
+              private:
+                //  -- Uniforms Handles --
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+                inline Skybox() noexcept;
+
+
+                //  == METHODS ==
+              public:
+                //  -- Rendering --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+            inline Skybox::Skybox() noexcept
+              : Shader(SKYBOX_SOURCE_VERT, SKYBOX_SOURCE_FRAG)
+            {
+            }
+
+
+
+            //  == METHODS ==
+            //  -- Rendering --
+
+
+
+        } // namespace shader
+    }     // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_SHADER_SKYBOX_HPP
