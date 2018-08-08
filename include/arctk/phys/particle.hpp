65d393351f6a3fe9e3c60791305932badd06e877
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-08 03:24:19 +0100

    Added path field.

--- include/arctk/phys/particle.hpp
+++ include/arctk/phys/particle.hpp
@@ -41,6 +41,9 @@ namespace arc //! arctk namespace
             vec3 _pos; //!< Position of the particle.
             vec3 _dir; //!< Direction of travel.
 
+            //  -- Data --
+            std::vector<vec3> _path; //!< Recorded path data.
+
 
             //  == INSTANTIATION ==
           public:
