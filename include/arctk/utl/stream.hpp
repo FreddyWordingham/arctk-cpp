7e444228901b65ad35a2458e852b98751a3de746
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-09 16:38:35 +0100

    Added rewind function.

--- include/arctk/utl/stream.hpp
+++ include/arctk/utl/stream.hpp
@@ -31,7 +31,30 @@ namespace arc //! arctk namespace
 
 
             //  == FUNCTION PROTOTYPES ==
-            //  -- Stream --
+            //  -- Manipulation --
+            template <typename T>
+            inline void rewind(T& stream_);
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Manipulation --
+            /**
+             *  Rewind a given stream.
+             *  Clear the state of the stream.
+             *  Place the get an put pointers to the start of the stream.
+             *
+             *  @tparam T   Type of stream to be rewound.
+             *
+             *  @param  stream_ Stream to be rewound.
+             */
+            template <typename T>
+            inline void rewind(T& stream_)
+            {
+                stream_.clear();
+                stream_.seekg(std::ios::beg);
+                stream_.seekp(std::ios::beg);
+            }
 
 
 
