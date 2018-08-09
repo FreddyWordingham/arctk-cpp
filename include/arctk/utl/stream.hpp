640f02c3eb0f7f922f4886b35e7b4e7a783c70dd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-09 16:34:51 +0100

    Added framework.

---
+++ include/arctk/utl/stream.hpp
@@ -0,0 +1,45 @@
+/**
+ *  @file   arctk/utl/stream.hpp
+ *  @date   09/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of stream utility functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_STREAM_HPP
+#define ARCTK_UTL_STREAM_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <sstream>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace utl //! utility namespace
+    {
+        namespace stream //! stream namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Stream --
+
+
+
+        } // namespace stream
+    }     // namespace utl
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_STREAM_HPP
