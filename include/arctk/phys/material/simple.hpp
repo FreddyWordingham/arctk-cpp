b21c55a27ba695c0caa06cb01abce677617844f1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-10 12:31:10 +0100

    Added framework.

---
+++ include/arctk/phys/material/simple.hpp
@@ -0,0 +1,70 @@
+/**
+ *  @file   arctk/phys/material/simple.hpp
+ *  @date   10/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Simple material physics class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_MATERIAL_SIMPLE_HPP
+#define ARCTK_PHYS_MATERIAL_SIMPLE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+        namespace material //! material namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Simple material class.
+             */
+            class Simple
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace material
+    }     // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_MATERIAL_SIMPLE_HPP
