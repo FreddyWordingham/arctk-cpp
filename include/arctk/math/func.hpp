8eae34506702e616e47a6eb6b110d30a77cdeedc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-11 18:40:03 +0100

    Added framework.

---
+++ include/arctk/math/func.hpp
@@ -0,0 +1,56 @@
+/**
+ *  @file   arctk/math/func.hpp
+ *  @date   11/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Math function class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_FUNC_HPP
+#define ARCTK_MATH_FUNC_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Mathematical function class.
+         *
+         *  @tparam T   Type of func domain.
+         *  @tparam S   Type returned from the func.
+         */
+        template <typename T, typename S>
+        class Func
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          public:
+            //  == METHODS ==
+          public:
+        };
+
+
+
+    } // namespace math
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_FUNC_HPP
