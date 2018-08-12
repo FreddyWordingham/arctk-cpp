7ab7888b45ceb4921e7f2ed4ba1e49879dbca1b8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-12 23:35:52 +0100

    Removed redundant else.

--- include/arctk/gui/shader.hpp
+++ include/arctk/gui/shader.hpp
@@ -325,12 +325,10 @@ namespace arc //! arctk namespace
 
                     std::exit(exit::error::TEXTURE_LOAD_FAILED);
                 }
-                else
-                {
-                    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 
-                    stbi_image_free(data);
-                }
+                glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+                stbi_image_free(data);
             }
 
             glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
