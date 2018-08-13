c3a22c824efe36739a9601e7ce02cf6240529c23
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-13 14:11:40 +0100

    Changed import path.

--- src/main.cpp
+++ src/main.cpp
@@ -75,7 +75,7 @@
 #include <arctk/math/container.hpp>
 #include <arctk/math/convert.hpp>
 #include <arctk/math/formula.hpp>
-#include <arctk/math/func/polynomial.hpp>
+#include <arctk/math/formula/polynomial.hpp>
 #include <arctk/math/geom.hpp>
 #include <arctk/math/mat.hpp>
 #include <arctk/math/mat2.hpp>
