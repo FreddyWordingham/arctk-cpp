0262dc684846dc31cb62a918ec62da42ec14ac46
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-15 17:34:39 +0100

    Added missing inline keyword.

--- include/arctk/math/formula/linear.hpp
+++ include/arctk/math/formula/linear.hpp
@@ -61,7 +61,7 @@ namespace arc //! arctk namespace
                 //  == OPERATORS ==
               public:
                 //  -- Call --
-                double operator()(double val_) noexcept override;
+                inline double operator()(double val_) noexcept override;
             };
 
 
