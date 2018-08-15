2fc6e475214786c294f4da27cb6a043d7724f863
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-15 17:27:50 +0100

    Added constructor method.

--- include/arctk/math/formula/constant.hpp
+++ include/arctk/math/formula/constant.hpp
@@ -64,6 +64,27 @@ namespace arc //! arctk namespace
 
             //  == INSTANTIATION ==
             //  -- Constructors --
+            /**
+             *  Construct a constant interpolation function.
+             *
+             *  @param  xs_ Vector of x-positions of the nodes.
+             *  @param  ys_ Vector of y-positions of the nodes.
+             *
+             *  @pre    xs_ must contain multiple values.
+             *  @pre    ys_ must contain multiple values.
+             *  @pre    xs_ size must equal the size of ys_.
+             *  @pre    xs_ must be sorted in ascending order.
+             */
+            inline Constant::Constant(const std::vector<double>& xs_, const std::vector<double>& ys_) noexcept
+              : Formula(xs_.front(), xs_.back())
+              , _xs(xs_)
+              , _ys(ys_)
+            {
+                PRE(xs_.size() > 1);
+                PRE(ys_.size() > 1);
+                PRE(xs_.size() == ys_.size());
+                PRE(utl::properties::ascending(xs_));
+            }
 
 
 
