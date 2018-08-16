7aeb36440995f4d8d209cdf43a0c258a68f2cf90
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-16 20:19:40 +0100

    Added wavelength colourmapping.

--- include/arctk/gui/shader/ray.hpp
+++ include/arctk/gui/shader/ray.hpp
@@ -63,19 +63,69 @@ namespace arc //! arctk namespace
 
 
 
+                //  == FUNCTION PROTOTYPES ==
+                vec4 colourmap(const float t_x);
+                float colourmap_red(const float t_x);
+                float colourmap_green(const float t_x);
+                float colourmap_blue(const float t_x);
+
+
+
                 //  == MAIN ==
                 void main()
                 {
                     gl_Position = proj * view * model * vec4(pos, 1.0);
 
-                    if (wavelength < 600e-9)
+                    vert_col = colourmap((wavelength - 400E-9) / 300E-9);
+                }
+
+
+
+                //  == FUNCTIONS ==
+                vec4 colourmap(const float t_x)
+                {
+                    // Check if t_x is outside limits.
+                    if ((t_x < 0.0) || (t_x > 1.0))
                     {
-                        vert_col = vec4(1.0, 0.0, 0.0, 1.0);
+                        return (vec4(1.0, 0.0, 1.0, 1.0));
                     }
-                    else
+
+                    // Determine red, green blue values.
+                    float red = clamp(colourmap_red(t_x), 0.0, 1.0);
+                    float green = clamp(colourmap_green(t_x), 0.0, 1.0);
+                    float blue = clamp(colourmap_blue(t_x), 0.0, 1.0);
+
+                    return vec4(red, green, blue, 1.0);
+                }
+
+                float colourmap_red(const float t_x)
+                {
+                    if (t_x < 0.7)
                     {
-                        vert_col = vec4(0.0, 0.0, 1.0, 1.0);
+                        return ((4.0 * t_x) - 1.5);
                     }
+
+                    return ((-4.0 * t_x) + 4.5);
+                }
+
+                float colourmap_green(const float t_x)
+                {
+                    if (t_x < 0.5)
+                    {
+                        return ((4.0 * t_x) - 0.5);
+                    }
+
+                    return ((-4.0 * t_x) + 3.5);
+                }
+
+                float colourmap_blue(const float t_x)
+                {
+                    if (t_x < 0.3)
+                    {
+                    return ((4.0 * t_x) + 0.5);
+                    }
+
+                    return ((-4.0 * t_x) + 2.5);
                 }
             )""; //!< Ray vertex subshader source code.
             constexpr const char* RAY_SOURCE_FRAG = R""(
