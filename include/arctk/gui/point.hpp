53de81211c06ce2d89566e83fe4ee82730dae57a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-16 19:59:46 +0100

    Added framework.

---
+++ include/arctk/gui/point.hpp
@@ -0,0 +1,52 @@
+/**
+ *  @file   arctk/gui/point.hpp
+ *  @date   16/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Data point class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GUI_POINT_HPP
+#define ARCTK_GUI_POINT_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Graphical --
+#include <glm/glm.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Data point class.
+         */
+        class Point
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+        };
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GUI_POINT_HPP
