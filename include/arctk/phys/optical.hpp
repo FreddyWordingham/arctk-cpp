f3598e655356fadcab5a26978c61b1aaf877bd31
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-16 16:17:15 +0100

    Added framework.

---
+++ include/arctk/phys/optical.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @file   arctk/phys/optical.hpp
+ *  @date   16/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Optical physics class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_OPTICAL_HPP
+#define ARCTK_PHYS_OPTICAL_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Physical particle class.
+         */
+        class Optical
+        {
+            //  == FIELDS ==
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_OPTICAL_HPP
