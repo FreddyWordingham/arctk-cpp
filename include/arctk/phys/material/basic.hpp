d06fc8714d3e41c3db5dc18145aac8ae590e0c5e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-16 16:37:28 +0100

    Added framework.

---
+++ include/arctk/phys/material/basic.hpp
@@ -0,0 +1,78 @@
+/**
+ *  @file   arctk/phys/material/basic.hpp
+ *  @date   16/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  basic material physics class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_MATERIAL_BASIC_HPP
+#define ARCTK_PHYS_MATERIAL_BASIC_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <vector>
+
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/math.hpp>
+#include <arctk/phys/material.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+        namespace material //! material namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Basic material particle class.
+             */
+            class Basic : public Material
+            {
+                //  == FIELDS ==
+              private:
+                //  -- Optical --
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+            //  -- Getters --
+
+
+
+        } // namespace material
+    }     // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_MATERIAL_BASIC_HPP
