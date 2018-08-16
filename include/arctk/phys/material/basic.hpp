7207dd0566577c2146f5bb3e9f3d87491e5b7825
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-16 17:02:28 +0100

    Added init_interact_coef method.

--- include/arctk/phys/material/basic.hpp
+++ include/arctk/phys/material/basic.hpp
@@ -53,7 +53,12 @@ namespace arc //! arctk namespace
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-                inline Basic(std::vector<double>& wavelength_, std::vector<double>& ref_index_, std::vector<double>& scat_coef_, std::vector<double>& abs_coef, std::vector<double>& asym_) noexcept;
+                inline Basic(std::vector<double>& wavelength_, std::vector<double>& ref_index_, std::vector<double>& scat_coef_, std::vector<double>& abs_coef_, std::vector<double>& asym_) noexcept;
+
+              private:
+                //  -- Initialisation --
+                inline math::formula::Linear init_interact_coef(std::vector<double>& wavelength_, std::vector<double>& scat_coef_, std::vector<double>& abs_coef_) const noexcept;
+                inline math::formula::Linear init_albedo(std::vector<double>& wavelength_, std::vector<double>& scat_coef_, std::vector<double>& abs_coef_) const noexcept;
 
 
                 //  == METHODS ==
@@ -65,23 +70,44 @@ namespace arc //! arctk namespace
 
             //  == INSTANTIATION ==
             //  -- Constructors --
-            inline Basic::Basic(std::vector<double>& wavelength_, std::vector<double>& ref_index_, std::vector<double>& scat_coef_, std::vector<double>& abs_coef, std::vector<double>& asym_) noexcept
+            inline Basic::Basic(std::vector<double>& wavelength_, std::vector<double>& ref_index_, std::vector<double>& scat_coef_, std::vector<double>& abs_coef_, std::vector<double>& asym_) noexcept
               : _Material(wavelength_.front(), wavelength_.back())
               , _ref_index(math::formula::Linear(wavelength_, ref_index_))
-              , _interact_coef(init_coef(wavelength_, scat_coef_, abs_coef))
-              , _albedo(init_albedo(wavelength_, scat_coef_, abs_coef))
+              , _interact_coef(init_interact_coef(wavelength_, scat_coef_, abs_coef_))
+              , _albedo(init_albedo(wavelength_, scat_coef_, abs_coef_))
               , _asym(math::formula::Linear(wavelength_, asym_))
             {
                 PRE(wavelength_.size() > 1);
                 PRE(ref_index_.size() > 1);
                 PRE(scat_coef_.size() > 1);
-                PRE(abs_coef.size() > 1);
+                PRE(abs_coef_.size() > 1);
                 PRE(asym_.size() > 1);
                 PRE(wavelength_.size() == ref_index_.size());
                 PRE(wavelength_.size() == scat_coef_.size());
-                PRE(wavelength_.size() == abs_coef.size());
+                PRE(wavelength_.size() == abs_coef_.size());
                 PRE(wavelength_.size() == asym_.size());
                 PRE(utl::properties::ascending(wavelength_));
+            }
+
+
+            //  -- Initialisation --
+            inline math::formula::Linear Basic::init_interact_coef(std::vector<double>& wavelength_, std::vector<double>& scat_coef_, std::vector<double>& abs_coef_) const noexcept
+            {
+                PRE(wavelength_.size() > 1);
+                PRE(scat_coef_.size() > 1);
+                PRE(abs_coef_.size() > 1);
+                PRE(wavelength_.size() == scat_coef_.size());
+                PRE(wavelength_.size() == abs_coef_.size());
+                PRE(utl::properties::ascending(wavelength_));
+
+                std::vector<double> interact_coef(scat_coef_.size());
+
+                for (size_t i = 0; i < interact_coef.size(); ++i)
+                {
+                    interact_coef[i] = scat_coef_[i] + abs_coef_[i];
+                }
+
+                return (math::formula::Linear(wavelength_, interact_coef));
             }
 
 
