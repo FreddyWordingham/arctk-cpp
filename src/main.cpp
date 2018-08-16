5aba2262fef5a311995d73697171dac9e15c6df7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-16 16:14:49 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -92,6 +92,7 @@
 #include <arctk/phys/material.hpp>
 #include <arctk/phys/material/basic.hpp>
 #include <arctk/phys/material/simple.hpp>
+#include <arctk/phys/optical.hpp>
 #include <arctk/phys/particle.hpp>
 #include <arctk/phys/particle/photon.hpp>
 #include <arctk/random.hpp>
