4ff0e992a0114722bda845046888c69394c2ecc8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-17 12:06:40 +0100

    Added constructor method.

--- include/arctk/phys/material/fluorescent.hpp
+++ include/arctk/phys/material/fluorescent.hpp
@@ -56,9 +56,14 @@ namespace arc //! arctk namespace
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
+                inline Fluorescent(const std::vector<double>& wavelength_, const std::vector<double>& ref_index_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_, const std::vector<double>& asym_,
+                                   const std::vector<double>& flu_coef_, const std::vector<double>& delta_wavelength_) noexcept;
 
               private:
                 //  -- Initialisation --
+                inline math::formula::Linear init_interact_coef(const std::vector<double>& wavelength_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_, const std::vector<double>& flu_coef_) const noexcept;
+                inline math::formula::Linear init_albedo(const std::vector<double>& wavelength_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_, const std::vector<double>& flu_coef_) const noexcept;
+                inline math::formula::Linear init_flu_prob(const std::vector<double>& wavelength_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_, const std::vector<double>& flu_coef_) const noexcept;
 
 
                 //  == METHODS ==
@@ -75,13 +80,51 @@ namespace arc //! arctk namespace
 
             //  == INSTANTIATION ==
             //  -- Constructors --
-            inline Flourescent::Flourescent() noexcept
+            inline Flourescent::Flourescent(const std::vector<double>& wavelength_, const std::vector<double>& ref_index_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_, const std::vector<double>& asym_,
+                                            const std::vector<double>& flu_coef_, const std::vector<double>& delta_wavelength_) noexcept
               : Material(wavelength_.front(), wavelength_.back())
+              , _ref_index(math::formula::Linear(wavelength_, ref_index_))
+              , _interact_coef(init_interact_coef(wavelength_, scat_coef_, abs_coef_, flu_coef_))
+              , _albedo(init_albedo(wavelength_, scat_coef_, abs_coef_, flu_coef_))
+              , _asym(math::formula::Linear(wavelength_, asym_))
+              , _flu_prob(init_albedo(wavelength_, scat_coef_, abs_coef_, flu_coef_))
+              , _delta_wavelength(math::formula::Linear(wavelength_, delta_wavelength_))
             {
+                PRE(wavelength_.size() > 1);
+                PRE(ref_index_.size() > 1);
+                PRE(scat_coef_.size() > 1);
+                PRE(abs_coef_.size() > 1);
+                PRE(asym_.size() > 1);
+                PRE(flu_coef_.size() > 1);
+                PRE(delta_wavelength_.size() > 1);
+                PRE(wavelength_.size() == ref_index_.size());
+                PRE(wavelength_.size() == scat_coef_.size());
+                PRE(wavelength_.size() == abs_coef_.size());
+                PRE(wavelength_.size() == asym_.size());
+                PRE(wavelength_.size() == flu_coef_.size());
+                PRE(wavelength_.size() == delta_wavelength_.size());
+                PRE(utl::properties::ascending(wavelength_));
+                PRE(utl::properties::always_greater_than_or_equal_to(wavelength_, 0.0));
+                PRE(utl::properties::always_greater_than_or_equal_to(scat_coef_, 0.0));
+                PRE(utl::properties::always_greater_than_or_equal_to(abs_coef_, 0.0));
+                PRE(utl::properties::always_greater_than_or_equal_to(asym_, -1.0));
+                PRE(utl::properties::always_less_than_or_equal_to(asym_, 1.0));
+                PRE(utl::properties::always_greater_than_or_equal_to(flu_coef_, 0.0));
             }
 
 
             //  -- Initialisation --
+            inline math::formula::Linear Fluorescent::init_interact_coef(const std::vector<double>& wavelength_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_, const std::vector<double>& flu_coef_) const noexcept
+            {
+            }
+
+            inline math::formula::Linear Fluorescent::init_albedo(const std::vector<double>& wavelength_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_, const std::vector<double>& flu_coef_) const noexcept
+            {
+            }
+
+            inline math::formula::Linear Fluorescent::init_flu_prob(const std::vector<double>& wavelength_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_, const std::vector<double>& flu_coef_) const noexcept
+            {
+            }
 
 
 
