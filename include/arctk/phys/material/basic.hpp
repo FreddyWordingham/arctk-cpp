521cdda1e01cfadbc566622058cf87a4eedb1ddd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-17 15:07:19 +0100

    Added documentation to init_albedo method.

--- include/arctk/phys/material/basic.hpp
+++ include/arctk/phys/material/basic.hpp
@@ -173,6 +173,29 @@ namespace arc //! arctk namespace
                 return (math::formula::Linear(wavelength_, interact_coef));
             }
 
+            /**
+             *  Initialise the singled scattering albedo formula.
+             *  Interaction coefficient values are calculated from the sum of coefficient values.
+             *
+             *  @param  wavelength_ Vector of wavelengths.
+             *  @param  scat_coef_  Vector of corresponding scattering coefficients.
+             *  @param  abs_coef_   Vector of corresponding absorption coefficients.
+             *
+             *  @pre    wavelength_ must contain multiple values.
+             *  @pre    scat_coef_ must contain multiple values.
+             *  @pre    abs_coef_ must contain multiple values.
+             *  @pre    wavelength_ size must match scat_coef_ size.
+             *  @pre    wavelength_ size must match abs_coef_ size.
+             *  @pre    wavelength_ values must be in ascending order.
+             *  @pre    wavelength_ values must all be non-negative.
+             *  @pre    scat_coef_ values must all be non-negative.
+             *  @pre    abs_coef_ values must all be non-negative.
+             *
+             *  @post   albedo values must all be non-negative.
+             *  @post   albedo values must all be less than, or equal to, unity.
+             *
+             *  @return Singled scattering albedo formula.
+             */
             inline math::formula::Linear Basic::init_albedo(const std::vector<double>& wavelength_, const std::vector<double>& scat_coef_, const std::vector<double>& abs_coef_) const noexcept
             {
                 PRE(wavelength_.size() > 1);
