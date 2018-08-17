0be1289ac8fc6f0977b77db027c7dc2c77511af5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-17 15:14:18 +0100

    Added documentation to interact_coef field.

--- include/arctk/phys/material/fluorescent.hpp
+++ include/arctk/phys/material/fluorescent.hpp
@@ -45,8 +45,8 @@ namespace arc //! arctk namespace
                 //  == FIELDS ==
               private:
                 //  -- Optical --
-                const math::formula::Linear _ref_index; //!< Refractive index as a function of wavelength.
-                const math::formula::Linear _interact_coef;
+                const math::formula::Linear _ref_index;     //!< Refractive index as a function of wavelength.
+                const math::formula::Linear _interact_coef; //!< Interaction coefficient albedo as a function of wavelength.
                 const math::formula::Linear _albedo;
                 const math::formula::Linear _asym;
                 const math::formula::Linear _flu_prob;
