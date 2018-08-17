883b9641f18032bb42e7ee9e79f1e3e4f9ad74b2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-17 15:14:58 +0100

    Added documentation to flu_prob field.

--- include/arctk/phys/material/fluorescent.hpp
+++ include/arctk/phys/material/fluorescent.hpp
@@ -49,7 +49,7 @@ namespace arc //! arctk namespace
                 const math::formula::Linear _interact_coef; //!< Interaction coefficient albedo as a function of wavelength.
                 const math::formula::Linear _albedo;        //!< Single scattering albedo as a function of wavelength.
                 const math::formula::Linear _asym;          //!< Asymmetry parameter as a function of wavelength.
-                const math::formula::Linear _flu_prob;
+                const math::formula::Linear _flu_prob;      //!< Fluorescence probability as a function of wavelength.
                 const math::formula::Linear _delta_wavelength;
 
 
