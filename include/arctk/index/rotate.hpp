71b2c5853cbc21ccc4eb06aa133ed5b7ecce64ab
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-20 15:40:16 +0100

    Added framework.

---
+++ include/arctk/index/rotate.hpp
@@ -0,0 +1,44 @@
+/**
+ *  @file   arctk/index/rotate.hpp
+ *  @date   20/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Index rotation functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_INDEX_ROTATE_HPP
+#define ARCTK_INDEX_ROTATE_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace index //! indexing namespace
+    {
+        namespace rotate //! rotation namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Rotate --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Rotate --
+
+
+
+        } // namespace rotate
+    }     // namespace index
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_INDEX_ROTATE_HPP
