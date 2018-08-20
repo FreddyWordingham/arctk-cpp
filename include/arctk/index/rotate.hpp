0ae95b94e0bf83da6120c7b5b9077c49e4a12895
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-20 16:13:37 +0100

    Added prev function.

--- include/arctk/index/rotate.hpp
+++ include/arctk/index/rotate.hpp
@@ -27,6 +27,7 @@ namespace arc //! arctk namespace
             //  == FUNCTION PROTOTYPES ==
             //  -- Rotate --
             inline size_t next(const size_t cur_, const size_t size_, const size_t step_ = 1) noexcept;
+            inline size_t prev(const size_t cur_, const size_t size_, const size_t step_ = 1) noexcept;
 
 
 
@@ -39,6 +40,13 @@ namespace arc //! arctk namespace
                 const size_t next = cur_ + step_;
 
                 return ((next < size_) ? next : (next - size_));
+            }
+
+            inline size_t prev(const size_t cur_, const size_t size_, const size_t step_) noexcept
+            {
+                PRE(step_ < size_);
+
+                return (step_ <= cur_) ? (cur_ - step_) : (size_ + cur_ - step_));
             }
 
 
