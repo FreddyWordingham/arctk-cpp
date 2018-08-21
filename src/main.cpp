bc681d47878451ffdc1544376c6b75576b9290d1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-22 00:10:57 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,7 @@
 #include <arctk/geom.hpp>
 #include <arctk/geom/shape.hpp>
 #include <arctk/geom/shape/aabb.hpp>
+#include <arctk/geom/shape/circle.hpp>
 #include <arctk/geom/shape/mesh.hpp>
 #include <arctk/geom/shape/plane.hpp>
 #include <arctk/geom/shape/sphere.hpp>
