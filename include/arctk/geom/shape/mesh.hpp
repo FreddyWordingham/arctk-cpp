67056ac98d6e76231421bf26d5b94878e90a537b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-21 02:12:56 +0100

    Fixed typos.

--- include/arctk/geom/shape/mesh.hpp
+++ include/arctk/geom/shape/mesh.hpp
@@ -72,7 +72,7 @@ namespace arc //! arctk namespace
 
               private:
                 //  -- Initialisation --
-                inline std::vector<Triangle> init_tris() const noexcept;
+                inline std::vector<Triangle> init_tris(const std::string& serial_, const mat4& transform_) const noexcept;
                 inline std::vector<double>   init_areas() const noexcept;
 
 
