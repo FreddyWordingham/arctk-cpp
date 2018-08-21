4f2641c95098fcdb4a9862038a506d450da2fdd3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-21 17:34:53 +0100

    Added constructor method.

--- include/arctk/data/image/colour.hpp
+++ include/arctk/data/image/colour.hpp
@@ -88,14 +88,14 @@ namespace arc //! arctk namespace
             }
 
             /**
-             *  Construct a greyscale image from pixel of values.
+             *  Construct a colour image from pixel of values.
              *
              *  @param  pixels_ Two-dimensional vector of pixel values.
              *
              *  @pre    pixels_ may not be empty.
              *  @pre    pixels_ front element may not be empty.
              */
-            inline Colour::Colour(const std::vector<std::vector<double>>& pixels_) noexcept
+            inline Colour::Colour(const std::vector<std::vector<arc::vec3>>& pixels_) noexcept
               : Image(pixels_.size(), pixels_.empty() ? 0 : pixels_.front().size())
               , _pixels(pixels_)
             {
