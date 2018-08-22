0b40f2ce04c410b92efadf3516ba748cbbc96bde
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-22 02:00:55 +0100

    Removed redundant const qualification in method decleration.

--- include/arctk/geom/shape/aabb.hpp
+++ include/arctk/geom/shape/aabb.hpp
@@ -70,7 +70,7 @@ namespace arc //! arctk namespace
                 inline const vec3& max() const noexcept;
 
                 //  -- Emission --
-                inline vec3 random_pos(random::Generator* const rng_) const noexcept override;
+                inline vec3 random_pos(random::Generator* rng_) const noexcept override;
 
                 //  -- Collision --
                 inline std::optional<double>                  collision(const vec3& pos_, const vec3& dir_) const noexcept override;
