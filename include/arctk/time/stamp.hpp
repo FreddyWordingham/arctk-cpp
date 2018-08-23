9f9f8430d48878289acc8d974bfd4660320099f4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 00:47:41 +0100

    Added framework.

---
+++ include/arctk/time/stamp.hpp
@@ -0,0 +1,52 @@
+/**
+ *  @file   arctk/time/stamp.hpp
+ *  @date   24/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Time stamp class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_TIME_STAMP_HPP
+#define ARCTK_TIME_STAMP_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace gui //! graphical-user-interface namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Time stamp class.
+         */
+        class Stamp
+        {
+            //  == FIELDS ==
+          public:
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+    } // namespace gui
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_TIME_STAMP_HPP
