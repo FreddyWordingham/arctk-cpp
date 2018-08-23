6117b640568bdc599a4004880f956f15968a81f6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 00:42:42 +0100

    Added framework.

---
+++ include/arctk/time.hpp
@@ -0,0 +1,22 @@
+/**
+ *  @file   arctk/time.hpp
+ *  @date   24/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of time classes and functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_TIME_HPP
+#define ARCTK_TIME_HPP
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_TIME_HPP
