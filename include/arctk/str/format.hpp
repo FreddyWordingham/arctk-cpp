3a0e2a29ec8f4ac11a18bdfc1a4cd8f93fef1dce
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 00:26:33 +0100

    Added framework.

---
+++ include/arctk/str/format.hpp
@@ -0,0 +1,50 @@
+/**
+ *  @file   arctk/str/format.hpp
+ *  @date   24/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of string formatting functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_STR_FORMAT_HPP
+#define ARCTK_STR_FORMAT_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace str //! string namespace
+    {
+        namespace format //! formatting namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Time --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Time --
+
+
+
+        } // namespace format
+    }     // namespace str
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_STR_FORMAT_HPP
