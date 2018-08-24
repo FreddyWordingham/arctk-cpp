1ef370a848026bd39904baed43e14b906eb24bcc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 10:12:50 +0100

    Added counts field.

--- include/arctk/sys/balancer.hpp
+++ include/arctk/sys/balancer.hpp
@@ -38,6 +38,10 @@ namespace arc //! arctk namespace
         {
             //  == FIELDS ==
           private:
+            //  -- Counts --
+
+            std::vector<unsigned long int> _counts; //!< Count of loops made by each thread.
+
             //  -- Timing --
             const unsigned int                                       _update_delta; //!< Minimum number of milliseconds required to pass before balancing.
             const std::chrono::time_point<std::chrono::system_clock> _start_time;   //!< Time of construction.
