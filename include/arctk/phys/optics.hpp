231f0a63e0d9e644c9d0614406f25de8c216ba0c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 20:36:26 +0100

    Added reflection_prob function.

--- include/arctk/phys/optics.hpp
+++ include/arctk/phys/optics.hpp
@@ -36,17 +36,25 @@ namespace arc //! arctk namespace
 
             //  == FUNCTION PROTOTYPES ==
             //  -- Reflection and Refraction --
-            inline double reflection_prob(double incident_ang_, double ref_index_in_, double ref_index_out_) noexcept;
+            inline double reflection_prob(double ang_in_, double ref_index_in_, double ref_index_out_) noexcept;
 
 
 
             //  == FUNCTIONS ==
             //  -- Reflection and Refraction --
-            inline double reflection_prob(const double incident_ang_, const double ref_index_in_, const double ref_index_out_) noexcept
+            inline double reflection_prob(const double ang_in_, const double ref_index_in_, const double ref_index_out_) noexcept
             {
-                PRE((incident_ang_ >= 0.0) && (incident_ang_ < consts::math::HALF_PI));
+                PRE((ang_in_ >= 0.0) && (ang_in_ < consts::math::HALF_PI));
                 PRE(ref_index_in_ >= 1.0);
                 PRE(ref_index_out_ >= 1.0);
+
+                const double cos_ang_in  = std::cos(ang_in);
+                const double cos_ang_out = std::cos(std::asin(std::sin(ang_in_) * (ref_index_in_ / ref_index_out_)));
+
+                const double ref_prob_s = math::pow::sq(((ref_index_in_ * cos_ang_in) - (ref_index_out_ * cos_ang_out)) / ((ref_index_in_ * cos_ang_in) + (ref_index_out_ * cos_ang_out)));
+                const double ref_prob_p = math::pow::sq(((ref_index_in_ * cos_ang_out) - (ref_index_out_ * cos_ang_in)) / ((ref_index_in_ * cos_ang_out) + (ref_index_out_ * cos_ang_in)));
+
+                return (0.5 * (ref_prob_s + ref_prob_p));
             }
 
 
