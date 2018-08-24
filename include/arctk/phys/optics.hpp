cd9465faff612bd2f2fd9d7f2ae6f78dc92c905c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 20:19:24 +0100

    Added import.

--- include/arctk/phys/optics.hpp
+++ include/arctk/phys/optics.hpp
@@ -19,6 +19,7 @@
 #include <cmath>
 
 //  -- Arctk --
+#include <arctk/debug.hpp>
 #include <arctk/math.hpp>
 
 
@@ -35,6 +36,18 @@ namespace arc //! arctk namespace
 
             //  == FUNCTION PROTOTYPES ==
             //  -- Reflection and Refraction --
+            inline double reflection_prob(double incident_ang_, double ref_index_in_, double ref_index_out_) noexcept;
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Reflection and Refraction --
+            inline double reflection_prob(const double incident_ang_, const double ref_index_in_, const double ref_index_out_) noexcept
+            {
+                PRE((incident_ang_ >= 0.0) && (incident_ang_ < consts::math::HALF_PI));
+                PRE(ref_index_in_ >= 1.0);
+                PRE(ref_index_out_ >= 1.0);
+            }
 
 
 
