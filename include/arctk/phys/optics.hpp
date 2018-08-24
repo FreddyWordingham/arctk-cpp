588dbdd376859793eba0706dbea869b41523151f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 20:09:24 +0100

    Added framework.

---
+++ include/arctk/phys/optics.hpp
@@ -0,0 +1,39 @@
+/**
+ *  @file   arctk/phys/optics.hpp
+ *  @date   24/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Optical physics class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_OPTICS_HPP
+#define ARCTK_PHYS_OPTICS_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+        namespace optics //! optical namespace
+        {
+
+
+
+        } // namespace optics
+    }     // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_OPTICS_HPP
