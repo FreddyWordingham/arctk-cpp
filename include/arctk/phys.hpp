0662d22774a93b6eee819a2f6a357e0e57a7a949
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 20:03:14 +0100

    Linked to phys collection header file.

--- include/arctk/phys.hpp
+++ include/arctk/phys.hpp
@@ -20,6 +20,7 @@
 #include <arctk/phys/material/basic.hpp>
 #include <arctk/phys/material/fluorescent.hpp>
 #include <arctk/phys/optical.hpp>
+#include <arctk/phys/optics.hpp>
 #include <arctk/phys/particle.hpp>
 #include <arctk/phys/particle/photon.hpp>
 
