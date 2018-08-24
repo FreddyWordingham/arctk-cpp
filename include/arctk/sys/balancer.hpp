4837ea4e0c8e185fab507634d98af823a06a900c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 10:14:03 +0100

    Added finished field.

--- include/arctk/sys/balancer.hpp
+++ include/arctk/sys/balancer.hpp
@@ -39,8 +39,8 @@ namespace arc //! arctk namespace
             //  == FIELDS ==
           private:
             //  -- Counts --
-
-            std::vector<unsigned long int> _counts; //!< Count of loops made by each thread.
+            bool                           _finished; //!< False until balancer is finished with.
+            std::vector<unsigned long int> _counts;   //!< Count of loops made by each thread.
 
             //  -- Timing --
             const unsigned int                                       _update_delta; //!< Minimum number of milliseconds required to pass before balancing.
