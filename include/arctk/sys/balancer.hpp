e0203d245c02d4eccfc01e6ad7561de8bcbb2ef9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-24 01:06:32 +0100

    Added framework.

---
+++ include/arctk/sys/balancer.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @file   arctk/sys/balancer.hpp
+ *  @date   24/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Thread balancer class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SYS_BALANCE_HPP
+#define ARCTK_SYS_BALANCE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace sys //! system namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Thread balancing class.
+         */
+        class Balancer
+        {
+            //  == FIELDS ==
+          private:
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+            //  -- Initialisation --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION --
+        //  -- Constructors --
+
+        //  -- Initialisation --
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace sys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SYS_BALANCE_HPP
