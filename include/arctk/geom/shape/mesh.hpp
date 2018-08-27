b803c843b1fb3022181ca054cc6e944fc6a98e96
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-27 03:31:05 +0100

    Added documentation to random_pos_and_norm method.

--- include/arctk/geom/shape/mesh.hpp
+++ include/arctk/geom/shape/mesh.hpp
@@ -302,6 +302,13 @@ namespace arc //! arctk namespace
                 return (_tris[utl::search::lower(_areas, rng_->gen())].random_pos(rng_));
             }
 
+            /**
+             *  Generate a random position, and corresponding normal, on the surface of the mesh.
+             *
+             *  @param  rng_    Random number generator.
+             *
+             *  @return Random position, and corresponding normal, on the surface of the mesh.
+             */
             inline std::pair<vec3, vec3> Mesh::random_pos_and_norm(random::Generator* rng_) const noexcept
             {
                 return (_tris[utl::search::lower(_areas, rng_->gen())].random_pos_and_norm(rng_));
