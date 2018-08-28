d5c72724754fd370d5711dea990f213e1c7430d3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-29 00:53:14 +0100

    Added framework.

---
+++ include/arctk/geom/aabb.hpp
@@ -0,0 +1,61 @@
+/**
+ *  @file   arctk/geom/aabb.hpp
+ *  @date   29/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Geometric axis-aligned bounding box class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_AABB_HPP
+#define ARCTK_GEOM_AABB_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+//  -- Arctk --
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Axis-aligned bounding box.
+         */
+        class Aabb
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_AABB_HPP
