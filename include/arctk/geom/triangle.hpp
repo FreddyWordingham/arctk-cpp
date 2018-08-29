494dc4f30cdf5df011a6fe62e217cb375d453999
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-29 01:17:46 +0100

    Added framework.

---
+++ include/arctk/geom/triangle.hpp
@@ -0,0 +1,75 @@
+/**
+ *  @file   arctk/geom/triangle.hpp
+ *  @date   29/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Geometric triangle class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_TRIANGLE_HPP
+#define ARCTK_GEOM_TRIANGLE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <optional>
+
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Geometric triangle.
+         */
+        class Triangle
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Positioning --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+
+            //  -- Collision --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_TRIANGLE_HPP
