b7f9d2d17cff2ec06e522013264d158547762641
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-29 02:05:38 +0100

    Added framework.

---
+++ include/arctk/geom/mesh.hpp
@@ -0,0 +1,73 @@
+/**
+ *  @file   arctk/geom/mesh.hpp
+ *  @date   29/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Geometric triangle mesh class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_MESH_HPP
+#define ARCTK_GEOM_MESH_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <optional>
+#include <vector>
+
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/index.hpp>
+#include <arctk/math.hpp>
+#include <arctk/random.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Geometric triangle mesh.
+         */
+        class Mesh
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_MESH_HPP
