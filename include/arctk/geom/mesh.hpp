fd7f8a25d2a3bea6af96fce2b6013c73ea6e5e58
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-29 13:11:17 +0100

    Added constructor method.

--- include/arctk/geom/mesh.hpp
+++ include/arctk/geom/mesh.hpp
@@ -53,6 +53,7 @@ namespace arc //! arctk namespace
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
+            inline explicit Mesh(const std::string& serial_, const vec3& scale_ = vec3(1.0, 1.0, 1.0), const vec3& rot_ = vec3(0.0, 0.0, 0.0), const vec3& trans_ = vec3(0.0, 0.0, 0.0)) noexcept;
 
 
             //  == METHODS ==
@@ -64,6 +65,23 @@ namespace arc //! arctk namespace
 
         //  == INSTANTIATION ==
         //  -- Constructors --
+        /**
+         *  Construct a mesh by parsing a serialised wavefront object.
+         *  Additional transformations may be applied to the base mesh.
+         *
+         *  @param  serial_ String of the serialised wavefront object.
+         *  @param  scale_  Scale transformation to apply to the base wavefront object.
+         *  @param  rot_    Rotation transformation to apply to the base wavefront object.
+         *  @param  trans_  Translation transformation to apply to the base wavefront object.
+         *
+         *  @pre    serial_ may not be empty.
+         */
+        inline Mesh::Mesh(const std::string& serial_, const vec3& scale_, const vec3& rot_, const vec3& trans_) noexcept
+          : _tris(init_tris(serial_, math::mat::transform(scale_, rot_, trans_)))
+          , _areas(init_areas())
+        {
+            PRE(!serial_.empty());
+        }
 
 
 
