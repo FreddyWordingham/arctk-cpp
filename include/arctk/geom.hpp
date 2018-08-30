c916a62f1b540d0dec3f750d5bc38ceb78d1df4f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-30 15:23:36 +0100

    Added imports.

--- include/arctk/geom.hpp
+++ include/arctk/geom.hpp
@@ -16,6 +16,12 @@
 
 //  == IMPORTS ==
 //  -- Arctk --
+#include <arctk/geom/shape.hpp>
+#include <arctk/geom/shape/aabb.hpp>
+#include <arctk/geom/shape/circle.hpp>
+#include <arctk/geom/shape/plane.hpp>
+#include <arctk/geom/shape/sphere.hpp>
+#include <arctk/geom/shape/triangle.hpp>
 
 
 
