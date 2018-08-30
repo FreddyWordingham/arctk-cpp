cc181cf2db5f4a257793c2e1da8369f94c8f3c88
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-30 15:56:02 +0100

    Linked to geom collection header file.

--- include/arctk/geom.hpp
+++ include/arctk/geom.hpp
@@ -19,6 +19,7 @@
 #include <arctk/geom/shape.hpp>
 #include <arctk/geom/shape/aabb.hpp>
 #include <arctk/geom/shape/circle.hpp>
+#include <arctk/geom/shape/mesh.hpp>
 #include <arctk/geom/shape/plane.hpp>
 #include <arctk/geom/shape/sphere.hpp>
 #include <arctk/geom/shape/triangle.hpp>
