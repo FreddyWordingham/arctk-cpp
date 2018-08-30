474016a3958090a70530addea22ea4d5dd8bc58e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-30 01:24:51 +0100

    Added framework.

---
+++ include/arctk/dom.hpp
@@ -0,0 +1,23 @@
+/**
+ *  @file   arctk/dom.hpp
+ *  @date   30/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of domain classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_HPP
+#define ARCTK_DOM_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_HPP
