566580418a1a658d180a4427e66f558fc7e2b38e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-30 02:26:10 +0100

    Added framework.

---
+++ include/arctk/dom/grid/Fixed.hpp
@@ -0,0 +1,72 @@
+/**
+ *  @file   arctk/dom/grid/fixed.hpp
+ *  @date   30/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Fixed domain gridding class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_GRID_FIXED_HPP
+#define ARCTK_DOM_GRID_FIXED_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/dom/cell.hpp>
+#include <arctk/dom/grid.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace dom //! domain namespace
+    {
+        namespace grid //! gridding namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Fixed domain gridding class.
+             */
+            class Fixed : public Grid
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+                inline const Cell& cell(const vec3& pos_) const noexcept override;
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+            //  -- Getters --
+
+
+
+        } // namespace grid
+    }     // namespace dom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_GRID_FIXED_HPP
