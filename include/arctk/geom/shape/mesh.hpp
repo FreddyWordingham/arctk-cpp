49d227c684603e61bed5c991458d3802290899a8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-08-30 15:58:49 +0100

    Added framework.

---
+++ include/arctk/geom/shape/mesh.hpp
@@ -0,0 +1,74 @@
+/**
+ *  @file   arctk/geom/shape/mesh.hpp
+ *  @date   30/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  Geometric mesh shape class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_SHAPE_MESH_HPP
+#define ARCTK_GEOM_SHAPE_MESH_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <optional>
+
+//  -- Arctk --
+#include <arctk/geom/shape.hpp>
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+        namespace shape //! shape namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Mesh of primitives shape.
+             */
+            class Mesh : public Shape
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+            //  -- Getters --
+
+
+
+        } // namespace shape
+    }     // namespace geom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_SHAPE_MESH_HPP
