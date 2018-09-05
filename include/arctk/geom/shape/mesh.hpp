dcc725bebae4fe55060a421319551c1c21472bca
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-05 15:12:33 +0100

    Removed old constructor method.

--- include/arctk/geom/shape/mesh.hpp
+++ include/arctk/geom/shape/mesh.hpp
@@ -74,9 +74,6 @@ namespace arc //! arctk namespace
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-                inline explicit Mesh(const std::string& serial_, const vec3& scale_ = vec3(1.0, 1.0, 1.0), const vec3& rot_ = vec3(0.0, 0.0, 0.0), const vec3& trans_ = vec3(0.0, 0.0, 0.0)) noexcept;
-
-
                 inline Mesh(const std::vector<vec3>& poss_, const std::vector<vec3>& norms_, const std::vector<std::array<std::array<size_t, 3>, 2>> faces_) noexcept;
 
               private:
@@ -117,25 +114,6 @@ namespace arc //! arctk namespace
 
             //  == INSTANTIATION ==
             //  -- Constructors --
-            /**
-             *  Construct a mesh by parsing a serialised wavefront object.
-             *  Additional transformations may be applied to the base mesh.
-             *
-             *  @param  serial_ String of the serialised wavefront object.
-             *  @param  scale_  Scale transformation to apply to the base wavefront object.
-             *  @param  rot_    Rotation transformation to apply to the base wavefront object.
-             *  @param  trans_  Translation transformation to apply to the base wavefront object.
-             *
-             *  @pre    serial_ may not be empty.
-             */
-            inline Mesh::Mesh(const std::string& serial_, const vec3& scale_, const vec3& rot_, const vec3& trans_) noexcept
-              : _tris(init_tris(serial_, math::mat::transform(scale_, rot_, trans_)))
-              , _areas(init_areas())
-              , _box(init_box())
-            {
-                PRE(!serial_.empty());
-            }
-
             inline Mesh(const std::vector<vec3>& poss_, const std::vector<vec3>& norms_, const std::vector<std::array<std::array<size_t, 3>, 2>> faces_) noexcept
               : _tris(init_tris(poss_, norms_, faces_))
               , _areas(init_areas())
@@ -148,139 +126,6 @@ namespace arc //! arctk namespace
 
 
             //  -- Initialisation --
-            /**
-             *  Initialise the vector of triangles that will form the mesh.
-             *
-             *  @param  serial_ String of the serialised wavefront object.
-             *  @param  transform_  Transformation matrix to apply to the base mesh.
-             *
-             *  @pre    serial_ may not be empty.
-             *
-             *  @post   tris_ may not be empty.
-             *
-             *  @return Initialised vector of triangles that will form the mesh.
-             */
-            inline std::vector<Triangle> Mesh::init_tris(const std::string& serial_, const mat4& transform_) const noexcept // NOLINT
-            {
-                PRE(!serial_.empty());
-
-                std::vector<Triangle> tris;
-
-                std::stringstream serial_stream(serial_);
-                std::string       line;
-
-                std::vector<vec3> vert_pos, vert_norm;
-                const mat4        inv_transform = transform_.inv().trans();
-                unsigned int      num_verts     = 0;
-                unsigned int      num_faces     = 0;
-                while (std::getline(serial_stream, line))
-                {
-                    std::stringstream line_stream(line);
-                    std::string       word;
-                    line_stream >> word;
-
-                    if (word == POS_KEYWORD)
-                    {
-                        ++num_verts;
-
-                        vec4 pos;
-                        line_stream >> pos.x >> pos.y >> pos.z;
-                        pos.w = 1.0;
-
-                        pos = transform_ * pos;
-
-                        vert_pos.emplace_back(pos.x, pos.y, pos.z);
-                    }
-                    else if (word == NORM_KEYWORD)
-                    {
-                        vec4 norm;
-                        line_stream >> norm.x >> norm.y >> norm.z;
-                        norm.w = 0.0;
-
-                        norm = inv_transform * norm;
-
-                        vert_norm.emplace_back(vec3(norm.x, norm.y, norm.z).normal());
-                    }
-
-                    if (line_stream.fail())
-                    {
-                        std::cerr << "Unable to construct mesh object.\n"
-                                  << "Unable to parse line: `" << line << "`.\n";
-                    }
-                }
-
-                utl::stream::rewind(serial_stream);
-                while (std::getline(serial_stream, line))
-                {
-                    std::stringstream line_stream(line);
-                    std::string       word;
-                    line_stream >> word;
-
-                    if (word == FACE_KEYWORD)
-                    {
-                        ++num_faces;
-
-                        std::array<std::string, 3> face;
-                        line_stream >> face[index::vertex::ALPHA] >> face[index::vertex::BETA] >> face[index::vertex::GAMMA];
-
-                        if (line_stream.rdbuf()->in_avail() != 0)
-                        {
-                            std::cerr << "Unable to construct mesh object.\n"
-                                      << "Non-triangular face located at line: `" << line << "`.\n";
-
-                            std::exit(exit::error::FAILED_PARSE);
-                        }
-
-                        std::array<size_t, 3> pos_index{}, norm_index{};
-                        for (size_t i = 0; i < 3; ++i)
-                        {
-                            const size_t first_slash = face[i].find_first_of('/');
-                            const size_t last_slash  = face[i].find_last_of('/');
-
-                            std::stringstream pos(face[i].substr(0, first_slash));
-                            pos >> pos_index[i];
-                            --pos_index[i];
-
-                            std::stringstream norm(face[i].substr(last_slash + 1));
-                            norm >> norm_index[i];
-                            --norm_index[i];
-
-                            if (pos.fail() || norm.fail())
-                            {
-                                std::cerr << "Unable to construct mesh object.\n"
-                                          << "Unable to parse line: `" << line << "`.\n";
-
-                                std::exit(exit::error::FAILED_PARSE);
-                            }
-                        }
-
-                        tris.push_back(Triangle({{vert_pos[pos_index[index::vertex::ALPHA]], vert_pos[pos_index[index::vertex::BETA]], vert_pos[pos_index[index::vertex::GAMMA]]}},
-                                                {{vert_norm[norm_index[index::vertex::ALPHA]], vert_norm[norm_index[index::vertex::BETA]], vert_norm[norm_index[index::vertex::GAMMA]]}}));
-                    }
-
-                    if (line_stream.fail())
-                    {
-                        std::cerr << "Unable to construct mesh object.\n"
-                                  << "Unable to parse line: `" << line << "`.\n";
-
-                        std::exit(exit::error::FAILED_PARSE);
-                    }
-                }
-
-                unsigned int num_edges = (num_faces * 3) / 2;
-                if (((num_faces % 2) != 0) || (((num_verts + num_faces) - num_edges) != 2))
-                {
-                    std::cerr << "Unable to construct mesh object.\n"
-                              << "Triangles of mesh must form a closed surface.\n";
-
-                    std::exit(exit::error::FAILED_INITIALISATION);
-                }
-
-                POST(!tris.empty());
-
-                return (tris);
-            }
-
             inline std::vector<Triangle> Mesh::init_tris(const std::vector<vec3>& poss_, const std::vector<vec3>& norms_, const std::vector<std::array<std::array<size_t, 3>, 2>> faces_) const noexcept
             {
                 PRE(poss_.size() >= 3);
