a07c2fbcd568626c38153afaa78a3a4388c2f67f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-07 12:56:44 +0100

    Added framework.

---
+++ include/arctk/dom/grid.hpp
@@ -0,0 +1,65 @@
+/**
+ *  @file   arctk/dom/grid.hpp
+ *  @date   07/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Domain gridding base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_GRID_HPP
+#define ARCTK_DOM_GRID_HPP
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace dom //! domain namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Domain gridding class.
+         */
+        class Grid
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace dom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_GRID_HPP
