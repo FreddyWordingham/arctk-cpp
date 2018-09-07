c1fcbb90be0afa1fcef70bef22bdb2cb17a9df4c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-07 16:17:42 +0100

    Added framework.

---
+++ include/arctk/data/domain.hpp
@@ -0,0 +1,70 @@
+/**
+ *  @file   arctk/data/domain.hpp
+ *  @date   07/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Data domain class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_DOMAIN_HPP
+#define ARCTK_DATA_DOMAIN_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <vector>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Data domain class.
+         */
+        class Domain
+        {
+            //  == FIELDS ==
+          protected:
+            //  -- Information --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_DOMAIN_HPP
