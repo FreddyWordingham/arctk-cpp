1e6479b144e80321192cc84c4a86f0412d0b0bd9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-07 13:00:02 +0100

    Added framework.

---
+++ include/arctk/dom/cell.hpp
@@ -0,0 +1,65 @@
+/**
+ *  @file   arctk/dom/cell.hpp
+ *  @date   07/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Domain cell base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_CELL_HPP
+#define ARCTK_DOM_CELL_HPP
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace dom //! domain namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Domain cell class.
+         */
+        class Cell
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace dom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_CELL_HPP
