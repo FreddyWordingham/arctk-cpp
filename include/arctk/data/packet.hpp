ba39473cdc74ba6ada3019abbb86d3941e615eb5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-07 16:10:24 +0100

    Renamed quanta to packet.

---
+++ include/arctk/data/packet.hpp
@@ -0,0 +1,77 @@
+/**
+ *  @file   arctk/data/image.hpp
+ *  @date   12/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Image forming base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_IMAGE_HPP
+#define ARCTK_DATA_IMAGE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Image data writer.
+         */
+        class Image
+        {
+            //  == FIELDS ==
+          protected:
+            //  -- Information --
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_IMAGE_HPP
