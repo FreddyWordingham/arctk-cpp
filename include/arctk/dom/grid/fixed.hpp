1ba92fe953ba8d951e72cb4d0f72294405d9aaf0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-07 13:57:36 +0100

    Added framework.

---
+++ include/arctk/dom/grid/fixed.hpp
@@ -0,0 +1,70 @@
+/**
+ *  @file   arctk/dom/grid/fixed.hpp
+ *  @date   07/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Fixed domain gridding base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_GRID_FIXED_HPP
+#define ARCTK_DOM_GRID_FIXED_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/dom/grid.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace dom //! domain namespace
+    {
+        namespace grid //! gridding namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Fixed domain gridding class.
+             */
+            class Fixed : public Grid
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+            //  -- Getters --
+
+
+
+        } // namespace grid
+    }     // namespace dom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_GRID_FIXED_HPP
