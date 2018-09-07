3da4f82ab443cab4bd4c6afe38f645485e670406
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-07 13:54:37 +0100

    Added comment.

--- include/arctk/dom/cell/leaf.hpp
+++ include/arctk/dom/cell/leaf.hpp
@@ -36,6 +36,9 @@ namespace arc //! arctk namespace
             {
                 //  == FIELDS ==
               private:
+                //  -- Data --
+
+
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
