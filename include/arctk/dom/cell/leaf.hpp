29fbb3967c39e426ae5c60b49e80c1700b0a4f75
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-07 13:05:54 +0100

    Added framework.

---
+++ include/arctk/dom/cell/leaf.hpp
@@ -0,0 +1,68 @@
+/**
+ *  @file   arctk/dom/cell/leaf.hpp
+ *  @date   07/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Domain leaf cell class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_CELL_LEAF_HPP
+#define ARCTK_DOM_CELL_LEAF_HPP
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace dom //! domain namespace
+    {
+        namespace cell //! cell namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Domain leaf cell class.
+             */
+            class Leaf
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+            //  -- Getters --
+
+
+
+        } // namespace cell
+    }     // namespace dom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_CELL_LEAF_HPP
