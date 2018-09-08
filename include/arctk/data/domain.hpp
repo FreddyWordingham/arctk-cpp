f8473b7e4fde8666b0b2ea09ecca7665116c421c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-08 17:49:35 +0100

    Removed superflous parenthesis.

--- include/arctk/data/domain.hpp
+++ include/arctk/data/domain.hpp
@@ -80,7 +80,7 @@ namespace arc //! arctk namespace
         inline Domain::Domain(const vec3& min_, const vec3& max_, const std::array<size_t, 3>& res_, const T& pack_) noexcept
           : geom::shape::Aabb(min_, max_)
           , _packet_size(init_packet_size(min_, max_, res_))
-          , _packets(init_packets<T>(res_), pack_))
+          , _packets(init_packets<T>(res_), pack_)
         {
             PRE(min_.x < max_.x);
             PRE(min_.y < max_.y);
