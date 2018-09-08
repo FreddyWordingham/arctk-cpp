7df00515cc8c9a9239fd35cabe3e28547e3a89aa
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-08 17:58:47 +0100

    Added special methods.

--- include/arctk/data/packet.hpp
+++ include/arctk/data/packet.hpp
@@ -38,7 +38,19 @@ namespace arc //! arctk namespace
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
-            inline Packet() = default;
+            inline Packet()                       = default;
+            inline Packet(const Packet&) noexcept = default; //!< Defaulted copy constructor.
+            inline Packet(Packet&&) noexcept      = default; //!< Defaulted move constructor.
+
+            //  -- Destructors --
+            virtual inline ~Packet() noexcept = 0;
+
+
+            //  == OPERATORS ==
+          public:
+            //  -- Assignment --
+            inline Packet& operator=(const Packet&) noexcept = default; //!< Defaulted copy operator. @return Reference to copied object.
+            inline Packet& operator=(Packet&&) noexcept = default;      //!< Defaulted move operator. @return Reference to moved object.
         };
 
 
