0f06c488782fe0d1fb2431e363f45a75cb15a91a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-10 23:08:43 +0100

    Added save double method.

--- include/arctk/data/cube3.hpp
+++ include/arctk/data/cube3.hpp
@@ -258,6 +258,23 @@ namespace arc //! arctk namespace
             save_scalar(path_, name_, var_name_, min_, max_, "int");
         }
 
+        template <>
+        inline void Cube<double, 3>::save(const std::string& path_, const std::string& name_, const std::string& var_name_, const vec3& min_, const vec3& max_) const noexcept
+        {
+            PRE(!path_.empty());
+            PRE(!name_.empty());
+            PRE(name_.find_first_of('\n') == std::string::npos);
+            PRE(name_.find_first_of(' ') == std::string::npos);
+            PRE(!var_name_.empty());
+            PRE(var_name_.find_first_of('\n') == std::string::npos);
+            PRE(var_name_.find_first_of(' ') == std::string::npos);
+            PRE(min_.x < max_.x);
+            PRE(min_.y < max_.y);
+            PRE(min_.z < max_.z);
+
+            save_scalar(path_, name_, var_name_, min_, max_, "double");
+        }
+
         template <typename T>
         inline void Cube<T, 3>::save_scalar(const std::string& path_, const std::string& name_, const std::string& var_name_, const vec3& min_, const vec3& max_, const std::string& typename_) const noexcept
         {
