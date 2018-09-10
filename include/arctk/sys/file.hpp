e1aa4d13b560df088baba4dedba16796d38a8235
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-10 13:58:33 +0100

    Added framework.

---
+++ include/arctk/sys/file.hpp
@@ -0,0 +1,40 @@
+/**
+ *  @file   arctk/sys/file.hpp
+ *  @date   10/08/2018
+ *  @author Freddy Wordingham
+ *
+ *  System file managemet functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SYS_FILE_HPP
+#define ARCTK_SYS_FILE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace sys //! system namespace
+    {
+        namespace file //! file namespace
+        {
+
+
+
+        } // namespace file
+    }     // namespace sys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SYS_FILE_HPP
