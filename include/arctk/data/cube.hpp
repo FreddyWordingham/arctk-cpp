77f379b4cef143dfb8cc62cdd08a3e9e796e74d3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-11 11:50:27 +0100

    Added framework.

---
+++ include/arctk/data/cube.hpp
@@ -0,0 +1,62 @@
+/**
+ *  @file   arctk/data/cube.hpp
+ *  @date   11/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Data cube class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_CUBE_HPP
+#define ARCTK_DATA_CUBE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Data table class.
+         *
+         *  @tparam A   Types stored in data columns.
+         */
+        template <typename... A>
+        class Cube
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Data --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_CUBE_HPP
