82e96818e99b3a4fcf0fa334dc475466cb56993b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-12 11:46:28 +0100

    Added default arguments.

--- include/arctk/data/cube2.hpp
+++ include/arctk/data/cube2.hpp
@@ -78,7 +78,7 @@ namespace arc //! arctk namespace
             inline typename std::tuple_element<I, std::tuple<A...>>::type max() const noexcept;
 
             //  -- Saving --
-            inline void save(const std::string& path_, const std::string& set_name_, const std::array<std::string, sizeof...(A)>& var_names_, const vec2& min_, const vec2& max_) const noexcept;
+            inline void save(const std::string& path_, const std::string& set_name_, const std::array<std::string, sizeof...(A)>& var_names_, const vec2& min_ = vec2(0.0, 0.0), const vec2& max_ = vec2(1.0, 1.0)) const noexcept;
 
           private:
             //  -- Saving --
