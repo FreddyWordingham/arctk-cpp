988ad9d1736acce0f2dacf30b2347adb17211845
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-12 20:09:45 +0100

    Linked to data collection header file.

--- include/arctk/data.hpp
+++ include/arctk/data.hpp
@@ -33,6 +33,7 @@
 #include <arctk/data/image.hpp>
 #include <arctk/data/image/colour.hpp>
 #include <arctk/data/image/greyscale.hpp>
+#include <arctk/data/packet.hpp>
 #include <arctk/data/table.hpp>
 
 
