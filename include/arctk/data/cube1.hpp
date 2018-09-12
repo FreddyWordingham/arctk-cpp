48a0e560635d742f334c3389304a2e4d3c048452
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-12 14:05:12 +0100

    Documented data field.

--- include/arctk/data/cube1.hpp
+++ include/arctk/data/cube1.hpp
@@ -51,8 +51,8 @@ namespace arc //! arctk namespace
             //  == FIELDS ==
           private:
             //  -- Data --
-            const size_t                  _res; //!< Resolution of the data.
-            std::vector<std::tuple<A...>> _data;
+            const size_t                  _res;  //!< Resolution of the data.
+            std::vector<std::tuple<A...>> _data; //!< Stored data.
 
 
             //  == INSTANTIATION ==
