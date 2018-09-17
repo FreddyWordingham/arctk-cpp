b524232a43fc3cd646cdc36db0cbd5c901d9e306
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 14:41:59 +0100

    Added framework.

---
+++ include/arctk/equip.hpp
@@ -0,0 +1,23 @@
+/**
+ *  @file   arctk/equip.hpp
+ *  @date   17/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of equipment classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EQUIP_HPP
+#define ARCTK_EQUIP_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EQUIP_HPP
