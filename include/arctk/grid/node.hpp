715c3dc5074d25dbf6a4a256bcced5264e246e75
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 16:49:27 +0100

    Added framework.

---
+++ include/arctk/grid/node.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @file   arctk/data/node.hpp
+ *  @date   17/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Grid node base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GRID_NODE_HPP
+#define ARCTK_GRID_NODE_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace grid //! grid namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Grid node base class.
+         */
+        class Node
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+            inline Node() noexcept            = default;
+            inline Node(const Node&) noexcept = default; //!< Defaulted copy constructor.
+            inline Node(Node&&) noexcept      = default; //!< Defaulted move constructor.
+
+            //  -- Destructors --
+            virtual inline ~Node() noexcept = 0;
+
+
+            //  == OPERATORS ==
+          public:
+            //  -- Assignment --
+            inline Node& operator=(const Node&) noexcept = default; //!< Defaulted copy operator. @return Reference to copied object.
+            inline Node& operator=(Node&&) noexcept = default;      //!< Defaulted move operator. @return Reference to moved object.
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Destructors --
+        inline Node::~Node() noexcept = default;
+
+
+
+    } // namespace grid
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GRID_NODE_HPP
