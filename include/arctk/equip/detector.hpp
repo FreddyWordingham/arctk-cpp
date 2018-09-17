3aab4705161d3ad4c703eab62288de7a60b4344f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 15:01:19 +0100

    Added framework.

---
+++ include/arctk/equip/detector.hpp
@@ -0,0 +1,51 @@
+/**
+ *  @file   arctk/equip/detector.hpp
+ *  @date   17/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Detector equipment base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EQUIP_DETECTOR_HPP
+#define ARCTK_EQUIP_DETECTOR_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace equip //! equipment namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Detector class.
+         */
+        class Detector
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+    } // namespace equip
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EQUIP_DETECTOR_HPP
