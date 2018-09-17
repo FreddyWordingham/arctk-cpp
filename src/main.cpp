c1d56ea10b11832b4046f70eb04fb1e010e41943
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 14:50:28 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,7 @@
 #include <arctk/debug/location.hpp>
 #include <arctk/equip.hpp>
 #include <arctk/equip/entity.hpp>
+#include <arctk/equip/light.hpp>
 #include <arctk/exit.hpp>
 #include <arctk/exit/error.hpp>
 #include <arctk/exit/success.hpp>
