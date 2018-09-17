bc64a47b999b9a44675e25bc5fbc5c166a24e376
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 15:00:16 +0100

    Added framework.

---
+++ include/arctk/equip/light.hpp
@@ -0,0 +1,51 @@
+/**
+ *  @file   arctk/equip/light.hpp
+ *  @date   17/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Light source equipment class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EQUIP_LIGHT_HPP
+#define ARCTK_EQUIP_LIGHT_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace equip //! equipment namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Light source class.
+         */
+        class Light
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+    } // namespace equip
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EQUIP_LIGHT_HPP
