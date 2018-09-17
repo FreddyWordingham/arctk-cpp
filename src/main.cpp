6bb267ba32a8592c1714f54b9dbc62e263235687
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 14:39:29 +0100

    Removed redundant import.

--- src/main.cpp
+++ src/main.cpp
@@ -103,7 +103,6 @@
 #include <arctk/phys/material.hpp>
 #include <arctk/phys/material/basic.hpp>
 #include <arctk/phys/material/fluorescent.hpp>
-#include <arctk/phys/optical.hpp>
 #include <arctk/phys/optics.hpp>
 #include <arctk/phys/particle.hpp>
 #include <arctk/random.hpp>
