7b66571f2f7f24ce022778f25b3ffe4b1d70f4ff
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 16:30:45 +0100

    Added framework.

---
+++ include/arctk/grid.hpp
@@ -0,0 +1,24 @@
+/**
+ *  @file   arctk/grid.hpp
+ *  @date   17/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of gridding classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GRID_HPP
+#define ARCTK_GRID_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/grid.hpp>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GRID_HPP
