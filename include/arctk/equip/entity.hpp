770577f1e4c4953354661d4e8e6fe325bbdb8ec8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 15:00:48 +0100

    Added framework.

---
+++ include/arctk/equip/entity.hpp
@@ -0,0 +1,51 @@
+/**
+ *  @file   arctk/equip/entity.hpp
+ *  @date   17/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Entity equipment class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EQUIP_ENTITY_HPP
+#define ARCTK_EQUIP_ENTITY_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace equip //! equipment namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Entity class.
+         */
+        class Entity
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+    } // namespace equip
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EQUIP_ENTITY_HPP
