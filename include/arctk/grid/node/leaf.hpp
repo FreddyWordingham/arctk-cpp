571e6aad4e069adf82a926ff662d669411e74c75
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-17 17:11:05 +0100

    Added framework.

---
+++ include/arctk/grid/node/leaf.hpp
@@ -0,0 +1,60 @@
+/**
+ *  @file   arctk/data/node/leaf.hpp
+ *  @date   17/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Leaf node class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GRID_NODE_LEAF_HPP
+#define ARCTK_GRID_NODE_LEAF_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/geom.hpp>
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace grid //! grid namespace
+    {
+        namespace node //! node namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Leaf node class.
+             */
+            class Leaf : public Node
+            {
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+        } // namespace node
+    }     // namespace grid
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GRID_NODE_LEAF_HPP
