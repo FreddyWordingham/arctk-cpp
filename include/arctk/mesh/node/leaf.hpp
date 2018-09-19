0a1db5367b978477465fe30a238130b45d781b60
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 12:50:52 +0100

    Updated guard.

--- include/arctk/mesh/node/leaf.hpp
+++ include/arctk/mesh/node/leaf.hpp
@@ -9,8 +9,8 @@
 
 
 //  == GUARD ==
-#ifndef ARCTK_GRID_NODE_LEAF_HPP
-#define ARCTK_GRID_NODE_LEAF_HPP
+#ifndef ARCTK_MESH_NODE_LEAF_HPP
+#define ARCTK_MESH_NODE_LEAF_HPP
 
 
 
@@ -128,4 +128,4 @@ namespace arc //! arctk namespace
 
 
 //  == GUARD END ==
-#endif // ARCTK_GRID_NODE_LEAF_HPP
+#endif // ARCTK_MESH_NODE_LEAF_HPP
