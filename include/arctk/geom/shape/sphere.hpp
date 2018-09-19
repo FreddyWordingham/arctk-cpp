e5fec38bf58e5d322c40bce8587505d6c47b0d72
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 14:38:09 +0100

    Added documentation to shape_list method.

--- include/arctk/geom/shape/sphere.hpp
+++ include/arctk/geom/shape/sphere.hpp
@@ -108,6 +108,11 @@ namespace arc //! arctk namespace
 
             //  == METHODS ==
             //  -- Getters --
+            /**
+             *  Create a vector containing pointers to all shapes forming this shape.
+             *
+             *  @return Vector containing pointers to all shapes forming this shape.
+             */
             inline std::vector<const Shape*> Sphere::shape_list() const noexcept
             {
                 return (std::vector<const Shape*>({this}));
