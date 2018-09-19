7b164606bcfe7c7e1800a2553c8e91db006779fc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 12:50:17 +0100

    Updated guard.

--- include/arctk/mesh/node/branch.hpp
+++ include/arctk/mesh/node/branch.hpp
@@ -9,8 +9,8 @@
 
 
 //  == GUARD ==
-#ifndef ARCTK_GRID_NODE_BRANCH_HPP
-#define ARCTK_GRID_NODE_BRANCH_HPP
+#ifndef ARCTK_MESH_NODE_BRANCH_HPP
+#define ARCTK_MESH_NODE_BRANCH_HPP
 
 
 
@@ -332,4 +332,4 @@ namespace arc //! arctk namespace
 
 
 //  == GUARD END ==
-#endif // ARCTK_GRID_NODE_BRANCH_HPP
+#endif // ARCTK_MESH_NODE_BRANCH_HPP
