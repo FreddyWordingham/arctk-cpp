1af76b8aec63cab5ff0f148a5a4d02e56e91d8d6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 13:41:18 +0100

    Added framework.

---
+++ include/arctk/mesh/adaptive.hpp
@@ -0,0 +1,58 @@
+/**
+ *  @file   arctk/mesh/adaptive.hpp
+ *  @date   19/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Adaptive mesh class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MESH_ADAPTIVE_HPP
+#define ARCTK_MESH_ADAPTIVE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace mesh //! mesh namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Mesh node base class.
+         */
+        class Adaptive
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+    } // namespace mesh
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MESH_ADAPTIVE_HPP
