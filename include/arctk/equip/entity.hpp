a7aef20acae0c27a7937a4d91ffe42cbdd0d0e9b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 21:13:45 +0100

    Changed type of surf field to use unique_ptr.

--- include/arctk/equip/entity.hpp
+++ include/arctk/equip/entity.hpp
@@ -16,6 +16,7 @@
 
 //  == IMPORTS ==
 //  -- Std --
+#include <memory>
 #include <string>
 
 //  -- Arctk --
@@ -42,8 +43,8 @@ namespace arc //! arctk namespace
             //  == FIELDS ==
           private:
             //  -- Structure --
-            const geom::Shape    _surf; //!< Shape forming the surface boundary of the entity.
-            const phys::Material _mat;  //!< Matrial composing the entity's volume.
+            const std::unique_ptr<geom::Shape> _surf; //!< Shape forming the surface boundary of the entity.
+            const phys::Material               _mat;  //!< Matrial composing the entity's volume.
 
 
             //  == INSTANTIATION ==
