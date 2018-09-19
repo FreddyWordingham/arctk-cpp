eb4d4401a7ecf7f86277ef9813d00c4b179e6da3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 16:54:32 +0100

    Documented constructor method.

--- include/arctk/mesh/node/leaf.hpp
+++ include/arctk/mesh/node/leaf.hpp
@@ -76,6 +76,7 @@ namespace arc //! arctk namespace
             //  == INSTANTIATION ==
             //  -- Constructors --
             /**
+             *  Construct a leaf node.
              *
              *  @param  min_            Minimum bound of the node.
              *  @param  max_            Maximum bound of the node.
@@ -83,21 +84,6 @@ namespace arc //! arctk namespace
              *  @param  entities_       Vector of entities that are found within the node's bounds.
              *  @param  detectors_      Vector of detectors that are found within the node's bounds.
              *  @param  cur_depth_      Current depth of the node.
-
-             * @return
-             */
-
-            /**
-             *  Construct a branch node.
-             *
-             *  @param  min_            Minimum bound of the node.
-             *  @param  max_            Maximum bound of the node.
-             *  @param  lights_         Vector of lights that are found within the node's bounds.
-             *  @param  entities_       Vector of entities that are found within the node's bounds.
-             *  @param  detectors_      Vector of detectors that are found within the node's bounds.
-             *  @param  cur_depth_      Current depth of the node.
-             *  @param  max_depth_      Maximum depth the mesh may reach.
-             *  @param  target_shapes_  Target maximum number of shapes to find within each leaf node of the mesh.
              *
              *  @pre    min_.x must be less than max_.x.
              *  @pre    min_.y must be less than max_.y.
