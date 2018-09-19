6995bc130493f2416bae956d9018b114e7e0d9f7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 19:35:11 +0100

    Removed references from fields.

--- include/arctk/equip/light.hpp
+++ include/arctk/equip/light.hpp
@@ -41,7 +41,7 @@ namespace arc //! arctk namespace
             //  == FIELDS ==
           private:
             //  -- Structure --
-            const geom::Shape&                         _surf; //!< Shape forming the emission surface of the light.
+            const geom::Shape                          _surf; //!< Shape forming the emission surface of the light.
             const random::distribution::Linear<double> _spec; //!< Spectrum used to draw photon wavelength values from.
 
 
