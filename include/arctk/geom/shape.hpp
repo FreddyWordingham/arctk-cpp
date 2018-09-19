efc0e6aa6245c53f0657af6926511438a467fe45
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 17:25:10 +0100

    Removed virtual inlines.

--- include/arctk/geom/shape.hpp
+++ include/arctk/geom/shape.hpp
@@ -70,30 +70,30 @@ namespace arc //! arctk namespace
             //  == METHODS ==
           public:
             //  -- Getters --
-            virtual inline std::vector<const Shape*> shape_list() const noexcept = 0; //!< Create a vector containing pointers to all shapes forming this shape.   @return Vector containing pointers to all shapes forming this shape.
+            virtual std::vector<const Shape*> shape_list() const noexcept = 0; //!< Create a vector containing pointers to all shapes forming this shape.   @return Vector containing pointers to all shapes forming this shape.
 
             //  -- Properties --
-            virtual vec3          min() const noexcept    = 0; //!< Get the minimum vec of the smallest bounding box containing the shape.  @return Minimum vec of the smallest bounding box containing the shape.
-            virtual vec3          max() const noexcept    = 0; //!< Get the maximum vec of the smallest bounding box containing the shape.  @return Maximum vec of the smallest bounding box containing the shape.
-            virtual inline double area() const noexcept   = 0; //!< Get the total surface area of the shape.    @return Total surface area of the shape.
-            virtual inline double vol() const noexcept    = 0; //!< Get the total volume of the shape.    @return Total volume of the shape.
-            virtual inline bool   closed() const noexcept = 0; //!< Get the topology of the shape.    @return True if the surface of the shape forms a bounded volume.
+            virtual vec3   min() const noexcept    = 0; //!< Get the minimum vec of the smallest bounding box containing the shape.  @return Minimum vec of the smallest bounding box containing the shape.
+            virtual vec3   max() const noexcept    = 0; //!< Get the maximum vec of the smallest bounding box containing the shape.  @return Maximum vec of the smallest bounding box containing the shape.
+            virtual double area() const noexcept   = 0; //!< Get the total surface area of the shape.    @return Total surface area of the shape.
+            virtual double vol() const noexcept    = 0; //!< Get the total volume of the shape.    @return Total volume of the shape.
+            virtual bool   closed() const noexcept = 0; //!< Get the topology of the shape.    @return True if the surface of the shape forms a bounded volume.
 
             //  -- Emission --
-            virtual inline vec3 random_pos(random::Generator* rng_) const noexcept = 0; //!< Generate a random position on the surface of the shape. @param  rng_    Random number generator.    @return Randomly generated point on the surface of the shape.
-            virtual inline std::pair<vec3, vec3> random_pos_and_norm(random::Generator* rng_) const
+            virtual vec3 random_pos(random::Generator* rng_) const noexcept = 0; //!< Generate a random position on the surface of the shape. @param  rng_    Random number generator.    @return Randomly generated point on the surface of the shape.
+            virtual std::pair<vec3, vec3> random_pos_and_norm(random::Generator* rng_) const
               noexcept = 0; //!< Generate a random position, and corresponding normal, on the surface of the shape. @param  rng_    Random number generator.    @return Randomly generated point, and corresponding normal, on the surface of the shape.
 
             //  -- Intersection --
-            virtual inline bool intersect_surf(const shape::Aabb& aabb_) const noexcept = 0; //!< Determine if an intersection occurs between the shape's surface and an axis-aligned bounding box.   @param  aabb_   Axis-aligned bounding box to test. @return
-                                                                                             //!< True if the shape's surface intersects with the axis-aligned bounding box.
-            virtual inline bool intersect_vol(const shape::Aabb& aabb_) const noexcept = 0; //!< Determine if an intersection occurs between the shape's volume and an axis-aligned bounding box.    @param  aabb_   Axis-aligned bounding box to test.  @return
-                                                                                            //!< True if the shape's volume intersects with the axis-aligned bounding box.
+            virtual bool intersect_surf(const shape::Aabb& aabb_) const noexcept = 0; //!< Determine if an intersection occurs between the shape's surface and an axis-aligned bounding box.   @param  aabb_   Axis-aligned bounding box to test. @return
+                                                                                      //!< True if the shape's surface intersects with the axis-aligned bounding box.
+            virtual bool intersect_vol(const shape::Aabb& aabb_) const noexcept = 0;  //!< Determine if an intersection occurs between the shape's volume and an axis-aligned bounding box.    @param  aabb_   Axis-aligned bounding box to test.  @return
+                                                                                      //!< True if the shape's volume intersects with the axis-aligned bounding box.
 
             //  -- Collision --
-            virtual inline std::optional<double> collision(const vec3& pos_, const vec3& dir_) const
+            virtual std::optional<double> collision(const vec3& pos_, const vec3& dir_) const
               noexcept = 0; //!< Get the collision distance (if one occurs) of a ray.    @param  pos_    Initial position of the ray.    @param  dir_    Direction of ray travel.    @return Optional distance to collision event.
-            virtual inline std::optional<std::pair<double, vec3>> collision_norm(const vec3& pos_, const vec3& dir_) const
+            virtual std::optional<std::pair<double, vec3>> collision_norm(const vec3& pos_, const vec3& dir_) const
               noexcept = 0; //!< Get the collision distance (if one occurs) and the normal of the intersection point of a ray.   @param  pos_    Initial position of the ray.    @param  dir_    Direction of ray travel.    @return Optional distance and
                             //!< normal pair to collision event.
         };
