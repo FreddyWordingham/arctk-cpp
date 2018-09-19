eac72e06ae20a4af3d2affa1bce1695c6a16e229
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 21:04:34 +0100

    Changed surf to use shared_ptr.

--- include/arctk/equip/detector.hpp
+++ include/arctk/equip/detector.hpp
@@ -42,7 +42,7 @@ namespace arc //! arctk namespace
             //  == FIELDS ==
           private:
             //  -- Structure --
-            const std::unique_ptr<geom::Shape> _surf; //!< Shape forming the surface of the detector.
+            const std::shared_ptr<geom::Shape> _surf; //!< Shape forming the surface of the detector.
 
 
             //  == INSTANTIATION ==
@@ -67,7 +67,7 @@ namespace arc //! arctk namespace
          *  @param  surf_   Surface of the detector.
          */
         inline Detector::Detector(geom::Shape surf_) noexcept
-          : _surf(std::make_unique<geom::Shape>(surf_))
+          : _surf(std::make_shared<geom::Shape>(surf_))
         {
         }
 
