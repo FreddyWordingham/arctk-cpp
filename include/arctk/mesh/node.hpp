c6f2635a399ea14034730962626697de3492b0d8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 16:43:09 +0100

    Added documentation to max_depth getter method.

--- include/arctk/mesh/node.hpp
+++ include/arctk/mesh/node.hpp
@@ -75,7 +75,7 @@ namespace arc //! arctk namespace
           public:
             //  -- Getters --
             inline size_t                          depth() const noexcept;
-            virtual size_t                         max_depth() const noexcept  = 0;
+            virtual size_t                         max_depth() const noexcept  = 0; //!< Get the maximum depth of this node and any of its descendents.  @return Maximum depth of this node and any of its descendents.
             virtual size_t                         max_shapes() const noexcept = 0;
             virtual size_t                         num_nodes() const noexcept  = 0;
             virtual std::vector<geom::shape::Aabb> boxes() const noexcept      = 0;
