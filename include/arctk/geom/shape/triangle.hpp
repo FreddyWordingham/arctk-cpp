17f442f5372cf2a1df30e115c10f0de153884858
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 14:38:31 +0100

    Added documentation to shape_list method.

--- include/arctk/geom/shape/triangle.hpp
+++ include/arctk/geom/shape/triangle.hpp
@@ -148,6 +148,11 @@ namespace arc //! arctk namespace
 
             //  == METHODS ==
             //  -- Getters --
+            /**
+             *  Create a vector containing pointers to all shapes forming this shape.
+             *
+             *  @return Vector containing pointers to all shapes forming this shape.
+             */
             inline std::vector<const Shape*> Triangle::shape_list() const noexcept
             {
                 return (std::vector<const Shape*>({this}));
