0196c39f51ea61f188751fb2bf281097db69019f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-19 16:59:50 +0100

    Added documentation to detectors field.

--- include/arctk/mesh/node/leaf.hpp
+++ include/arctk/mesh/node/leaf.hpp
@@ -49,7 +49,7 @@ namespace arc //! arctk namespace
                 //  -- Content --
                 const std::vector<std::pair<const geom::Shape&, const equip::Light&>>    _lights; //!< Vector of lights that are found within the node's bounds.
                 const std::vector<std::pair<const geom::Shape&, const equip::Entity&>>   _entities;
-                const std::vector<std::pair<const geom::Shape&, const equip::Detector&>> _detectors;
+                const std::vector<std::pair<const geom::Shape&, const equip::Detector&>> _detectors; //!< Vector of detectors that are found within the node's bounds.
 
 
                 //  == INSTANTIATION ==
