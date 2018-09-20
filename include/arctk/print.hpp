8d328441a28ebf6ad42aa6a90c0fd18b49c70605
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 00:29:28 +0100

    Added framework.

---
+++ include/arctk/print.hpp
@@ -0,0 +1,23 @@
+/**
+ *  @file   arctk/print.hpp
+ *  @date   21/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of printing operators and functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PRINT_HPP
+#define ARCTK_PRINT_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PRINT_HPP
