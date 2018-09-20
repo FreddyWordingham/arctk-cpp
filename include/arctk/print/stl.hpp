33552c78759a88913092ac4fb41188e08caa7680
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 00:48:12 +0100

    Added framework.

---
+++ include/arctk/print/stl.hpp
@@ -0,0 +1,34 @@
+/**
+ *  @file   arctk/print/stl.hpp
+ *  @date   21/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of standard template library printing operator functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PRINT_STL_HPP
+#define ARCTK_PRINT_STL_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace print //! printing namespace
+    {
+
+    } // namespace print
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PRINT_STL_HPP
