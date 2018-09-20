40f8c3b9d1e0f30ee2c6c4724c39cf87024c67f3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-20 19:33:52 +0100

    Added framework.

---
+++ include/arctk/data/json.hpp
@@ -0,0 +1,67 @@
+/**
+ *  @file   arctk/data/json.hpp
+ *  @date   20/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Json data class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_JSON_HPP
+#define ARCTK_DATA_JSON_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Json parser and data class.
+         */
+        class Json
+        {
+            //  == FIELDS ==
+          private:
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_JSON_HPP
