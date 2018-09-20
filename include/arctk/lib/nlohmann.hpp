ca5edfb8ed656dd038b34569e6addb14d3ada430
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-20 19:46:24 +0100

    Suppresed warnings in library header file.

--- include/arctk/lib/nlohmann.hpp
+++ include/arctk/lib/nlohmann.hpp
@@ -1,4 +1,6 @@
 #pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
+#pragma clang diagnostic ignored "-Wcovered-switch-default"
 #pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
 #pragma clang diagnostic ignored "-Wswitch-enum"
 
