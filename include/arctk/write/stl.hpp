909c0f7e33985889e9df891d96ab4f1d3542b9af
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 01:43:22 +0100

    Added arr function.

--- include/arctk/write/stl.hpp
+++ include/arctk/write/stl.hpp
@@ -34,6 +34,8 @@ namespace arc //! arctk namespace
             //  -- Vector --
             template <typename T>
             inline std::ostream& vec(std::ostream& stream_, const std::vector<T>& vec_, const std::string& open_ = '{', const std::string& close_ = '}', const std::string& delim_ = ", ") noexcept;
+            template <typename T, size_t N>
+            inline std::ostream& vec(std::ostream& stream_, const std::array<T, N>& arr_, const std::string& open_ = '{', const std::string& close_ = '}', const std::string& delim_ = ", ") noexcept;
 
 
 
@@ -46,6 +48,19 @@ namespace arc //! arctk namespace
                 for (size_t i = 1; i < vec_.size(); ++i)
                 {
                     stream_ << delim_ << vec_[i];
+                }
+                stream_ << close_;
+
+                return (stream_);
+            }
+
+            template <typename T, size_t N>
+            inline std::ostream& arr(std::ostream& stream_, const std::array<T, N>& arr_, const std::string& open_, const std::string& close_, const std::string& delim_) noexcept
+            {
+                stream_ << open_ << arr_[0];
+                for (size_t i = 1; i < N; ++i)
+                {
+                    stream_ << delim_ << arr_[i];
                 }
                 stream_ << close_;
 
