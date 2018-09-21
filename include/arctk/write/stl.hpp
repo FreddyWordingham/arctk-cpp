1f9e3f8f14709a4204d19a07584ad4aeb3ec2ef0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 02:38:29 +0100

    Removed stl write functions.

--- include/arctk/write/stl.hpp
+++ include/arctk/write/stl.hpp
@@ -30,66 +30,6 @@ namespace arc //! arctk namespace
 
 
 
-            //  == FUNCTION PROTOTYPES ==
-            //  -- Vector --
-            template <typename T>
-            inline std::ostream& vector(std::ostream& stream_, const std::vector<T>& vec_, const std::string& open_ = '(', const std::string& close_ = ')', const std::string& delim_ = ", ") noexcept;
-            template <typename T, size_t N>
-            inline std::ostream& array(std::ostream& stream_, const std::array<T, N>& arr_, const std::string& open_ = '[', const std::string& close_ = ']', const std::string& delim_ = ", ") noexcept;
-            template <typename T, typename S>
-            inline std::ostream& map(std::ostream& stream_, const std::map<T, S>& map_, const std::string& link_ = "\t->\t") noexcept;
-
-
-
-            //  == FUNCTIONS ==
-            //  -- Vector --
-            template <typename T>
-            inline std::ostream& vector(std::ostream& stream_, const std::vector<T>& vec_, const std::string& open_, const std::string& close_, const std::string& delim_) noexcept
-            {
-                stream_ << open_ << vec_[0];
-                for (size_t i = 1; i < vec_.size(); ++i)
-                {
-                    stream_ << delim_ << vec_[i];
-                }
-                stream_ << close_;
-
-                return (stream_);
-            }
-
-            template <typename T, size_t N>
-            inline std::ostream& array(std::ostream& stream_, const std::array<T, N>& arr_, const std::string& open_, const std::string& close_, const std::string& delim_) noexcept
-            {
-                stream_ << open_ << arr_[0];
-                for (size_t i = 1; i < N; ++i)
-                {
-                    stream_ << delim_ << arr_[i];
-                }
-                stream_ << close_;
-
-                return (stream_);
-            }
-
-            template <typename T, typename S>
-            inline std::ostream& map(std::ostream& stream_, const std::map<T, S>& map_, const std::string& link_ = "\t->\t") noexcept
-            {
-                bool first = true;
-                for (const auto& [key, val] : map_)
-                {
-                    if (!first)
-                    {
-                        stream_ << '\n';
-                    }
-
-                    stream_ << key << link_ << val;
-
-                    first = false;
-                }
-
-                return (stream_);
-            }
-
-
-
         } // namespace stl
     }     // namespace write
 } // namespace arc
