068365d3b75b258d6bfafc99aaab08f80c20d6c9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 01:30:52 +0100

    Updated guard and file tag.

--- include/arctk/write/vector.hpp
+++ include/arctk/write/vector.hpp
@@ -1,64 +1,24 @@
 /**
- *  @file   arctk/write/stl.hpp
+ *  @file   arctk/write/vector.hpp
  *  @date   21/09/2018
  *  @author Freddy Wordingham
  *
- *  Collection of standard template library writing operator functions.
+ *  Vector writing operator and function.
  */
 
 
 
 //  == GUARD ==
-#ifndef ARCTK_WRITE_STL_HPP
-#define ARCTK_WRITE_STL_HPP
+#ifndef ARCTK_WRITE_VECTOR_HPP
+#define ARCTK_WRITE_VECTOR_HPP
 
 
 
 //  == IMPORTS ==
 //  -- Std --
-#include <array>
 #include <vector>
 
 
 
-//  == OPERATOR PROTOTYPES ==
-//  -- Vector --
-template <typename T, typename S>
-inline T& operator<<(T& stream_, const std::vector<S>& vec_) noexcept;
-template <typename T, typename S, size_t N>
-inline T& operator<<(T& stream_, const std::array<S, N>& arr_) noexcept;
-
-
-
-//  == OPERATORS ==
-//  -- Vector --
-template <typename T, typename S>
-inline T& operator<<(T& stream_, const std::vector<S>& vec_) noexcept
-{
-    stream_ << '{' << vec_[0];
-    for (size_t i = 1; i < vec_.size(); ++i)
-    {
-        stream_ << ", " << vec_[i];
-    }
-    stream_ << '}';
-
-    return (stream_);
-}
-
-template <typename T, typename S, size_t N>
-inline T& operator<<(T& stream_, const std::array<S, N>& arr_) noexcept
-{
-    stream_ << '{' << arr_[0];
-    for (size_t i = 1; i < N; ++i)
-    {
-        stream_ << ", " << arr_[i];
-    }
-    stream_ << '}';
-
-    return (stream_);
-}
-
-
-
 //  == GUARD END ==
-#endif // ARCTK_WRITE_STL_HPP
+#endif // ARCTK_WRITE_VECTOR_HPP
