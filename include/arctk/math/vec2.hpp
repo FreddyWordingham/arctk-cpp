a75baeed1a919859f90f68cbbc61a242c55e4749
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 15:28:41 +0100

    Updated namespace path.

--- include/arctk/math/vec2.hpp
+++ include/arctk/math/vec2.hpp
@@ -178,7 +178,7 @@ namespace arc //! arctk namespace
         template <typename T>
         constexpr inline std::ostream& Vec<T, 2>::operator<<(std::ostream& stream_) const noexcept
         {
-            stream_ << write::format::VEC_START << std::setw(write::format::FIELD_WIDTH) << x << write::format::DELIM << std::setw(write::format::FIELD_WIDTH) << y << write::format::VEC_END;
+            stream_ << io::format::VEC_START << std::setw(io::format::FIELD_WIDTH) << x << io::format::DELIM << std::setw(io::format::FIELD_WIDTH) << y << io::format::VEC_END;
 
             return (stream_);
         }
