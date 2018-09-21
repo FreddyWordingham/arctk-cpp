0ac8befd6903fbfed26bcf7f82eb25d7210d36a7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 01:21:56 +0100

    Added print operator for array.

--- include/arctk/print/stl.hpp
+++ include/arctk/print/stl.hpp
@@ -24,6 +24,8 @@
 //  -- Vector --
 template <typename T, typename S>
 inline T& operator<<(T& stream_, const std::vector<S>& vec_) noexcept;
+template <typename T, typename S, size_t N>
+inline T& operator<<(T& stream_, const std::array<S, N>& arr_) noexcept;
 
 
 
@@ -36,6 +38,19 @@ inline T& operator<<(T& stream_, const std::vector<S>& vec_) noexcept
     for (size_t i = 1; i < vec_.size(); ++i)
     {
         stream_ << ", " << vec_[i];
+    }
+    stream_ << '}';
+
+    return (stream_);
+}
+
+template <typename T, typename S, size_t N>
+inline T& operator<<(T& stream_, const std::array<S, N>& arr_) noexcept
+{
+    stream_ << '{' << arr_[0];
+    for (size_t i = 1; i < N; ++i)
+    {
+        stream_ << ", " << arr_[i];
     }
     stream_ << '}';
 
