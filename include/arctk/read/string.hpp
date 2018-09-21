6a31f45fbb160c96af83978f892b57b0d795fab4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 14:46:48 +0100

    Added framework.

---
+++ include/arctk/read/string.hpp
@@ -0,0 +1,34 @@
+/**
+ *  @file   arctk/read/string.hpp
+ *  @date   21/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  String reading functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_READ_STRING_HPP
+#define ARCTK_READ_STRING_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace read //! reading namespace
+    {
+        namespace string //! string namespace
+        {
+
+
+
+        } // namespace string
+    }     // namespace read
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_READ_STRING_HPP
