9eb4a9ff129d52fa599df7b640ac13fa1c781830
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 03:44:28 +0100

    Added framework.

---
+++ include/arctk/write/format.hpp
@@ -0,0 +1,41 @@
+/**
+ *  @file   arctk/write/format.hpp
+ *  @date   21/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Writing formatting constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_WRITING_FORMAT_HPP
+#define ARCTK_WRITING_FORMAT_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace write //! writing namespace
+    {
+        namespace format //! formatting namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Bookends --
+
+            //  -- Delimiters --
+
+
+
+        } // namespace format
+    }     // namespace write
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_WRITING_FORMAT_HPP
