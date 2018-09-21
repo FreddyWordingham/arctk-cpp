dddfde6a19211b7ebb8cbf5de7d86bb792c36bab
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 03:51:33 +0100

    Updated to use formatting settings.

--- include/arctk/math/vec3.hpp
+++ include/arctk/math/vec3.hpp
@@ -210,7 +210,8 @@ namespace arc //! arctk namespace
         template <typename T>
         constexpr inline std::ostream& Vec<T, 3>::operator<<(std::ostream& stream_) const noexcept
         {
-            stream_ << '{' << x << ", " << y << ", " << z << '}';
+            stream_ << write::format::VEC_START << std::setw(write::format::FIELD_WIDTH) << x << write::format::DELIM << std::setw(write::format::FIELD_WIDTH) << y << write::format::DELIM << std::setw(write::format::FIELD_WIDTH) << z
+                    << write::format::VEC_END;
 
             return (stream_);
         }
