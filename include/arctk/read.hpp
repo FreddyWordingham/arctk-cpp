af56c30e1462a13e9fe6590a47335f19401b68d8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-21 13:49:08 +0100

    Added framework.

---
+++ include/arctk/read.hpp
@@ -0,0 +1,23 @@
+/**
+ *  @file   arctk/read.hpp
+ *  @date   21/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of reading operators and functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_READ_HPP
+#define ARCTK_READ_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_READ_HPP
