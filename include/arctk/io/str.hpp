392019fa30b095b17b1e3809bb4d4c9c64407199
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-23 02:34:41 +0100

    Added error handle.

--- include/arctk/io/str.hpp
+++ include/arctk/io/str.hpp
@@ -18,6 +18,10 @@
 //  -- Std --
 #include <string>
 
+//  -- Arctk --
+#include <arctk/exit.hpp>
+#include <arctk/io/format.hpp>
+
 
 
 //  == NAMESPACE ==
@@ -32,12 +36,36 @@ namespace arc //! arctk namespace
 
             //  == FUNCTION PROTOTYPES ==
             //  -- Pre-processing --
+            inline filter_comments(std::string* const str_) noexcept;
+
             //  -- Parsing --
 
 
 
             //  == FUNCTIONS ==
             //  -- Pre-processing --
+            inline filter_comments(std::string* const str_) noexcept
+            {
+                for (size_t i = 0; i < str_.size(); ++i)
+                {
+                    if (str_[i] == format::OPENERS[format::container::STRING])
+                    {
+                        const size_t string_container_end_pos = str_.find_first_of(CLOSERS[i], i + 1);
+
+                        if (string_container_end_pos == std::string::npos)
+                        {
+                            std::cerr << "Unable to parse string.\n"
+                                      << "String contains string opener but does not contain a corresponding closer.\n";
+
+                            std::exit(exit::error::FAILED_PARSE);
+                        }
+
+                        i = string_container_end_pos;
+                    }
+                }
+            }
+
+
             //  -- Parsing --
 
 
