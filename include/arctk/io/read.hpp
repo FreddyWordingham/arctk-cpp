b0797581eb2d2bbb3e70c4aeba882b1026bc0d47
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-23 02:10:28 +0100

    Removed str functions.

--- include/arctk/io/read.hpp
+++ include/arctk/io/read.hpp
@@ -38,80 +38,11 @@ namespace arc //! arctk namespace
 
             //  == FUNCTION PROTOTYPES ==
             //  -- Primitive --
-            template <typename T>
-            inline T str(const std::string& str_) noexcept;
-            template <typename T, typename S>
-            inline std::pair<T, S> str(const std::string& str_0_, const std::string& str_1_) noexcept;
 
 
 
             //  == FUNCTIONS ==
             //  -- Primitive --
-            template <typename T>
-            inline T str(const std::string& str_) noexcept
-            {
-                static_assert(std::is_fundamental<T>::value);
-
-                std::stringstream stream;
-                stream << str_;
-
-                T val{};
-                stream >> val;
-
-                if (stream.fail())
-                {
-                    std::cerr << "Unable to parse string: '" << str_ << "' to type.\n"
-                              << "String: '" << str_ << "' can not be parsed to type: '" << typeid(T).name() << "'.\n";
-
-                    std::exit(exit::error::FAILED_PARSE);
-                }
-
-                if (stream.rdbuf()->in_avail() != 0)
-                {
-                    std::cerr << "Unable to parse string to type.\n"
-                              << "String: '" << str_ << "' contains leftover characters after parsing to type: '" << typeid(T).name() << "'.\n";
-
-                    std::exit(exit::error::FAILED_PARSE);
-                }
-
-                return (val);
-            }
-
-            template <>
-            inline bool str(const std::string& str_) noexcept
-            {
-                if (str_ == "0")
-                {
-                    return (false);
-                }
-                if (str_ == "1")
-                {
-                    return (true);
-                }
-
-                std::string str = str_;
-                std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c_) { return std::tolower(c_); });
-
-                if (str == "false")
-                {
-                    return (false);
-                }
-                if (str == "true")
-                {
-                    return (true);
-                }
-
-                std::cerr << "Unable to parse string: '" << str_ << "' to type.\n"
-                          << "String: '" << str_ << "' can not be parsed to type: '" << typeid(bool).name() << "'.\n";
-
-                std::exit(exit::error::FAILED_PARSE);
-            }
-
-            template <typename T, typename S>
-            inline std::pair<T, S> str(const std::string& str_0_, const std::string& str_1_) noexcept
-            {
-                return (std::make_pair(str<T>(str_0_), str<S>(str_1_)));
-            }
 
 
 
