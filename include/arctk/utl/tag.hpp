08b11f90a0c8c03e2e0c8cadf210364d89807d5b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-23 02:07:47 +0100

    Added framework.

---
+++ include/arctk/utl/tag.hpp
@@ -0,0 +1,36 @@
+/**
+ *  @file   arctk/utl/tag.hpp
+ *  @date   23/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Tag dispatch class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_UTL_TAG_HPP
+#define ARCTK_UTL_TAG_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace utl //! utility namespace
+    {
+
+
+
+        //  == STRUCTURES ==
+        //  -- Tag --
+
+
+
+    } // namespace utl
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_UTL_TAG_HPP
