47d64a491a2a7ed59a206c93cf3af8cc90b07a0d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-24 21:42:58 +0100

    Fixed print operator prototype.

--- include/arctk/math/vec3.hpp
+++ include/arctk/math/vec3.hpp
@@ -97,8 +97,9 @@ namespace arc //! arctk namespace
             //  -- Conversion --
             constexpr inline explicit operator std::array<T, 3>() const noexcept;
 
-            //  -- Io --
-            constexpr inline std::ostream& operator<<(std::ostream& stream_) const noexcept;
+            //  -- Printing --
+            template <typename S>
+            friend constexpr inline std::ostream& operator<<(std::ostream& stream_, const Vec<S, 3>& vec_) noexcept;
 
             //  -- Assignment --
             constexpr inline Vec<T, 3>& operator+=(T val_) noexcept;
