ea7c9292bea9ea8b09d2d391d59b62c5745844b7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-24 11:43:47 +0100

    Added framework.

---
+++ include/arctk/io/tuple.hpp
@@ -0,0 +1,51 @@
+/**
+ *  @file   arctk/io/tuple.hpp
+ *  @date   24/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Tuple input/output functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_IO_TUPLE_HPP
+#define ARCTK_IO_TUPLE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <ostream>
+#include <tuple>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace io //! input/output namespace
+    {
+        namespace tuple //! tuple namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Pre-processing --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Pre-processing --
+
+
+
+        } // namespace tuple
+    }     // namespace io
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_IO_TUPLE_HPP
