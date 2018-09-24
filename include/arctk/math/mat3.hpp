52086105e315782c12d81c77b000f78ae03459f6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-24 21:56:53 +0100

    Fixed typos.

--- include/arctk/math/mat3.hpp
+++ include/arctk/math/mat3.hpp
@@ -135,7 +135,7 @@ namespace arc //! arctk namespace
         template <typename T>
         constexpr inline std::ostream& operator<<(std::ostream& stream_, const Mat<T, 3>& mat_) noexcept
         {
-            stream_ << io::format::OPENERS[io::format::container::MAT] << io::format::DELIM << x << io::format::DELIM << y << io::format::DELIM << z << io::format::CLOSERS[io::format::container::MAT];
+            stream_ << io::format::OPENERS[io::format::container::MAT] << io::format::DELIM << mat_.x << io::format::DELIM << mat_.y << io::format::DELIM << mat_.z << io::format::CLOSERS[io::format::container::MAT];
 
             return (stream_);
         }
