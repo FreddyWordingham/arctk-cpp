62d2ee0a7f751faa5799a2ff5ea956aeb076332f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-25 16:08:45 +0100

    Documented pre-condition.

--- include/arctk/geom/shape/mesh.hpp
+++ include/arctk/geom/shape/mesh.hpp
@@ -857,6 +857,8 @@ namespace arc //! arctk namespace
              *
              *  @param  rng_    Random number generator.
              *
+             *  @pre    rng_ may not be nullptr.
+             *
              *  @return Random position on the surface of the mesh.
              */
             inline vec3 Mesh::random_pos(random::Generator* const rng_) const noexcept
