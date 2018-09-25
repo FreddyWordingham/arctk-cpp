5ac4da99139807556e5836a142e857e404c5545a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-25 15:30:33 +0100

    Fixed typo.

--- include/arctk/io.hpp
+++ include/arctk/io.hpp
@@ -189,7 +189,7 @@ inline std::ostream& operator<<(std::ostream& stream_, const std::string& str_) 
 }
 
 /**
- *  Print an string to a stream.
+ *  Print an pair to a stream.
  *
  *  @tparam T   First stored by the pair.
  *  @tparam S   Second stored by the pair.
