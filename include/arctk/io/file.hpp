aac7f1beadeea7739464d70b0c0ba8a03f653e28
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-25 13:21:26 +0100

    Added framework.

---
+++ include/arctk/io/file.hpp
@@ -0,0 +1,47 @@
+/**
+ *  @file   arctk/io/file.hpp
+ *  @date   25/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  File functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_IO_FILE_HPP
+#define ARCTK_IO_FILE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace io //! input/output namespace
+    {
+        namespace file //! file namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace file
+    }     // namespace io
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_IO_FILE_HPP
