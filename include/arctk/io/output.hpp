6da397aca54bf197d530a24396cb5f9f178e2bf2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-25 01:50:36 +0100

    Added framework.

---
+++ include/arctk/io/output.hpp
@@ -0,0 +1,47 @@
+/**
+ *  @file   arctk/io/output.hpp
+ *  @date   25/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Output functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_IO_OUTPUT_HPP
+#define ARCTK_IO_OUTPUT_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace io //! input/output namespace
+    {
+        namespace output //! output namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace output
+    }     // namespace io
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_IO_OUTPUT_HPP
