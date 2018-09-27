117b04d6603e2ea4debd582fbb05dbbf455b9c2e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-27 01:06:37 +0100

    Added framework.

---
+++ include/arctk/phys/particle/photon.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @file   arctk/phys/particle/photon.hpp
+ *  @date   27/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Photon particle physics class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_PARTICLE_PHOTON_HPP
+#define ARCTK_PHYS_PARTICLE_PHOTON_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+        namespace particle //! particle namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Photon particle class.
+             */
+            class Photon
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+            //  -- Getters --
+
+
+
+        } // namespace particle
+    }     // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_PARTICLE_PHOTON_HPP
