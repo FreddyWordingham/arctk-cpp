159e5bf165455ef3808d17b583be7a6335d2174c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-27 14:21:48 +0100

    Corrected import.

--- include/arctk/phys/material/fluorescent.hpp
+++ include/arctk/phys/material/fluorescent.hpp
@@ -22,7 +22,7 @@
 //  -- Arctk --
 #include <arctk/debug.hpp>
 #include <arctk/math.hpp>
-#include <arctk/phys/cell.hpp>
+#include <arctk/phys/cell/fluorescent.hpp>
 #include <arctk/phys/material.hpp>
 
 
