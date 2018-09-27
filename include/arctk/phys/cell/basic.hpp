622fb876a5e0b00ba604a00d41d6300e7319a9d5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-27 13:42:34 +0100

    Added framework.

---
+++ include/arctk/phys/cell/basic.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @file   arctk/phys/cell/basic.hpp
+ *  @date   27/09/2018
+ *  @author Freddy Wordingham
+ *
+ *  Basic cell physics class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_CELL_BASIC_HPP
+#define ARCTK_PHYS_CELL_BASIC_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/phys/cell.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+        namespace cell //! cell namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Basic material particle class.
+             */
+            class Basic : public Cell
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+
+
+
+        } // namespace cell
+    }     // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_CELL_BASIC_HPP
