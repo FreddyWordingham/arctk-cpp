6d1a3c89fb5c57aec58358681fc68c36352f4872
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-27 01:01:50 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -121,6 +121,7 @@
 #include <arctk/phys/material/fluorescent.hpp>
 #include <arctk/phys/optics.hpp>
 #include <arctk/phys/particle.hpp>
+#include <arctk/phys/particle/photon.hpp>
 #include <arctk/random.hpp>
 #include <arctk/random/distribution.hpp>
 #include <arctk/random/distribution/constant.hpp>
