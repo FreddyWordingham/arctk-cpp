9e0e2c7861c2133f36f9f5f6073b56f3234e156e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-09-28 12:30:35 +0100

    Added missing namespace qualifier.

--- include/arctk/phys/material/basic.hpp
+++ include/arctk/phys/material/basic.hpp
@@ -24,6 +24,7 @@
 #include <arctk/math.hpp>
 #include <arctk/phys/cell/basic.hpp>
 #include <arctk/phys/material.hpp>
+#include <arctk/random.hpp>
 
 
 
@@ -72,7 +73,7 @@ namespace arc //! arctk namespace
                 //  == METHODS ==
               public:
                 //  -- Getters --
-                inline double interact(Generator* const rng_, const particle::Photon& phot_, const Cell& /*unused*/) const noexcept override;
+                inline double interact(random::Generator* const rng_, const particle::Photon& phot_, const Cell& /*unused*/) const noexcept override;
 
                 //  -- Cells --
                 inline std::unique_ptr<Cell> create_cell(const vec3& min_, const vec3& max_) const noexcept override;
@@ -231,9 +232,9 @@ namespace arc //! arctk namespace
 
             //  == METHODS ==
             //  -- Getters --
-            inline double Basic::interact(Generator* const rng_, const particle::Photon& phot_, const Cell& /*unused*/) const noexcept
+            inline double Basic::interact(random::Generator* const rng_, const particle::Photon& phot_, const Cell& /*unused*/) const noexcept
             {
-                return (1.0);
+                return (-std::log(rng_.gen()) / _interact_coef(phot_.wavelength()));
             }
 
 
