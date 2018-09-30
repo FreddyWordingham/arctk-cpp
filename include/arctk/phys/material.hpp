9d5355d5eed03930a8d15478de76956aca04bedc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-01 00:03:13 +0100

    Added virtual pure ref_index method.

--- include/arctk/phys/material.hpp
+++ include/arctk/phys/material.hpp
@@ -74,7 +74,8 @@ namespace arc //! arctk namespace
             //  -- Cells --
             virtual std::unique_ptr<Cell> create_cell(const vec3& min_, const vec3& max_) const noexcept = 0;
 
-            //  -- Interaction --
+            //  -- Optical Properties --
+            virtual double ref_index() const noexcept                                                                                    = 0;
             virtual double interact_dist(random::Generator* const rng_, const particle::Photon& phot_, const Cell& cell_) const noexcept = 0;
             virtual void   interact(random::Generator* const rng_, particle::Photon* const phot_, Cell* const cell_) const noexcept      = 0;
         };
--- include/arctk/phys/material/fluorescent.hpp
+++ include/arctk/phys/material/fluorescent.hpp
@@ -78,7 +78,7 @@ namespace arc //! arctk namespace
                 //  -- Cells --
                 inline std::unique_ptr<Cell> create_cell(const vec3& min_, const vec3& max_) const noexcept override;
 
-                //  -- Interaction --
+                //  -- Optical Properties --
                 inline double interact_dist(random::Generator* const rng_, const particle::Photon& phot_, const Cell& /*unused*/) const noexcept override;
                 inline void   interact(random::Generator* const rng_, particle::Photon* const phot_, Cell* const cell_) const noexcept override;
             };
