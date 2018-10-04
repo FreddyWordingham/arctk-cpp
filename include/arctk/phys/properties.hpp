905725bbac7e3a3674978fd255eb9be887933019
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-04 17:39:19 +0100

    Added framework.

---
+++ include/arctk/phys/properties.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @file   arctk/phys/properties.hpp
+ *  @date   04/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Optical properties physics class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_PROPERTIES_HPP
+#define ARCTK_PHYS_PROPERTIES_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Optical properties class.
+         */
+        class Properties
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Optical --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_PROPERTIES_HPP
