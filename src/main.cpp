52bb8612d939e2301d0d33a0d7d1276d0d8be9ca
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-06 22:34:03 +0100

    Linked to main.

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,7 @@
 #include <arctk/geom/collision.hpp>
 #include <arctk/geom/shape.hpp>
 #include <arctk/geom/shape/box.hpp>
+#include <arctk/geom/shape/mesh.hpp>
 #include <arctk/geom/shape/triangle.hpp>
 #include <arctk/gui.hpp>
 #include <arctk/gui/actor.hpp>
