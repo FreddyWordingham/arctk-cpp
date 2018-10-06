d4f1deb362932c8caa9188c2098275883a573638
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-06 23:40:19 +0100

    Added init_tris method.

--- include/arctk/geom/shape/mesh.hpp
+++ include/arctk/geom/shape/mesh.hpp
@@ -23,6 +23,7 @@
 //  -- Arctk --
 #include <arctk/debug.hpp>
 #include <arctk/geom/shape/triangle.hpp>
+#include <arctk/index.hpp>
 #include <arctk/math.hpp>
 
 
@@ -66,6 +67,10 @@ namespace arc //! arctk namespace
                 //  -- Constructors --
                 inline Mesh(const std::vector<vec3>& poss_, const std::vector<vec3>& norms_, const std::vector<std::pair<std::array<size_t, 3>, std::array<size_t, 3>>>& faces_) noexcept;
 
+              private:
+                //  -- Initialisation --
+                inline std::vector<Triangle> init_tris(const std::vector<vec3>& poss_, const std::vector<vec3>& norms_, const std::vector<std::pair<std::array<size_t, 3>, std::array<size_t, 3>>>& faces_) const noexcept;
+
 
                 //  == METHODS ==
               public:
@@ -86,6 +91,37 @@ namespace arc //! arctk namespace
               , _num_faces(init_num_faces())
               , _closed((_num_verts + _num_faces - _num_edges) == 2)
             {
+            }
+
+
+            //  -- Initialisation --
+            inline std::vector<Triangle> Mesh::init_tris(const std::vector<vec3>& poss_, const std::vector<vec3>& norms_, const std::vector<std::pair<std::array<size_t, 3>, std::array<size_t, 3>>>& faces_) const noexcept
+            {
+                PRE(poss_.size() >= 3);
+                PRE(!norms_.empty());
+                PRE(!faces_.empty());
+
+                std::vector<Triangle> tris;
+                tris.reserve(faces_.size());
+
+                for (size_t i = 0; i < faces_.size(); ++i)
+                {
+                    const std::array<size_t, 3>& pos_indices  = faces_[i].first;
+                    const std::array<size_t, 3>& norm_indices = faces_[i].second;
+
+                    for (size_t j = 0; j < 3; ++j)
+                    {
+                        PRE(pos_indices[j] < poss_.size());
+                        PRE(norm_indices[j] < norms_.size());
+                    }
+
+                    tris.emplace_back(std::array<vec3, 3>({{poss_[pos_indices[index::dim::cartesian::X]], poss_[pos_indices[index::dim::cartesian::Y]], poss_[pos_indices[index::dim::cartesian::Z]]}}),
+                                      std::array<vec3, 3>({{norms_[norm_indices[index::dim::cartesian::X]], norms_[norm_indices[index::dim::cartesian::Y]], norms_[norm_indices[index::dim::cartesian::Z]]}}));
+                }
+
+                POST(!tris.empty());
+
+                return (tris);
             }
 
 
