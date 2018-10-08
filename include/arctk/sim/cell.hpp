41843cd010d446683140c1fc7d41c838ad213956
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-08 21:57:59 +0100

    Added interactions field.

--- include/arctk/sim/cell.hpp
+++ include/arctk/sim/cell.hpp
@@ -38,6 +38,7 @@ namespace arc //! arctk namespace
             //  == FIELDS ==
           private:
             //  -- Data --
+            double _interactions;
 
 
             //  == INSTANTIATION ==
