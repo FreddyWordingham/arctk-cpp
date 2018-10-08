ccea138ad54921374d68ea9ec724f2da69df9eac
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-08 21:54:31 +0100

    Added framework.

---
+++ include/arctk/scene.hpp
@@ -0,0 +1,23 @@
+/**
+ *  @file   arctk/scene.hpp
+ *  @date   08/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of scene classes.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SCENE_HPP
+#define ARCTK_SCENE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SCENE_HPP
