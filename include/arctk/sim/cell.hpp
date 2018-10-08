d7e055aede7e61aad9670fd8e841b309639ae0d1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-08 21:56:55 +0100

    Added framework.

---
+++ include/arctk/sim/cell.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @file   arctk/scene/cell.hpp
+ *  @date   08/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Scene cell information class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SCENE_CELL_HPP
+#define ARCTK_SCENE_CELL_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace scene //! scene namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Scene information cell.
+         */
+        class Cell
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Data --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == METHODS ==
+
+
+
+    } // namespace scene
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SCENE_CELL_HPP
