e77fe4801474b1095a6373f4dd759a82fd3d86fe
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-08 21:22:57 +0100

    Added framework.

---
+++ include/arctk/geom/intersection.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/geom/intersection.hpp
+ *  @date   08/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Geometric intersection functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_INTERSECTION_HPP
+#define ARCTK_GEOM_INTERSECTION_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/math.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+        namespace intersection //! intersection namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace intersection
+    }     // namespace geom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_INTERSECTION_HPP
