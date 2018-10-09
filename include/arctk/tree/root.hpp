2a5bf929d03a9c9941d6c41a3784982a5c1faa4a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-09 23:42:52 +0100

    Began work on init_trunk method.

--- include/arctk/tree/root.hpp
+++ include/arctk/tree/root.hpp
@@ -38,18 +38,22 @@ namespace arc //! arctk namespace
         /**
          *  Tree node base class.
          */
-        class Root : public geom::shape::Box
+        class Root
         {
             //  == FIELDS ==
           protected:
             //  -- Depth --
-            const std::unique_ptr<Node>& _trunk;
+            const std::unique_ptr<Node> _trunk;
 
 
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
             inline Root(const vec3& min_, const vec3& max_, const std::vector<const equip::Entity*>& ents_, size_t max_depth_, size_t tar_tris_) noexcept;
+
+          private:
+            //  -- Initialsation --
+            inline std::unique_ptr<Node> init_trunk(const vec3& min_, const vec3& max_, const std::vector<const equip::Entity*>& ents_, size_t max_depth_, size_t tar_tris_) const noexcept;
 
 
             //  == METHODS ==
@@ -62,13 +66,26 @@ namespace arc //! arctk namespace
         //  == INSTANTIATION ==
         //  -- Constructors --
         inline Root::Root(const vec3& min_, const vec3& max_, const std::vector<const equip::Entity*>& ents_, const size_t max_depth_, const size_t tar_tris_) noexcept
-          : _trunk(init_trunk())
+          : _trunk(init_trunk(min_, max_, ents_, max_depth_, tar_tris_))
         {
             PRE(min_.x <= max_.x);
             PRE(min_.y <= max_.y);
             PRE(min_.z <= max_.z);
             PRE(max_depth_ > 0);
             PRE(tar_tris_ > 0);
+        }
+
+
+        //  -- Initialisation --
+        inline Root::init_trunk(const vec3& min_, const vec3& max_, const std::vector<const equip::Entity*>& ents_, const size_t max_depth_, const size_t tar_tris_) noexcept
+        {
+            std::vector<std::pair<const equip::Entity*, std::vector<const geom::shape::Triangle*>>> ent_tris;
+            ent_tris.reserve(ents_.size());
+
+            for (size_t i = 0; i < ent_tris.size(); ++i)
+            {
+                ent_tris.emplace_back(std::make_pair<const equip::Entity*, std::vector<const geom::shape::Triangle*>>(ent_tris[i], ent_tris[i]->tri_list()));
+            }
         }
 
 
