f2ca2d95a8caf32b249e46272fa4b5a415f1f3df
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-09 01:51:13 +0100

    Removed exception specification from copy and move constructor methods.

--- include/arctk/equip/entity.hpp
+++ include/arctk/equip/entity.hpp
@@ -40,8 +40,8 @@ namespace arc //! arctk namespace
           public:
             //  -- Constructors --
             inline Entity(const std::string& serial_, const vec3& scale_, const vec3& rot_, const vec3& trans_) noexcept;
-            inline Entity(const Entity&) noexcept = default; //!< Defaulted copy constructor.
-            inline Entity(Entity&&) noexcept      = default; //!< Defaulted move constructor.
+            inline Entity(const Entity&) = default; //!< Defaulted copy constructor.
+            inline Entity(Entity&&)      = default; //!< Defaulted move constructor.
 
             //  -- Destructors --
             inline ~Entity() noexcept override;
