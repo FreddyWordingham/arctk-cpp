7f94c5230f63e33c0682a10b1c76626c7e1cbbf0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-09 14:43:25 +0100

    Added virtual pure getter methods.

--- include/arctk/tree/node.hpp
+++ include/arctk/tree/node.hpp
@@ -74,7 +74,11 @@ namespace arc //! arctk namespace
             //  == METHODS ==
           public:
             //  -- Getters --
-            inline size_t depth() const noexcept;
+            inline size_t                         depth() const noexcept;
+            virtual size_t                        max_depth() const noexcept  = 0;
+            virtual size_t                        max_shapes() const noexcept = 0;
+            virtual size_t                        num_nodes() const noexcept  = 0;
+            virtual std::vector<geom::shape::Box> boxes() const noexcept      = 0;
 
             //  -- Retrieval --
             virtual const node::Leaf& leaf(const vec3& pos_) const noexcept = 0;
