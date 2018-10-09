bf0e6e85f328ffce6847cd3ea152ac0d0cb153fc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-09 13:44:33 +0100

    Added constructor method.

--- include/arctk/tree/node/branch.hpp
+++ include/arctk/tree/node/branch.hpp
@@ -58,6 +58,7 @@ namespace arc //! arctk namespace
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
+                inline Branch(const vec3& min_, const vec3& max_, const std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>>& ents_, size_t cur_depth_, size_t max_depth_, size_t tar_tris_) noexcept;
 
 
                 //  == METHODS ==
@@ -69,6 +70,17 @@ namespace arc //! arctk namespace
 
             //  == INSTANTIATION ==
             //  -- Constructors --
+            inline Branch::Branch(const vec3& min_, const vec3& max_, const std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>>& ents_, const size_t cur_depth_, const size_t max_depth_, const size_t tar_tris_) noexcept
+              : Node(min_, max_, cur_depth_)
+              , _centre((min_ + max_) / 2.0)
+              , _childs(init_childs(min_, max_, ents_, cur_depth_, max_depth_, tar_tris_))
+            {
+                PRE(min_.x <= max_.x);
+                PRE(min_.y <= max_.y);
+                PRE(min_.z <= max_.z);
+                PRE(cur_depth_ > 0);
+            }
+
             //  -- Initialisation --
 
 
