abf9fc0076e2d91ecdc7f005501f2bd7bb70d188
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-09 02:30:02 +0100

    Added bump constant.

---
+++ include/arctk/consts/num.hpp
@@ -0,0 +1,40 @@
+/**
+ *  @file   arctk/consts/num.hpp
+ *  @date   09/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Numerical precision constants.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_CONSTS_NUM_HPP
+#define ARCTK_CONSTS_NUM_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace consts //! constants namespace
+    {
+        namespace num //! numerical precision namespace
+        {
+
+
+
+            //  == CONSTANTS ==
+            //  -- Buffering --
+            const double BUMP = 1e-6;
+
+
+
+        } // namespace num
+    }     // namespace consts
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_CONSTS_NUM_HPP
