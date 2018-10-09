7b05329a771569fce57dbed6667fdf90f194b187
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-09 14:31:32 +0100

    Added init_childs.

--- include/arctk/tree/node/branch.hpp
+++ include/arctk/tree/node/branch.hpp
@@ -17,7 +17,7 @@
 //  == IMPORTS ==
 //  -- Std --
 #include <array>
-#include <functional>
+#include <memory>
 
 //  -- Arctk --
 #include <arctk/debug.hpp>
@@ -52,13 +52,18 @@ namespace arc //! arctk namespace
                 const vec3 _centre; //!< Centre coordinate of the node.
 
                 //  -- Children --
-                const std::array<std::array<std::array<std::reference_wrapper<Node>, 2>, 2>, 2> _childs;
+                const std::array<std::array<std::array<std::unique_ptr<Node>, 2>, 2>, 2> _childs;
 
 
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-                inline Branch(const vec3& min_, const vec3& max_, const std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>>& ents_, size_t cur_depth_, size_t max_depth_, size_t tar_tris_) noexcept;
+                inline Branch(const vec3& min_, const vec3& max_, const std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>>& tris_, size_t cur_depth_, size_t max_depth_, size_t tar_tris_) noexcept;
+
+              private:
+                //  -- Initialisation --
+                inline std::array<std::array<std::array<std::unique_ptr<Node>, 2>, 2>, 2> init_childs(const vec3& min_, const vec3& max_, const std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>>& tris_, size_t cur_depth_,
+                                                                                                      size_t max_depth_, size_t tar_tris_) const noexcept;
 
 
                 //  == METHODS ==
@@ -70,10 +75,10 @@ namespace arc //! arctk namespace
 
             //  == INSTANTIATION ==
             //  -- Constructors --
-            inline Branch::Branch(const vec3& min_, const vec3& max_, const std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>>& ents_, const size_t cur_depth_, const size_t max_depth_, const size_t tar_tris_) noexcept
+            inline Branch::Branch(const vec3& min_, const vec3& max_, const std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>>& tris_, const size_t cur_depth_, const size_t max_depth_, const size_t tar_tris_) noexcept
               : Node(min_, max_, cur_depth_)
               , _centre((min_ + max_) / 2.0)
-              , _childs(init_childs(min_, max_, ents_, cur_depth_, max_depth_, tar_tris_))
+              , _childs(init_childs(min_, max_, tris_, cur_depth_, max_depth_, tar_tris_))
             {
                 PRE(min_.x <= max_.x);
                 PRE(min_.y <= max_.y);
@@ -81,7 +86,50 @@ namespace arc //! arctk namespace
                 PRE(cur_depth_ > 0);
             }
 
+
             //  -- Initialisation --
+            inline std::array<std::array<std::array<std::unique_ptr<Node>, 2>, 2>, 2> Branch::init_childs(const vec3& min_, const vec3& max_, const std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>>& tris_, const size_t cur_depth_,
+                                                                                                          const size_t max_depth_, const size_t tar_tris_) const noexcept
+            {
+                std::array<std::array<std::array<std::unique_ptr<Node>, 2>, 2>, 2> childs;
+
+                const vec3 size                = half_width();
+                const bool depth_limit_reached = cur_depth_ >= max_depth_;
+
+                for (size_t i = 0; i < 2; ++i)
+                {
+                    for (size_t j = 0; j < 2; ++j)
+                    {
+                        for (size_t k = 0; k < 2; ++k)
+                        {
+                            const vec3 min(min_.x + (i * size.x), min_.y + (j * size.y), min_.z + (k * size.z));
+                            const vec3 max = min + size;
+
+                            const geom::shape::Box                                                     box(min, max);
+                            std::vector<std::pair<const equip::Entity&, const geom::shape::Triangle&>> tris;
+
+                            for (size_t l = 0; l < tris_.size(); ++l)
+                            {
+                                if (box.intersect(tris_[l].second))
+                                {
+                                    tris.emplace_back(tris_[l]);
+                                }
+                            }
+
+                            if (depth_limit_reached || (tris.size() <= tar_tris_))
+                            {
+                                childs[i][j][k] = std::make_unique<Leaf>(min, max, tris, cur_depth_ + 1);
+                            }
+                            else
+                            {
+                                childs[i][j][k] = std::make_unique<Branch>(min, max, tris, cur_depth_ + 1, max_depth_, tar_tris_);
+                            }
+                        }
+                    }
+                }
+
+                return (childs);
+            }
 
 
 
