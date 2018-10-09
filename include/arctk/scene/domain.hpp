9d7c6ca9e4a577bb1edfe2de0ac658b8ac8960e1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-09 01:05:07 +0100

    Added init_cells initialisation method.

--- include/arctk/scene/domain.hpp
+++ include/arctk/scene/domain.hpp
@@ -54,6 +54,10 @@ namespace arc //! arctk namespace
             //  -- Constructors --
             inline Domain(const vec3& min_, const vec3& max_, const std::array<size_t, 3>& res_) noexcept;
 
+          private:
+            //  -- Initialisation --
+            inline std::vector<std::vector<std::vector<Cell>>> init_cells(const vec3& min_, const vec3& max_, const std::array<size_t, 3>& res_) const noexcept;
+
 
             //  == METHODS ==
           public:
@@ -66,7 +70,7 @@ namespace arc //! arctk namespace
         inline Domain::Domain(const vec3& min_, const vec3& max_, const std::array<size_t, 3>& res_) noexcept
           : Box(min_, max_)
           , _res(res_)
-          , _cells(std::vector<std::vector<std::vector<Cell>>>(res_[index::dim::cartesian::X], std::vector<std::vector<Cell>>(res_[index::dim::cartesian::Y], std::vector<Cell>(res_[index::dim::cartesian::Z]))))
+          , _cells(init_cells(min_, max_, res_))
         {
             PRE(min_.x <= max_.x);
             PRE(min_.y <= max_.y);
@@ -74,6 +78,43 @@ namespace arc //! arctk namespace
             PRE(res_[index::dim::cartesian::X] > 0);
             PRE(res_[index::dim::cartesian::Y] > 0);
             PRE(res_[index::dim::cartesian::Z] > 0);
+        }
+
+
+        //  -- Initialisation --
+        inline std::vector<std::vector<std::vector<Cell>>> Domain::init_cells(const vec3& min_, const vec3& max_, const std::array<size_t, 3>& res_) const noexcept
+        {
+            PRE(min_.x <= max_.x);
+            PRE(min_.y <= max_.y);
+            PRE(min_.z <= max_.z);
+            PRE(res_[index::dim::cartesian::X] > 0);
+            PRE(res_[index::dim::cartesian::Y] > 0);
+            PRE(res_[index::dim::cartesian::Z] > 0);
+
+            vec3 size = max_ - min_;
+            size.x /= res_[index::dim::cartesian::X];
+            size.y /= res_[index::dim::cartesian::Y];
+            size.z /= res_[index::dim::cartesian::Z];
+
+            std::vector<std::vector<std::vector<Cell>>> cells(res_[index::dim::cartesian::X], std::vector<std::vector<Cell>>(res_[index::dim::cartesian::Y]));
+            for (size_t i = 0; i < res_[index::dim::cartesian::X]; ++i)
+            {
+                const double x = min_.x + (i * size.x);
+
+                for (size_t j = 0; j < res_[index::dim::cartesian::Y]; ++j)
+                {
+                    const double y = min_.y + (j * size.y);
+
+                    for (size_t k = 0; k < res_[index::dim::cartesian::Z]; ++k)
+                    {
+                        const double z = min_.z + (k * size.z);
+
+                        cells[i][j].emplace_back(Cell(vec3(x, y, z), vec3(x, y, z) + size));
+                    }
+                }
+            }
+
+            return (cells);
         }
 
 
