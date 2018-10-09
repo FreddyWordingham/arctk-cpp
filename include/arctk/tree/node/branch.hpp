31bf72354812cc50eb24b7484c9d3153f6eb46ee
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-09 12:04:16 +0100

    Added framework.

---
+++ include/arctk/tree/node/branch.hpp
@@ -0,0 +1,79 @@
+/**
+ *  @file   arctk/tree/node/branch.hpp
+ *  @date   09/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Branch node class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_TREE_NODE_BRANCH_HPP
+#define ARCTK_TREE_NODE_BRANCH_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <array>
+
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/equip.hpp>
+#include <arctk/geom.hpp>
+#include <arctk/math.hpp>
+#include <arctk/tree/node/leaf.hpp>
+#include <arctk/utl.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace tree //! tree namespace
+    {
+        namespace node //! node namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Branch node class.
+             */
+            class Branch : public Node
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Retrieval --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+            //  -- Initialisation --
+
+
+
+            //  == METHODS ==
+            //  -- Retrieval --
+
+
+
+        } // namespace node
+    }     // namespace tree
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_TREE_NODE_BRANCH_HPP
