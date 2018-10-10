e6085591f4bbf054d23b2115f92c08d6078512b3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-10 16:41:26 +0100

    Added interact_dist virtual pure method.

--- include/arctk/phys/env.hpp
+++ include/arctk/phys/env.hpp
@@ -48,6 +48,7 @@ namespace arc //! arctk namespace
             //  == METHODS ==
           public:
             //  -- Getters --
+            virtual double interact_dist(random::Generator* rng_, const phys::Photon& phot_, const phys::Cell& cell_) const noexcept = 0;
 
             //  -- Interaction --
             virtual bool interact(random::Generator* rng_, phys::Photon* phot_, phys::Cell* cell_, const double dist_) const noexcept = 0;
