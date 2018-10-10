184ae14ce21cbb6e47f71a1405dd98e045f67c2d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-10 16:29:00 +0100

    Added framework.

---
+++ include/arctk/phys/env.hpp
@@ -0,0 +1,63 @@
+/**
+ *  @file   arctk/phys/env.hpp
+ *  @date   10/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Physical environment control class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PHYS_ENV_HPP
+#define ARCTK_PHYS_ENV_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Photon environment control class.
+         */
+        class Env
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Data --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+    } // namespace phys
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PHYS_ENV_HPP
