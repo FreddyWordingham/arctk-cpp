df111b995a753e20d02804400e5ab4b8951b8e96
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-12 17:16:04 +0100

    Added constructor method.

--- include/arctk/phys/mat/dumb.hpp
+++ include/arctk/phys/mat/dumb.hpp
@@ -39,18 +39,20 @@ namespace arc //! arctk namespace
             /**
              *  Material with dumb optical properties.
              */
-            class Dumb
+            class Dumb : public Sop
             {
                 //  == INSTANTIATION ==
               private:
                 //  -- Optical Properties --
                 const double _dist;
+                const double _albedo;
+                const double _asym;
 
 
                 //  == INSTANTIATION ==
               public:
                 //  -- Constructors --
-                inline Dumb() noexcept;
+                inline Dumb(double ref_index_, double dist_, double albedo_ = 1.0, double asym_ = 1.0) noexcept;
 
 
                 //  == METHODS ==
@@ -62,7 +64,18 @@ namespace arc //! arctk namespace
 
 
             //  == INSTANTIATION ==
-            //  -- Destructors --
+            //  -- Constructors --
+            inline Dumb::Dumb(const double ref_index_, const double dist_, const double albedo_, const double asym_) noexcept
+              : Sop(ref_index_)
+              , _dist(dist_)
+              , _albedo(albedo_)
+              , _asym(asym_)
+            {
+                PRE(ref_index_ >= 1.0);
+                PRE(dist_ > 0.0);
+                PRE((albedo_ >= 0.0) || (albedo_ <= 1.0));
+                PRE((asym_ >= -1.0) || (asym_ <= 1.0));
+            }
 
 
 
