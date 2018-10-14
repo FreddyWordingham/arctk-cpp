2ca5746688942b91c13f65f5eed49184083e8c0a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-14 01:19:55 +0100

    Added framework.

---
+++ include/arctk/equip/entity/light.hpp
@@ -0,0 +1,76 @@
+/**
+ *  @file   arctk/equip/entity/light.hpp
+ *  @date   14/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Light source entity equipment base class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EQUIP_ENTITY_LIGHT_HPP
+#define ARCTK_EQUIP_ENTITY_LIGHT_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/consts.hpp>
+#include <arctk/debug.hpp>
+#include <arctk/equip/entity.hpp>
+#include <arctk/phys.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace equip //! equipment namespace
+    {
+        namespace entity //! entity namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Light entity equipment base class.
+             */
+            class Light : public Entity
+            {
+                //  == FIELDS ==
+              private:
+                //  -- Absorption --
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              private:
+                //  -- Collision --
+            };
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+            //  -- Collision --
+
+
+
+        } // namespace entity
+    }     // namespace equip
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EQUIP_ENTITY_LIGHT_HPP
