606aaa55e94f45944d75a5af468b361ed5017938
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-14 01:32:54 +0100

    Added framework.

---
+++ include/arctk/equip/entity/light/laser.hpp
@@ -0,0 +1,73 @@
+/**
+ *  @file   arctk/equip/entity/light/laser.hpp
+ *  @date   14/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Laser light source entity class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EQUIP_ENTITY_LIGHT_LASER_HPP
+#define ARCTK_EQUIP_ENTITY_LIGHT_LASER_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/debug.hpp>
+#include <arctk/equip/entity/light.hpp>
+#include <arctk/phys.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace equip //! equipment namespace
+    {
+        namespace entity //! entity namespace
+        {
+            namespace light //! light source namespace
+            {
+
+
+
+                //  == CLASS ==
+                /**
+                 *  Laser light entity equipment class.
+                 */
+                class Laser : public Light
+                {
+                    //  == FIELDS ==
+                  private:
+                    //  == INSTANTIATION ==
+                  public:
+                    //  -- Constructors --
+
+
+                    //  == METHODS ==
+                  private:
+                };
+
+
+
+                //  == INSTANTIATION ==
+                //  -- Constructors --
+
+
+
+                //  == METHODS ==
+
+
+
+            } // namespace light
+        }     // namespace entity
+    }         // namespace equip
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EQUIP_ENTITY_LIGHT_LASER_HPP
