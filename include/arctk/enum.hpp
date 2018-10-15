bb2477bb2b93aea9b8080687fa0778d5ae0579b3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-15 02:02:54 +0100

    Added framework.

---
+++ include/arctk/enum.hpp
@@ -0,0 +1,23 @@
+/**
+ *  @file   arctk/enum.hpp
+ *  @date   15/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection class enumerations.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_ENUM_HPP
+#define ARCTK_ENUM_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_ENUM_HPP
