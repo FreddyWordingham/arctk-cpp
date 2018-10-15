2246c639aa8872b729e5d279cd76a68579a2469b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-15 12:00:13 +0100

    Added interaction measurement.

--- include/arctk/phys/sop/dumb.hpp
+++ include/arctk/phys/sop/dumb.hpp
@@ -61,7 +61,7 @@ namespace arc //! arctk namespace
                 inline double interact_dist(random::Generator* /*unused*/, const phys::Cell* /*unused*/) const noexcept override;
 
                 //  -- Interaction --
-                inline bool interact(random::Generator* rng_, phys::Photon* phot_, const phys::Cell* /*unused*/, double dist_) const noexcept override;
+                inline bool interact(random::Generator* rng_, phys::Photon* phot_, const phys::Cell* cell_, double dist_) const noexcept override;
             };
 
 
@@ -91,8 +91,10 @@ namespace arc //! arctk namespace
 
 
             //  -- Interaction --
-            inline bool Dumb::interact(random::Generator* rng_, phys::Photon* phot_, const phys::Cell* /*unused*/, const double dist_) const noexcept
+            inline bool Dumb::interact(random::Generator* rng_, phys::Photon* phot_, const phys::Cell* cell_, const double dist_) const noexcept
             {
+                cell_->add_interaction(phot->weight());
+
                 phot_->move(dist_, _ref_index);
                 phot_->rotate(random::distribution::henyey_greenstein(rng_, _asym), rng_->gen() * consts::math::TWO_PI);
                 phot_->multiply_weight(_albedo);
