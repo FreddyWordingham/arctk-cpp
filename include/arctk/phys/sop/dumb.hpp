9feaca121cfa95a52b068cc6a188a2490285f5bc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-15 01:50:27 +0100

    Changed parameter type from reference to pointer.

--- include/arctk/phys/sop/dumb.hpp
+++ include/arctk/phys/sop/dumb.hpp
@@ -58,7 +58,7 @@ namespace arc //! arctk namespace
                 //  == METHODS ==
               public:
                 //  -- Getters --
-                inline double interact_dist(random::Generator* /*unused*/, const phys::Cell& /*unused*/) const noexcept override;
+                inline double interact_dist(random::Generator* /*unused*/, const phys::Cell* /*unused*/) const noexcept override;
 
                 //  -- Interaction --
                 inline bool interact(random::Generator* rng_, phys::Photon* phot_, const phys::Cell* /*unused*/, double dist_) const noexcept override;
