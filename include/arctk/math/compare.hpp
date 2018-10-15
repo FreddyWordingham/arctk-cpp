a5f806aabb33a95ecacab3a25b8ed0ecc3bc52b6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-15 03:04:13 +0100

    Added tolerance parameter.

--- include/arctk/math/compare.hpp
+++ include/arctk/math/compare.hpp
@@ -33,11 +33,11 @@ namespace arc //! arctk namespace
             //  == FUNCTION PROTOTYPES ==
             //  -- Comparison --
             template <typename T>
-            inline bool zero(T x_) noexcept;
+            inline bool zero(T x_, T tol_ = std::numeric_limits<T>::epsilon()) noexcept;
             template <typename T>
-            inline bool unity(T x_) noexcept;
+            inline bool unity(T x_, T tol_ = std::numeric_limits<T>::epsilon()) noexcept;
             template <typename T>
-            inline bool equal(T x_, T y_) noexcept;
+            inline bool equal(T x_, T y_, T tol_ = std::numeric_limits<T>::epsilon()) noexcept;
 
 
 
@@ -45,51 +45,54 @@ namespace arc //! arctk namespace
             //  -- Comparison --
             /**
              *  Test if a value can be considered equal to zero.
-             *  Value is considered equal to zero if it is within epsilon of zero.
+             *  Value is considered equal to zero if it is within the given tolerance of zero.
              *
              *  @tparam T   Type of value to test.
              *
-             *  @param  x_  Value to test.
+             *  @param  x_      Value to test.
+             *  @param  tol_    Tolerance of the test/
              *
              *  @return true if the value can be considered equal to zero.
              */
             template <typename T>
-            inline bool zero(const T x_) noexcept
+            inline bool zero(const T x_, const T tol_) noexcept
             {
-                return ((x_ > -std::numeric_limits<double>::epsilon()) && (x_ < std::numeric_limits<double>::epsilon()));
+                return ((x_ > -tol_) && (x_ < tol_));
             }
 
             /**
              *  Test if a value can be considered equal to unity.
-             *  Value is considered equal to unity if it is within epsilon of one.
+             *  Value is considered equal to unity if it is within the given tolerance of one.
              *
              *  @tparam T   Type of value to test.
              *
-             *  @param  x_  Value to test.
+             *  @param  x_      Value to test.
+             *  @param  tol_    Tolerance of the test/
              *
              *  @return true if the value can be considered equal to unity.
              */
             template <typename T>
-            inline bool unity(const T x_) noexcept
+            inline bool unity(const T x_, const T tol_) noexcept
             {
-                return (zero(x_ - T{1.0}));
+                return (zero(x_ - T{1.0}, tol_));
             }
 
             /**
              *  Test if a pair of values can be considered equal to one another.
-             *  Values are considered equal to zero if they are equal to within epsilon.
+             *  Values are considered equal to zero if they are equal to within the given tolerance.
              *
              *  @tparam T   Type of values to test.
              *
-             *  @param  x_  Fist value to test.
-             *  @param  y_  Second value to test.
+             *  @param  x_      Fist value to test.
+             *  @param  y_      Second value to test.
+             *  @param  tol_    Tolerance of the test/
              *
              *  @return true if the values can be considered equal.
              */
             template <typename T>
-            inline bool equal(const T x_, const T y_) noexcept // NOLINT
+            inline bool equal(const T x_, const T y_, const T tol_) noexcept // NOLINT
             {
-                return (zero(x_ - y_));
+                return (zero(x_ - y_, tol_));
             }
 
 
