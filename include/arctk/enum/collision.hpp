548bf7c73657836f36be15ebed8aba31733a07ce
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-15 02:12:44 +0100

    Added framework.

---
+++ include/arctk/enum/collision.hpp
@@ -0,0 +1,36 @@
+/**
+ *  @file   arctk/enum/collision.hpp
+ *  @date   15/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collision enumerations.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_ENUM_COLLISION_HPP
+#define ARCTK_ENUM_COLLISION_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace enum //! enumeration namespace
+    {
+
+
+        //  == ENUMERATIONS ==
+        //  -- Types --
+        enum class collision{};
+
+
+
+    } // namespace arc
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_ENUM_COLLISION_HPP
