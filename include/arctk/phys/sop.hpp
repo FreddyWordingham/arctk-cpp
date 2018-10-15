2634c2b146193984652c5664935b52cf29fe57b8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-15 01:48:23 +0100

    Removed photon parameter from interact_dist method.

--- include/arctk/phys/sop.hpp
+++ include/arctk/phys/sop.hpp
@@ -63,7 +63,7 @@ namespace arc //! arctk namespace
           public:
             //  -- Getters --
             inline double  ref_index() const noexcept;
-            virtual double interact_dist(random::Generator* rng_, const phys::Photon& phot_, const phys::Cell& cell_) const noexcept = 0;
+            virtual double interact_dist(random::Generator* rng_, const phys::Cell& cell_) const noexcept = 0;
 
             //  -- Interaction --
             virtual bool interact(random::Generator* rng_, phys::Photon* phot_, const phys::Cell* cell_, const double dist_) const noexcept = 0;
