4381843be4547c38c93fa9c851e6c4b138c2725d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-17 00:14:37 +0100

    Added documentation to leaf retrieval method.

--- include/arctk/tree/node/branch.hpp
+++ include/arctk/tree/node/branch.hpp
@@ -241,6 +241,13 @@ namespace arc //! arctk namespace
 
 
             //  -- Retrieval --
+            /**
+             *  Retrieve a pointer to the leaf cell for the given position.
+             *
+             *  @param  pos_    Position to retrieve the corresponding leaf cell for.
+             *
+             *  @return Pointer to the leaf cell for the given position.
+             */
             inline const node::Leaf* Branch::leaf(const vec3& pos_) const noexcept
             {
                 PRE(intersect(pos_));
