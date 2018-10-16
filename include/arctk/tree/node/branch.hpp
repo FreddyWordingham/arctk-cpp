b8a282e43b1f1c6d02097da2197b277359777118
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-17 00:24:17 +0100

    Added documentation for pre-condition.

--- include/arctk/tree/node/branch.hpp
+++ include/arctk/tree/node/branch.hpp
@@ -298,6 +298,8 @@ namespace arc //! arctk namespace
              *
              *  @param  pos_    Position to retrieve the corresponding leaf cell for.
              *
+             *  @pre    pos_ must intersect this node.
+             *
              *  @return Pointer to the leaf cell for the given position.
              */
             inline const node::Leaf* Branch::leaf(const vec3& pos_) const noexcept
