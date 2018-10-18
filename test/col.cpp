7ec00ce8a4b2afd80f58fec64f21da98552c0104
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-18 02:59:02 +0100

    Added unit testing.

---
+++ test/col.cpp
@@ -0,0 +1,30 @@
+/**
+ *  @file   main.cpp
+ *  @date   15/05/2018
+ *  @author Freddy Wordingham
+ *
+ *  Main source file of arctk.
+ *  Lists all header files fo static-analysis.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/col/convert.hpp>
+#include <arctk/col/map.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Main function of Arctk.
+ *  Serves only as a main entity.
+ *
+ *  @return Zero upon a successful 0run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
