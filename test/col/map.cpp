519dccb5a14d51d915cc9054804342d3eaa9e489
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-18 22:40:55 +0100

    Changed unit test name generation.

---
+++ test/col/map.cpp
@@ -0,0 +1,27 @@
+/**
+ *  @file   col/map.cpp
+ *  @date   18/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Colour mapping functions test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/col/map.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk colour collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
