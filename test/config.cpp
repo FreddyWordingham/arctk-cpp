94fa3a6dc453544d5e4eace6c2e7c45abcfe95c6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-18 22:28:48 +0100

    Added framework.

---
+++ test/config.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   config.cpp
+ *  @date   18/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Configuration collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/config/build.hpp>
+#include <arctk/config/version.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk configuration collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
