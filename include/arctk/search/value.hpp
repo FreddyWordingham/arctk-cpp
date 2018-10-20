8bc1794c7160efc27f0b21443c446801c3257966
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-21 00:08:32 +0100

    Added framework.

---
+++ include/arctk/search/value.hpp
@@ -0,0 +1,49 @@
+/**
+ *  @file   arctk/search/value.hpp
+ *  @date   20/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of value searching functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SEARCH_VALUE_HPP
+#define ARCTK_SEARCH_VALUE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace search //! searching namespace
+    {
+        namespace value //! value namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Value --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Value --
+
+
+
+        } // namespace value
+    }     // namespace search
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SEARCH_VALUE_HPP
