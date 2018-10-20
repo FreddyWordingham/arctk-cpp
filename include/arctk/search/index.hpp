b1bdd0aa85149162795023fb2996b1f3a2320360
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-21 00:07:06 +0100

    Moved index searching functions to new header file.

--- include/arctk/search/index.hpp
+++ include/arctk/search/index.hpp
@@ -16,31 +16,91 @@
 
 //  == IMPORTS ==
 //  -- Std --
+#include <cstddef>
 
 
 
 //  == NAMESPACE ==
 namespace arc //! arctk namespace
 {
-    namespace prop //! properties namespace
+    namespace search //! searching namespace
     {
-        namespace limits //! limits namespace
+        namespace index //! indexing namespace
         {
 
 
 
             //  == FUNCTION PROTOTYPES ==
             //  -- Index --
+            template <typename C, typename T = typename C::value_type, typename I = typename C::const_iterator>
+            inline size_t lower(const C& cont_, const T& val_) noexcept;
+            template <typename C, typename T = typename C::value_type, typename I = typename C::const_iterator>
+            inline size_t upper(const C& cont_, const T& val_) noexcept;
 
 
 
             //  == FUNCTIONS ==
             //  -- Index --
+            /**
+             *  Find the index of the first element of the container that is not greater than or equal to the value given.
+             *
+             *  @tparam C   Type of container.
+             *  @tparam T   Type stored by C.
+             *  @tparam I   Type of const iterator of C.
+             *
+             *  @param  cont_   Container to search.
+             *  @param  val_    Value to place.
+             *
+             *  @pre    cont_ must not be empty.
+             *  @pre    cont_ must be sorted in ascending order.
+             *  @pre    val_ must be within the range of cont_.
+             *
+             *  @return Index of the first element of the container that is not greater than or equal to the value given.
+             */
+            template <typename C, typename T, typename I>
+            inline size_t lower(const C& cont_, const T& val_) noexcept
+            {
+                assert(!cont_.empty());
+                assert(utl::properties::ascending(cont_));
+                assert(utl::properties::within(cont_, val_));
+
+                const auto index = static_cast<size_t>(std::distance(std::begin(cont_), std::lower_bound(std::begin(cont_), std::end(cont_), val_)));
+
+                return ((index == 0) ? index : (index - 1));
+            }
+
+            /**
+             *  Find the index of the first element of the container that is greater than the value given.
+             *
+             *  @tparam C   Type of container.
+             *  @tparam T   Type stored by C.
+             *  @tparam I   Type of const iterator of C.
+             *
+             *  @param  cont_   Container to search.
+             *  @param  val_    Value to place.
+             *
+             *  @pre    cont_ must not be empty.
+             *  @pre    cont_ must be sorted in ascending order.
+             *  @pre    val_ must be within the range of cont_.
+             *
+             *  @return Index of the first element of the container that is greater than the value given.
+             */
+            template <typename C, typename T, typename I>
+            inline size_t upper(const C& cont_, const T& val_) noexcept
+            {
+                assert(!cont_.empty());
+                assert(properties::ascending(cont_));
+                assert(properties::within(cont_, val_));
+
+                const auto index = static_cast<size_t>(std::distance(std::begin(cont_), std::upper_bound(std::begin(cont_), std::end(cont_), val_)));
+
+                return ((index == cont_.size()) ? (index - 1) : index);
+            }
 
 
 
-        } // namespace limits
-    }     // namespace prop
+        } // namespace index
+    }     // namespace search
 } // namespace arc
 
 
