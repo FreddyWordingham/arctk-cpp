9a1978e8b32fb04957d51958cd021cd43116e359
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-20 23:32:36 +0100

    Moved contents of type utl header into template type header file.

---
+++ include/arctk/type/template.hpp
@@ -0,0 +1,72 @@
+/**
+ *  @file   arctk/utl/template.hpp
+ *  @date   20/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of template identifying structures.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_TYPE_TEMPLATE_HPP
+#define ARCTK_TYPE_TEMPLATE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <vector>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace type //! typing namespace
+    {
+        namespace template //! templating namespace
+        {
+
+
+
+            //  == STRUCTURES ==
+            //  -- Vector --
+            /**
+             *  Structure used to determine if a type is a vector.
+             *
+             *  @tparam T   Type stored by vector.
+             */
+            template <class T>
+            struct is_vector // NOLINT
+            {
+                //  == FIELDS ==
+              public:
+                //  -- Properties --
+                static const bool value = false; //!< True if T is a vector.
+            };
+
+            /**
+             *  Specialisation for vector types.
+             *
+             *  @tparam T   Type stored by vector.
+             */
+            template <class T>
+            struct is_vector<std::vector<T>>
+            {
+                //  == FIELDS ==
+              public:
+                //  -- Properties --
+                static const bool value = true; //!< True if T is a vector.
+            };
+
+
+
+        } // namespace template
+    }     // namespace type
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_TYPE_TEMPLATE_HPP
