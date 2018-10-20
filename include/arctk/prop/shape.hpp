698c8bf2e2574264f4748bed3b7ef6296e106a01
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-20 23:51:24 +0100

    Added framework.

---
+++ include/arctk/prop/shape.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/prop/shape.hpp
+ *  @date   20/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of shape property functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PROP_SHAPE_HPP
+#define ARCTK_PROP_SHAPE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace prop //! properties namespace
+    {
+        namespace shape //! shape namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Order --
+
+
+            //  == FUNCTIONS ==
+            //  -- Order --
+
+
+
+        } // namespace shape
+    }     // namespace prop
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PROP_SHAPE_HPP
