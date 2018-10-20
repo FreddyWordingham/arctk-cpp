2315d16c36350668506c1bd0d7322687cb4a5a04
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-20 16:16:12 +0100

    Added framework.

---
+++ test/all.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   all.cpp
+ *  @date   20/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  All test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/col/convert.hpp>
+#include <arctk/col/map.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk all headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
