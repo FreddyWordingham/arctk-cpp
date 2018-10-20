409e77ffec005fee10ddb257cb671af69a7edd49
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-21 00:02:23 +0100

    Added framework.

---
+++ include/arctk/search/index.hpp
@@ -0,0 +1,49 @@
+/**
+ *  @file   arctk/search/index.hpp
+ *  @date   20/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of index searching functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_SEARCH_INDEX_HPP
+#define ARCTK_SEARCH_INDEX_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace prop //! properties namespace
+    {
+        namespace limits //! limits namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Index --
+
+
+
+            //  == FUNCTIONS ==
+            //  -- Index --
+
+
+
+        } // namespace limits
+    }     // namespace prop
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_SEARCH_INDEX_HPP
