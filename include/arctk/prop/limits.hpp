6a831010e67b8cf989ac8c9e8cd34352ecfb51ec
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-20 23:50:23 +0100

    Added framework.

---
+++ include/arctk/prop/limits.hpp
@@ -0,0 +1,50 @@
+/**
+ *  @file   arctk/prop/limits.hpp
+ *  @date   20/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of limits property functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PROP_LIMITS_HPP
+#define ARCTK_PROP_LIMITS_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <algorithm>
+#include <iterator>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace prop //! properties namespace
+    {
+        namespace limits //! limits namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Limits --
+
+
+            //  == FUNCTIONS ==
+            //  -- Limits --
+
+
+
+        } // namespace limits
+    }     // namespace prop
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PROP_LIMITS_HPP
