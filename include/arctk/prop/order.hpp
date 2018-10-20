9c94a04c2ec882e81159a0335085b6f4fe9caea3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-20 23:50:57 +0100

    Added framework.

---
+++ include/arctk/prop/order.hpp
@@ -0,0 +1,48 @@
+/**
+ *  @file   arctk/prop/order.hpp
+ *  @date   20/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of order property functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PROP_ORDER_HPP
+#define ARCTK_PROP_ORDER_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace prop //! properties namespace
+    {
+        namespace order //! ordering namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+            //  -- Order --
+
+
+            //  == FUNCTIONS ==
+            //  -- Order --
+
+
+
+        } // namespace order
+    }     // namespace prop
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PROP_ORDER_HPP
