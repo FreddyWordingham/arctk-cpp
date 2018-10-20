f233253cebadae3572d709d63cb5370ada4b7259
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-20 23:53:19 +0100

    Moved order property functions to new header file.

--- include/arctk/prop/shape.hpp
+++ include/arctk/prop/shape.hpp
@@ -16,6 +16,7 @@
 
 //  == IMPORTS ==
 //  -- Std --
+#include <vector>
 
 
 
@@ -37,8 +38,68 @@ namespace arc //! arctk namespace
             inline bool cube(const std::vector<std::vector<std::vector<T>>>& vec_) noexcept;
 
 
+
             //  == FUNCTIONS ==
             //  -- Shape --
+            /**
+             *  Determine if a given two-dimensional vector may be considered square.
+             *  To be considered square, all sub-vectors must be of the same size.
+             *
+             *  @tparam T   Type stored by the vector.
+             *
+             *  @param  vec_    Vector to test.
+             *
+             *  @return True if the given vector can be considered square.
+             */
+            template <typename T>
+            inline bool square(const std::vector<std::vector<T>>& vec_) noexcept
+            {
+                const size_t size = vec_.front().size();
+
+                for (size_t i = 1; i < vec_.size(); ++i)
+                {
+                    if (vec_[i].size() != size)
+                    {
+                        return (false);
+                    }
+                }
+
+                return (true);
+            }
+
+            /**
+             *  Determine if a given three-dimensional vector may be considered cube.
+             *  To be considered cube, all sub-vectors must be of the same size and all sub-sub-vectors must be of the same size.
+             *
+             *  @tparam T   Type stored by the vector.
+             *
+             *  @param  vec_    Vector to test.
+             *
+             *  @return True if the given vector can be considered cube.
+             */
+            template <typename T>
+            inline bool cube(const std::vector<std::vector<std::vector<T>>>& vec_) noexcept
+            {
+                const std::array<size_t, 2> size({{vec_.front().size(), vec_.front().front().size()}});
+
+                for (size_t i = 0; i < vec_.size(); ++i)
+                {
+                    if (vec_[i].size() != size[0])
+                    {
+                        return (false);
+                    }
+
+                    for (size_t j = 0; j < vec_.size(); ++j)
+                    {
+                        if (vec_[i][j].size() != size[1])
+                        {
+                            return (false);
+                        }
+                    }
+                }
+
+                return (true);
+            }
 
 
 
