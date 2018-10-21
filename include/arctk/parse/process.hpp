86133224c26617cc7c5b430e20646bf32b64fdad
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 00:20:41 +0100

    Added framework.

---
+++ include/arctk/parse/process.hpp
@@ -0,0 +1,47 @@
+/**
+ *  @file   arctk/parse/process.hpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Pre-processing parsing functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PARSE_PROCESS_HPP
+#define ARCTK_PARSE_PROCESS_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace parse //! parsing namespace
+    {
+        namespace process //! processing namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace process
+    }     // namespace parse
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PARSE_PROCESS_HPP
