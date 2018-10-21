57181bfb525534139a0a1095844f03e68a9bffe8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-21 23:56:32 +0100

    Added framework.

---
+++ include/arctk/parse/read.hpp
@@ -0,0 +1,47 @@
+/**
+ *  @file   arctk/parse/read.hpp
+ *  @date   21/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Read parsing functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PARSE_READ_HPP
+#define ARCTK_PARSE_READ_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace parse //! parsing namespace
+    {
+        namespace read //! reading namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace read
+    }     // namespace parse
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PARSE_READ_HPP
