9bfc2407358ebba2f324b4e8c55fa83980a251a1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-21 22:38:04 +0100

    Removed redundant import.

--- test/all.cpp
+++ test/all.cpp
@@ -81,7 +81,6 @@
 #include <arctk/math/vec3.hpp>
 #include <arctk/math/vec4.hpp>
 #include <arctk/phys.hpp>
-#include <arctk/phys/cell.hpp>
 #include <arctk/phys/photon.hpp>
 #include <arctk/random.hpp>
 #include <arctk/random/distribution.hpp>
