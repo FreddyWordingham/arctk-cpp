36824727df82edd7a0861a8040a3c417a7889a08
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-21 23:57:08 +0100

    Added framework.

---
+++ include/arctk/parse/write.hpp
@@ -0,0 +1,47 @@
+/**
+ *  @file   arctk/parse/write.hpp
+ *  @date   21/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Write parsing functions.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_PARSE_WRITE_HPP
+#define ARCTK_PARSE_WRITE_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace parse //! parsing namespace
+    {
+        namespace write //! writing namespace
+        {
+
+
+
+            //  == FUNCTION PROTOTYPES ==
+
+
+
+            //  == FUNCTIONS ==
+
+
+
+        } // namespace write
+    }     // namespace parse
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_PARSE_WRITE_HPP
