614f8f73311afa3f5f10466c83a9aaceaa4959aa
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:47:18 +0100

    Added framework.

---
+++ test/gui.cpp
@@ -0,0 +1,43 @@
+/**
+ *  @file   gui.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Graphical-user-interface collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/gui/actor.hpp>
+#include <arctk/gui/camera.hpp>
+#include <arctk/gui/camera/fly.hpp>
+#include <arctk/gui/camera/orbit.hpp>
+#include <arctk/gui/keybind.hpp>
+#include <arctk/gui/keymap.hpp>
+#include <arctk/gui/lens.hpp>
+#include <arctk/gui/lens/orthographic.hpp>
+#include <arctk/gui/lens/perspective.hpp>
+#include <arctk/gui/point.hpp>
+#include <arctk/gui/shader.hpp>
+#include <arctk/gui/shader/ambient.hpp>
+#include <arctk/gui/shader/diffuse.hpp>
+#include <arctk/gui/shader/normal.hpp>
+#include <arctk/gui/shader/ray.hpp>
+#include <arctk/gui/shader/specular.hpp>
+#include <arctk/gui/window.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk graphical-user-interface collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
