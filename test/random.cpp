6637b8ba9d1dd5023b23f0dbf6609528267af5fa
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:58:03 +0100

    Added framework.

---
+++ test/random.cpp
@@ -0,0 +1,32 @@
+/**
+ *  @file   random.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Random collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/random/distribution.hpp>
+#include <arctk/random/distribution/constant.hpp>
+#include <arctk/random/distribution/discrete.hpp>
+#include <arctk/random/distribution/linear.hpp>
+#include <arctk/random/generator.hpp>
+#include <arctk/random/generator/quality.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk random collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
