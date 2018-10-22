0909558b06157bcd08526d1c965c635b8d20f70c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:59:49 +0100

    Added framework.

---
+++ test/tree.cpp
@@ -0,0 +1,30 @@
+/**
+ *  @file   tree.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Tree collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/tree/node.hpp>
+#include <arctk/tree/node/branch.hpp>
+#include <arctk/tree/node/leaf.hpp>
+#include <arctk/tree/root.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk tree collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
