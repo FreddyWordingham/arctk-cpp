ff57c6ad085a91cab82b15305fe2b69e2fe5c687
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:54:42 +0100

    Added framework.

---
+++ test/parse.cpp
@@ -0,0 +1,30 @@
+/**
+ *  @file   parse.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Parsing collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/parse/print.hpp>
+#include <arctk/parse/process.hpp>
+#include <arctk/parse/read.hpp>
+#include <arctk/parse/write.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk parsing collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
