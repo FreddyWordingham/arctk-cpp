b0dbb0b3c315bc278f34a74ee245d7dc5c601b9a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:40:31 +0100

    Added framework.

---
+++ test/dom.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   dom.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Domain collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/dom/cell.hpp>
+#include <arctk/dom/region.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk domain collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
