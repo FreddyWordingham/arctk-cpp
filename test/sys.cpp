e1cc76b0580251d6018dfeaa4d160ac2b98df950
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:59:10 +0100

    Added framework.

---
+++ test/sys.cpp
@@ -0,0 +1,29 @@
+/**
+ *  @file   sys.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  System collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/sys/args.hpp>
+#include <arctk/sys/balancer.hpp>
+#include <arctk/sys/file.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk system collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
