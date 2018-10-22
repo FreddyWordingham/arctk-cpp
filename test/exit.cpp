eaa3a2998e359daf0431d31d610d092962065e8a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:44:06 +0100

    Added framework.

---
+++ test/exit.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   exit.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Exit code collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/exit/error.hpp>
+#include <arctk/exit/success.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk exit code collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
