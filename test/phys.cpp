31424287e000936459dc3d14030feb4e13db5868
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:56:31 +0100

    Added framework.

---
+++ test/phys.cpp
@@ -0,0 +1,27 @@
+/**
+ *  @file   phys.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Physics collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/parse/photon.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk physics collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
