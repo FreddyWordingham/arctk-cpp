ba462323b4fde71a96d7e975923efd4ec8f2e0a1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-22 23:43:30 +0100

    Added framework.

---
+++ test/equip.cpp
@@ -0,0 +1,31 @@
+/**
+ *  @file   equip.cpp
+ *  @date   22/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Equipment collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/equip/entity.hpp>
+#include <arctk/equip/entity/body.hpp>
+#include <arctk/equip/entity/light.hpp>
+#include <arctk/equip/entity/light/laser.hpp>
+#include <arctk/equip/entity/mirror.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk equipment collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
