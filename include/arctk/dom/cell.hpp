db81e4d107378fdd64fcd1f833dcdd0ee6d606f2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-23 23:04:45 +0100

    Split dom cell header into decleration and definition.

--- include/arctk/dom/cell.hpp
+++ include/arctk/dom/cell.hpp
@@ -14,13 +14,27 @@
 
 
 
-//  == IMPORTS ==
-//  -- Std --
-#include <cassert>
+//  == BASE ==
+#include <arctk/geom/shape/box.hpp>
 
-//  -- Arctk --
-#include <arctk/geom.hpp>
-#include <arctk/math/vec3.hpp>
+
+
+//  == CLASS PROTOTYPES ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == CLASS PROTOTYPES ==
+        template <typename T, size_t N>
+        class Vec;
+
+
+
+    } // namespace math
+} // namespace arc
 
 
 
@@ -61,84 +75,6 @@ namespace arc //! arctk namespace
             inline void add_scatter(double scatters_) noexcept;
             inline void add_energy(double energy_) noexcept;
         };
-
-
-
-        //  == INSTANTIATION ==
-        //  -- Constructors --
-        /**
-         *  Construct a cell with given bounds.
-         *
-         *  @param  min_    Vertex position of the box.
-         *  @param  max_    Vertex position of the box.
-         *
-         *  @pre    min_.x must be less than, or equal to, max_.x.
-         *  @pre    min_.y must be less than, or equal to, max_.y.
-         *  @pre    min_.z must be less than, or equal to, max_.z.
-         */
-        inline Cell::Cell(const vec3& min_, const vec3& max_) noexcept
-          : Box(min_, max_)
-          , _scatters(0.0)
-          , _energy(0.0)
-        {
-            assert(min_.x <= max_.x);
-            assert(min_.y <= max_.y);
-            assert(min_.z <= max_.z);
-        }
-
-
-
-        //  == METHODS ==
-        //  -- Getters --
-        /**
-         *  Get the scattering density of the cell.
-         *
-         *  @return Scattering density of the cell.
-         */
-        inline double Cell::scatter_dens() const noexcept
-        {
-            return (_scatters / vol());
-        }
-
-        /**
-         *  Get the energy density of the cell.
-         *
-         *  @return Energy density of the cell.
-         */
-        inline double Cell::energy_dens() const noexcept
-        {
-            return (_energy / vol());
-        }
-
-
-        //  -- Settings --
-        /**
-         *  Add a scattering count to the cell.
-         *
-         *  @param  scatters_   Scattering count to add.
-         *
-         *  @pre    scatters_ must be positive.
-         */
-        inline void Cell::add_scatter(const double scatters_) noexcept
-        {
-            assert(scatters_ > 0.0);
-
-            _scatters += scatters_;
-        }
-
-        /**
-         *  Add a energy count to the cell.
-         *
-         *  @param  energy_ Energy to add.
-         *
-         *  @pre    energy_ must be positive.
-         */
-        inline void Cell::add_energy(const double energy_) noexcept
-        {
-            assert(energy_ > 0.0);
-
-            _energy += energy_;
-        }
 
 
 
---
+++ include/arctk/dom/cell.inl
@@ -0,0 +1,118 @@
+/**
+ *  @file   arctk/dom/cell.inl
+ *  @date   08/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Domain cell information class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_CELL_INL
+#define ARCTK_DOM_CELL_INL
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <cassert>
+
+//  -- Arctk --
+#include <arctk/math/vec3.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace dom //! domain namespace
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+        /**
+         *  Construct a cell with given bounds.
+         *
+         *  @param  min_    Vertex position of the box.
+         *  @param  max_    Vertex position of the box.
+         *
+         *  @pre    min_.x must be less than, or equal to, max_.x.
+         *  @pre    min_.y must be less than, or equal to, max_.y.
+         *  @pre    min_.z must be less than, or equal to, max_.z.
+         */
+        inline Cell::Cell(const vec3& min_, const vec3& max_) noexcept
+          : Box(min_, max_)
+          , _scatters(0.0)
+          , _energy(0.0)
+        {
+            assert(min_.x <= max_.x);
+            assert(min_.y <= max_.y);
+            assert(min_.z <= max_.z);
+        }
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+        /**
+         *  Get the scattering density of the cell.
+         *
+         *  @return Scattering density of the cell.
+         */
+        inline double Cell::scatter_dens() const noexcept
+        {
+            return (_scatters / vol());
+        }
+
+        /**
+         *  Get the energy density of the cell.
+         *
+         *  @return Energy density of the cell.
+         */
+        inline double Cell::energy_dens() const noexcept
+        {
+            return (_energy / vol());
+        }
+
+
+        //  -- Settings --
+        /**
+         *  Add a scattering count to the cell.
+         *
+         *  @param  scatters_   Scattering count to add.
+         *
+         *  @pre    scatters_ must be positive.
+         */
+        inline void Cell::add_scatter(const double scatters_) noexcept
+        {
+            assert(scatters_ > 0.0);
+
+            _scatters += scatters_;
+        }
+
+        /**
+         *  Add a energy count to the cell.
+         *
+         *  @param  energy_ Energy to add.
+         *
+         *  @pre    energy_ must be positive.
+         */
+        inline void Cell::add_energy(const double energy_) noexcept
+        {
+            assert(energy_ > 0.0);
+
+            _energy += energy_;
+        }
+
+
+
+    } // namespace dom
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_CELL_INL
