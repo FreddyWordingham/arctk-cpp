bec8194b0a36182d5d69ef48a03a05f3c0bcb2e0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-23 22:51:51 +0100

    Split math formula constant header into decleration and definition.

--- include/arctk/math/formula/constant.hpp
+++ include/arctk/math/formula/constant.hpp
@@ -14,15 +14,14 @@
 
 
 
+//  == BASE ==
+#include <arctk/math/formula.hpp>
+
+
+
 //  == IMPORTS ==
 //  -- Std --
-#include <cassert>
 #include <vector>
-
-//  -- Arctk --
-#include <arctk/math/formula.hpp>
-#include <arctk/prop/order.hpp>
-#include <arctk/search/index.hpp>
 
 
 
@@ -63,59 +62,14 @@ namespace arc //! arctk namespace
 
 
 
-            //  == INSTANTIATION ==
-            //  -- Constructors --
-            /**
-             *  Construct a constant interpolation function.
-             *
-             *  @param  xs_ Vector of x-positions of the nodes.
-             *  @param  ys_ Vector of y-positions of the nodes.
-             *
-             *  @pre    xs_ must contain multiple values.
-             *  @pre    ys_ must contain multiple values.
-             *  @pre    xs_ size must equal the size of ys_.
-             *  @pre    xs_ must be sorted in ascending order.
-             */
-            inline Constant::Constant(const std::vector<double>& xs_, const std::vector<double>& ys_) noexcept
-              : Formula(xs_.front(), xs_.back())
-              , _xs(xs_)
-              , _ys(ys_)
-            {
-                assert(xs_.size() > 1);
-                assert(ys_.size() > 1);
-                assert(xs_.size() == ys_.size());
-                assert(prop::order::ascending(xs_));
-            }
-
-
-
-            //  == OPERATORS ==
-            //  -- Call --
-            /**
-             *  Determine the result of the function for a given value.
-             *
-             *  @param  val_    Domain value of the function.
-             *
-             *  @pre    val_ must be less than, or equal to, _min.
-             *  @pre    val_ must be greater than, or equal to, _max.
-             *
-             *  @return Result of the func for the given value.
-             */
-            inline double Constant::operator()(const double val_) const noexcept
-            {
-                assert(val_ >= _min);
-                assert(val_ <= _max);
-
-                const size_t index = search::index::lower(_xs, val_);
-
-                return (_ys[index]);
-            }
-
-
-
         } // namespace formula
     }     // namespace math
 } // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/math/formula/constant.inl>
 
 
 
