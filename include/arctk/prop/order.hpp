c81c338bb6dea1e9ec4a7419e935ef3f83eb370e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-25 13:45:27 +0100

    Split prop order header into decleration and definition.

--- include/arctk/prop/order.hpp
+++ include/arctk/prop/order.hpp
@@ -16,7 +16,6 @@
 
 //  == IMPORTS ==
 //  -- Std --
-#include <cmath>
 #include <iterator>
 
 
@@ -44,132 +43,14 @@ namespace arc //! arctk namespace
 
 
 
-            //  == FUNCTIONS ==
-            //  -- Order --
-            /**
-             *  Test if a container's elements are sorted in ascending order.
-             *  Container is still considered ascending if consecutive values are equal.
-             *
-             *  @tparam C   Type of container.
-             *  @tparam T   Type stored by C.
-             *  @tparam I   Type of const iterator of C.
-             *
-             *  @param  cont_   Container to test.
-             *
-             *  @pre    cont_ must not be empty.
-             *
-             *  @return True if the container's elements are sorted in ascending order.
-             */
-            template <typename C, typename T, typename I>
-            inline bool ascending(const C& cont_) noexcept
-            {
-                assert(!cont_.empty());
-
-                for (I it = std::begin(cont_); it != std::prev(std::end(cont_)); std::advance(it, 1))
-                {
-                    if (*it > *std::next(it))
-                    {
-                        return (false);
-                    }
-                }
-
-                return (true);
-            }
-
-            /**
-             *  Test if a container's elements are sorted in descending order.
-             *  Container is still considered descending if consecutive values are equal.
-             *
-             *  @tparam C   Type of container.
-             *  @tparam T   Type stored by C.
-             *  @tparam I   Type of const iterator of C.
-             *
-             *  @param  cont_   Container to test.
-             *
-             *  @pre    cont_ must not be empty.
-             *
-             *  @return True if the container's elements are sorted in descending order.
-             */
-            template <typename C, typename T, typename I>
-            inline bool descending(const C& cont_) noexcept
-            {
-                assert(!cont_.empty());
-
-                for (I it = std::begin(cont_); it != std::prev(std::end(cont_)); std::advance(it, 1))
-                {
-                    if (*it < *std::next(it))
-                    {
-                        return (false);
-                    }
-                }
-
-                return (true);
-            }
-
-            /**
-             *  Test if a container's elements are sorted in monotonic order.
-             *  Container is still considered monotonic if consecutive values are equal.
-             *
-             *  @tparam C   Type of container.
-             *  @tparam T   Type stored by C.
-             *  @tparam I   Type of const iterator of C.
-             *
-             *  @param  cont_   Container to test.
-             *
-             *  @pre    cont_ must not be empty.
-             *
-             *  @return True if the container's elements are sorted in monotonic order.
-             */
-            template <typename C, typename T, typename I>
-            inline bool monotonic(const C& cont_) noexcept
-            {
-                assert(!cont_.empty());
-
-                return (ascending(cont_) || descending(cont_));
-            }
-
-            /**
-             *  Test if a container's elements are uniformly spaced.
-             *
-             *  @tparam C   Type of container.
-             *  @tparam T   Type stored by C.
-             *  @tparam I   Type of const iterator of C.
-             *
-             *  @param  cont_   Container to test.
-             *
-             *  @pre    cont_ must not be empty.
-             *
-             *  @return True if the container's elements are uniformly spaced.
-             */
-            template <typename C, typename T, typename I>
-            inline bool uniform(const C& cont_) noexcept
-            {
-                assert(!cont_.empty());
-
-                if (cont_.size() == 1)
-                {
-                    return (true);
-                }
-
-                const T delta = *std::next(std::begin(cont_)) - *std::begin(cont_);
-
-                for (I it = std::begin(cont_); it != std::prev(std::end(cont_)); std::advance(it, 1))
-                {
-
-                    if (std::abs((*std::next(it) - *it) - delta) > std::numeric_limits<T>::epsilon())
-                    {
-                        return (false);
-                    }
-                }
-
-                return (true);
-            }
-
-
-
         } // namespace order
     }     // namespace prop
 } // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/prop/order.inl>
 
 
 
