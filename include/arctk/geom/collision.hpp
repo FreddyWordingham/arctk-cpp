4d8de9a49cd3737c985285f1ef5d8bc6a73770ce
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-25 10:16:40 +0100

    Split geom collision header into decleration and definition.

--- include/arctk/geom/collision.hpp
+++ include/arctk/geom/collision.hpp
@@ -15,9 +15,6 @@
 
 
 //  == IMPORTS ==
-//  -- Std --
-#include <cassert>
-
 //  -- Arctk --
 #include <arctk/math/vec3.hpp>
 
@@ -61,65 +58,13 @@ namespace arc //! arctk namespace
 
 
 
-        //  == INSTANTIATION ==
-        //  -- Constructors --
-        /**
-         *  Construct a collision event.
-         *
-         *  @param  front_  True if the collision ocurred on the front of the surface.
-         *  @param  dist_   Distance to the collision.
-         *  @param  norm_   Normal of the surface at the point of collision.
-         *
-         *  @pre    dist_ must be positive.
-         *  @pre    norm_ must be normalised.
-         */
-        inline Collision::Collision(const bool front_, const double dist_, const vec3& norm_) noexcept
-          : _front(front_)
-          , _dist(dist_)
-          , _norm(norm_)
-        {
-            assert(dist_ > 0.0);
-            assert(norm_.normalised());
-        }
-
-
-
-        //  == METHODS ==
-        //  -- Getters --
-        /**
-         *  Get the front hit status of the collision.
-         *
-         *  @return True if the collision ocurred on the front of the surface.
-         */
-        inline bool Collision::front() const noexcept
-        {
-            return (_front);
-        }
-
-        /**
-         *  Get the distance to the collision event.
-         *
-         *  @return Distance to the collision event.
-         */
-        inline double Collision::dist() const noexcept
-        {
-            return (_dist);
-        }
-
-        /**
-         *  Get the normal of the surface at the point of collision.
-         *
-         *  @return Normal of the surface at the point of collision.
-         */
-        inline const vec3& Collision::norm() const noexcept
-        {
-            return (_norm);
-        }
-
-
-
     } // namespace geom
 } // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/geom/collision.inl>
 
 
 
