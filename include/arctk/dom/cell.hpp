45c9b9bceb8130428636e94fa7060443a0add137
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-25 13:36:33 +0100

    Added missing alias documentation.

--- include/arctk/dom/cell.hpp
+++ include/arctk/dom/cell.hpp
@@ -26,8 +26,8 @@ namespace arc //! arctk namespace
     {
         template <typename T, size_t N>
         class Vec;
-    } // namespace math
-    using vec3 = math::Vec<double, 3>;
+    }                                  // namespace math
+    using vec3 = math::Vec<double, 3>; //!< Three-dimensional double precision vec.
 } // namespace arc
 
 
