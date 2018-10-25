4e214c4d892e2f65cf42ea734b429e30972f87d2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-25 13:43:17 +0100

    Split prop limits header into decleration and definition.

--- include/arctk/prop/limits.hpp
+++ include/arctk/prop/limits.hpp
@@ -43,117 +43,14 @@ namespace arc //! arctk namespace
 
 
 
-            //  == FUNCTIONS ==
-            //  -- Limits --
-            /**
-             *  Determine if all elements of a container are less than a given limit.
-             *
-             *  @tparam C   Type of container.
-             *  @tparam T   Type stored by C.
-             *  @tparam I   Type of const iterator of C.
-             *
-             *  @param  cont_   Container to test.
-             *  @param  limit_  Limit to compare elements against.
-             *
-             *  @return True if all elements are less than the limit.
-             */
-            template <typename C, typename T, typename I>
-            inline bool always_less_than(const C& cont_, const T& limit_) noexcept
-            {
-                for (I it = std::begin(cont_); it != std::end(cont_); std::advance(it, 1))
-                {
-                    if (!(*it < limit_))
-                    {
-                        return (false);
-                    }
-                }
-
-                return (true);
-            }
-
-            /**
-             *  Determine if all elements of a container are less than, or equal to, a given limit.
-             *
-             *  @tparam C   Type of container.
-             *  @tparam T   Type stored by C.
-             *  @tparam I   Type of const iterator of C.
-             *
-             *  @param  cont_   Container to test.
-             *  @param  limit_  Limit to compare elements against.
-             *
-             *  @return True if all elements are less than, or equal to, the limit.
-             */
-            template <typename C, typename T, typename I>
-            inline bool always_less_than_or_equal_to(const C& cont_, const T& limit_) noexcept
-            {
-                for (I it = std::begin(cont_); it != std::end(cont_); std::advance(it, 1))
-                {
-                    if (!(*it <= limit_))
-                    {
-                        return (false);
-                    }
-                }
-
-                return (true);
-            }
-
-            /**
-             *  Determine if all elements of a container are greater than a given limit.
-             *
-             *  @tparam C   Type of container.
-             *  @tparam T   Type stored by C.
-             *  @tparam I   Type of const iterator of C.
-             *
-             *  @param  cont_   Container to test.
-             *  @param  limit_  Limit to compare elements against.
-             *
-             *  @return True if all elements are greater than the limit.
-             */
-            template <typename C, typename T, typename I>
-            inline bool always_greater_than(const C& cont_, const T& limit_) noexcept
-            {
-                for (I it = std::begin(cont_); it != std::end(cont_); std::advance(it, 1))
-                {
-                    if (!(*it > limit_))
-                    {
-                        return (false);
-                    }
-                }
-
-                return (true);
-            }
-
-            /**
-             *  Determine if all elements of a container are greater than, or equal to, a given limit.
-             *
-             *  @tparam C   Type of container.
-             *  @tparam T   Type stored by C.
-             *  @tparam I   Type of const iterator of C.
-             *
-             *  @param  cont_   Container to test.
-             *  @param  limit_  Limit to compare elements against.
-             *
-             *  @return True if all elements are greater than, or equal to, the limit.
-             */
-            template <typename C, typename T, typename I>
-            inline bool always_greater_than_or_equal_to(const C& cont_, const T& limit_) noexcept
-            {
-                for (I it = std::begin(cont_); it != std::end(cont_); std::advance(it, 1))
-                {
-                    if (!(*it >= limit_))
-                    {
-                        return (false);
-                    }
-                }
-
-                return (true);
-            }
-
-
-
         } // namespace limits
     }     // namespace prop
 } // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/prop/limits.inl>
 
 
 
