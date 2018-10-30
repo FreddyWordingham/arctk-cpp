7f21942e166550032edb581cd044b7ce71e6ae3d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-30 00:17:31 +0000

    Cleaned build files and updated documentation.

--- docs/search/classes_10.js
+++ docs/search/classes_10.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['shader',['Shader',['../classarc_1_1gui_1_1_shader.html',1,'arc::gui']]],
   ['shape',['Shape',['../classarc_1_1geom_1_1_shape.html',1,'arc::geom']]],
+  ['simple',['Simple',['../classarc_1_1opt_1_1sop_1_1_simple.html',1,'arc::opt::sop::Simple'],['../classarc_1_1opt_1_1mat_1_1_simple.html',1,'arc::opt::mat::Simple']]],
   ['sop',['Sop',['../classarc_1_1opt_1_1_sop.html',1,'arc::opt']]],
   ['specular',['Specular',['../classarc_1_1gui_1_1shader_1_1_specular.html',1,'arc::gui::shader']]]
 ];
--- docs/search/files_10.js
+++ docs/search/files_10.js
@@ -4,6 +4,8 @@ var searchData=
   ['shader_2einl',['shader.inl',['../shader_8inl.html',1,'']]],
   ['shape_2ehpp',['shape.hpp',['../geom_2shape_8hpp.html',1,'(Global Namespace)'],['../prop_2shape_8hpp.html',1,'(Global Namespace)']]],
   ['shape_2einl',['shape.inl',['../geom_2shape_8inl.html',1,'(Global Namespace)'],['../prop_2shape_8inl.html',1,'(Global Namespace)']]],
+  ['simple_2ehpp',['simple.hpp',['../mat_2simple_8hpp.html',1,'(Global Namespace)'],['../sop_2simple_8hpp.html',1,'(Global Namespace)']]],
+  ['simple_2einl',['simple.inl',['../mat_2simple_8inl.html',1,'(Global Namespace)'],['../sop_2simple_8inl.html',1,'(Global Namespace)']]],
   ['sop_2ehpp',['sop.hpp',['../sop_8hpp.html',1,'']]],
   ['sop_2einl',['sop.inl',['../sop_8inl.html',1,'']]],
   ['specular_2ehpp',['specular.hpp',['../specular_8hpp.html',1,'']]],
