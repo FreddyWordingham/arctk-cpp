20c0aa1fd423a0db36c3ed5f371739f7ef064d22
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-31 17:10:49 +0000

    Added framework.

---
+++ include/arctk/multi/vector.hpp
@@ -0,0 +1,37 @@
+/**
+ *  @file   arctk/multi/vector.hpp
+ *  @date   31/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Multi-dimensional vector aliases.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MULTI_VECTOR_HPP
+#define ARCTK_MULTI_VECTOR_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <vector>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace mutli //! mutliple-dimension namespace
+    {
+
+
+
+    } // namespace mutli
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MULTI_VECTOR_HPP
