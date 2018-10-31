5eb3618c80fa463b39b3ec88f2fc4412c54dcd7d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-10-31 14:40:42 +0000

    Added framework.

---
+++ include/arctk/type/prop.hpp
@@ -0,0 +1,44 @@
+/**
+ *  @file   arctk/type/prop.hpp
+ *  @date   31/10/2018
+ *  @author Freddy Wordingham
+ *
+ *  Collection of property identifying structures.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_TYPE_PROP_HPP
+#define ARCTK_TYPE_PROP_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace type //! typing namespace
+    {
+        namespace prop //! properties namespace
+        {
+
+
+
+            //  == STRUCTURES ==
+            //  -- Containers --
+
+
+
+        } // namespace prop
+    }     // namespace type
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_TYPE_PROP_HPP
