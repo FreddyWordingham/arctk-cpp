5a10ee80ef2ce1a043526ca756f7d0f9644258af
Freddy Wordingham <fjmw201@exeter.ac.uk>
2018-11-01 09:32:22 +0000

    Added framework.

---
+++ test/multi.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   multi.cpp
+ *  @date   01/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Multi-dimensional collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/multi/array.hpp>
+#include <arctk/multi/vector.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk multi-dimensional container alias collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
