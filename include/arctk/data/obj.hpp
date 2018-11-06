990ec72f5f7b811865995e880a0c0348339c0e50
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-06 23:06:13 +0000

    Added header framework.

---
+++ include/arctk/data/obj.hpp
@@ -0,0 +1,58 @@
+/**
+ *  @file   arctk/data/obj.hpp
+ *  @date   06/12/2018
+ *  @author Freddy Wordingham
+ *
+ *  Data object class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_OBJ_HPP
+#define ARCTK_DATA_OBJ_HPP
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Data object class.
+         */
+        class Obj
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Data --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/data/obj.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_OBJ_HPP
