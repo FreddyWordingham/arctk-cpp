ac1360de1bfec586b173b2345f887d8e00a19ba0
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-06 23:10:30 +0000

    Added data unit test file source code.

---
+++ test/data.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   data.cpp
+ *  @date   06/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Data collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/data/obj.hpp>
+#include <arctk/data/table.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk data collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
