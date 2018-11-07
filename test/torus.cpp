1857c9373699cfbc030ac83cbd5829624edfc543
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-07 23:48:50 +0000

    Added unit test framework.

---
+++ test/torus.cpp
@@ -0,0 +1,27 @@
+/**
+ *  @file   torus.cpp
+ *  @date   07/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Torus collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/torus/sim.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk torus collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
