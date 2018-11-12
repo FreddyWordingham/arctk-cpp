cdb29c86d29a0739e73807250d3b97138bd90105
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-12 12:51:26 +0000

    Simplified base class.

--- include/arctk/img/bitmap.hpp
+++ include/arctk/img/bitmap.hpp
@@ -39,41 +39,10 @@ namespace arc //! arctk namespace
          */
         class Bitmap
         {
-            //  == FIELDS ==
-          private:
-            //  -- Data --
-            multi::vector<double, 2> _pixels;
-            double                   _min;
-            double                   _max;
-
-            //  -- Mapping --
-            std::function<double(double)> _scale_map;
-            std::function<vec3(double)>   _colour_map;
-
-
-            //  == INSTANTIATION ==
-          public:
-            //  -- Constructors --
-            inline explicit Bitmap(const multi::vector<double, 2>& pixels_) noexcept;
-
-
-            //  == OPERATORS ==
-          public:
-            //  -- Access --
-            inline std::vector<double>&       operator[](const size_t index_) noexcept;
-            inline const std::vector<double>& operator[](const size_t index_) const noexcept;
-
-
             //  == METHODS ==
           public:
-            //  -- Setters --
-            inline void set_min(const double min_) noexcept;
-            inline void set_max(const double max_) noexcept;
-            inline void set_scale_map(std::function<double(double)> scale_map_) noexcept;
-            inline void set_colour_map(std::function<vec3(double)> colour_map_) noexcept;
-
             //  -- Saving --
-            inline void save(const std::string& path_) const noexcept;
+            virtual inline void save(const std::string& path_) const noexcept = 0;
         };
 
 
