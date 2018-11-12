8bb367afa9cf1bd66e18217fae78f8ffb642c33f
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-12 12:15:29 +0000

    Added framework.

---
+++ test/img.cpp
@@ -0,0 +1,26 @@
+/**
+ *  @file   img.cpp
+ *  @date   12/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Image collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk image collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
