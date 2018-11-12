e94eee8050196f443a84727cd6c364e5eff533ab
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-12 18:25:01 +0000

    Added framework.

---
+++ include/arctk/geom/wavefront.hpp
@@ -0,0 +1,60 @@
+/**
+ *  @file   arctk/geom/wavefront.hpp
+ *  @date   12/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Wavefront reader helper class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_GEOM_WAVEFRONT_HPP
+#define ARCTK_GEOM_WAVEFRONT_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/math/vec3.hpp>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace geom //! geometry namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Wavefront helper class.
+         */
+        class Wavefront
+        {
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+    } // namespace geom
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/geom/wavefront.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_GEOM_WAVEFRONT_HPP
