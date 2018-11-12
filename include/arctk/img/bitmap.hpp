b1ed3aba1e37d01e92201f36bde4ec2e029310c7
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-12 12:09:45 +0000

    Added framework.

---
+++ include/arctk/img/bitmap.hpp
@@ -0,0 +1,67 @@
+/**
+ *  @file   arctk/img/bitmap.hpp
+ *  @date   12/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Bitmap image class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_IMG_BITMAP_HPP
+#define ARCTK_IMG_BITMAP_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Bitmap image class.
+         */
+        class Bitmap
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Data --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == OPERATORS ==
+          public:
+            //  -- Access --
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/img/bitmap.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_IMG_BITMAP_HPP
