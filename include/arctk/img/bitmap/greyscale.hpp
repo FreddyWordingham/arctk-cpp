981078371b43b2209ca58d68a20f00825344080a
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-12 12:50:29 +0000

    Added framework.

--- include/arctk/img/bitmap/greyscale.hpp
+++ include/arctk/img/bitmap/greyscale.hpp
@@ -1,16 +1,21 @@
 /**
- *  @file   arctk/img/bitmap.hpp
+ *  @file   arctk/img/bitmap/greyscale.hpp
  *  @date   12/11/2018
  *  @author Freddy Wordingham
  *
- *  Bitmap image class.
+ *  Greyscale bitmap image class.
  */
 
 
 
 //  == GUARD ==
-#ifndef ARCTK_IMG_BITMAP_HPP
-#define ARCTK_IMG_BITMAP_HPP
+#ifndef ARCTK_IMG_BITMAP_GREYSCALE_HPP
+#define ARCTK_IMG_BITMAP_GREYSCALE_HPP
+
+
+
+//  == BASE ==
+#include <arctk/img/bitmap.hpp>
 
 
 
@@ -30,63 +35,66 @@ namespace arc //! arctk namespace
 {
     namespace img //! image namespace
     {
-
-
-
-        //  == CLASS ==
-        /**
-         *  Bitmap image class.
-         */
-        class Bitmap
+        namespace bitmap //! bitmap namespace
         {
-            //  == FIELDS ==
-          private:
-            //  -- Data --
-            multi::vector<double, 2> _pixels;
-            double                   _min;
-            double                   _max;
-
-            //  -- Mapping --
-            std::function<double(double)> _scale_map;
-            std::function<vec3(double)>   _colour_map;
-
-
-            //  == INSTANTIATION ==
-          public:
-            //  -- Constructors --
-            inline explicit Bitmap(const multi::vector<double, 2>& pixels_) noexcept;
-
-
-            //  == OPERATORS ==
-          public:
-            //  -- Access --
-            inline std::vector<double>&       operator[](const size_t index_) noexcept;
-            inline const std::vector<double>& operator[](const size_t index_) const noexcept;
-
-
-            //  == METHODS ==
-          public:
-            //  -- Setters --
-            inline void set_min(const double min_) noexcept;
-            inline void set_max(const double max_) noexcept;
-            inline void set_scale_map(std::function<double(double)> scale_map_) noexcept;
-            inline void set_colour_map(std::function<vec3(double)> colour_map_) noexcept;
-
-            //  -- Saving --
-            inline void save(const std::string& path_) const noexcept;
-        };
 
 
 
-    } // namespace img
+            //  == CLASS ==
+            /**
+             *  Greyscale bitmap image class.
+             */
+            class Greyscale : public Bitmap
+            {
+                //  == FIELDS ==
+              private:
+                //  -- Data --
+                multi::vector<double, 2> _pixels;
+                double                   _min;
+                double                   _max;
+
+                //  -- Mapping --
+                std::function<double(double)> _scale_map;
+                std::function<vec3(double)>   _colour_map;
+
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+                inline explicit Bitmap(const multi::vector<double, 2>& pixels_) noexcept;
+
+
+                //  == OPERATORS ==
+              public:
+                //  -- Access --
+                inline std::vector<double>&       operator[](const size_t index_) noexcept;
+                inline const std::vector<double>& operator[](const size_t index_) const noexcept;
+
+
+                //  == METHODS ==
+              public:
+                //  -- Setters --
+                inline void set_min(const double min_) noexcept;
+                inline void set_max(const double max_) noexcept;
+                inline void set_scale_map(std::function<double(double)> scale_map_) noexcept;
+                inline void set_colour_map(std::function<vec3(double)> colour_map_) noexcept;
+
+                //  -- Saving --
+                inline void save(const std::string& path_) const noexcept;
+            };
+
+
+
+        } // namespace bitmap
+    }     // namespace img
 } // namespace arc
 
 
 
 //  == IMPLEMENTATION ==
-#include <arctk/img/bitmap.inl>
+#include <arctk/img/bitmap/greyscale.inl>
 
 
 
 //  == GUARD END ==
-#endif // ARCTK_IMG_BITMAP_HPP
+#endif // ARCTK_IMG_BITMAP_GREYSCALE_HPP
