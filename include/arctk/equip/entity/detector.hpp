8375356f3bc700dae5e8ada865854539b5787661
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-12 17:07:35 +0000

    Added framework.

---
+++ include/arctk/equip/entity/detector.hpp
@@ -0,0 +1,66 @@
+/**
+ *  @file   arctk/equip/entity/detector.hpp
+ *  @date   12/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Detector entity equipment class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EQUIP_ENTITY_DETECTOR_HPP
+#define ARCTK_EQUIP_ENTITY_DETECTOR_HPP
+
+
+
+//  == BASE ==
+#include <arctk/equip/entity.hpp>
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <memory>
+#include <string>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace equip //! equipment namespace
+    {
+        namespace entity //! entity namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Detector entity equipment class.
+             */
+            class Detector : public Entity
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+            };
+
+
+
+        } // namespace entity
+    }     // namespace equip
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/equip/entity/detector.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EQUIP_ENTITY_DETECTOR_HPP
---
+++ include/arctk/equip/entity/detector.inl
@@ -0,0 +1,44 @@
+/**
+ *  @file   arctk/equip/entity/detector.inl
+ *  @date   12/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Detector entity equipment class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_EQUIP_ENTITY_DETECTOR_INL
+#define ARCTK_EQUIP_ENTITY_DETECTOR_INL
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace equip //! equipment namespace
+    {
+        namespace entity //! entity namespace
+        {
+
+
+
+            //  == INSTANTIATION ==
+            //  -- Constructors --
+
+
+
+        } // namespace entity
+    }     // namespace equip
+} // namespace arc
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_EQUIP_ENTITY_DETECTOR_INL
