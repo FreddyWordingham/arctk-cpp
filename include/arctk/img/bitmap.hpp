f791bf035c47c9af0bc72b27c519a21ded2422e8
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-18 23:06:16 +0000

    Added documentation to height getter method.

--- include/arctk/img/bitmap.hpp
+++ include/arctk/img/bitmap.hpp
@@ -61,7 +61,7 @@ namespace arc //! arctk namespace
           public:
             //  -- Getters --
             virtual inline size_t width() const noexcept  = 0; //!< Get the width of the bitmap in pixels.  @return Width of the bitmap in pixels.
-            virtual inline size_t height() const noexcept = 0;
+            virtual inline size_t height() const noexcept = 0; //!< Get the height of the bitmap in pixels.  @return Height of the bitmap in pixels.
 
             //  -- Saving --
             virtual inline void save(const std::string& path_) const noexcept = 0;
