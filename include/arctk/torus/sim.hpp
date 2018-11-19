ce10698449bef74bfd7897fefcaad435007ec0d9
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-19 17:28:49 +0000

    Suppressed false clang-tidy warning.

--- include/arctk/torus/sim.hpp
+++ include/arctk/torus/sim.hpp
@@ -159,8 +159,9 @@ namespace arc //! arctk namespace
             inline void                                 run_post_flight() const noexcept;
             inline void                                 simulate() const noexcept;
             inline std::vector<std::vector<gui::Point>> simulate_thread(size_t thread_index_, unsigned long int num_phot_, std::vector<unsigned long int>* thread_phot_, size_t light_index_, dom::Region* dom_, const tree::Root& tree_) const noexcept;
-            inline type::collision collide(double inter_, const std::optional<std::pair<equip::Entity*, geom::Collision>>& ent_, const std::optional<double>& leaf_, const std::optional<double>& cell_, const std::optional<double>& dom_) const noexcept;
-            inline void            report(unsigned long int num_phot_, std::vector<unsigned long int>* thread_phot_, size_t light_index_) const noexcept;
+            inline type::collision collide(double inter_, const std::optional<std::pair<equip::Entity*, geom::Collision>>& ent_, const std::optional<double>& leaf_, const std::optional<double>& cell_, const std::optional<double>& dom_) const
+              noexcept; // NOLINT
+            inline void report(unsigned long int num_phot_, std::vector<unsigned long int>* thread_phot_, size_t light_index_) const noexcept;
 
             //  -- Rendering --
           private:
