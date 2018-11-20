3c7eea4767eff0e1987a6c190bf66daa6e123442
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-20 12:41:43 +0000

    Added framework.

---
+++ include/arctk/index/finder.hpp
@@ -0,0 +1,55 @@
+/**
+ *  @file   arctk/index/finder.hpp
+ *  @date   20/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Index finding class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_INDEX_FINDER_HPP
+#define ARCTK_INDEX_FINDER_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <cstddef>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Array element finding class.
+         *
+         *  @tparam N   Dimensionality of the finder.
+         */
+        template <size_t N>
+        class Finder
+        {
+        };
+
+
+
+    } // namespace data
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/index/finder.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_INDEX_FINDER_HPP
