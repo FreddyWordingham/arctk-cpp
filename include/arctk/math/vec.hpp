7f5194cf46915d6a6fb89cb0eb2e5519b1698fe5
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-20 14:28:52 +0000

    Condensed vec specialisations into a single header.

--- include/arctk/math/vec.hpp
+++ include/arctk/math/vec.hpp
@@ -135,16 +135,324 @@ namespace arc //! arctk namespace
 
 
 
+        //  == CLASS SPECIALISATIONS ==
+        /**
+         *  Two-dimensional mathematical vector class.
+         *
+         *  @tparam T   Type stored by the vec.
+         */
+        template <typename T>
+        class Vec<T, 2>
+        {
+            //  == FIELDS ==
+          public:
+            //  -- Data --
+            /**
+             *  First vec element.
+             */
+            union
+            {
+                T x,   //!< Cartesian x-dimension.
+                  rho; //!< Polar radial-dimension.
+            };
+
+            /**
+             *  Second vec element.
+             */
+            union
+            {
+                T y,     //!< Cartesian y-dimension.
+                  theta; //!< Polar inclination-dimension.
+            };
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+            constexpr inline Vec() noexcept;
+            constexpr inline Vec(T x_, T y_) noexcept;
+
+
+            //  == OPERATORS ==
+          public:
+            //  -- Printing --
+            template <typename S>
+            friend constexpr inline std::ostream& operator<<(std::ostream& stream_, const Vec<S, 2>& vec_) noexcept; // NOLINT
+
+            //  -- Assignment --
+            constexpr inline Vec<T, 2>& operator+=(T val_) noexcept;
+            constexpr inline Vec<T, 2>& operator+=(const Vec<T, 2>& vec_) noexcept;
+            constexpr inline Vec<T, 2>& operator-=(T val_) noexcept;
+            constexpr inline Vec<T, 2>& operator-=(const Vec<T, 2>& vec_) noexcept;
+            constexpr inline Vec<T, 2>& operator*=(T val_) noexcept;
+            constexpr inline Vec<T, 2>& operator/=(T val_) noexcept;
+
+            //  -- Increment / Decrement --
+            constexpr inline Vec<T, 2>&      operator++() noexcept;
+            constexpr inline const Vec<T, 2> operator++(int) noexcept;
+            constexpr inline Vec<T, 2>&      operator--() noexcept;
+            constexpr inline const Vec<T, 2> operator--(int) noexcept;
+
+            //  -- Arithmetic --
+            constexpr inline Vec<T, 2> operator+() const noexcept;
+            constexpr inline Vec<T, 2> operator-() const noexcept;
+            constexpr inline Vec<T, 2> operator+(T val_) const noexcept;
+            constexpr inline Vec<T, 2> operator+(const Vec<T, 2>& vec_) const noexcept;
+            constexpr inline Vec<T, 2> operator-(T val_) const noexcept;
+            constexpr inline Vec<T, 2> operator-(const Vec<T, 2>& vec_) const noexcept;
+            constexpr inline Vec<T, 2> operator*(T val_) const noexcept;
+            constexpr inline T         operator*(const Vec<T, 2>& vec_) const noexcept;
+            constexpr inline Vec<T, 2> operator*(const Mat<T, 2>& mat_) const noexcept;
+            constexpr inline Vec<T, 2> operator/(T val_) const noexcept;
+
+            //  -- Access --
+            constexpr inline T&       operator[](size_t index_) noexcept;
+            constexpr inline const T& operator[](size_t index_) const noexcept;
+
+
+            //  == METHODS ==
+          public:
+            //  -- Properties --
+            constexpr inline bool normalised(T tol_ = std::numeric_limits<T>::epsilon()) const noexcept;
+
+            //  -- Mathematical --
+            constexpr inline T         sum() const noexcept;
+            constexpr inline T         mag() const noexcept;
+            constexpr inline T         mag_sq() const noexcept;
+            constexpr inline Vec<T, 2> normal() const noexcept;
+            constexpr inline void      normalise() noexcept;
+        };
+
+
+        /**
+         *  Three-dimensional mathematical vector class.
+         *
+         *  @tparam T   Type stored by the vec.
+         */
+        template <typename T>
+        class Vec<T, 3>
+        {
+            //  == FIELDS ==
+          public:
+            //  -- Data --
+            /**
+             *  First vec element.
+             */
+            union
+            {
+                T x,   //!< Cartesian x-dimension.
+                  rho, //!< Polar radial-dimension.
+                  r;   //!< Colour red-dimension.
+            };
+
+            /**
+             *  Second vec element.
+             */
+            union
+            {
+                T y,     //!< Cartesian y-dimension.
+                  theta, //!< Polar inclination-dimension.
+                  g;     //!< Colour green-dimension.
+            };
+
+            /**
+             *  Third vec element.
+             */
+            union
+            {
+                T z,   //!< Cartesian z-dimension.
+                  phi, //!< Polar azimuthal-dimension.
+                  b;   //!< Colour blue-dimension.
+            };
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+            constexpr inline Vec() noexcept;
+            constexpr inline Vec(T x_, T y_, T z_) noexcept;
+            constexpr inline Vec(const Vec<T, 2>& vec_, T z_) noexcept;
+
+
+            //  == OPERATORS ==
+          public:
+            //  -- Printing --
+            template <typename S>
+            friend constexpr inline std::ostream& operator<<(std::ostream& stream_, const Vec<S, 3>& vec_) noexcept; // NOLINT
+
+            //  -- Assignment --
+            constexpr inline Vec<T, 3>& operator+=(T val_) noexcept;
+            constexpr inline Vec<T, 3>& operator+=(const Vec<T, 3>& vec_) noexcept;
+            constexpr inline Vec<T, 3>& operator-=(T val_) noexcept;
+            constexpr inline Vec<T, 3>& operator-=(const Vec<T, 3>& vec_) noexcept;
+            constexpr inline Vec<T, 3>& operator*=(T val_) noexcept;
+            constexpr inline Vec<T, 3>& operator/=(T val_) noexcept;
+            constexpr inline Vec<T, 3>& operator^=(const Vec<T, 3>& vec_) noexcept;
+
+            //  -- Increment / Decrement --
+            constexpr inline Vec<T, 3>&      operator++() noexcept;
+            constexpr inline const Vec<T, 3> operator++(int) noexcept;
+            constexpr inline Vec<T, 3>&      operator--() noexcept;
+            constexpr inline const Vec<T, 3> operator--(int) noexcept;
+
+            //  -- Arithmetic --
+            constexpr inline Vec<T, 3> operator+() const noexcept;
+            constexpr inline Vec<T, 3> operator-() const noexcept;
+            constexpr inline Vec<T, 3> operator+(T val_) const noexcept;
+            constexpr inline Vec<T, 3> operator+(const Vec<T, 3>& vec_) const noexcept;
+            constexpr inline Vec<T, 3> operator-(T val_) const noexcept;
+            constexpr inline Vec<T, 3> operator-(const Vec<T, 3>& vec_) const noexcept;
+            constexpr inline Vec<T, 3> operator*(T val_) const noexcept;
+            constexpr inline T         operator*(const Vec<T, 3>& vec_) const noexcept;
+            constexpr inline Vec<T, 3> operator*(const Mat<T, 3>& mat_) const noexcept;
+            constexpr inline Vec<T, 3> operator/(T val_) const noexcept;
+            constexpr inline Vec<T, 3> operator^(const Vec<T, 3>& vec_) const noexcept;
+
+            //  -- Access --
+            constexpr inline T&       operator[](size_t index_) noexcept;
+            constexpr inline const T& operator[](size_t index_) const noexcept;
+
+
+            //  == METHODS ==
+          public:
+            //  -- Properties --
+            constexpr inline bool normalised(T tol_ = std::sqrt(std::numeric_limits<T>::epsilon())) const noexcept;
+
+            //  -- Mathematical --
+            constexpr inline T         sum() const noexcept;
+            constexpr inline T         mag() const noexcept;
+            constexpr inline T         mag_sq() const noexcept;
+            constexpr inline Vec<T, 3> normal() const noexcept;
+            constexpr inline void      normalise() noexcept;
+            constexpr inline void      rotate(const math::Vec<T, 3>& axis_, T ang_) noexcept;
+        };
+
+
+        /**
+         *  Four-dimensional mathematical vector class.
+         *
+         *  @tparam T   Type stored by the vec.
+         */
+        template <typename T>
+        class Vec<T, 4>
+        {
+            //  == FIELDS ==
+          public:
+            //  -- Data --
+            /**
+             *  First vec element.
+             */
+            union
+            {
+                T x, //!< Cartesian x-dimension.
+                  r; //!< Colour red-dimension.
+            };
+
+            /**
+             *  Second vec element.
+             */
+            union
+            {
+                T y, //!< Cartesian y-dimension.
+                  g; //!< Colour green-dimension.
+            };
+
+            /**
+             *  Third vec element.
+             */
+            union
+            {
+                T z, //!< Cartesian z-dimension.
+                  b; //!< Colour blue-dimension.
+            };
+
+            /**
+             *  Fourth vec element.
+             */
+            union
+            {
+                T w, //!< Cartesian w-dimension.
+                  a; //!< Colour alpha-dimension.
+            };
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+            constexpr inline Vec() noexcept;
+            constexpr inline Vec(T x_, T y_, T z_, T w_) noexcept;
+            constexpr inline Vec(const Vec<T, 2>& vec_, T z_, T w_) noexcept;
+            constexpr inline Vec(const Vec<T, 3>& vec_, T w_) noexcept;
+
+
+            //  == OPERATORS ==
+          public:
+            //  -- Printing --
+            template <typename S>
+            friend constexpr inline std::ostream& operator<<(std::ostream& stream_, const Vec<S, 4>& vec_) noexcept; // NOLINT
+
+            //  -- Assignment --
+            constexpr inline Vec<T, 4>& operator+=(T val_) noexcept;
+            constexpr inline Vec<T, 4>& operator+=(const Vec<T, 4>& vec_) noexcept;
+            constexpr inline Vec<T, 4>& operator-=(T val_) noexcept;
+            constexpr inline Vec<T, 4>& operator-=(const Vec<T, 4>& vec_) noexcept;
+            constexpr inline Vec<T, 4>& operator*=(T val_) noexcept;
+            constexpr inline Vec<T, 4>& operator/=(T val_) noexcept;
+
+            //  -- Increment / Decrement --
+            constexpr inline Vec<T, 4>&      operator++() noexcept;
+            constexpr inline const Vec<T, 4> operator++(int) noexcept;
+            constexpr inline Vec<T, 4>&      operator--() noexcept;
+            constexpr inline const Vec<T, 4> operator--(int) noexcept;
+
+            //  -- Arithmetic --
+            constexpr inline Vec<T, 4> operator+() const noexcept;
+            constexpr inline Vec<T, 4> operator-() const noexcept;
+            constexpr inline Vec<T, 4> operator+(T val_) const noexcept;
+            constexpr inline Vec<T, 4> operator+(const Vec<T, 4>& vec_) const noexcept;
+            constexpr inline Vec<T, 4> operator-(T val_) const noexcept;
+            constexpr inline Vec<T, 4> operator-(const Vec<T, 4>& vec_) const noexcept;
+            constexpr inline Vec<T, 4> operator*(T val_) const noexcept;
+            constexpr inline T         operator*(const Vec<T, 4>& vec_) const noexcept;
+            constexpr inline Vec<T, 4> operator*(const Mat<T, 4>& mat_) const noexcept;
+            constexpr inline Vec<T, 4> operator/(T val_) const noexcept;
+
+            //  -- Access --
+            constexpr inline T&       operator[](size_t index_) noexcept;
+            constexpr inline const T& operator[](size_t index_) const noexcept;
+
+
+            //  == METHODS ==
+          public:
+            //  -- Properties --
+            constexpr inline bool normalised(T tol_ = std::numeric_limits<T>::epsilon()) const noexcept;
+
+            //  -- Mathematical --
+            constexpr inline T         sum() const noexcept;
+            constexpr inline T         mag() const noexcept;
+            constexpr inline T         mag_sq() const noexcept;
+            constexpr inline Vec<T, 4> normal() const noexcept;
+            constexpr inline void      normalise() noexcept;
+        };
+
+
+
     } // namespace math
 
 
 
     //  == ALIASES ==
     //  -- Float --
+    using vec2f = math::Vec<float, 2>; //!< Two-dimensional float precision vec.    // NOLINT
+    using vec3f = math::Vec<float, 3>; //!< Three-dimensional float precision vec.    // NOLINT
+    using vec4f = math::Vec<float, 4>; //!< Four-dimensional float precision vec.    // NOLINT
     template <size_t N>
     using vecNf = math::Vec<float, N>; //!< N-dimensional float precision vec.  // NOLINT
 
     //  -- Double --
+    using vec2 = math::Vec<double, 2>; //!< Two-dimensional double precision vec.   // NOLINT
+    using vec3 = math::Vec<double, 3>; //!< Three-dimensional double precision vec.   // NOLINT
+    using vec4 = math::Vec<double, 4>; //!< Four-dimensional double precision vec.   // NOLINT
     template <size_t N>
     using vecN = math::Vec<double, N>; //!< N-dimensional double precision vec. // NOLINT
 
@@ -156,6 +464,9 @@ namespace arc //! arctk namespace
 
 //  == IMPLEMENTATION ==
 #include <arctk/math/vec.inl>
+#include <arctk/math/vec2.inl>
+#include <arctk/math/vec3.inl>
+#include <arctk/math/vec4.inl>
 
 
 
---
+++ legacy/vec2.hpp
@@ -0,0 +1,71 @@
+/**
+ *  @file   arctk/math/vec2.hpp
+ *  @date   10/07/2018
+ *  @author Freddy Wordingham
+ *
+ *  Two-dimensional mathematical vector class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_MATH_VEC2_HPP
+#define ARCTK_MATH_VEC2_HPP
+
+
+
+//  == BASE ==
+#include <arctk/math/vec.hpp>
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <cstddef>
+#include <limits>
+
+
+
+//  == CLASS PROTOTYPES ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+        template <typename T, size_t N>
+        class Mat;
+    } // namespace math
+} // namespace arc
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace math //! mathematical namespace
+    {
+
+
+
+        //  == CLASS ==
+
+
+
+    } // namespace math
+
+
+
+    //  == ALIASES ==
+
+
+
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/math/vec2.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_MATH_VEC2_HPP
