88c28ca81be00e333f57928c8a29caa9dbd65519
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-20 01:14:59 +0000

    Linked to equip unit test source file.

--- test/equip.cpp
+++ test/equip.cpp
@@ -14,6 +14,7 @@
 #include <arctk/equip/entity/body.hpp>
 #include <arctk/equip/entity/detector.hpp>
 #include <arctk/equip/entity/detector/phaseometer.hpp>
+#include <arctk/equip/entity/detector/photometer.hpp>
 #include <arctk/equip/entity/light.hpp>
 #include <arctk/equip/entity/light/laser.hpp>
 #include <arctk/equip/entity/mirror.hpp>
