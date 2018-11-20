e1f11ce66aa773c84537c512969add35fdf31231
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-20 14:17:39 +0000

    Removed finder index classes.

--- test/index.cpp
+++ test/index.cpp
@@ -12,11 +12,12 @@
 //  -- Arctk --
 #include <arctk/index/col.hpp>
 #include <arctk/index/dim.hpp>
+#include <arctk/index/finder.hpp>
 #include <arctk/index/rotate.hpp>
 #include <arctk/index/vertex.hpp>
 
-
-
+#include <arctk/parse/print.hpp>
+#include <iostream>
 //  == MAIN ==
 //  -- Function --
 /**
@@ -26,5 +27,15 @@
  */
 int main(const int /*unused*/, const char** /*unused*/)
 {
+    arc::multi::vector<double, 2> vec(4, std::vector<double>(4));
+
+    std::cout << vec << '\n';
+
+    arc::index::Finder find(arc::vec2(0.0, 0.0), arc::vec2(1.0, 1.0), std::array<size_t, 2>({{2, 4}}));
+
+    find.get<double>(vec, 0.25, 0.25) = 2.4;
+
+    std::cout << vec << '\n';
+
     return (0);
 }
