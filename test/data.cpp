88976bf958eb5a7a2265ccfe76b2056bbb2b4d7a
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-20 13:04:30 +0000

    Removed link and test code.

--- test/data.cpp
+++ test/data.cpp
@@ -10,10 +10,10 @@
 
 //  == IMPORTS ==
 //  -- Arctk --
-#include <arctk/data/bin.hpp>
 #include <arctk/data/obj.hpp>
 #include <arctk/data/table.hpp>
 #include <arctk/parse/print.hpp>
+
 
 
 //  == MAIN ==
@@ -25,11 +25,5 @@
  */
 int main(const int /*unused*/, const char** /*unused*/)
 {
-    arc::data::Bin<int, 1> bin(0.0, 1.0, 2);
-
-    bin.count(0.75, 1);
-
-    //    std::cout << bin.data() << '\n';
-
     return (0);
 }
