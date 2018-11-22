dce6a5da241efbe216ec4411d7c5556d16cecf90
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-22 01:02:37 +0000

    Added framework.

---
+++ include/arctk/data/bin/regular/fixed.hpp
@@ -0,0 +1,73 @@
+/**
+ *  @file   arctk/data/bin/fixed.hpp
+ *  @date   22/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Fixed range regular interval binning class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DATA_BIN_FIXED_HPP
+#define ARCTK_DATA_BIN_FIXED_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace data //! data namespace
+    {
+        namespace bin //! binning namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Fixed range regular interval binning class.
+             *
+             *  @tparam T   Type binned.
+             */
+            template <typename T>
+            class Regular
+            {
+                //  == FIELDS ==
+              private:
+
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == OPERATORS ==
+              public:
+                //  -- Access --
+
+
+                //  == METHODS ==
+              public:
+                //  -- Getters --
+            };
+
+
+
+        } // namespace bin
+    }     // namespace data
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/data/bin/fixed.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DATA_BIN_FIXED_HPP
