48e3dc6466a4d110f8e2ae9dd4d2ccdb23e81fce
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-26 00:17:44 +0000

    Removed redundant imports.

--- test/opt.cpp
+++ test/opt.cpp
@@ -14,11 +14,9 @@
 #include <arctk/opt/mat.hpp>
 #include <arctk/opt/mat/dumb.hpp>
 #include <arctk/opt/mat/raman.hpp>
-#include <arctk/opt/mat/simple.hpp>
 #include <arctk/opt/sop.hpp>
 #include <arctk/opt/sop/dumb.hpp>
 #include <arctk/opt/sop/raman.hpp>
-#include <arctk/opt/sop/simple.hpp>
 
 
 
