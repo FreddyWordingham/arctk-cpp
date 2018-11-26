c44fa9a1bd8269eb705ba86fdd445b909c68f417
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-26 18:58:48 +0000

    Added framework.

---
+++ include/arctk/opt/block.hpp
@@ -0,0 +1,64 @@
+/**
+ *  @file   arctk/opt/block.hpp
+ *  @date   26/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Local information class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_OPT_BLOCK_HPP
+#define ARCTK_OPT_BLOCK_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace opt //! optical namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Local information cell.
+         */
+        class Block
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Data --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+    } // namespace opt
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/opt/block.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_OPT_BLOCK_HPP
