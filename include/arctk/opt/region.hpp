186c6e0dc4324efce5ce063034a48342e471321e
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-26 22:13:21 +0000

    Added framework.

---
+++ include/arctk/opt/region.hpp
@@ -0,0 +1,67 @@
+/**
+ *  @file   arctk/opt/region.hpp
+ *  @date   26/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Optical region class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_OPT_REGION_HPP
+#define ARCTK_OPT_REGION_HPP
+
+
+
+//  == BASE ==
+
+
+
+//  == IMPORTS ==
+
+
+
+//  == CLASS PROTOTYPES ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace opt //! optical namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Body of material entity equipment class.
+         */
+        class Region
+        {
+            //  == FIELDS ==
+          private:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          private:
+        };
+
+
+
+    } // namespace opt
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/opt/region.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_OPT_REGION_HPP
