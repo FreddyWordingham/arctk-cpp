51c5876079756a07bc72e1e69522cd1f1e9603d5
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-27 16:04:01 +0000

    Added res field.

---
+++ include/arctk/dom/region.hpp
@@ -0,0 +1,70 @@
+/**
+ *  @file   arctk/dom/region.hpp
+ *  @date   27/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Domain region controller class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_REGION_HPP
+#define ARCTK_DOM_REGION_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace dom //! domain namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Domain region controller class.
+         */
+        class Region
+        {
+            //  == FIELDS ==
+          protected:
+            //  -- Blocks --
+            const std::array<size_t, 3> _res;
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+    } // namespace dom
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/dom/region.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_REGION_HPP
