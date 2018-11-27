a3f7b93972f53b4acb8221cb1755e85444c50018
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-27 15:30:04 +0000

    Added special methosds.

--- include/arctk/dom/driver.hpp
+++ include/arctk/dom/driver.hpp
@@ -38,19 +38,18 @@ namespace arc //! arctk namespace
             //  == INSTANTIATION ==
           public:
             //  -- Constructors --
-            //     inline Node(const vec3& min_, const vec3& max_, size_t depth_) noexcept;
-            //     inline Block(const Block&) noexcept = default; //!< Defaulted copy constructor.
-            //     inline Block(Block&&) noexcept      = default; //!< Defaulted move constructor.
+            inline Driver(const Driver&) noexcept = default; //!< Defaulted copy constructor.
+            inline Driver(Driver&&) noexcept      = default; //!< Defaulted move constructor.
 
             //  -- Destructors --
             inline ~Driver() noexcept override;
 
 
-            //     //  == OPERATORS ==
-            //   public:
-            //     //  -- Assignment --
-            //     inline Block& operator=(const Block&) noexcept = default; //!< Defaulted copy operator. @return Reference to copied object.
-            //     inline Block& operator=(Block&&) noexcept = default;      //!< Defaulted move operator. @return Reference to moved object.
+            //  == OPERATORS ==
+          public:
+            //  -- Assignment --
+            inline Driver& operator=(const Driver&) noexcept = default; //!< Defaulted copy operator. @return Reference to copied object.
+            inline Driver& operator=(Driver&&) noexcept = default;      //!< Defaulted move operator. @return Reference to moved object.
 
 
             //  == METHODS ==
