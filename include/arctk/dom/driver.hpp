a2dbf27838bc3a7be6e47e487819b5ef6d5e46d7
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-27 15:25:51 +0000

    Added framework.

---
+++ include/arctk/dom/driver.hpp
@@ -0,0 +1,74 @@
+/**
+ *  @file   arctk/dom/driver.hpp
+ *  @date   27/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Photon controller class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_DOM_DRIVER_HPP
+#define ARCTK_DOM_DRIVER_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace dom //! domain namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Photon controller class.
+         */
+        class Driver
+        {
+            //  == FIELDS ==
+          protected:
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+            //     inline Node(const vec3& min_, const vec3& max_, size_t depth_) noexcept;
+            //     inline Block(const Block&) noexcept = default; //!< Defaulted copy constructor.
+            //     inline Block(Block&&) noexcept      = default; //!< Defaulted move constructor.
+
+            //     //  -- Destructors --
+            //     inline ~Block() noexcept override;
+
+
+            //     //  == OPERATORS ==
+            //   public:
+            //     //  -- Assignment --
+            //     inline Block& operator=(const Block&) noexcept = default; //!< Defaulted copy operator. @return Reference to copied object.
+            //     inline Block& operator=(Block&&) noexcept = default;      //!< Defaulted move operator. @return Reference to moved object.
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+    } // namespace dom
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/dom/driver.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_DOM_DRIVER_HPP
