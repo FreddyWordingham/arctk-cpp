daf8981a4c669167ae1531757380ce6321ebcb28
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-28 23:59:11 +0000

    Added framework.

---
+++ include/arctk/opt/material/elastic.hpp
@@ -0,0 +1,82 @@
+/**
+ *  @file   arctk/opt/material/elastic.hpp
+ *  @date   28/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Elastic scattering material class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_OPT_MATERIAL_ELASTIC_HPP
+#define ARCTK_OPT_MATERIAL_ELASTIC_HPP
+
+
+
+//  == BASE ==
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == CLASS PROTOTYPES ==
+namespace arc //! arctk namespace
+{
+    namespace phys //! physics namespace
+    {
+        class Photon;
+    }                // namespace phys
+    namespace random //! random number namespace
+    {
+        class Generator;
+    } // namespace random
+} // namespace arc
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace opt //! optical namespace
+    {
+        namespace material //! material namespace
+        {
+
+
+
+            //  == CLASS ==
+            /**
+             *  Elastic scattering material class.
+             */
+            class Elastic
+            {
+                //  == FIELDS ==
+              private:
+                //  == INSTANTIATION ==
+              public:
+                //  -- Constructors --
+
+
+                //  == METHODS ==
+              public:
+            };
+
+
+
+        } // namespace material
+    }     // namespace opt
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/opt/material/elastic.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_OPT_MATERIAL_ELASTIC_HPP
