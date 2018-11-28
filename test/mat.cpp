ec1977455d183fdf2e97e129cda3cd7c3db83fe6
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-11-28 21:08:54 +0000

    Added framework.

---
+++ test/mat.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   opt.cpp
+ *  @date   28/11/2018
+ *  @author Freddy Wordingham
+ *
+ *  Optical collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+#include <arctk/opt/driver.hpp>
+#include <arctk/opt/mat.hpp>
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk optical container alias collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
