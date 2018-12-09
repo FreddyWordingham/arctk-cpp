176a3257c9299ebe53f12a7ea69668331f165f50
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-12-09 02:21:26 +0000

    Added output_dir field.

--- include/arctk/torus/sim.hpp
+++ include/arctk/torus/sim.hpp
@@ -77,6 +77,9 @@ namespace arc //! arctk namespace
             bool _pre_render;
             bool _post_render;
 
+            //  -- Output --
+            std::string _output_dir;
+
 
             //  == INSTANTIATION ==
           public:
