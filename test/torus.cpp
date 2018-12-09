060563b80e5eb41842152e0c0224075ae32b72a4
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-12-09 01:02:07 +0000

    Added framework.

---
+++ test/torus.cpp
@@ -0,0 +1,26 @@
+/**
+ *  @file   torus.cpp
+ *  @date   09/12/2018
+ *  @author Freddy Wordingham
+ *
+ *  Transport of radiation under skin collection test programme.
+ */
+
+
+
+//  == IMPORTS ==
+//  -- Arctk --
+
+
+
+//  == MAIN ==
+//  -- Function --
+/**
+ *  Unit test function of Arctk transport of radiation under skin collection headers.
+ *
+ *  @return Zero upon a successful run.
+ */
+int main(const int /*unused*/, const char** /*unused*/)
+{
+    return (0);
+}
