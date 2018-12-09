d2676d1b716f4253b89d0d5a7a085507d42a7705
FreddyWordingham <FJMW201@exeter.ac.uk>
2018-12-09 01:06:39 +0000

    Added framework.

---
+++ include/arctk/torus/sim.hpp
@@ -0,0 +1,69 @@
+/**
+ *  @file   arctk/torus/sim.hpp
+ *  @date   09/12/2018
+ *  @author Freddy Wordingham
+ *
+ *  Radiative transfer simulation class.
+ */
+
+
+
+//  == GUARD ==
+#ifndef ARCTK_TORUS_SIM_HPP
+#define ARCTK_TORUS_SIM_HPP
+
+
+
+//  == IMPORTS ==
+//  -- Std --
+
+//  -- Arctk --
+
+
+
+//  == CLASS PROTOTYPES ==
+
+
+
+//  == NAMESPACE ==
+namespace arc //! arctk namespace
+{
+    namespace torus //! transport of radiation under skin namespace
+    {
+
+
+
+        //  == CLASS ==
+        /**
+         *  Radiative transfer simulation class.
+         */
+        class Sim
+        {
+            //  == FIELDS ==
+          private:
+            //  -- Bounds --
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+            //  == METHODS ==
+          public:
+        };
+
+
+
+    } // namespace torus
+} // namespace arc
+
+
+
+//  == IMPLEMENTATION ==
+#include <arctk/torus/sim.inl>
+
+
+
+//  == GUARD END ==
+#endif // ARCTK_TORUS_SIM_HPP
