7b8b9c5f622d13432729c1bf7d574357d6ffd104
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-01-23 20:48:16 +0000

    Added utility bash functions.

---
+++ resources/bash/utl.sh
@@ -0,0 +1,40 @@
+#   == UTILITY ==
+#   -- Search --
+arctk.search()
+{
+    if [ "$#" != "1" ]; then
+        printf "Error! Unable to search Arc files.\n"
+        printf "Search requires a single regex string.\n"
+
+        return
+    fi
+
+    cd $ARCTK_DIR/include/arctk >/dev/null
+
+    grep -r "$1" .
+
+    cd - >/dev/null
+}
+
+
+#   -- List --
+arctk.list()
+{
+    cd $ARCTK_DIR >/dev/null
+
+    find . -name "*.cpp"
+    find . -name "*.hpp"
+    find . -name "*.inl"
+
+    cd - >/dev/null
+}
+
+
+#   -- VSCode --
+arctk.extension()
+{
+    cp -r $ARCTK_DIR/res/vscode/arctk-extension ~/.vscode/extensions/arctk-extension
+}
+
+
+printf "Arctk utility bash functions loaded.\n"
