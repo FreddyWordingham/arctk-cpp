75e67abcfdfae1bae1e484324d333f5846261c46
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-01-24 18:00:15 +0000

    Cleaned build files.

--- testing/unit/si/unit.cpp
+++ testing/unit/si/unit.cpp
@@ -53,52 +53,52 @@ namespace arc
         //  -- Tests --
         inline void scalar() noexcept
         {
-            si::Scalar scal_0;
+            const si::Scalar scal_0;
             assert(math::zero(scal_0.mag()));
 
-            si::Scalar scal_1{1.23};
+            const si::Scalar scal_1{1.23};
             assert(math::equal(scal_1.mag(), 1.23));
 
-            double doub_0{scal_0};
+            const double doub_0{scal_0};
             assert(math::zero(doub_0));
 
-            double doub_1{scal_1};
+            const double doub_1{scal_1};
             assert(math::equal(doub_1, 1.23));
         }
 
         inline void mass() noexcept
         {
-            si::Mass mass_0;
+            const si::Mass mass_0;
             assert(math::zero(mass_0.mag()));
 
-            si::Mass mass_1{1.23};
+            const si::Mass mass_1{1.23};
             assert(math::equal(mass_1.mag(), 1.23));
 
-            si::Mass mass_2{mass_1 * 2.0};
+            const si::Mass mass_2{mass_1 * 2.0};
             assert(math::equal(mass_2.mag(), 2.46));
 
-            si::Mass mass_3{0.5 * mass_2};
+            const si::Mass mass_3{0.5 * mass_2};
             assert(math::equal(mass_3.mag(), 1.23));
 
-            si::Energy energy_0{mass_1 * (si::Length{1.0} * si::Length{1.0}) / (si::Time{0.5} * si::Time{0.5})};
+            const si::Energy energy_0{mass_1 * (si::Length{1.0} * si::Length{1.0}) / (si::Time{0.5} * si::Time{0.5})};
             assert(math::equal(energy_0.mag(), 4.92));
         }
 
         inline void length() noexcept
         {
-            si::Length len_0;
+            const si::Length len_0;
             assert(math::zero(len_0.mag()));
 
-            si::Length len_1{1.23};
+            const si::Length len_1{1.23};
             assert(math::equal(len_1.mag(), 1.23));
 
-            si::Length len_2{len_1 * 2.0};
+            const si::Length len_2{len_1 * 2.0};
             assert(math::equal(len_2.mag(), 2.46));
 
-            si::Length len_3{0.5 * len_2};
+            const si::Length len_3{0.5 * len_2};
             assert(math::equal(len_3.mag(), 1.23));
 
-            si::Velocity vel_0{len_1 / si::Time{0.5}};
+            const si::Velocity vel_0{len_1 / si::Time{0.5}};
             assert(math::equal(vel_0.mag(), 2.46));
         }
 
