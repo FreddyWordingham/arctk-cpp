4bd0b878404b25ab026a1c481aa465081366ff34
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-01-24 00:49:00 +0000

    Added more template functions.

--- resources/bash/templates.sh
+++ resources/bash/templates.sh
@@ -8,3 +8,36 @@ arctk.class()
 
     cd - > /dev/null
 }
+
+
+#   -- Structure --
+arctk.structure()
+{
+    cd $ARCTK_DIR/resources/templates/structure > /dev/null
+
+    python3 -B gen.py "$@"
+
+    cd - > /dev/null
+}
+
+
+#   -- Functions --
+arctk.functions()
+{
+    cd $ARCTK_DIR/resources/templates/functions > /dev/null
+
+    python3 -B gen.py "$@"
+
+    cd - > /dev/null
+}
+
+
+#   -- Constants --
+arctk.constants()
+{
+    cd $ARCTK_DIR/resources/templates/constants > /dev/null
+
+    python3 -B gen.py "$@"
+
+    cd - > /dev/null
+}
