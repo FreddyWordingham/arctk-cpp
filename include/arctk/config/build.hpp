b26922461361dbe72757daff3cd9ccb9b36cffb1
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-01-24 18:04:38 +0000

    Cleaned build files.

--- include/arctk/config/build.hpp
+++ include/arctk/config/build.hpp
@@ -15,7 +15,7 @@ namespace arc
         //  -- Build Information --
         constexpr const char* const DIR{"/Users/freddy/Projects/arctk"};
         constexpr const char* const BRANCH{"master"};
-        constexpr const char* const HASH{"75e67abcf"};
+        constexpr const char* const HASH{"ba85d4abf"};
         constexpr const char* const COMPILER{"Clang"};
         constexpr const char* const TYPE{"debug"};
         constexpr const char* const DATE{"2019-01-24"};
