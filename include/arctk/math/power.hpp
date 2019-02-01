26cab3e07cf062bef14d2a363a35bbd24934d74f
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-02-01 16:05:26 +0000

    Added power math function files.

---
+++ include/arctk/math/power.hpp
@@ -0,0 +1,28 @@
+//  == GUARD ==
+#pragma once
+
+
+
+//  == IMPORTS ==
+//  -- Arc --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace math
+    {
+
+
+
+        //  == FUNCTIONS ==
+
+
+
+        //  == OPERATORS ==
+
+
+
+    } // namespace math
+} // namespace arc
