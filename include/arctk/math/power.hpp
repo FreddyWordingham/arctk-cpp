a501e08ff48adc3911de2fff46559773bad2cb7f
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-02-01 16:09:47 +0000

    Added math pow unit test.

--- include/arctk/math/power.hpp
+++ include/arctk/math/power.hpp
@@ -17,10 +17,15 @@ namespace arc
 
 
         //  == FUNCTIONS ==
+        //  -- Integral --
+        template <typename T, typename R = type::arithmetic::mult<T, T>>
+        constexpr inline R sq(const T& x_) noexcept;
+        template <typename T, typename R = type::arithmetic::mult<type::arithmetic::mult<T, T>, T>>
+        constexpr inline R cube(const T& x_) noexcept;
 
-
-
-        //  == OPERATORS ==
+        //  -- Power --
+        template <typename T>
+        constexpr inline T pow(const T& x_, const int power_) noexcept;
 
 
 
--- testing/unit/math/power.cpp
+++ testing/unit/math/power.cpp
@@ -17,6 +17,9 @@ namespace arc
 
         //  == FUNCTION PROTOTYPES ==
         //  -- Tests --
+        inline void sq() noexcept;
+        inline void cube() noexcept;
+        inline void pow() noexcept;
 
 
 
@@ -28,6 +31,10 @@ namespace arc
 //  == MAIN ==
 int main()
 {
+    arc::test::sq();
+    arc::test::cube();
+    arc::test::pow();
+
     return (0);
 }
 
@@ -43,6 +50,26 @@ namespace arc
 
         //  == FUNCTIONS ==
         //  -- Tests --
+        inline void sq() noexcept
+        {
+            static_assert(math::sq(2) == 4);
+            static_assert(math::sq(-2) == 4);
+            static_assert(math::sq(2U) == 4);
+        }
+
+        inline void cube() noexcept
+        {
+            static_assert(math::cube(2) == 8);
+            static_assert(math::cube(-2) == -8);
+            static_assert(math::cube(2U) == 8);
+        }
+
+        inline void pow() noexcept
+        {
+            static_assert(math::pow(2, 4) == 16);
+            static_assert(math::pow(-2, 4) == 16);
+            static_assert(math::pow(2U, 4) == 16);
+        }
 
 
 
