1d03e2a7a5a5ff91e84e76abfbc1bfc2d23ba883
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-02-01 15:21:48 +0000

    Added type arithemeric struct files and unit test.

---
+++ include/arctk/arc/type/arithmetic.hpp
@@ -0,0 +1,28 @@
+//  == GUARD ==
+#pragma once
+
+
+
+//  == IMPORTS ==
+//  -- Arc --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace type
+    {
+
+
+
+        //  == CLASSES ==
+        //  -- Arithmetic --
+        struct arithmetic
+        {
+        };
+
+
+
+    } // namespace type
+} // namespace arc
---
+++ testing/unit/arc/type/arithmetic.cpp
@@ -0,0 +1,65 @@
+//  == IMPORTS ==
+//  -- Arc --
+#include <arctk/arc/type/arithmetic.inl>
+
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace test
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Tests --
+        template <typename TL, typename TR, typename ADD, typename SUB, typename MULT, typename DIV>
+        inline void set() noexcept;
+
+
+
+    } // namespace test
+} // namespace arc
+
+
+
+//  == MAIN ==
+int main()
+{
+    arc::test::set<int, int, int, int, int, int>();
+    arc::test::set<unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int>();
+    arc::test::set<double, double, double, double, double, double>();
+    arc::test::set<int, double, double, double, double, double>();
+    arc::test::set<double, int, double, double, double, double>();
+
+    return (0);
+}
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace test
+    {
+
+
+
+        //  == FUNCTIONS ==
+        //  -- Tests --
+        template <typename TL, typename TR, typename ADD, typename SUB, typename MULT, typename DIV>
+        inline void set() noexcept
+        {
+            static_assert(std::is_same<type::arithmetic::add<TL, TR>, ADD>::value);
+            static_assert(std::is_same<type::arithmetic::sub<TL, TR>, SUB>::value);
+            static_assert(std::is_same<type::arithmetic::mult<TL, TR>, MULT>::value);
+            static_assert(std::is_same<type::arithmetic::div<TL, TR>, DIV>::value);
+        }
+
+
+
+    } // namespace test
+} // namespace arc
