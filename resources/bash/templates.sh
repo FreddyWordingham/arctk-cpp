80f1a482aceb999800d2d60d9edb4c2fd65a348d
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-02-13 16:23:27 +0000

    Cleaned up template functions.

--- resources/bash/templates.sh
+++ resources/bash/templates.sh
@@ -2,42 +2,42 @@
 #   -- Class --
 arctk.class()
 {
-    cd $ARCTK_DIR/resources/templates/class > /dev/null
+    cd $ARCTK_DIR/resources/templates/class
 
     python3 -B gen.py "$@"
 
-    cd - > /dev/null
+    cd -
 }
 
 
 #   -- Structure --
 arctk.structure()
 {
-    cd $ARCTK_DIR/resources/templates/structure > /dev/null
+    cd $ARCTK_DIR/resources/templates/structure
 
     python3 -B gen.py "$@"
 
-    cd - > /dev/null
+    cd -
 }
 
 
 #   -- Functions --
 arctk.functions()
 {
-    cd $ARCTK_DIR/resources/templates/functions > /dev/null
+    cd $ARCTK_DIR/resources/templates/functions
 
     python3 -B gen.py "$@"
 
-    cd - > /dev/null
+    cd -
 }
 
 
 #   -- Constants --
 arctk.constants()
 {
-    cd $ARCTK_DIR/resources/templates/constants > /dev/null
+    cd $ARCTK_DIR/resources/templates/constants
 
     python3 -B gen.py "$@"
 
-    cd - > /dev/null
+    cd -
 }
