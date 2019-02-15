a21cbdc2a1009d11a1705fba6324576a2d1ccd2c
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-02-15 15:51:28 +0000

    Added is_prime function.

---
+++ include/arctk/math/properties.hpp
@@ -0,0 +1,22 @@
+//  == GUARD ==
+#pragma once
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace math
+    {
+
+
+
+        //  == FUNCTIONS ==
+        //  -- Properties --
+        template <typename T>
+        constexpr inline bool is_prime(const T x_) noexcept;
+
+
+
+    } // namespace math
+} // namespace arc
---
+++ testing/unit/math/properties.cpp
@@ -0,0 +1,49 @@
+//  == IMPORTS ==
+//  -- Arc --
+#include "arctk/math/properties.inl"
+
+//  -- Std --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace test
+    {
+
+
+
+        //  == FUNCTION PROTOTYPES ==
+        //  -- Tests --
+
+
+
+    } // namespace test
+} // namespace arc
+
+
+
+//  == MAIN ==
+int main()
+{
+    return (0);
+}
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace test
+    {
+
+
+
+        //  == FUNCTIONS ==
+        //  -- Tests --
+
+
+
+    } // namespace test
+} // namespace arc
