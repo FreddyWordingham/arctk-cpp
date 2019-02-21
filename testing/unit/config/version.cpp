a6d06e86b3a5e5981dc07a0a508b3eac2d2df1ba
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-02-21 23:16:21 +0000

    Added config version test.

---
+++ testing/unit/config/version.cpp
@@ -0,0 +1,52 @@
+//  == IMPORTS ==
+//  -- Arc --
+#include "arctk/config/version.inl"
+
+//  -- GTest --
+#include <gtest/gtest.h>
+
+
+
+//  == PRAGMAS ==
+//  -- Warnings --
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wglobal-constructors"
+#endif
+
+
+
+//  == NAMESPACE ==
+namespace test
+{
+
+
+
+    //  == TESTS ==
+    //  -- Prime --
+    TEST(VERSION_MAJOR, non_negative) // NOLINT
+    {
+        ASSERT_GE(arc::config::VERSION_MAJOR, 0);
+    }
+
+    TEST(VERSION_MINOR, non_negative) // NOLINT
+    {
+        ASSERT_GE(arc::config::VERSION_MINOR, 0);
+    }
+
+    TEST(VERSION_PATCH, non_negative) // NOLINT
+    {
+        ASSERT_GE(arc::config::VERSION_PATCH, 0);
+    }
+
+
+
+} // namespace test
+
+
+
+//  == CLEAN UP ==
+//  -- Warnings --
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
