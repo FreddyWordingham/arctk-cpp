8b2347bd4e10ffc23629bb58f1fd158fe5a5eb82
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-02-21 23:00:06 +0000

    Improved traits test.

--- testing/unit/type/traits.cpp
+++ testing/unit/type/traits.cpp
@@ -34,51 +34,51 @@ namespace test
     //  -- Ranges --
     TEST(is_rangeable, primitive) // NOLINT
     {
-        ASSERT_FALSE(arc::type::is_rangeable<void>::value);
-        ASSERT_FALSE(arc::type::is_rangeable<int>::value);
-        ASSERT_FALSE(arc::type::is_rangeable<double>::value);
-        ASSERT_FALSE(arc::type::is_rangeable<std::size_t>::value);
+        ASSERT_FALSE(arc::type::is_rangeable_v<void>);
+        ASSERT_FALSE(arc::type::is_rangeable_v<int>);
+        ASSERT_FALSE(arc::type::is_rangeable_v<double>);
+        ASSERT_FALSE(arc::type::is_rangeable_v<std::size_t>);
     }
 
     TEST(is_rangeable, array) // NOLINT
     {
-        ASSERT_TRUE((arc::type::is_rangeable<std::array<int, 1>>::value));
-        ASSERT_TRUE((arc::type::is_rangeable<std::array<double, 2>>::value));
-        ASSERT_TRUE((arc::type::is_rangeable<std::array<std::size_t, 0>>::value));
-        constexpr auto array_vector_int = arc::type::is_rangeable<std::array<std::vector<int>, 2>>::value;
+        ASSERT_TRUE((arc::type::is_rangeable_v<std::array<int, 1>>));
+        ASSERT_TRUE((arc::type::is_rangeable_v<std::array<double, 2>>));
+        ASSERT_TRUE((arc::type::is_rangeable_v<std::array<std::size_t, 0>>));
+        const bool array_vector_int{arc::type::is_rangeable_v<std::array<std::vector<int>, 2>>};
         ASSERT_TRUE(array_vector_int);
     }
 
     TEST(is_rangeable, vector) // NOLINT
     {
-        ASSERT_TRUE(arc::type::is_rangeable<std::vector<int>>::value);
-        ASSERT_TRUE(arc::type::is_rangeable<std::vector<double>>::value);
-        ASSERT_TRUE(arc::type::is_rangeable<std::vector<std::size_t>>::value);
-        ASSERT_TRUE(arc::type::is_rangeable<std::vector<std::vector<int>>>::value);
+        ASSERT_TRUE(arc::type::is_rangeable_v<std::vector<int>>);
+        ASSERT_TRUE(arc::type::is_rangeable_v<std::vector<double>>);
+        ASSERT_TRUE(arc::type::is_rangeable_v<std::vector<std::size_t>>);
+        ASSERT_TRUE(arc::type::is_rangeable_v<std::vector<std::vector<int>>>);
     }
 
     TEST(is_rangeable, stack) // NOLINT
     {
-        ASSERT_FALSE(arc::type::is_rangeable<std::stack<int>>::value);
-        ASSERT_FALSE(arc::type::is_rangeable<std::stack<double>>::value);
-        ASSERT_FALSE(arc::type::is_rangeable<std::stack<std::size_t>>::value);
-        ASSERT_FALSE(arc::type::is_rangeable<std::stack<std::stack<int>>>::value);
+        ASSERT_FALSE(arc::type::is_rangeable_v<std::stack<int>>);
+        ASSERT_FALSE(arc::type::is_rangeable_v<std::stack<double>>);
+        ASSERT_FALSE(arc::type::is_rangeable_v<std::stack<std::size_t>>);
+        ASSERT_FALSE(arc::type::is_rangeable_v<std::stack<std::stack<int>>>);
     }
 
     TEST(is_rangeable, string) // NOLINT
     {
-        ASSERT_TRUE(arc::type::is_rangeable<std::string>::value);
+        ASSERT_TRUE(arc::type::is_rangeable_v<std::string>);
     }
 
     TEST(is_rangeable, map) // NOLINT
     {
-        constexpr auto map_string_int = arc::type::is_rangeable<std::array<std::vector<int>, 2>>::value;
+        const bool map_string_int{arc::type::is_rangeable_v<std::map<std::vector<int>, bool>>};
         ASSERT_TRUE(map_string_int);
-        constexpr auto map_int_int = arc::type::is_rangeable<std::array<std::vector<int>, 2>>::value;
+        const bool map_int_int{arc::type::is_rangeable_v<std::map<std::string, int>>};
         ASSERT_TRUE(map_int_int);
-        constexpr auto map_void_double = arc::type::is_rangeable<std::array<std::vector<int>, 2>>::value;
+        const bool map_void_double{arc::type::is_rangeable_v<std::map<int, char>>};
         ASSERT_TRUE(map_void_double);
-        constexpr auto map_double_void = arc::type::is_rangeable<std::array<std::vector<int>, 2>>::value;
+        const bool map_double_void{arc::type::is_rangeable_v<std::map<std::array<int, 2>, std::size_t>>};
         ASSERT_TRUE(map_double_void);
     }
 
