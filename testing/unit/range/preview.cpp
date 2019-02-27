a830cd6ad4404259190c5376d05a1e02e578335f
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-02-27 13:09:03 +0000

    Added missing range preview test.

--- testing/unit/range/preview.cpp
+++ testing/unit/range/preview.cpp
@@ -10,8 +10,9 @@
 //  == PRAGMAS ==
 //  -- Warnings --
 #ifdef __clang__
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wglobal-constructors"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wglobal-constructors"
+#pragma clang diagnostic ignored "-Wunused-variable"
 #endif
 
 
@@ -22,24 +23,12 @@ namespace test
 
 
 
-        //  == FIXTURES ==
-        //  -- Array Int Comparison --
-//      class array_int_test : public ::testing::Test
-//      {
-//          //  == FIELDS ==
-//        protected:
-//          //  -- Data --
-//          const std::array<int, 4>                _arr_0{1, 2, 3, 4};
-//      };
-
-
-
-        //  == TESTS ==
-//      //  -- Prime --
-//      TEST(prime, special) // NOLINT
-//      {
-//          ASSERT_TRUE(is_prime(2));
-//      }
+    //  == TESTS ==
+    //  -- Preview --
+    TEST(preview, construction) // NOLINT
+    {
+        arc::range::Preview preview{};
+    }
 
 
 
@@ -50,5 +39,5 @@ namespace test
 //  == CLEAN UP ==
 //  -- Warnings --
 #ifdef __clang__
-    #pragma clang diagnostic pop
+#pragma clang diagnostic pop
 #endif
