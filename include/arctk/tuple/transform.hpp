86ef672b0f3f98eb6217a94a119219340f755c87
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-03-06 17:23:34 +0000

    Removed mutable for_each_zip functions.

--- include/arctk/tuple/transform.hpp
+++ include/arctk/tuple/transform.hpp
@@ -33,13 +33,11 @@ namespace arc
         constexpr inline void for_each_zip(const std::tuple<A...>& tuple_0_, const std::tuple<B...>& tuple_1_, const F& func_) noexcept;
         template <typename... A, typename... B, typename F, typename std::size_t... I>
         constexpr inline void for_each_zip_helper(const std::tuple<A...>& tuple_0_, const std::tuple<B...>& tuple_1_, const F& func_, std::index_sequence<I...> /*unused*/) noexcept;
-
-
-
         template <typename... A, typename F, typename R>
         inline void for_each_zip(const std::tuple<A...>& tuple_0_, const std::tuple<A...>& tuple_1_, const F& func_, R* res_) noexcept;
         template <typename... A, typename F, typename R, typename std::size_t... I>
         inline void for_each_zip_helper(const std::tuple<A...>& tuple_0_, const std::tuple<A...>& tuple_1_, const F& func_, R* res_, std::index_sequence<I...> /*unused*/) noexcept;
+
         template <typename A, typename F, std::size_t... I>
         auto transform_helper(const A& arg_, const F& func_, std::index_sequence<I...> /*unused*/) noexcept;
         template <typename... A, typename F>
@@ -54,14 +52,6 @@ namespace arc
         constexpr inline void for_each(std::tuple<A...>* tuple_, const F& func_, R* res_) noexcept;
         template <typename... A, typename F, typename R, typename std::size_t... I>
         constexpr inline void for_each_helper(std::tuple<A...>* tuple_, const F& func_, R* res_, std::index_sequence<I...> /*unused*/) noexcept;
-        template <typename... A, typename F>
-        constexpr inline void for_each_zip(std::tuple<A...>* tuple_0_, std::tuple<A...>* tuple_1_, const F& func_) noexcept;
-        template <typename... A, typename F, typename std::size_t... I>
-        constexpr inline void for_each_zip_helper(std::tuple<A...>* tuple_0_, std::tuple<A...>* tuple_1_, const F& func_, std::index_sequence<I...> /*unused*/) noexcept;
-        template <typename... A, typename F, typename R>
-        constexpr inline void for_each_zip(std::tuple<A...>* tuple_0_, std::tuple<A...>* tuple_1_, const F& func_, R* res_) noexcept;
-        template <typename... A, typename F, typename R, typename std::size_t... I>
-        constexpr inline void for_each_zip_helper(std::tuple<A...>* tuple_0_, std::tuple<A...>* tuple_1_, const F& func_, R* res_, std::index_sequence<I...> /*unused*/) noexcept;
 
 
 
