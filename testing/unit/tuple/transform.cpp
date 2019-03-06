a24ee67e391e2565abb0ee27d069cd8bc4c42dcc
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-03-06 16:35:47 +0000

    Added for_each_res unit tests.

--- testing/unit/tuple/transform.cpp
+++ testing/unit/tuple/transform.cpp
@@ -1,6 +1,8 @@
 //  == IMPORTS ==
 //  -- Arc --
 #include "arctk/tuple/transform.inl"
+#include "arctk/math/comparison.inl"
+#include "arctk/math/properties.inl"
 
 //  -- GTest --
 #include <gtest/gtest.h>
@@ -54,6 +56,24 @@ namespace test
         std::stringstream stream_2;
         arc::tuple::for_each(_tup_char_size, [&](const auto t_) { stream_2 << t_; });
         ASSERT_EQ(stream_2.str(), "x7");
+    }
+
+    TEST_F(tuples, for_each_res_immutable) // NOLINT
+    {
+        std::vector<bool> res_0;
+        arc::tuple::for_each(_tup_char_int, [](const auto t_) {return (arc::math::is_prime(t_));}, &res_0);
+        const std::vector<bool> expected_res_0{true, false};
+        ASSERT_EQ(res_0, expected_res_0);
+
+        std::vector<bool> res_1;
+        arc::tuple::for_each(_tup_int_double, [](const auto t_) {return (arc::math::equal(static_cast<double>(t_), 3.14159));}, &res_1);
+        const std::vector<bool> expected_res_1{false, true};
+        ASSERT_EQ(res_1, expected_res_1);
+
+        std::vector<bool> res_2;
+        arc::tuple::for_each(_tup_char_size, [](const auto t_) {return (arc::math::is_prime(t_));}, &res_2);
+        const std::vector<bool> expected_res_2{false, true};
+        ASSERT_EQ(res_2, expected_res_2);
     }
 
 
