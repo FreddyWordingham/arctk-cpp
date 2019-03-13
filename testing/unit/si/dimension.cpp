9f75a2acf9f33669e3e011fda30d0456f294f962
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-03-13 15:45:00 +0000

    Added si dimension unit test.

--- testing/unit/si/dimension.cpp
+++ testing/unit/si/dimension.cpp
@@ -62,7 +62,40 @@ namespace test
         ASSERT_EQ(time.mag(), 2.0);
     }
 
-    TEST(si_dimension_test, assignment) // NOLINT
+    TEST_F(si_dimension_test, assignment) // NOLINT
+    {
+        arc::si::Scalar scalar = 0.0;
+        ASSERT_EQ(scalar.mag(), 0.0);
+
+        scalar += 1.0;
+        ASSERT_EQ(scalar.mag(), 1.0);
+
+        scalar -= -1.0;
+        ASSERT_EQ(scalar.mag(), 2.0);
+
+        scalar *= 2.0;
+        ASSERT_EQ(scalar.mag(), 4.0);
+
+        scalar /= 4.0;
+        ASSERT_EQ(scalar.mag(), 1.0);
+
+        arc::si::Mass mass{};
+        ASSERT_EQ(mass.mag(), 0.0);
+
+        mass += arc::si::Mass{1.0};
+        ASSERT_EQ(mass.mag(), 1.0);
+
+        mass -= arc::si::Mass{-1.0};
+        ASSERT_EQ(mass.mag(), 2.0);
+
+        mass *= 2.0;
+        ASSERT_EQ(mass.mag(), 4.0);
+
+        mass /= 4.0;
+        ASSERT_EQ(mass.mag(), 1.0);
+    }
+
+    TEST_F(si_dimension_test, increment_decrement) // NOLINT
     {
         arc::si::Scalar scalar = 0.0;
         ASSERT_EQ(scalar.mag(), 0.0);
