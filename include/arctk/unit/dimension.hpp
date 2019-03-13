4807d8259433b4e821f95c1f846a98cc2b487e67
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-03-13 14:09:04 +0000

    Added arc unit dimension class files.

---
+++ include/arctk/unit/dimension.hpp
@@ -0,0 +1,47 @@
+//  == GUARD ==
+#pragma once
+
+
+
+//  == IMPORTS ==
+//  -- Arc --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace unit
+    {
+
+
+
+        //  == CLASSES ==
+        //  -- Dimension --
+        class Dimension
+        {
+            //  == FIELDS ==
+          private:
+
+
+
+            //  == INSTANTIATION ==
+          public:
+            //  -- Constructors --
+
+
+
+            //  == OPERATORS ==
+          public:
+
+
+
+            //  == METHODS ==
+          public:
+            //  -- Getters --
+        };
+
+
+
+    } // namespace unit
+} // namespace arc
---
+++ include/arctk/unit/dimension.inl
@@ -0,0 +1,40 @@
+//  == GUARD ==
+#pragma once
+
+
+
+//  == DECLARATIONS ==
+//  -- Arc --
+#include "arctk/unit/dimension.hpp" // IWYU pragma: export
+
+
+
+//  == IMPORTS ==
+//  -- Arc --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace unit
+    {
+
+
+
+        //  == INSTANTIATION ==
+        //  -- Constructors --
+
+
+
+        //  == OPERATORS ==
+
+
+
+        //  == METHODS ==
+        //  -- Getters --
+
+
+
+    } // namespace unit
+} // namespace arc
