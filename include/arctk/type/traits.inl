0cd77260b296de16273b73f44cb1d27462af70aa
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-03-19 19:29:17 +0000

    Added traits unit test.

--- include/arctk/type/traits.inl
+++ include/arctk/type/traits.inl
@@ -6,21 +6,3 @@
 //  == DECLARATIONS ==
 //  -- Arc --
 #include "arctk/type/traits.hpp" // IWYU pragma: export
-
-
-
-//  == IMPORTS ==
-//  -- Arc --
-
-
-
-//  == NAMESPACE ==
-namespace arc
-{
-    namespace type
-    {
-
-
-
-    } // namespace type
-} // namespace arc
--- testing/unit/type/traits.cpp
+++ testing/unit/type/traits.cpp
@@ -5,6 +5,13 @@
 //  -- GTest --
 #include <gtest/gtest.h>
 
+//  -- Std --
+#include <array>
+#include <list>
+#include <tuple>
+#include <utility>
+#include <vector>
+
 
 
 //  == PRAGMAS ==
@@ -23,11 +30,28 @@ namespace test
 
 
     //  == TESTS ==
-    //      //  -- Prime --
-    //      TEST(prime, special) // NOLINT
-    //      {
-    //          ASSERT_TRUE(is_prime(2));
-    //      }
+    //  -- Ranges --
+    TEST(is_rangeable, false) // NOLINT
+    {
+        ASSERT_FALSE(arc::type::is_rangeable_v<int>);
+        ASSERT_FALSE(arc::type::is_rangeable_v<double>);
+        ASSERT_FALSE(arc::type::is_rangeable_v<char>);
+
+        const auto ans_0{arc::type::is_rangeable_v<std::pair<char, int>>};
+        ASSERT_FALSE(ans_0);
+
+        const auto ans_1{arc::type::is_rangeable_v<std::tuple<bool, char, int>>};
+        ASSERT_FALSE(ans_1);
+    }
+
+    TEST(is_rangeable, true) // NOLINT
+    {
+        ASSERT_TRUE(arc::type::is_rangeable_v<std::vector<int>>);
+        ASSERT_TRUE(arc::type::is_rangeable_v<std::list<double>>);
+
+        const auto ans{arc::type::is_rangeable_v<std::array<char, 5>>};
+        ASSERT_TRUE(ans);
+    }
 
 
 
