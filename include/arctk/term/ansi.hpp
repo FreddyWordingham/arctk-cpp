570d7e0b872ed7704ef7a49f7bf367bcc82abff7
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-03-23 20:34:09 +0000

    Cleaned build files.

---
+++ include/arctk/term/ansi.hpp
@@ -0,0 +1,24 @@
+//  == GUARD ==
+#pragma once
+
+
+
+//  == IMPORTS ==
+//  -- Arc --
+
+
+
+//  == NAMESPACE ==
+namespace arc
+{
+    namespace term
+    {
+
+
+
+        //  == CONSTANTS ==
+
+
+
+    } // namespace term
+} // namespace arc
---
+++ include/arctk/term/ansi.inl
@@ -0,0 +1,8 @@
+//  == GUARD ==
+#pragma once
+
+
+
+//  == DECLARATIONS ==
+//  -- Arc --
+#include "arctk/term/ansi.hpp" // IWYU pragma: export
