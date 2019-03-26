2a0b44a68c61b5b367647a0ff6a511974b5083a1
FreddyWordingham <FJMW201@exeter.ac.uk>
2019-03-26 11:13:36 +0000

    Added range unit test.

--- testing/unit/alg/mat.cpp
+++ testing/unit/alg/mat.cpp
@@ -98,29 +98,6 @@ namespace test
         ASSERT_TRUE(len_mat[2][1] == ans[2][1]);
     }
 
-    // TEST(vec, dot_product) // NOLINT
-    // {
-    //     using namespace arc;
-    //     arc::alg::Vec<arc::si::Velocity, 3> vel_vec{arc::si::Velocity{1.0}, arc::si::Velocity{2.0}, arc::si::Velocity{3.0}};
-    //     arc::alg::Vec<arc::si::Time, 3>     time_vec{arc::si::Time{1.0}, arc::si::Time{-2.0}, arc::si::Time{1.0 / 3.0}};
-    //     arc::si::Length                     len{vel_vec * time_vec};
-
-    //     ASSERT_TRUE(len == arc::si::Length{-2.0});
-    // }
-
-    // TEST(vec, cross_product) // NOLINT
-    // {
-    //     using namespace arc;
-    //     arc::alg::Vec<arc::si::Velocity, 3> vel_vec{arc::si::Velocity{1.0}, arc::si::Velocity{2.0}, arc::si::Velocity{3.0}};
-    //     arc::alg::Vec<arc::si::Time, 3>     time_vec{arc::si::Time{1.0}, arc::si::Time{-2.0}, arc::si::Time{1.0 / 3.0}};
-    //     arc::alg::Vec<arc::si::Length, 3>   len_vec{vel_vec ^ time_vec};
-
-    //     const arc::alg::Vec<arc::si::Length, 3> ans{arc::si::Length{20.0 / 3.0}, arc::si::Length{8.0 / 3.0}, arc::si::Length{-4.0}};
-    //     ASSERT_TRUE(len_vec[0] == ans[0]);
-    //     ASSERT_TRUE(len_vec[1] == ans[1]);
-    //     ASSERT_TRUE(len_vec[2] == ans[2]);
-    // }
-
 
     // //  -- Access --
     // TEST(vec, access) // NOLINT
@@ -148,33 +125,22 @@ namespace test
     // }
 
 
-    // //  -- Range --
-    // TEST(vec, range) // NOLINT
-    // {
-    //     const arc::alg::Vec<int, 4> vec_0{3, 5, 2, -9};
-    //     int                         total_0{0};
+    //  -- Range --
+    TEST(vec, range) // NOLINT
+    {
+        const arc::alg::Mat<int, 4, 3> mat_0{arc::alg::Row<int, 3>{3, 5, 2}, arc::alg::Row<int, 3>{-9, -3, 4}, arc::alg::Row<int, 3>{0, 1, 2}, arc::alg::Row<int, 3>{-2, -3, -4}};
+        int                            total_0{0};
 
-    //     for (const auto& x : vec_0)
-    //     {
-    //         total_0 += x;
-    //     }
+        for (const auto& row : mat_0)
+        {
+            for (const auto& x : row)
+            {
+                total_0 += x;
+            }
+        }
 
-    //     ASSERT_EQ(total_0, 1);
-
-    //     arc::alg::Vec<int, 4> vec_1{3, 5, 2, -9};
-    //     int                   total_1{0};
-
-    //     for (auto& x : vec_1)
-    //     {
-    //         x = 0;
-    //     }
-    //     for (const auto& x : vec_1)
-    //     {
-    //         total_1 += x;
-    //     }
-
-    //     ASSERT_EQ(total_1, 0);
-    // }
+        ASSERT_EQ(total_0, -4);
+    }
 
 
 
